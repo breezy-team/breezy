3debacadaaaf1978e2b0e2266f0abdd366e11f41
Martin Pool <mbp@sourcefrog.net>
2005-03-14 18:07:24 +1100

    - add setup.py and install instructions - rename main script to just bzr

---
+++ setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+# This is an installation script for bzr.  Run it with
+# './setup.py install', or
+# './setup.py --help' for more options
+
+from distutils.core import setup
+
+setup(name='bzr',
+      version='0.0.0',
+      author='Martin Pool',
+      author_email='mbp@sourcefrog.net',
+      url='http://www.bazaar-ng.org/',
+      description='Friendly distributed version control system',
+      license='GNU GPL v2',
+      packages=['bzrlib'],
+      scripts=['bzr'])
