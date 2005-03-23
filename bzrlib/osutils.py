429fbb185e95cc24a77a120d65838401d58baa12
mbp@sourcefrog.net <mbp@sourcefrog.net>
2005-03-23 17:37:30 +1100

    fix time.localtime call for python 2.3

--- bzrlib/osutils.py
+++ bzrlib/osutils.py
@@ -179,6 +179,10 @@ def compare_files(a, b):
 
 def local_time_offset(t=None):
     """Return offset of local zone from GMT, either at present or at time t."""
+    # python2.3 localtime() can't take None
+    if t is None:
+        t = time.time()
+        
     if time.localtime(t).tm_isdst and time.daylight:
         return -time.altzone
     else:
