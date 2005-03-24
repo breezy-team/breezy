ffedba33dc2ed6a6563b96c6dc6094b54555cf72
mbp@sourcefrog.net <mbp@sourcefrog.net>
2005-03-24 11:21:35 +1100

    todo

--- bzrlib/info.py
+++ bzrlib/info.py
@@ -23,6 +23,9 @@ import bzrlib
 from osutils import format_date
 
 def show_info(b):
+    # TODO: Maybe show space used by working tree, versioned files,
+    # unknown files, text store.
+    
     print 'branch format:', b.controlfile('branch-format', 'r').readline().rstrip('\n')
 
     def plural(n, base='', pl=None):
