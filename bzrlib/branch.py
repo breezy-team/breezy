965f33e314ac62f89cc75418d6454aa3c11db51c
mbp@sourcefrog.net <mbp@sourcefrog.net>
2005-04-05 19:09:54 +1000

    more random bytes in revision and file ids

--- bzrlib/branch.py
+++ bzrlib/branch.py
@@ -898,7 +898,7 @@ def is_control_file(filename):
 def _gen_revision_id(when):
     """Return new revision-id."""
     s = '%s-%s-' % (user_email(), compact_date(when))
-    s += hexlify(rand_bytes(8))
+    s += hexlify(rand_bytes(12))
     return s
 
 
@@ -914,7 +914,7 @@ def gen_file_id(name):
 
     name = name.lstrip('.')
 
-    s = hexlify(rand_bytes(8))
+    s = hexlify(rand_bytes(12))
     return '-'.join((name, compact_date(time.time()), s))
 
 
