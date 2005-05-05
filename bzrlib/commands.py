b263f941a7b94d359e4dc367ec250a575408d8e3
Martin Pool <mbp@sourcefrog.net>
2005-05-05 17:00:55 +1000

    - Fix up bzr log command

--- bzrlib/commands.py
+++ bzrlib/commands.py
@@ -426,9 +426,10 @@ class cmd_log(Command):
     takes_options = ['timezone', 'verbose', 'show-ids']
     def run(self, timezone='original', verbose=False, show_ids=False):
         b = Branch('.', lock_mode='r')
-        b.show_log(show_timezone=timezone,
-                   verbose=verbose,
-                   show_ids=show_ids)
+        bzrlib.show_log(b,
+                        show_timezone=timezone,
+                        verbose=verbose,
+                        show_ids=show_ids)
 
 
 class cmd_ls(Command):
