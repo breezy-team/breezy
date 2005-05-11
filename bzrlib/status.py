bc7a46e34b7b882fb29beeb5dfd2fc32583e5e4e
Martin Pool <mbp@sourcefrog.net>
2005-05-11 15:15:13 +1000

    - remove dead code

--- bzrlib/status.py
+++ bzrlib/status.py
@@ -15,38 +15,3 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
-
-def find_modified(branch):
-    """Return a list of files that have been modified in the working copy.
-
-    This does not consider renames and does not include files added or
-    deleted.
-
-    Each modified file is returned as (PATH, ENTRY).
-    """
-    import cache
-
-    inv = branch.read_working_inventory()
-    cc = cache.update_cache(branch, inv)
-    basis_inv = branch.basis_tree().inventory
-    
-    for path, entry in inv.iter_entries():
-        if entry.kind != 'file':
-            continue
-        
-        file_id = entry.file_id
-        ce = cc.get(file_id)
-        if not ce:
-            continue                    # not in working dir
-
-        if file_id not in basis_inv:
-            continue                    # newly added
-
-        old_entry = basis_inv[file_id]
- 
-        if (old_entry.text_size == ce[3]
-            and old_entry.text_sha1 == ce[1]):
-            continue
-
-        yield path, entry
-        
