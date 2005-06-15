10b89c8f3df9fda21d882d5bacb42ae2c48a42bb
Martin Pool <mbp@sourcefrog.net>
2005-06-15 16:03:16 +1000

    0.0.5 release

--- bzrlib/__init__.py
+++ bzrlib/__init__.py
@@ -47,7 +47,7 @@ del locale
 
 __copyright__ = "Copyright 2005 Canonical Development Ltd."
 __author__ = "Martin Pool <mbp@canonical.com>"
-__version__ = '0.0.5pre'
+__version__ = '0.0.5'
 
 
 def get_bzr_revision():
