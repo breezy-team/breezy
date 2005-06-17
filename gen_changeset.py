707f9c3b98b34daa263737aef2c9c5eafd3b61c5
John Arbash Meinel <john@arbash-meinel.com>
2005-06-17 16:42:31 -0500

    Updated to now include the diffs

--- gen_changeset.py
+++ gen_changeset.py
@@ -5,6 +5,11 @@ Just some work for generating a changeset.
 
 import bzrlib, bzrlib.errors
 
+try:
+    set
+except NameError:
+    from sets import Set as set
+
 def _canonicalize_revision(branch, revision=None):
     """Turn some sort of revision information into a single
     set of from-to revision ids.
@@ -78,10 +83,25 @@ class MetaInfoHeader(object):
     changeset.
     """
 
-    def __init__(self, branch, revisions, delta):
+    def __init__(self, branch, revisions, delta,
+            full_remove=False, full_rename=False,
+            external_diff_options = None,
+            new_tree=None, old_tree=None,
+            old_label = '', new_label = ''):
+        """
+        :param full_remove: Include the full-text for a delete
+        :param full_rename: Include an add+delete patch for a rename
+        """
         self.branch = branch
         self.delta = delta
         self._get_revision_list(revisions)
+        self.full_remove=full_remove
+        self.full_rename=full_rename
+        self.external_diff_options = external_diff_options
+        self.old_label = old_label
+        self.new_label = new_label
+        self.old_tree = old_tree
+        self.new_tree = new_tree
 
     def _get_revision_list(self, revisions):
         """This generates the list of all revisions from->to.
@@ -110,7 +130,11 @@ class MetaInfoHeader(object):
             self.revision_list.append(_fake_working_revision(self.branch))
 
     def write_meta_info(self, to_file):
-        """Write out the meta-info portion to the supplied file."""
+        """Write out the meta-info portion to the supplied file.
+
+        :param to_file: Write out the meta information to the supplied
+                        file
+        """
         from bzrlib.osutils import username
         def write(txt, key=None):
             if key:
@@ -136,46 +160,138 @@ class MetaInfoHeader(object):
                 else:
                     write(' '*11 + rev.revision_id)
 
-        if self.revision_list[-1].revision_id is None:
-            final_tree = self.branch.working_tree()
-        else:
-            final_tree = self.branch.revision_tree(
-                    self.revision_list[-1])
+        seen_ids = set(['TREE_ROOT'])
+        need_ids = set()
 
-        class _write_file(object):
-            first = True
+        def _write_entry(file_id, path=None):
+            if file_id in self.new_tree.inventory:
+                ie = self.new_tree.inventory[file_id]
+            elif file_id in self.old_tree.inventory:
+                ie = self.new_tree.inventory[file_id]
+            else:
+                ie = None
+            if not path and ie:
+                path = ie.name
+            to_file.write(path.encode('utf-8'))
+            to_file.write('\t')
+            to_file.write(file_id.encode('utf-8'))
+            if ie and ie.parent_id:
+                to_file.write('\t')
+                to_file.write(ie.parent_id.encode('utf-8'))
+                if ie.parent_id not in seen_ids:
+                    need_ids.add(ie.parent_id)
+            seen_ids.add(ie.file_id)
+            to_file.write('\n')
+
+        class _write_kind(object):
+            def __init__(self, kind):
+                self.first = True
+                self.kind = kind
             def __call__(self, info):
                 if self.first:
                     self.first = False
-                    to_file.write('# file ids: ')
+                    to_file.write('# %s ids: ' % self.kind)
                 else:
-                    to_file.write('#           ')
-                to_file.write(info[0].encode('utf-8'))
-                to_file.write('\t')
-                to_file.write(info[1].encode('utf-8'))
-                inv = final_tree.inventory[info[1]]
-                if inv.parent_id:
-                    to_file.write('\t')
-                    to_file.write(inv.parent_id)
-                to_file.write('\n')
-        write_file = _write_file()
+                    to_file.write('#')
+                    to_file.write(' ' * (len(self.kind) + 7))
+                _write_entry(info[1], info[0])
 
-        for info in self.delta.removed:
-            if info[2] == 'file':
-                write_file(info)
-        for info in self.delta.added:
-            if info[2] == 'file':
-                write_file(info)
-        for info in self.delta.renamed:
-            if info[3] == 'file':
-                write_file(info[1:2])
-        for info in self.delta.modified:
-            if info[2] == 'file':
-                write_file(info)
+        def _write_all(kind):
+            writer = _write_kind(kind)
+            for info in self.delta.removed:
+                if info[2] == kind:
+                    writer(info)
+            for info in self.delta.added:
+                if info[2] == kind:
+                    writer(info)
+            for info in self.delta.renamed:
+                if info[3] == kind:
+                    writer(info[1:2])
+            for info in self.delta.modified:
+                if info[2] == kind:
+                    writer(info)
 
+        _write_all('file')
+        _write_all('directory')
+
+        first = True
+        while len(need_ids) > 0:
+            file_id = need_ids.pop()
+            if file_id in seen_ids:
+                continue
+            seen_ids.add(file_id)
+            if first:
+                to_file.write('# parent ids: ')
+                first = False
+            else:
+                to_file.write('#             ')
+            _write_entry(file_id)
+
+        self._write_diffs(to_file)
+
+    def _write_diffs(self, to_file):
+        """Write out the specific diffs"""
+        from bzrlib.diff import internal_diff, external_diff
+        DEVNULL = '/dev/null'
+
+        if self.external_diff_options:
+            assert isinstance(self.external_diff_options, basestring)
+            opts = self.external_diff_options.split()
+            def diff_file(olab, olines, nlab, nlines, to_file):
+                external_diff(olab, olines, nlab, nlines, to_file, opts)
+        else:
+            diff_file = internal_diff
+
+        for path, file_id, kind in self.delta.removed:
+            print >>to_file, '*** removed %s %r' % (kind, path)
+            if kind == 'file' and self.full_remove:
+                diff_file(self.old_label + path,
+                          self.old_tree.get_file(file_id).readlines(),
+                          DEVNULL, 
+                          [],
+                          to_file)
+    
+        for path, file_id, kind in self.delta.added:
+            print >>to_file, '*** added %s %r' % (kind, path)
+            if kind == 'file':
+                diff_file(DEVNULL,
+                          [],
+                          self.new_label + path,
+                          self.new_tree.get_file(file_id).readlines(),
+                          to_file)
+    
+        for old_path, new_path, file_id, kind, text_modified in self.delta.renamed:
+            print >>to_file, '*** renamed %s %r => %r' % (kind, old_path, new_path)
+            if self.full_rename and kind == 'file':
+                diff_file(self.old_label + old_path,
+                          self.old_tree.get_file(file_id).readlines(),
+                          DEVNULL, 
+                          [],
+                          to_file)
+                diff_file(DEVNULL,
+                          [],
+                          self.new_label + new_path,
+                          self.new_tree.get_file(file_id).readlines(),
+                          to_file)
+            elif text_modified:
+                    diff_file(self.old_label + old_path,
+                              self.old_tree.get_file(file_id).readlines(),
+                              self.new_label + new_path,
+                              self.new_tree.get_file(file_id).readlines(),
+                              to_file)
+    
+        for path, file_id, kind in self.delta.modified:
+            print >>to_file, '*** modified %s %r' % (kind, path)
+            if kind == 'file':
+                diff_file(self.old_label + path,
+                          self.old_tree.get_file(file_id).readlines(),
+                          self.new_label + path,
+                          self.new_tree.get_file(file_id).readlines(),
+                          to_file)
 
 def show_changeset(branch, revision=None, specific_files=None,
-        external_diff_options=None, to_file=None):
+        external_diff_options=None, to_file=None,
+        include_full_diff=False):
     from bzrlib.diff import compare_trees
 
     if to_file is None:
@@ -188,7 +304,9 @@ def show_changeset(branch, revision=None, specific_files=None,
     delta = compare_trees(old_tree, new_tree, want_unchanged=False,
                           specific_files=specific_files)
 
-    meta = MetaInfoHeader(branch, revisions, delta)
+    meta = MetaInfoHeader(branch, revisions, delta,
+            external_diff_options=external_diff_options,
+            old_tree=old_tree, new_tree=new_tree)
     meta.write_meta_info(to_file)
 
 
