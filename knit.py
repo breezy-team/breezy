accd27a91dd7b97e903f6c730f9ed8f5dac348c0
Martin Pool <mbp@sourcefrog.net>
2005-06-27 12:38:05 +1000

    Import testsweet module adapted from bzr.
    Start new test-driven development pattern, with a very trivial 
    test that stores and retrieves a single text.

--- knit.py
+++ knit.py
@@ -7,21 +7,38 @@
 # Author: Martin Pool <mbp@canonical.com>
 
 
-"""knit - a weave-like structure
+"""knit - a weave-like structure"""
 
-A Knit manages versions of line-based text files, keeping track of the
-originating version for each line.
 
-Versions are referenced by nonnegative integers.  In a full system
-this will be just a local shorthand for some universally-unique
-version identifier.
+class Knit(object):
+    """knit - versioned text file storage.
+    
+    A Knit manages versions of line-based text files, keeping track of the
+    originating version for each line.
 
-Texts are represented as a seqence of (version, text, live) tuples.
-An empty sequence represents an empty text.  The version is the
-version in which the line was introduced.  The *live* flag is false if
-the line is no longer present and being tracked only for the purposes
-of merging.
-"""
+    Versions are referenced by nonnegative integers.  In a full system
+    this will be just a local shorthand for some universally-unique
+    version identifier.
+
+    Texts are represented as a seqence of (version, text, live) tuples.
+    An empty sequence represents an empty text.  The version is the
+    version in which the line was introduced.  The *live* flag is false if
+    the line is no longer present and being tracked only for the purposes
+    of merging.
+
+    _l
+        Stores the actual weave.
+    """
+        
+    def add(self, text):
+        """Add a single text on top of the weave."""
+        if not isinstance(text, list):
+            raise ValueError("text should be a list, not %s" % type(text))
+        self._l = text
+
+    
+    def get(self):
+        return self._l[:]
 
 
 text1 = [(0, "hello world", True)]
@@ -82,25 +99,25 @@ def update_knit(knit, new_vers, new_lines):
 
     return new_knit
         
-    
 
 
-print '***** annotated:'
-show_annotated(knit2)
+def main(): 
+    print '***** annotated:'
+    show_annotated(knit2)
 
-print '***** plain text:'
-print '\n'.join(knit2text(knit2))
+    print '***** plain text:'
+    print '\n'.join(knit2text(knit2))
 
-text3 = """hello world
-an inserted line
-hello boys""".split('\n')
+    text3 = """hello world
+    an inserted line
+    hello boys""".split('\n')
 
-print repr(knit2text(knit2))
-print repr(text3)
-knit3 = update_knit(knit2, 3, text3)
+    print repr(knit2text(knit2))
+    print repr(text3)
+    knit3 = update_knit(knit2, 3, text3)
 
-print '***** result of update:'
-show_annotated(knit3)
+    print '***** result of update:'
+    show_annotated(knit3)
 
 
 
---
+++ testknit.py
@@ -0,0 +1,61 @@
+# Copyright (C) 2005 by Canonical Ltd
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+
+
+"""test case for knit/weave algorithm"""
+
+
+from testsweet import TestBase
+from knit import Knit
+
+
+# texts for use in testing
+TEXTS = [
+    ["This is my text"], 
+]
+
+
+class Easy(TestBase):
+    def runTest(self):
+        k = Knit()
+
+
+class StoreText(TestBase):
+    """Store and retrieve a simple text."""
+    def runTest(self):
+        k = Knit()
+        k.add(TEXTS[0])
+        self.assertEqual(k.get(), TEXTS[0])
+
+
+def testknit():
+    import testsweet
+    from unittest import TestSuite, TestLoader
+    import testknit
+
+    tl = TestLoader()
+    suite = TestSuite()
+    suite.addTest(tl.loadTestsFromModule(testknit))
+    
+    return testsweet.run_suite(suite)
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(testknit())
+    
