d01fdbd5876105b342974e35274fc6542c60b92f
Martin Pool <mbp@sourcefrog.net>
2005-07-04 17:34:10 +1000

    - Test case for validate_revision_id

--- bzrlib/selftest/whitebox.py
+++ bzrlib/selftest/whitebox.py
@@ -19,6 +19,32 @@ class Unknowns(InTempDir):
                           ['hello.txt'])
 
 
+
+class ValidateRevisionId(TestBase):
+    def runTest(self):
+        from bzrlib.revision import validate_revision_id
+        validate_revision_id('mbp@sourcefrog.net-20050311061123-96a255005c7c9dbe')
+        
+        self.assertRaises(ValueError,
+                          validate_revision_id,
+                          ' asdkjas')
+
+
+        self.assertRaises(ValueError,
+                          validate_revision_id,
+                          'mbp@sourcefrog.net-20050311061123-96a255005c7c9dbe\n')
+
+
+        self.assertRaises(ValueError,
+                          validate_revision_id,
+                          ' mbp@sourcefrog.net-20050311061123-96a255005c7c9dbe')
+
+        self.assertRaises(ValueError,
+                          validate_revision_id,
+                          'Martin Pool <mbp@sourcefrog.net>-20050311061123-96a255005c7c9dbe')
+
+
+
 class Revert(InTempDir):
     """Test selected-file revert"""
     def runTest(self):
