72295c1990892f2fb4e412e67de7c87e306912ad
John Arbash Meinel <john@arbash-meinel.com>
2005-08-02 01:27:15 -0500

    Modified async_http to actually be a queue, so that it can utilize keepalive.

--- effbot/org/http_client.py
+++ effbot/org/http_client.py
@@ -36,32 +36,25 @@ class Redirect(CloseConnection):
     def __init__(self, location):
         self.location = location
 
-##
-# Asynchronous HTTP/1.1 client.
-
-class async_http(asyncore.dispatcher_with_send):
-    # asynchronous http client
+class Request(object):
+    """This keeps track of all the information for a single request.
+    """
 
     user_agent = "http_client.py 1.2 (http://effbot.org/zone)"
     http_version = "1.1"
-
     proxies = urllib.getproxies()
 
     def __init__(self, uri, consumer, extra_headers=None):
-        asyncore.dispatcher_with_send.__init__(self)
-
+        self.consumer = consumer
         # turn the uri into a valid request
         scheme, host, path, params, query, fragment = urlparse.urlparse(uri)
 
-        # use origin host
+        self.scheme = scheme
         self.host = host
 
         # get proxy settings, if any
         proxy = self.proxies.get(scheme)
-        if proxy:
-            scheme, host, x, x, x, x = urlparse.urlparse(proxy)
-
-        assert scheme == "http", "only supports HTTP requests (%s)" % scheme
+        self.proxy = proxy
 
         if not path:
             path = "/"
@@ -73,15 +66,6 @@ class async_http(asyncore.dispatcher_with_send):
             path = scheme + "://" + self.host + path
 
         self.path = path
-
-        # get port number
-        try:
-            host, port = host.split(":", 1)
-            port = int(port)
-        except (TypeError, ValueError):
-            port = 80 # default port
-
-        self.consumer = consumer
 
         self.status = None
         self.header = None
@@ -101,14 +85,10 @@ class async_http(asyncore.dispatcher_with_send):
         self.timestamp = time.time()
 
         self.extra_headers = extra_headers
+        self.requested = False
 
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            self.connect((host, port))
-        except socket.error:
-            self.consumer.http(0, self, sys.exc_info())
-
-    def handle_connect(self):
+    def http_request(self, conn):
+        """Place the actual http request on the server"""
         # connection succeeded
 
         request = [
@@ -131,21 +111,18 @@ class async_http(asyncore.dispatcher_with_send):
 
         request = string.join(request, "\r\n") + "\r\n\r\n"
 
-        self.send(request)
+        conn.send(request)
 
         self.bytes_out = self.bytes_out + len(request)
+        self.requested = True
 
-    def handle_expt(self):
-        # connection failed (windows); notify consumer
-
-        if sys.platform == "win32":
-            self.close()
-            self.consumer.http(0, self)
-
-    def handle_read(self):
-        # handle incoming data
-
-        data = self.recv(2048)
+    def add_data(self, data):
+        """Some data has been downloaded, let the consumer know.
+        :return: True- download is completed, there may be extra data in self.data which
+                       should be transfered to the next item
+                 False- download failed
+                 None- continue downloading
+        """
 
         self.data = self.data + data
         self.bytes_in = self.bytes_in + len(data)
@@ -173,6 +150,7 @@ class async_http(asyncore.dispatcher_with_send):
                         )
                 except (ValueError, TypeError):
                     self.content_length = None
+                self.original_content_length = self.content_length
                 self.transfer_encoding = self.header.get("transfer-encoding")
                 self.content_encoding = self.header.get("content-encoding")
 
@@ -188,11 +166,9 @@ class async_http(asyncore.dispatcher_with_send):
                         do_request(
                             v.location, self.consumer, self.extra_headers
                             )
-                    self.close()
-                    return
+                    return True
                 except CloseConnection:
-                    self.close()
-                    return
+                    return False
 
             if self.transfer_encoding == "chunked" and self.chunk_size is None:
 
@@ -210,7 +186,8 @@ class async_http(asyncore.dispatcher_with_send):
                     if self.chunk_size <= 0:
                         raise ValueError
                 except ValueError:
-                    return self.handle_close()
+                    self.consumer.close()
+                    return False
 
             if not self.data:
                 return
@@ -219,6 +196,12 @@ class async_http(asyncore.dispatcher_with_send):
             self.data = ""
 
             chunk_size = self.chunk_size or len(data)
+
+            # Make sure to only feed the consumer whatever is left for
+            # this file.
+            if self.content_length:
+                if chunk_size > self.content_length:
+                    chunk_size = self.content_length
 
             if chunk_size < len(data):
                 self.data = data[chunk_size:]
@@ -235,19 +218,162 @@ class async_http(asyncore.dispatcher_with_send):
             if self.content_length:
                 self.content_length -= chunk_size
                 if self.content_length <= 0:
-                    return self.handle_close()
+                    self.consumer.close()
+                    return True
+
+
+##
+# Asynchronous HTTP/1.1 client.
+
+class async_http(asyncore.dispatcher_with_send):
+    """Asynchronous HTTP client.
+    This client keeps a queue of files to download, and
+    tries to asynchronously (and pipelined) download them,
+    alerting the consumer as bits are downloaded.
+    """
+
+    def __init__(self, scheme, host):
+        """Connect to the given host, extra requests are made on the
+        add_request member function.
+
+        :param scheme: The connection method, such as http/https, currently
+                       we only support http
+        :param host:   The host to connect to, either a proxy, or the actual host.
+        """
+        asyncore.dispatcher_with_send.__init__(self)
+
+        # use origin host
+        self.scheme = scheme
+        self.host = host
+
+        assert scheme == "http", "only supports HTTP requests (%s)" % scheme
+
+        self._connected = False
+        self._queue = []
+        self._current = None
+
+    def _connect(self):
+        # get port number
+        host = self.host
+        try:
+            host, port = self.host.split(":", 1)
+            port = int(port)
+        except (TypeError, ValueError):
+            port = 80 # default port
+
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.connect((host, port))
+            self._connected = True
+        except socket.error:
+            self.handle_error()
+
+    def _close(self):
+        if self._connected:
+            self.close()
+        self._connected = False
+
+    def _request_next(self):
+        extra_data = None
+        if self._current:
+            extra_data = self._current.data
+        if len(self._queue) > 0:
+            self._current = self._queue.pop(0)
+            if not self._current.requested:
+                self._current.http_request(self)
+            if extra_data:
+                self._update_current(extra_data)
+        else:
+            # TODO: Consider some sort of delayed closing,
+            # rather than closing the instant the
+            # queue is empty. But I don't know any way
+            # under async_core to be alerted at a later time.
+            # If this were a thread, I would sleep, waking
+            # up to check for more work, and eventually timing
+            # out and disconnecting.
+            self._close()
+
+    def _update_current(self, data):
+        res = self._current.add_data(data)
+        if res is None:
+            # Downloading is continuing
+            if self._current.original_content_length:
+                # We can pipeline our requests, since we
+                # are getting a content_length count back
+                if len(self._queue) > 0 and not self._queue[0].requested:
+                    cur = self._current.content_length
+                    orig = self._current.original_content_length
+                    if cur < orig / 2:
+                        # We are over 1/2 done with the previous download
+                        # go ahead and request the next one
+                        self._queue[0].http_request(self)
+            return
+        if res:
+            # We finished downloading the last file
+            self._request_next()
+        else:
+            # There was a failure
+            self.handle_error()
+
+    def add_request(self, request):
+        """Add a new Request into the queue."""
+        self._queue.append(request)
+        if not self._connected:
+            self._connect()
+
+    def handle_connect(self):
+        self._request_next()
+
+    def handle_expt(self):
+        # connection failed (windows); notify consumer
+        assert self._current
+
+        if sys.platform == "win32":
+            self._close()
+            self._current.consumer.http(0, self._current)
+
+    def handle_read(self):
+        # handle incoming data
+        assert self._current
+
+        data = self.recv(2048)
+
+        self._update_current(data)
+
 
     def handle_close(self):
-        self.consumer.close()
-        self.close()
+        """When does this event occur? Is it okay to start the next entry in the queue
+        (possibly reconnecting), or is this an indication that we should stop?
+        """
+        if self._current:
+            self._current.consumer.close()
+        self._close()
+        if len(self._queue) > 0:
+            self._connect()
 
     def handle_error(self):
-        self.consumer.http(0, self, sys.exc_info())
-        self.close()
+        if self._current:
+            self._current.consumer.http(0, self._current, sys.exc_info())
+        # Should this be broadcast to all other items waiting in the queue?
+        self._close()
+
+_connections = {}
 
 def do_request(uri, consumer, extra_headers=None):
+    global _connections
+    request = Request(uri, consumer, extra_headers)
 
-    return async_http(uri, consumer, extra_headers)
+    scheme = request.scheme
+    host = request.host
+    if request.proxy:
+        host = request.proxy
+    key = (scheme, host)
+    if not _connections.has_key(key):
+        _connections[key] = async_http(scheme, host)
+
+    _connections[key].add_request(request)
+
+    return request
 
 if __name__ == "__main__":
     class dummy_consumer:
@@ -256,13 +382,13 @@ if __name__ == "__main__":
             print "feed", repr(data[:20]), repr(data[-20:]), len(data)
         def close(self):
             print "close"
-        def http(self, ok, connection, **args):
-            print ok, connection, args
+        def http(self, ok, connection, *args, **kwargs):
+            print ok, connection, args, kwargs
             print "status", connection.status
             print "header", connection.header
-    try:
-        url = sys.argv[1]
-    except IndexError:
-        url = "http://www.cnn.com/"
-    do_request(url, dummy_consumer())
+    if len(sys.argv) < 2:
+        do_request('http://www.cnn.com/', dummy_consumer())
+    else:
+        for url in sys.argv[1:]:
+            do_request(url, dummy_consumer())
     asyncore.loop()
