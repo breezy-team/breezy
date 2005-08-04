75035108ab807dfc7592a2a28ec4b7de0c7d7ee6
Martin Pool <mbp@sourcefrog.net>
2005-08-04 19:07:31 -0300

    - fix up imports

--- bzrlib/revision.py
+++ bzrlib/revision.py
@@ -15,6 +15,7 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 
+import bzrlib.errors
 
 
 class RevisionReference(object):
@@ -113,10 +114,8 @@ class Revision(object):
 def unpack_revision(elt):
     """Convert XML element into Revision object."""
     # <changeset> is deprecated...
-    from bzrlib.errors import BzrError
-    
     if elt.tag not in ('revision', 'changeset'):
-        raise BzrError("unexpected tag in revision file: %r" % elt)
+        raise bzrlib.errors.BzrError("unexpected tag in revision file: %r" % elt)
 
     rev = Revision(committer = elt.get('committer'),
                    timestamp = float(elt.get('timestamp')),
@@ -177,7 +176,6 @@ def is_ancestor(revision_id, candidate_id, revision_source):
     behaves like Branch's.
     """
 
-    from bzrlib.branch import NoSuchRevision
     ancestors = (revision_id,)
     while len(ancestors) > 0:
         new_ancestors = []
@@ -202,10 +200,9 @@ class MultipleRevisionSources(object):
         self._revision_sources = args
 
     def get_revision(self, revision_id):
-        from bzrlib.branch import NoSuchRevision
         for source in self._revision_sources:
             try:
                 return source.get_revision(revision_id)
-            except NoSuchRevision, e:
+            except bzrlib.errors.NoSuchRevision, e:
                 pass
         raise e
