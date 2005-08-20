29f913368c9b73046a8706c588dbe8f8298a04c2
aaron.bentley@utoronto.ca <aaron.bentley@utoronto.ca>
2005-08-20 01:10:18 -0400

    Got greedy fetch working when the desired revision's not in the revision history

--- bzrlib/fetch.py
+++ bzrlib/fetch.py
@@ -21,18 +21,30 @@ from bzrlib.progress import ProgressBar
 import sys
 import os
 
-def greedy_fetch(from_branch, to_branch, last_revision=None):
+def greedy_fetch(to_branch, from_branch, last_revision=None):
+    """Copy a revision and all available ancestors from one branch to another
+    If no revision is specified, uses the last revision in the source branch's
+    revision history.
+    """
     from_history = from_branch.revision_history()
     if last_revision is not None:
-        from_history = from_history[:from_history.index(last_revision)+1]
+        try:
+            rev_index = from_history.index(last_revision)
+        except ValueError:
+            rev_index = None
+        if rev_index is not None:
+            from_history = from_history[:rev_index + 1]
+        else:
+            from_history = [last_revision]
     to_history = to_branch.revision_history()
     missing = []
     for rev_id in from_history:
         if not has_revision(to_branch, rev_id):
             missing.append(rev_id)
-
+    
+    count = 0
     while len(missing) > 0:
-        to_branch.install_revisions(from_branch, revision_ids=missing)
+        count += to_branch.install_revisions(from_branch, revision_ids=missing)
         new_missing = []
         for rev_id in missing:
             try:
@@ -43,9 +55,11 @@ def greedy_fetch(from_branch, to_branch, last_revision=None):
                 else:
                     continue
             for parent in [p.revision_id for p in revision.parents]:
+                print >> sys.stderr, "adding %s" % parent
                 if not has_revision(to_branch, parent):
                     new_missing.append(parent)
         missing = new_missing
+    return count
 
 
 from testsweet import InTempDir
@@ -67,31 +81,39 @@ class TestFetch(InTempDir):
         assert not has_revision(br_b, br_a.revision_history()[3])
         assert has_revision(br_b, br_a.revision_history()[2])
         assert len(br_b.revision_history()) == 5
-        greedy_fetch(br_a, br_b, br_a.revision_history()[2])
+        assert greedy_fetch(br_b, br_a, br_a.revision_history()[2]) == 0
 
         # greedy_fetch is not supposed to alter the revision history
         assert len(br_b.revision_history()) == 5
         assert not has_revision(br_b, br_a.revision_history()[3])
 
         assert len(br_b.revision_history()) == 5
-        greedy_fetch(br_a, br_b, br_a.revision_history()[3])
+        assert greedy_fetch(br_b, br_a, br_a.revision_history()[3]) == 1
         assert has_revision(br_b, br_a.revision_history()[3])
         assert not has_revision(br_a, br_b.revision_history()[3])
         assert not has_revision(br_a, br_b.revision_history()[4])
-        greedy_fetch(br_b, br_a)
+        assert greedy_fetch(br_a, br_b) == 2
         assert has_revision(br_a, br_b.revision_history()[3])
         assert has_revision(br_a, br_b.revision_history()[4])
 
         br_b2 = new_branch('br_b2')
-        greedy_fetch(br_b, br_b2)
+        assert greedy_fetch(br_b2, br_b) == 5
         assert has_revision(br_b2, br_b.revision_history()[4])
         assert has_revision(br_b2, br_a.revision_history()[2])
         assert not has_revision(br_b2, br_a.revision_history()[3])
 
         br_a2 = new_branch('br_a2')
-        greedy_fetch(br_a, br_a2)
+        assert greedy_fetch(br_a2, br_a) == 6
         assert has_revision(br_a2, br_b.revision_history()[4])
         assert has_revision(br_a2, br_a.revision_history()[3])
+
+        br_a3 = new_branch('br_a3')
+        assert greedy_fetch(br_a3, br_a2) == 0
+        for revno in range(4):
+            assert not has_revision(br_a3, br_a.revision_history()[revno])
+        assert greedy_fetch(br_a3, br_a2, br_a.revision_history()[2]) == 3
+        fetched = greedy_fetch(br_a3, br_a2, br_a.revision_history()[3])
+        assert fetched == 3, "fetched %d instead of 3" % fetched
 
 
 
