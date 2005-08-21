0329153b2eb2b3e8ad0fc4f356a8ee8cbaa518fc
aaron.bentley@utoronto.ca <aaron.bentley@utoronto.ca>
2005-08-21 00:15:04 -0400

    Added revision-based common-ancestor checking

--- bzrlib/fetch.py
+++ bzrlib/fetch.py
@@ -88,18 +88,18 @@ class TestFetch(InTempDir):
             os.mkdir(name)
             return Branch(name, init=True)
             
-        #highest indices a: 3, b: 4
+        #highest indices a: 5, b: 7
         br_a, br_b = make_branches()
         assert not has_revision(br_b, br_a.revision_history()[3])
         assert has_revision(br_b, br_a.revision_history()[2])
-        assert len(br_b.revision_history()) == 5
+        assert len(br_b.revision_history()) == 7
         assert greedy_fetch(br_b, br_a, br_a.revision_history()[2])[0] == 0
 
         # greedy_fetch is not supposed to alter the revision history
-        assert len(br_b.revision_history()) == 5
+        assert len(br_b.revision_history()) == 7
         assert not has_revision(br_b, br_a.revision_history()[3])
 
-        assert len(br_b.revision_history()) == 5
+        assert len(br_b.revision_history()) == 7
         assert greedy_fetch(br_b, br_a, br_a.revision_history()[3])[0] == 1
         assert has_revision(br_b, br_a.revision_history()[3])
         assert not has_revision(br_a, br_b.revision_history()[3])
@@ -109,21 +109,22 @@ class TestFetch(InTempDir):
         # exception
         br_a4 = new_branch('br_a4')
         count, failures = greedy_fetch(br_a4, br_a)
-        assert count == 4
-        assert failures == set((br_b.revision_history()[4],)) 
+        assert count == 6
+        assert failures == set((br_b.revision_history()[4],
+                                br_b.revision_history()[5])) 
 
-        assert greedy_fetch(br_a, br_b)[0] == 2
+        assert greedy_fetch(br_a, br_b)[0] == 4
         assert has_revision(br_a, br_b.revision_history()[3])
         assert has_revision(br_a, br_b.revision_history()[4])
 
         br_b2 = new_branch('br_b2')
-        assert greedy_fetch(br_b2, br_b)[0] == 5
+        assert greedy_fetch(br_b2, br_b)[0] == 7
         assert has_revision(br_b2, br_b.revision_history()[4])
         assert has_revision(br_b2, br_a.revision_history()[2])
         assert not has_revision(br_b2, br_a.revision_history()[3])
 
         br_a2 = new_branch('br_a2')
-        assert greedy_fetch(br_a2, br_a)[0] == 6
+        assert greedy_fetch(br_a2, br_a)[0] == 9
         assert has_revision(br_a2, br_b.revision_history()[4])
         assert has_revision(br_a2, br_a.revision_history()[3])
 
