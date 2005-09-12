191ac1a36d88f228e664b46ebb243531f4b3973f
Martin Pool <mbp@sourcefrog.net>
2005-09-12 19:11:23 +1000

    - InventoryEntry.copy() must copy text_version

--- bzrlib/inventory.py
+++ bzrlib/inventory.py
@@ -165,6 +165,7 @@ class InventoryEntry(object):
         other.text_id = self.text_id
         other.text_sha1 = self.text_sha1
         other.text_size = self.text_size
+        other.text_version = self.text_version
         # note that children are *not* copied; they're pulled across when
         # others are added
         return other
