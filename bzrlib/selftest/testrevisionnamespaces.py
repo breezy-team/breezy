d8fcd6f51262a74e57d0ce1d03baf8dc7271dbea
John Arbash Meinel <john@arbash-meinel.com>
2005-09-17 14:10:13 -0400

    RevisionSpec can be instantiated from another revision spec.

--- bzrlib/selftest/testrevisionnamespaces.py
+++ bzrlib/selftest/testrevisionnamespaces.py
@@ -74,3 +74,16 @@ class TestRevisionNamespaces(TestCaseInTempDir):
                           'b@r-0-4')
         self.assertEquals(RevisionSpec('ancestor:.').in_history(b3).rev_id,
                           'b@r-0-4')
+
+
+        # Make sure you can create a revision spec from another revision spec
+        spec = RevisionSpec('revid:a@r-0-4')
+        self.assertEquals(spec, RevisionSpec(spec))
+
+        spec = RevisionSpec('1')
+        self.assertEquals(spec, RevisionSpec(spec))
+
+        spec_1 = RevisionSpec(1)
+        self.assertEquals(spec_1, RevisionSpec(spec_1))
+        self.assertEquals(spec, spec_1)
+
