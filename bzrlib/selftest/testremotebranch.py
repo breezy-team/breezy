3acf443a175d83f5e648dd124ebf1c4fec54103b
John Arbash Meinel <john@arbash-meinel.com>
2005-09-17 21:35:05 -0400

    Removed bogus comment.

--- bzrlib/selftest/testremotebranch.py
+++ bzrlib/selftest/testremotebranch.py
@@ -24,9 +24,6 @@ from bzrlib.selftest.HTTPTestUtil import TestCaseWithWebserver
         
 
 class TestFetch(TestCaseWithWebserver):
-    # For some reason, this test *has* to be named runTest()
-    # trying to run it as test_fetch causes exceptions to be
-    # thrown involving "resource is temporarily unavailable".
     def test_fetch(self):
         from bzrlib.fetch import greedy_fetch, has_revision
 
