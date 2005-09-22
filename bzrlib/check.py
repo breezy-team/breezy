cdbe05cc3b6a69a37c2ddcadf8f722510ede1b42
Martin Pool <mbp@sourcefrog.net>
2005-09-22 10:19:45 +1000

    - more refactoring of check code

--- bzrlib/check.py
+++ bzrlib/check.py
@@ -22,6 +22,9 @@
 
 # TODO: Check for extra files in the control directory.
 
+# TODO: Check revision, inventory and entry objects have all 
+# required fields.
+
 
 import bzrlib.ui
 from bzrlib.trace import note, warning
@@ -42,131 +45,124 @@ class Check(object):
     def run(self):
         branch = self.branch
 
-        last_rev_id = None
-
-        missing_inventory_sha_cnt = 0
-        missing_revision_sha_cnt = 0
-        missing_revision_cnt = 0
+	self.checked_text_cnt = 0
+	self.checked_rev_cnt = 0
+        self.missing_inventory_sha_cnt = 0
+        self.missing_revision_cnt = 0
 
         history = branch.revision_history()
         revno = 0
         revcount = len(history)
 
-        checked_text_count = 0
-
-        progress = bzrlib.ui.ui_factory.progress_bar()
-
+        last_rev_id = None
+        self.progress = bzrlib.ui.ui_factory.progress_bar()
         for rev_id in history:
+            self.progress.update('checking revision', revno, revcount)
             revno += 1
-            progress.update('checking revision', revno, revcount)
-            # mutter('    revision {%s}' % rev_id)
-            rev = branch.get_revision(rev_id)
-            if rev.revision_id != rev_id:
-                raise BzrCheckError('wrong internal revision id in revision {%s}'
-                                    % rev_id)
-
-            # check the previous history entry is a parent of this entry
-            if rev.parent_ids:
-                if last_rev_id is None:
-                    raise BzrCheckError("revision {%s} has %d parents, but is the "
-                                        "start of the branch"
-                                        % (rev_id, len(rev.parent_ids)))
-                for parent_id in rev.parent_ids:
-                    if parent_id == last_rev_id:
-                        break
-                else:
-                    raise BzrCheckError("previous revision {%s} not listed among "
-                                        "parents of {%s}"
-                                        % (last_rev_id, rev_id))
-            elif last_rev_id:
-                raise BzrCheckError("revision {%s} has no parents listed "
-                                    "but preceded by {%s}"
-                                    % (rev_id, last_rev_id))
-
-            ## TODO: Check all the required fields are present on the revision.
-
-            if rev.inventory_sha1:
-                inv_sha1 = branch.get_inventory_sha1(rev_id)
-                if inv_sha1 != rev.inventory_sha1:
-                    raise BzrCheckError('Inventory sha1 hash doesn\'t match'
-                        ' value in revision {%s}' % rev_id)
-            else:
-                missing_inventory_sha_cnt += 1
-                mutter("no inventory_sha1 on revision {%s}" % rev_id)
-
-            tree = branch.revision_tree(rev_id)
-            inv = tree.inventory
-            seen_ids = {}
-            seen_names = {}
-
-            ## p('revision %d/%d file ids' % (revno, revcount))
-            for file_id in inv:
-                if file_id in seen_ids:
-                    raise BzrCheckError('duplicated file_id {%s} '
-                                        'in inventory for revision {%s}'
-                                        % (file_id, rev_id))
-                seen_ids[file_id] = True
-
-            i = 0
-            for file_id in inv:
-                i += 1
-                if i & 31 == 0:
-                    progress.tick()
-
-                ie = inv[file_id]
-
-                if ie.parent_id != None:
-                    if ie.parent_id not in seen_ids:
-                        raise BzrCheckError('missing parent {%s} in inventory for revision {%s}'
-                                % (ie.parent_id, rev_id))
-
-                if ie.kind == 'file':
-                    text = tree.get_file_text(file_id)
-                    checked_text_count += 1 
-                    if ie.text_size != len(text):
-                        raise BzrCheckError('text {%s} wrong size' % ie.text_id)
-                    if ie.text_sha1 != sha_string(text):
-                        raise BzrCheckError('text {%s} wrong sha1' % ie.text_id)
-                elif ie.kind == 'directory':
-                    if ie.text_sha1 != None or ie.text_size != None or ie.text_id != None:
-                        raise BzrCheckError('directory {%s} has text in revision {%s}'
-                                % (file_id, rev_id))
-
-            progress.tick()
-            for path, ie in inv.iter_entries():
-                if path in seen_names:
-                    raise BzrCheckError('duplicated path %s '
-                                        'in inventory for revision {%s}'
-                                        % (path, rev_id))
-                seen_names[path] = True
-            last_rev_id = rev_id
+	    self.check_one_rev(rev_id, last_rev_id)
+	    last_rev_id = rev_id
+        self.progress.clear()
+	self.report_results()
 
 
-        progress.clear()
+    def report_results(self):
+	note('checked branch %s format %d',
+	     self.branch.base, 
+	     self.branch._branch_format)
 
-        note('checked %d revisions, %d file texts' % (revcount, checked_text_count))
+        note('checked %d revisions, %d file texts',
+	     self.checked_rev_cnt,
+	     self.checked_text_cnt)
 
-        if missing_inventory_sha_cnt:
-            note('%d revisions are missing inventory_sha1' % missing_inventory_sha_cnt)
+        if self.missing_inventory_sha_cnt:
+            note('%d revisions are missing inventory_sha1',
+		 self.missing_inventory_sha_cnt)
 
-        ##if missing_revision_sha_cnt:
-        ##    note('%d parent links are missing revision_sha1' % missing_revision_sha_cnt)
-
-        if missing_revision_cnt:
-            note('%d revisions are mentioned but not present' % missing_revision_cnt)
-
-        if missing_revision_cnt:
-            print '%d revisions are mentioned but not present' % missing_revision_cnt
-
-        # stub this out for now because the main bzr branch has references
-        # to revisions that aren't present in the store -- mbp 20050804
-    #    if (missing_inventory_sha_cnt
-    #        or missing_revision_sha_cnt):
-    #        print '  (use "bzr upgrade" to fix them)'
+        if self.missing_revision_cnt:
+            note('%d revisions are mentioned but not present',
+		 self.missing_revision_cnt)
 
 
+    def check_one_rev(self, rev_id, last_rev_id):
+	# mutter('    revision {%s}' % rev_id)
+	branch = self.branch
+	rev = branch.get_revision(rev_id)
+	if rev.revision_id != rev_id:
+	    raise BzrCheckError('wrong internal revision id in revision {%s}'
+				% rev_id)
 
+	# check the previous history entry is a parent of this entry
+	if rev.parent_ids:
+	    if last_rev_id is None:
+		raise BzrCheckError("revision {%s} has %d parents, but is the "
+				    "start of the branch"
+				    % (rev_id, len(rev.parent_ids)))
+	    for parent_id in rev.parent_ids:
+		if parent_id == last_rev_id:
+		    break
+	    else:
+		raise BzrCheckError("previous revision {%s} not listed among "
+				    "parents of {%s}"
+				    % (last_rev_id, rev_id))
+	elif last_rev_id:
+	    raise BzrCheckError("revision {%s} has no parents listed "
+				"but preceded by {%s}"
+				% (rev_id, last_rev_id))
 
+	if rev.inventory_sha1:
+	    inv_sha1 = branch.get_inventory_sha1(rev_id)
+	    if inv_sha1 != rev.inventory_sha1:
+		raise BzrCheckError('Inventory sha1 hash doesn\'t match'
+		    ' value in revision {%s}' % rev_id)
+	else:
+	    missing_inventory_sha_cnt += 1
+	    mutter("no inventory_sha1 on revision {%s}" % rev_id)
+
+	tree = branch.revision_tree(rev_id)
+	inv = tree.inventory
+	seen_ids = {}
+	seen_names = {}
+
+	## p('revision %d/%d file ids' % (revno, revcount))
+	for file_id in inv:
+	    if file_id in seen_ids:
+		raise BzrCheckError('duplicated file_id {%s} '
+				    'in inventory for revision {%s}'
+				    % (file_id, rev_id))
+	    seen_ids[file_id] = True
+
+	i = 0
+	for file_id in inv:
+	    i += 1
+	    if i & 31 == 0:
+		self.progress.tick()
+
+	    ie = inv[file_id]
+
+	    if ie.parent_id != None:
+		if ie.parent_id not in seen_ids:
+		    raise BzrCheckError('missing parent {%s} in inventory for revision {%s}'
+			    % (ie.parent_id, rev_id))
+
+	    if ie.kind == 'file':
+		text = tree.get_file_text(file_id)
+		self.checked_text_cnt += 1 
+		if ie.text_size != len(text):
+		    raise BzrCheckError('text {%s} wrong size' % ie.text_id)
+		if ie.text_sha1 != sha_string(text):
+		    raise BzrCheckError('text {%s} wrong sha1' % ie.text_id)
+	    elif ie.kind == 'directory':
+		if ie.text_sha1 != None or ie.text_size != None or ie.text_id != None:
+		    raise BzrCheckError('directory {%s} has text in revision {%s}'
+			    % (file_id, rev_id))
+
+	self.progress.tick()
+	for path, ie in inv.iter_entries():
+	    if path in seen_names:
+		raise BzrCheckError('duplicated path %s '
+				    'in inventory for revision {%s}'
+				    % (path, rev_id))
+	    seen_names[path] = True
         
 
 
