b0449dc60beb13f114e4547471565b533ed1c849
John Arbash Meinel <john@arbash-meinel.com>
2005-09-23 13:30:46 -0500

    Adding ability to revert back to earlier revisions.

--- __init__.py
+++ __init__.py
@@ -20,12 +20,13 @@ class cmd_uncommit(bzrlib.commands.Command):
     In the future, uncommit will create a changeset, which can then
     be re-applied.
     """
-    takes_options = ['remove', 'dry-run', 'verbose']
+    takes_options = ['remove', 'dry-run', 'verbose', 'revision']
     takes_args = ['location?']
     aliases = []
 
     def run(self, location=None, remove=False,
-            dry_run=False, verbose=False):
+            dry_run=False, verbose=False,
+            revision=None):
         from bzrlib.branch import Branch
         from bzrlib.log import log_formatter
         import uncommit, sys
@@ -34,22 +35,28 @@ class cmd_uncommit(bzrlib.commands.Command):
             location = '.'
         b = Branch.open_containing(location)
 
-        revno = b.revno()
-        rev_id = b.last_patch()
+        if revision is None:
+            revno = b.revno()
+            rev_id = b.last_patch()
+        else:
+            revno, rev_id = revision[0].in_history(b)
         if rev_id is None:
             print 'No revisions to uncommit.'
 
-        lf = log_formatter('short', to_file=sys.stdout,show_timezone='original')
-        lf.show(revno, b.get_revision(rev_id), None)
+        for r in range(revno, b.revno()+1):
+            rev_id = b.get_rev_id(revno)
+            lf = log_formatter('short', to_file=sys.stdout,show_timezone='original')
+            lf.show(r, b.get_revision(rev_id), None)
 
-        print 'The above revision will be removed.'
+        print 'The above revision(s) will be removed.'
         val = raw_input('Are you sure [y/N]? ')
         if val.lower() not in ('y', 'yes'):
             print 'Canceled'
             return 0
 
         uncommit.uncommit(b, remove_files=remove,
-                dry_run=dry_run, verbose=verbose)
+                dry_run=dry_run, verbose=verbose,
+                revno=revno)
 
 bzrlib.commands.register_command(cmd_uncommit)
 bzrlib.commands.OPTIONS['remove'] = None
--- uncommit.py
+++ uncommit.py
@@ -19,7 +19,7 @@ def test_remove(filename):
 
 
 def uncommit(branch, remove_files=False,
-        dry_run=False, verbose=False):
+        dry_run=False, verbose=False, revno=None):
     """Remove the last revision from the supplied branch.
 
     :param remove_files: If True, remove files from the stores
@@ -27,53 +27,65 @@ def uncommit(branch, remove_files=False,
     """
     from bzrlib.atomicfile import AtomicFile
     rh = branch.revision_history()
-    rev_id = rh.pop()
-    rev = branch.get_revision(rev_id)
-    inv = branch.get_inventory(rev.inventory_id)
-    inv_prev = []
-    for p in rev.parents:
-        inv_prev.append(branch.get_revision_inventory(p.revision_id))
+    if revno is None:
+        revno = len(rh)
 
+    files_to_remove = []
     new_rev_history = AtomicFile(branch.controlfilename('revision-history'))
+    for r in range(revno-1, len(rh)):
+        rev_id = rh.pop()
+        if verbose:
+            print 'Removing revno %d: %s' % (len(rh)+1, rev_id)
+        rev = branch.get_revision(rev_id)
+        inv = branch.get_inventory(rev.inventory_id)
+        inv_prev = []
+        for p in rev.parents:
+            inv_prev.append(branch.get_revision_inventory(p.revision_id))
+
+        if remove_files:
+            # Figure out what text-store entries are new
+
+            # In the future, when we have text_version instead of
+            # text_id, we can just check to see if the text_version
+            # equals the current revision id.
+            for file_id in inv:
+                ie = inv[file_id]
+                if not hasattr(ie, 'text_id'):
+                    continue
+                for other_inv in inv_prev:
+                    if file_id in other_inv:
+                        other_ie = other_inv[file_id]
+                        if other_ie.text_id == ie.text_id:
+                            break
+                else:
+                    # None of the previous ancestors used
+                    # the same inventory
+                    files_to_remove.append(branch.controlfilename(['text-store',
+                        ie.text_id + '.gz']))
+            rev_file = branch.controlfilename(['revision-store',
+                    rev_id + '.gz'])
+            files_to_remove.append(rev_file)
+            inv_file = branch.controlfilename(['inventory-store',
+                    rev.inventory_id + '.gz'])
+            files_to_remove.append(inv_file)
+
+    if verbose:
+        print 'Removing files:'
+        for f in files_to_remove:
+            print '\t%s' % branch.relpath(f)
+
     new_rev_history.write('\n'.join(rh))
-    # Committing now, because even if we fail to remove all files
+
+    # Committing before we start removing files, because
     # once we have removed at least one, all the rest are invalid.
     if not dry_run:
         new_rev_history.commit()
-    else:
-        new_rev_history.abort()
-
-    if remove_files:
-        # Figure out what text-store entries are new
-        files_to_remove = []
-        for file_id in inv:
-            ie = inv[file_id]
-            if not hasattr(ie, 'text_id'):
-                continue
-            for other_inv in inv_prev:
-                if file_id in other_inv:
-                    other_ie = other_inv[file_id]
-                    if other_ie.text_id == ie.text_id:
-                        break
-            else:
-                # None of the previous ancestors used
-                # the same inventory
-                files_to_remove.append(branch.controlfilename(['text-store',
-                    ie.text_id + '.gz']))
-        rev_file = branch.controlfilename(['revision-store',
-                rev_id + '.gz'])
-        files_to_remove.append(rev_file)
-        inv_file = branch.controlfilename(['inventory-store',
-                rev.inventory_id + '.gz'])
-        files_to_remove.append(inv_file)
-
-        if verbose:
-            print 'Removing files:'
-            for f in files_to_remove:
-                print '\t%s' % branch.relpath(f)
-
-        if not dry_run:
+        if remove_files:
             # Actually start removing files
             for f in files_to_remove:
                 test_remove(f)
+
+    else:
+        new_rev_history.abort()
+
 
