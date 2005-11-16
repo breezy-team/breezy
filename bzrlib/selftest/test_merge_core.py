c4d57e3cae409cb2b31efd9734988dbf554d429e
Martin Pool <mbp@sourcefrog.net>
2005-11-16 18:47:03 +1100

    Add test for merging metadata change vs file deletion (currently passes but should fail)

--- bzrlib/selftest/test_merge_core.py
+++ bzrlib/selftest/test_merge_core.py
@@ -14,8 +14,8 @@ from bzrlib.osutils import file_kind, rename, sha_file
 from bzrlib import changeset
 from bzrlib.merge_core import (ApplyMerge3, make_merge_changeset,
                                BackupBeforeChange, ExecFlagMerge, WeaveMerge)
-from bzrlib.changeset import Inventory, apply_changeset, invert_dict
-from bzrlib.changeset import get_contents, ReplaceContents
+from bzrlib.changeset import Inventory, apply_changeset, invert_dict, \
+    get_contents, ReplaceContents, ChangeExecFlag
 from bzrlib.clone import copy_branch
 from bzrlib.merge import merge
 
@@ -625,3 +625,23 @@ class FunctionalMergeTest(TestCaseInTempDir):
         self.assert_(os.path.lexists('a/file'))
         self.assert_(os.path.lexists('a/file.moved'))
         self.assertEqual(a.pending_merges(), [b.last_revision()]) 
+
+    def test_merge_metadata_vs_deletion(self):
+        """Conflict deletion vs metadata change"""
+        os.mkdir('a')
+        a = Branch.initialize('a')
+        file('a/file', 'wb').write('contents\n')
+        a.add('file')
+        a.commit('r0')
+        copy_branch(a, 'b')
+        b = Branch.open('b')
+        os.chmod('b/file', 0755)
+        os.remove('a/file')
+        a.commit('removed a')
+        self.assertEqual(a.revno(), 2)
+        self.assertFalse(os.path.exists('a/file'))
+        b.commit('exec a')
+        merge(['b', -1], ['b', 0], this_dir='a')
+        self.assert_(os.path.exists('a/file'))
+        import pdb
+        pdb.set_trace()
