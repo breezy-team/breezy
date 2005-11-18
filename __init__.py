e19180bf8a04f4c4469a3b49ea15c9db344c1d5d
Jamie Wilkinson <jamie@anchor.net.au>
2005-11-18 12:44:56 +1100

    added stub plugin for bzr-buildpackage

---
+++ __init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from bzrlib.commands import Command, register_command
+from bzrlib.errors import (NoSuchFile, NotBranchError)
+from bzrlib.branch import Branch
+
+class cmd_buildpackage(Command):
+	"""Build the package
+	"""
+	takes_args = ['package', 'version']
+	takes_options = ['verbose']
+
+	def run(self, package, version, verbose=False):
+		retcode = 0
+
+		return retcode
+
+def test_suite():
+	from unittest import TestSuite, TestLoader
+	import test_buildpackage
+	suite = TestSuite()
+	suite.addTest(TestLoader().loadTestsFromModule(test_buildpackage))
+	return suite
+
+register_command(cmd_buildpackage)
