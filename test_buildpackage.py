2af2f8e2ba12e82afe325c4791e5f35a07436f16
Jamie Wilkinson <jamie@anchor.net.au>
2005-11-18 15:17:49 +1100

    added stub test code

---
+++ test_buildpackage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+"""A test suite for the buildpackage plugin.
+"""
+
+import os
+from bzrlib.selftest import TestCaseInTempDir
+from bzrlib.branch import Branch
+
+class TestBuildPackage(TestCaseInTempDir):
+	def build_branches(self):
+		self.build_tree(['foo',
+				'foo/bar'])
+		a = Branch.initialize('foo')
+		a.add('bar')
+		a.working_tree().commit('foo', rev_id='foo-1')
+
+	def test_buildpackage(self):
+		self.run_bzr('buildpackage', 'foo', '0.1-1')
