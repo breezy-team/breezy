bc291732acd2b910866dc196f85c6d29bc050476
Jamie Wilkinson <jamie@anchor.net.au>
2005-11-18 18:24:34 +1100

    add a --dry-run option, and a -n short option for the same

--- __init__.py
+++ __init__.py
@@ -4,14 +4,18 @@ import os
 import sys
 
 from bzrlib.commands import Command, register_command
+from bzrlib.option import Option
 from bzrlib.errors import (NoSuchFile, NotBranchError)
 from bzrlib.branch import Branch
 
 class cmd_buildpackage(Command):
 	"""Build the package
 	"""
+	dry_run_opt = Option('dry-run', help="don't do anything")
+	Option.SHORT_OPTIONS['n'] = dry_run_opt
 	takes_args = ['package', 'version?']
-	takes_options = ['verbose']
+	takes_options = ['verbose',
+					 dry_run_opt]
 
 	def run(self, package, version=None, verbose=False):
 		retcode = 0
