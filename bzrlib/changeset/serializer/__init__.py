60ba79dfc04a46dc4a60a3d693449f42548cf08f
John Arbash Meinel <john@arbash-meinel.com>
2005-11-19 19:37:41 -0600

    Created output format, slightly simplified code

--- bzrlib/changeset/serializer/__init__.py
+++ bzrlib/changeset/serializer/__init__.py
@@ -65,10 +65,11 @@ def read(f):
     return serializer.read(f)
 
 
-def write(csets, f, version=None):
+def write(source, revision_ids, f, version=None):
     """Serialize a list of changesets to a filelike object.
 
-    :param csets: The list of changesets to serialize
+    :param source: A source for revision information
+    :param revision_ids: The list of revision ids to serialize
     :param f: The file to output to
     :param version: [optional] target serialization version
     """
@@ -76,11 +77,107 @@ def write(csets, f, version=None):
     if not _serializers.has_key(version):
         raise errors.ChangesetNotSupported(version, 'unknown changeset format')
 
-    serializer = serializer[version](version)
-    return serializer.write(csets, f) 
+    serializer = _serializers[version](version)
+    return serializer.write(source, revision_ids, f) 
 
 
-def ChangesetSerializer(object):
+def format_highres_date(t, offset=0):
+    """Format a date, such that it includes higher precision in the
+    seconds field.
+
+    :param t:   The local time in fractional seconds since the epoch
+    :type t: float
+    :param offset:  The timezone offset in integer seconds
+    :type offset: int
+
+    Example: format_highres_date(time.time(), -time.timezone)
+    this will return a date stamp for right now,
+    formatted for the local timezone.
+
+    >>> from bzrlib.osutils import format_date
+    >>> format_date(1120153132.350850105, 0)
+    'Thu 2005-06-30 17:38:52 +0000'
+    >>> format_highres_date(1120153132.350850105, 0)
+    'Thu 2005-06-30 17:38:52.350850105 +0000'
+    >>> format_date(1120153132.350850105, -5*3600)
+    'Thu 2005-06-30 12:38:52 -0500'
+    >>> format_highres_date(1120153132.350850105, -5*3600)
+    'Thu 2005-06-30 12:38:52.350850105 -0500'
+    >>> format_highres_date(1120153132.350850105, 7200)
+    'Thu 2005-06-30 19:38:52.350850105 +0200'
+    """
+    import time
+    assert isinstance(t, float)
+    
+    # This has to be formatted for "original" date, so that the
+    # revision XML entry will be reproduced faithfully.
+    if offset == None:
+        offset = 0
+    tt = time.gmtime(t + offset)
+
+    return (time.strftime("%a %Y-%m-%d %H:%M:%S", tt)
+            + ('%.9f' % (t - int(t)))[1:] # Get the high-res seconds, but ignore the 0
+            + ' %+03d%02d' % (offset / 3600, (offset / 60) % 60))
+
+
+def unpack_highres_date(date):
+    """This takes the high-resolution date stamp, and
+    converts it back into the tuple (timestamp, timezone)
+    Where timestamp is in real UTC since epoch seconds, and timezone is an integer
+    number of seconds offset.
+
+    :param date: A date formated by format_highres_date
+    :type date: string
+
+    >>> import time, random
+    >>> unpack_highres_date('Thu 2005-06-30 12:38:52.350850105 -0500')
+    (1120153132.3508501, -18000)
+    >>> unpack_highres_date('Thu 2005-06-30 17:38:52.350850105 +0000')
+    (1120153132.3508501, 0)
+    >>> unpack_highres_date('Thu 2005-06-30 19:38:52.350850105 +0200')
+    (1120153132.3508501, 7200)
+    >>> from bzrlib.osutils import local_time_offset
+    >>> t = time.time()
+    >>> o = local_time_offset()
+    >>> t2, o2 = unpack_highres_date(format_highres_date(t, o))
+    >>> t == t2
+    True
+    >>> o == o2
+    True
+    >>> t -= 24*3600*365*2 # Start 2 years ago
+    >>> o = -12*3600
+    >>> for count in xrange(500):
+    ...   t += random.random()*24*3600*30
+    ...   o = ((o/3600 + 13) % 25 - 12)*3600 # Add 1 wrap around from [-12, 12]
+    ...   date = format_highres_date(t, o)
+    ...   t2, o2 = unpack_highres_date(date)
+    ...   if t != t2 or o != o2:
+    ...      print 'Failed on date %r, %s,%s diff:%s' % (date, t, o, t2-t)
+    ...      break
+
+    """
+    import time, calendar
+    # Up until the first period is a datestamp that is generated
+    # as normal from time.strftime, so use time.strptime to
+    # parse it
+    dot_loc = date.find('.')
+    if dot_loc == -1:
+        raise ValueError('Date string does not contain high-precision seconds: %r' % date)
+    base_time = time.strptime(date[:dot_loc], "%a %Y-%m-%d %H:%M:%S")
+    fract_seconds, offset = date[dot_loc:].split()
+    fract_seconds = float(fract_seconds)
+    offset = int(offset)
+    offset = int(offset / 100) * 3600 + offset % 100
+    
+    # time.mktime returns localtime, but calendar.timegm returns UTC time
+    timestamp = calendar.timegm(base_time)
+    timestamp -= offset
+    # Add back in the fractional seconds
+    timestamp += fract_seconds
+    return (timestamp, offset)
+
+
+class ChangesetSerializer(object):
     """The base class for Serializers.
 
     Common functionality should be included here.
@@ -96,12 +193,15 @@ def ChangesetSerializer(object):
         """
         raise NotImplementedError
 
-    def write(self, csets, f):
+    def write(self, source, revision_ids, f):
         """Write the changesets to the supplied files.
 
-        :param csets: A list of changesets to be serialized
-        :param f: The file to write to
+        :param source: A source for revision information
+        :param revision_ids: The list of revision ids to serialize
+        :param f: The file to output to
         """
+        raise NotImplementedError
+
 
 def register(version, klass, overwrite=False):
     """Register a ChangesetSerializer version.
@@ -130,8 +230,9 @@ def register_lazy(version, module, classname, overwrite=False):
         mod = __import__(module, globals(), locals(), [classname])
         klass = getattr(mod, classname)
         return klass(version)
-    register(scheme, _loader, overwrite=overwrite)
+    register(version, _loader, overwrite=overwrite)
 
 
 register_lazy('0.6', 'bzrlib.changeset.serializer.v06', 'ChangesetSerializerV06')
+register_lazy(None, 'bzrlib.changeset.serializer.v06', 'ChangesetSerializerV06')
 
--- bzrlib/changeset/serializer/v06.py
+++ bzrlib/changeset/serializer/v06.py
@@ -17,24 +17,193 @@
 """Serializer factory for reading and writing changesets.
 """
 
+import os
 import bzrlib.errors as errors
-from bzrlib.changeset.serializer import ChangesetSerializer
+from bzrlib.testament import Testament
+from bzrlib.changeset.serializer import (ChangesetSerializer, 
+        CHANGESET_HEADER,
+        format_highres_date, unpack_highres_date)
+from sha import sha
+from bzrlib.diff import internal_diff
+from bzrlib.delta import compare_trees
 
 
-def ChangesetSerializerV06(ChangesetSerializer):
+class ChangesetSerializerV06(ChangesetSerializer):
     def read(self, f):
         """Read the rest of the changesets from the supplied file.
 
         :param f: The file to read from
         :return: A list of changesets
         """
+        assert self.version == '0.6'
+        # The first line of the header should have been read
         raise NotImplementedError
 
-    def write(self, csets, f):
+    def write(self, source, revision_ids, f):
         """Write the changesets to the supplied files.
 
-        :param csets: A list of changesets to be serialized
-        :param f: The file to write to
+        :param source: A source for revision information
+        :param revision_ids: The list of revision ids to serialize
+        :param f: The file to output to
         """
-        raise NotImplementedError
+        self.source = source
+        self.revision_ids = revision_ids
+        self.to_file = f
+        source.lock_read()
+        try:
+            self._write_main_header()
+            self._write_revisions()
+        finally:
+            source.unlock()
+
+    def _write_main_header(self):
+        """Write the header for the changes"""
+        f = self.to_file
+        f.write(CHANGESET_HEADER)
+        f.write('0.6\n')
+        f.write('#\n')
+
+    def _write(self, key, value, indent=1):
+        """Write out meta information, with proper indenting, etc"""
+        assert indent > 0, 'indentation must be greater than 0'
+        f = self.to_file
+        f.write('#' + (' ' * indent))
+        f.write(key.encode('utf-8'))
+        if not value:
+            f.write(':\n')
+        elif '\n' not in value:
+            f.write(': ')
+            f.write(value.encode('utf-8'))
+            f.write('\n')
+        else:
+            f.write(':\n')
+            for line in value.split('\n'):
+                f.write('#' + (' ' * (indent+2)))
+                f.write(line)
+                f.write('\n')
+
+    def _write_revisions(self):
+        """Write the information for all of the revisions."""
+        # The next version of changesets will write a rollup
+        # at the top, and back-patches, or something else after that.
+        # For now, we just write the patches in order.
+
+        # Optimize for the case of revisions in order
+        last_rev_id = None
+        last_rev = None
+        last_rev_tree = None
+
+        for rev_id in self.revision_ids:
+            rev = self.source.get_revision(rev_id)
+            rev_tree = self.source.revision_tree(rev_id)
+
+            # Try to only grab bases which are in the
+            # revision set
+            base_id = None
+            for p_id in rev.parent_ids:
+                if p_id in self.revision_ids:
+                    base_id = p_id
+                    break
+            if base_id is None and rev.parent_ids:
+                base_id = rev.parent_ids[0]
+
+            if base_id == last_rev_id:
+                base_rev = last_rev
+                base_tree = last_rev_tree
+            else:
+                base_rev = self.source.get_revision(base_id)
+                base_tree = self.source.revision_tree(base_id)
+
+            self._write_revision(rev, rev_tree, base_rev, base_tree)
+
+            last_rev_id = rev_id
+            last_rev = rev
+            last_rev_tree = rev_tree
+
+    def _write_revision(self, rev, rev_tree, base_rev, base_tree):
+        """Write out the information for a revision."""
+        def w(key, value):
+            self._write(key, value, indent=1)
+
+        w('revision id', rev.revision_id)
+        w('committer', rev.committer)
+        w('date', format_highres_date(rev.timestamp, rev.timezone))
+        w('message', rev.message)
+        self.to_file.write('\n')
+
+        self._write_delta(rev_tree, base_tree)
+
+        s = sha()
+        t = Testament.from_revision(self.source, rev.revision_id)
+        map(s.update, t.as_text_lines())
+        w('sha1', s.hexdigest())
+        if rev.parent_ids:
+            w('parent ids', '\n'.join(rev.parent_ids))
+        if rev.properties:
+            self._write('properties', None, indent=1)
+            for name, value in rev.properties.items():
+                self._write(name, value, indent=3)
+        
+        # Add an extra blank space at the end
+        self.to_file.write('\n')
+
+    def _write_delta(self, new_tree, old_tree):
+        """Write out the changes between the trees."""
+        DEVNULL = '/dev/null'
+        old_label = ''
+        new_label = ''
+
+        def pjoin(*args):
+            # Only forward slashes in changesets
+            return os.path.join(*args).replace('\\', '/')
+
+        def do_diff(old_path, file_id, new_path, kind):
+            new_entry = new_tree.inventory[file_id]
+            old_tree.inventory[file_id].diff(internal_diff,
+                    pjoin(old_label, old_path), old_tree,
+                    pjoin(new_label, new_path), new_entry, new_tree,
+                    self.to_file)
+        def do_meta(file_id):
+            ie = new_tree.inventory[file_id]
+            w(' // executable:')
+            if ie.executable:
+                w('yes')
+            else:
+                w('no')
+
+
+        delta = compare_trees(old_tree, new_tree, want_unchanged=False)
+
+        w = self.to_file.write
+
+        for path, file_id, kind in delta.removed:
+            w('=== removed %s %s\n' % (kind, path))
+
+        for path, file_id, kind in delta.added:
+            w('=== added %s %s // file-id:%s\n' % (kind, path, file_id))
+            new_tree.inventory[file_id].diff(internal_diff,
+                    pjoin(new_label, path), new_tree,
+                    DEVNULL, None, None,
+                    self.to_file, reverse=True)
+
+        for (old_path, new_path, file_id, kind,
+             text_modified, meta_modified) in delta.renamed:
+            w('=== renamed %s %s // %s' % (kind, old_path, new_path))
+            if meta_modified:
+                do_meta(file_id)
+            w('\n')
+            if text_modified:
+                do_diff(old_path, file_id, new_path, text_modified)
+
+        for (path, file_id, kind,
+             text_modified, meta_modified) in delta.modified:
+            # TODO: Handle meta_modified
+            #prop_str = get_prop_change(meta_modified)
+            w('=== modified %s %s' % (kind, path))
+            if meta_modified:
+                do_meta(file_id)
+            w('\n')
+            if text_modified:
+                do_diff(path, file_id, path, kind)
+
 
