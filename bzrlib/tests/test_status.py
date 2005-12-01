03f45ecabfe562931dd05b1e2a7250fb5cbf1260
Martin Pool <mbp@sourcefrog.net>
2005-12-01 16:09:27 +1100

    Status tests include unicode character.
    Make it work by going to an encoded temporary file, not a StringIO.

--- bzrlib/tests/test_status.py
+++ bzrlib/tests/test_status.py
@@ -21,13 +21,16 @@ Most of these depend on the particular formatting used.
 """
 
 
+from cStringIO import StringIO
+from os import mkdir
+from tempfile import TemporaryFile
+import codecs
+
 from bzrlib.tests import TestCaseInTempDir
 from bzrlib.revisionspec import RevisionSpec
 from bzrlib.merge import merge
-from cStringIO import StringIO
 from bzrlib.status import show_status
 from bzrlib.branch import Branch
-from os import mkdir
 from bzrlib.clone import copy_branch
 
 class BranchStatus(TestCaseInTempDir):
@@ -97,26 +100,28 @@ class BranchStatus(TestCaseInTempDir):
                            '  hello.c\n'])
 
     def status_string(self, branch):
-        tof = StringIO()
+        # use a real file rather than StringIO because it doesn't handle
+        # Unicode very well.
+        tof = codecs.getwriter('utf-8')(TemporaryFile())
         show_status(branch, to_file=tof)
         tof.seek(0)
-        return tof.getvalue()
+        return tof.read().decode('utf-8')
 
     def test_pending(self):
-        """Pending merges display works"""
+        """Pending merges display works, including Unicode"""
         mkdir("./branch")
         b = Branch.initialize('./branch')
         b.working_tree().commit("Empty commit 1")
         b_2 = copy_branch(b, './copy')
-        b.working_tree().commit("Empty commit 2")
+        b.working_tree().commit(u"\N{TIBETAN DIGIT TWO} Empty commit 2")
         merge(["./branch", -1], [None, None], this_dir = './copy')
         message = self.status_string(b_2)
         self.assert_(message.startswith("pending merges:\n"))
         self.assert_(message.endswith("Empty commit 2\n")) 
         b_2.working_tree().commit("merged")
         # must be long to make sure we see elipsis at the end
-        b.working_tree().commit("Empty commit 3 blah blah blah blah blah blah blah blah blah"
-                 " blah blah blah blah blah blah bleh")
+        b.working_tree().commit("Empty commit 3 " + 
+                                "blah blah blah blah " * 10)
         merge(["./branch", -1], [None, None], this_dir = './copy')
         message = self.status_string(b_2)
         self.assert_(message.startswith("pending merges:\n"))
