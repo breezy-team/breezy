09ee9a3c703f0919b344cd1d602c07daa05f2d20
Hans Ulrich Niedermann <hun@n-dimensional.de>
2005-12-10 18:10:04 +0100

    Start implementing general doc generation plan:   * describe the plan   * steal some code from bzrlib.help to extract command help   * use subsections for each bzr command   * add COMMAND OVERVIEW section   * add USAGE section with a few common use cases   * remove copyright marks as I am going to sign over copyright to Canonical

--- bzr_man.py
+++ bzr_man.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
-# Marked portions Copyright (C) 2005 by Hans Ulrich Niedermann
-# Unmarked portions (C) 2005 Canonical Ltd.
+# Copyright 2005 Canonical Ltd.
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,9 +16,30 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+"""bzr_man.py - create man page from built-in bzr help and static text
+
+Plan (devised by jblack and ndim 2005-12-10):
+  * one bzr_gen_stuff.py script in top level dir right beside bzr
+  * one gen_stuff_extras/ directory
+  * several generator scripts like
+          gen_stuff_extras/gen_man_page.py
+                           gen_docbook_xml.py
+			   gen_html.py
+                           gen_bash_completion.py
+			   gen_zsh_completion.py
+  * scripts are called by running "bzr_gen_stuff.py --man-page" or
+    "--bash-completion"
+  * one test case which iterates through all gen_*.py scripts and
+    tries to generate all the file types, checking that all generators
+    work
+  * those generator scripts walk through the command and option data
+    structures to extract the required information
+  * the actual names are just prototypes and subject to change
+"""
+
 import os, sys
 import bzrlib, bzrlib.help, bzrlib.commands
-#<<< Begin (C) Hans Ulrich Niedermann
+import textwrap
 import time
 import re
 
@@ -43,10 +63,12 @@ def main():
 
     outfile.write(man_preamble % params)
     outfile.write(man_escape(man_head % params))
-#<<< End (C) Hans Ulrich Niedermann
-    outfile.write(man_escape(getcommand_help()))
-#<<< Begin (C) Hans Ulrich Niedermann
+
+    outfile.write(man_escape(getcommand_list(params)))
+    outfile.write(man_escape(getcommand_help(params)))
+
     outfile.write(man_escape(man_foot % params))
+
 
 def man_escape(string):
     result = string.replace("\\","\\\\")
@@ -54,20 +76,63 @@ def man_escape(string):
     result = result.replace("'","\\'")
     result = result.replace("-","\\-")
     return result
-#<<< End (C) Hans Ulrich Niedermann
 
-def format_command (cmd):
-    cmdusage = '.TP\n.B %s\n.PP\n' % (bzrlib.help.command_usage(cmd))
-    doc = "%s\n.PP\n.PP\n" % (cmd.__doc__)
-    return cmdusage + doc
 
-def getcommand_help():
-    output=""
+def getcommand_list (params):
+    bzrcmd = params["bzrcmd"]
+    output = '.SH "COMMAND OVERVIEW"\n'
     command_names = bzrlib.commands.builtin_command_names()
     command_names.sort()
-    for cmd_name in  command_names:
-        desc = bzrlib.commands.get_cmd_object(cmd_name)
-        output = output + format_command(desc)
+    for cmd_name in command_names:
+        cmd_object = bzrlib.commands.get_cmd_object(cmd_name)
+        if cmd_object.hidden:
+            continue
+        cmd_help = cmd_object.help()
+        if cmd_help:
+            firstline = cmd_help.split('\n', 1)[0]
+            tmp = '.TP\n.B "%s %s"\n%s\n' % (bzrcmd, cmd_name,
+                                                        firstline)
+        else:
+            tmp = '.TP\n.B "%s %s"\n%s\n' % (bzrcmd, cmd_name, "foo")            
+        output = output + tmp
+    return output
+
+
+def format_command (params, cmd):
+    subsection_header = '.SS "%s %s"\n' % (params["bzrcmd"], cmd.name())
+    cmdusage = '.TP\n.B "Usage: %s"\n.PP\n' % (bzrlib.help.command_usage(cmd))
+    doc = "%s\n" % (cmd.__doc__)
+    option_str = ""
+    options = cmd.options()
+    # option walk code stolen from bzrlib/help.py
+    if options:
+        option_str = "\nOptions:\n"
+        for option_name, option in sorted(options.items()):
+            l = '    --' + option_name
+            if option.type is not None:
+                l += ' ' + option.argname.upper()
+            short_name = option.short_name()
+            if short_name:
+                assert len(short_name) == 1
+                l += ', -' + short_name
+            l += (30 - len(l)) * ' ' + option.help
+            # TODO: split help over multiple lines with correct indenting and 
+            # wrapping
+            wrapped = textwrap.fill(l, initial_indent='',
+                                    subsequent_indent=30*' ')
+            option_str = option_str + wrapped + '\n'       
+    return subsection_header + cmdusage + doc + option_str
+
+
+def getcommand_help(params):
+    output='.SH "COMMAND REFERENCE"\n'
+    command_names = bzrlib.commands.builtin_command_names()
+    command_names.sort()
+    for cmd_name in command_names:
+        cmd_object = bzrlib.commands.get_cmd_object(cmd_name)
+        if cmd_object.hidden:
+            continue
+        output = output + format_command(params, cmd_object)
     return output
 
 
@@ -94,6 +159,55 @@ bazaar-ng (or
 ) is a project of Canonical Ltd. to develop an open source distributed version control system that is powerful, friendly, and scalable. Version control means a system that keeps track of previous revisions of software source code or similar information and helps people work on it in teams.
 .SS "Warning"
 bazaar-ng is at an early stage of development, and the design is still changing from week to week. This man page here may be inconsistent with itself, with other documentation or with the code, and sometimes refer to features that are planned but not yet written. Comments are still very welcome; please send them to bazaar-ng@lists.canonical.com.
+
+.SH "USAGE"
+Commands for a number of common use cases.
+
+.SS "Create a branch"
+    $ cd my-project
+    $ %(bzrcmd)s init
+
+.SS "Add files to be versioned"
+    $ cd my-project
+    $ %(bzrcmd)s add hello-world.c Makefile
+
+.SS "Review local changes"
+    $ cd my-project
+    $ %(bzrcmd)s status
+    $ %(bzrcmd)s diff
+
+.SS "Commit changes to branch"
+    $ cd my-project
+    $ %(bzrcmd)s commit -m \"initial import of files\"
+
+.SS "Branching off an existing branch"
+    $ %(bzrcmd)s branch http://other.com/project-foo
+    $ cd project-foo
+
+    $ %(bzrcmd)s branch http://other.com/project-foo foo-by-other
+    $ cd foo-by-other
+    
+    $ %(bzrcmd)s branch foo-by-other foo-with-my-changes
+    $ cd foo-with-my-changes
+
+.SS "Following upstream changes"
+If you have not modified anything in your local branch:
+    $ cd foo-by-other
+    $ %(bzrcmd)s pull
+
+If you have modified and committed things in your local branch:
+    $ cd foo-with-my-changes
+    $ %(bzrcmd)s merge
+
+.SS "Publishing your changes"
+Publishing your branch for the first time:
+    $ cd foo-with-my-changes
+    $ %(bzrcmd)s push sftp://user@host/public_html/foo-with-my-changes
+
+Publishing your branch subsequently is easier as %(bzrcmd)s remembers the push location:
+    $ cd foo-with-my-changes
+    $ %(bzrcmd)s push
+    
 """
 
 man_foot = """\
@@ -150,7 +264,6 @@ environment variable. Example content:
 .BR http://bazaar.canonical.com/BzrDocumentation
 """
 
-#<<< Begin (C) Hans Ulrich Niedermann
 man_preamble = """\
 .\\\" Man page for %(bzrcmd)s (bazaar-ng)
 .\\\"
@@ -164,4 +277,3 @@ man_preamble = """\
 
 if __name__ == '__main__':
     main()
-#<<< End (C) Hans Ulrich Niedermann
