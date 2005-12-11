cf64809c51144b5565c34b9f5cac1bc28d3d9df2
Hans Ulrich Niedermann <hun@n-dimensional.de>
2005-12-11 23:46:59 +0100

    Implement the bzr documentation/information autogeneration plan:   * Add bzr-infogen.py script.   * Moved main() from bzrinfogen/__init__py to bzr-infogen.py where it belongs Still missing:   * test case for autogeneration   * actual implementation of bash completion script

---
+++ bzr-infogen.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+
+# Copyright 2005 Canonical Ltd.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""bzr-infogen.py - generate information from built-in bzr help
+
+Plan (devised by jblack and ndim 2005-12-10):
+  * one bzr-infogengen.py script in top level dir right beside bzr
+  * one bzrinfogen/ directory
+  * several generator scripts like
+          bzrinfogen/gen_man_page.py
+                     gen_docbook_xml.py
+	             gen_html.py
+                     gen_bash_completion.py
+                     gen_zsh_completion.py
+  * scripts are called by running "bzr-infogen.py --man-page" or
+    "bzr-infogen.py --bash-completion"
+  * one test case which iterates through all gen_*.py scripts and
+    tries to generate all the file types, checking that all generators
+    work
+  * those generator scripts walk through the command and option data
+    structures to extract the required information
+  * the actual names are just prototypes and subject to change
+
+TODO (for man page):
+  * add command aliases
+"""
+
+import sys
+import bzrinfogen
+
+
+def main(argv):
+    from optparse import OptionParser
+    parser = OptionParser(usage="%prog [options] OUTPUT_FORMAT")
+    parser.add_option("-s", "--show-filename",
+                      action="store_true", dest="show_filename", default=False,
+                      help="print default filename on stdout")
+    parser.add_option("-f", "--file", dest="filename",
+                      help="write report to FILE", metavar="FILE")
+    parser.add_option("-b", "--bzr-name", dest="bzr_name", default="bzr",
+                      help="name of bzr executable", metavar="EXEC_NAME")
+    parser.add_option("-q", "--quiet",
+                      action="store_false", dest="verbose", default=True,
+                      help="don't print status messages to stdout")
+    (options, args) = parser.parse_args(argv)
+
+    if len(args) != 2:
+        parser.error("incorrect number of arguments")
+
+    infogen_type = args[1]
+    infogen_mod = bzrinfogen.get_infogen_mod(infogen_type)
+
+    if options.filename:
+        outfilename = options.filename
+    else:
+        outfilename = infogen_mod.get_filename(options)
+
+    if outfilename == "-":
+        outfile = sys.stdout
+    else:
+        outfile = open(outfilename,"w")
+
+    if options.show_filename and (outfilename != "-"):
+        print >>sys.stdout, outfilename
+    
+    infogen_mod.infogen(options, outfile)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
