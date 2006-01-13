8191d1cac4a9df0613afcb862dffd972e83254ce
Aaron Bentley <abentley@panoramicfeedback.com>
2006-01-13 13:29:47 -0500

    Ensure transform can be applied after resolution

--- bzrlib/tests/test_transform.py
+++ bzrlib/tests/test_transform.py
@@ -292,6 +292,7 @@ class TestTreeTransform(TestCaseInTempDir):
         self.assertIs(conflicts.final_file_id(old_dorothy), None)
         self.assertEqual(conflicts.final_name(new_dorothy), 'dorothy')
         self.assertIs(conflicts.final_file_id(new_dorothy), 'dorothy-id')
+        conflicts.apply()
 
     def test_moving_versioned_directories(self):
         create, root = self.get_transform()
