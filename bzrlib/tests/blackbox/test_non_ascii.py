bb5cd1b56d603eb7b876924ad7504748aeda0d77
John Arbash Meinel <john@arbash-meinel.com>
2006-01-12 22:22:06 -0600

    New encoder with multiple strings.

--- bzrlib/tests/blackbox/test_non_ascii.py
+++ bzrlib/tests/blackbox/test_non_ascii.py
@@ -25,43 +25,6 @@ import bzrlib
 from bzrlib.tests import TestCaseInTempDir, TestSkipped
 from bzrlib.trace import mutter, note
 
-_mu = u'\xb5'
-# Swedish?
-_erik = u'Erik B\xe5gfors'
-# Swedish 'räksmörgås' means shrimp sandwich
-_shrimp_sandwich = u'r\xe4ksm\xf6rg\xe5s'
-# TODO: jam 20060105 Is there a way we can decode punycode for people
-#       who have non-ascii email addresses? Does it matter to us, we
-#       really would have no problem just using utf-8 internally, since
-#       we don't actually ever send email to these addresses.
-_punycode_erik = 'Bgfors-iua'
-# Arabic, probably only Unicode encodings can handle this one
-_juju = u'\u062c\u0648\u062c\u0648'
-# Alternative for arabic
-_juju_alt = u'j\xfbj\xfa'
-# Russian
-_alexander = u'\u0410\u043b\u0435\u043a\u0441\u0430\u043d\u0434\u0440'
-# Kanji
-_nihonjin = u'\u65e5\u672c\u4eba'
-# It is a kanji sequence for nihonjin, or Japanese in English.
-# 
-# '\u4eba' being person, 'u\65e5' sun and '\u672c' origin. Ie,
-# sun-origin-person, 'native from the land where the sun rises'. Note, I'm
-# not a fluent speaker, so this is just my crude breakdown.
-# 
-# Wouter van Heyst
-
-
-class EncodingTestAdapter(object):
-    """A tool to generate a suite, testing multiple encodings for a single test.
-    
-    This is similar to bzrlib.transport.TransportTestProviderAdapter.
-    It is done by copying the test once for each encoding, and injecting
-    the encoding name, and the list of valid strings for that encoding.
-    Each copy is also given a new id() to make it easy to identify.
-    """
-
-
 class TestNonAscii(TestCaseInTempDir):
     """Test that bzr handles files/committers/etc which are non-ascii."""
 
