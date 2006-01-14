f3d2f142d1ba7d72e44eef497d160a35daaf22e8
abentley <abentley@lappy>
2006-01-13 19:03:30 -0500

    Fix bug when no paramiko

--- bzrlib/transport/__init__.py
+++ bzrlib/transport/__init__.py
@@ -530,9 +530,12 @@ class TransportTestProviderAdapter(object):
         """Return a list of the klass, server_factory pairs to test."""
         result = []
         for module in _get_transport_modules():
-            result.extend(self.get_transport_test_permutations(reduce(getattr, 
-                (module).split('.')[1:],
+            try:
+                result.extend(self.get_transport_test_permutations(reduce(getattr, 
+                    (module).split('.')[1:],
                  __import__(module))))
+            except ImportError:
+                pass
         return result
         
 
