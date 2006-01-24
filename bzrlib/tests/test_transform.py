89675a6142f705126944bc538dcd4c940c50ea30
Aaron Bentley <abentley@panoramicfeedback.com>
2006-01-24 15:42:15 -0500

    Cleaned up imports, added copyright statements

--- bzrlib/tests/test_transform.py
+++ bzrlib/tests/test_transform.py
@@ -1,11 +1,27 @@
-from bzrlib.tests import TestCaseInTempDir, TestSkipped
+# Copyright (C) 2006 Canonical Ltd
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import os
 from bzrlib.branch import Branch
-from bzrlib.transform import (TreeTransform, ROOT_PARENT, FinalPaths, 
-                              resolve_conflicts, Merge3Merger)
 from bzrlib.errors import (DuplicateKey, MalformedTransform, NoSuchFile,
                            ReusingTransform, CantMoveRoot)
 from bzrlib.osutils import file_kind, has_symlinks
-import os
+from bzrlib.tests import TestCaseInTempDir, TestSkipped
+from bzrlib.transform import (TreeTransform, ROOT_PARENT, FinalPaths, 
+                              resolve_conflicts, Merge3Merger)
 
 class TestTreeTransform(TestCaseInTempDir):
     def setUp(self):
--- bzrlib/transform.py
+++ bzrlib/transform.py
@@ -1,12 +1,28 @@
+# Copyright (C) 2006 Canonical Ltd
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 import os
+import errno
+
+from bzrlib import BZRDIR
 from bzrlib.errors import (DuplicateKey, MalformedTransform, NoSuchFile,
                            ReusingTransform, NotVersionedError, CantMoveRoot)
-from bzrlib.osutils import file_kind, supports_executable
 from bzrlib.inventory import InventoryEntry
-from bzrlib import BZRDIR
-from bzrlib.delta import compare_trees
-import errno
-from merge3 import Merge3
+from bzrlib.osutils import file_kind, supports_executable
+from bzrlib.merge3 import Merge3
 
 ROOT_PARENT = "root-parent"
 
