9abd95c6895ab70083ca7344dba46580941b64a1
Martin Pool <mbp@sourcefrog.net>
2006-03-07 18:19:40 +1100

    Style review fixes (thanks robertc)

--- bzrlib/transport/http/_pycurl.py
+++ bzrlib/transport/http/_pycurl.py
@@ -1,15 +1,15 @@
 # Copyright (C) 2006 Canonical Ltd
-
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -21,10 +21,10 @@
 from StringIO import StringIO
 
 import bzrlib
-from bzrlib.trace import mutter
 from bzrlib.errors import (TransportNotPossible, NoSuchFile,
                            TransportError, ConnectionError,
                            DependencyNotPresent)
+from bzrlib.trace import mutter
 from bzrlib.transport import Transport
 from bzrlib.transport.http import HttpTransportBase, extract_auth, HttpServer
 
