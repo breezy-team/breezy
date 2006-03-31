0c38b17fc0ce7e73c4b757d0a2e8c55919d59047
Robert Collins <robertc@robertcollins.net>
2006-03-31 11:47:11 +1100

    More knit micro-optimisations.

--- bzrlib/knit.py
+++ bzrlib/knit.py
@@ -69,7 +69,7 @@ import difflib
 import gzip
 from itertools import izip, chain
 import os
-
+import sys
 
 import bzrlib
 import bzrlib.errors as errors
@@ -161,6 +161,10 @@ class KnitAnnotateFactory(_KnitFactory):
         return KnitContent(lines)
 
     def parse_line_delta_iter(self, lines):
+        for result_item in self.parse_line_delta[lines]:
+            yield result_item
+
+    def parse_line_delta(self, lines, version):
         """Convert a line based delta into internal representation.
 
         line delta is in the form of:
@@ -170,17 +174,20 @@ class KnitAnnotateFactory(_KnitFactory):
         internal represnetation is
         (start, end, count, [1..count tuples (revid, newline)])
         """
-        while lines:
-            header = lines.pop(0)
-            start, end, c = [int(n) for n in header.split(',')]
+        result = []
+        lines = iter(lines)
+        next = lines.next
+        # walk through the lines parsing.
+        for header in lines:
+            start, end, count = [int(n) for n in header.split(',')]
             contents = []
-            for i in range(c):
-                origin, text = lines.pop(0).split(' ', 1)
+            remaining = count
+            while remaining:
+                origin, text = next().split(' ', 1)
+                remaining -= 1
                 contents.append((origin.decode('utf-8'), text))
-            yield start, end, c, contents
-
-    def parse_line_delta(self, lines, version):
-        return list(self.parse_line_delta_iter(lines))
+            result.append((start, end, count, contents))
+        return result
 
     def lower_fulltext(self, content):
         """convert a fulltext content record into a serializable form.
@@ -192,7 +199,7 @@ class KnitAnnotateFactory(_KnitFactory):
     def lower_line_delta(self, delta):
         """convert a delta into a serializable form.
 
-        See parse_line_delta_iter which this inverts.
+        See parse_line_delta which this inverts.
         """
         out = []
         for start, end, c, lines in delta:
@@ -487,6 +494,12 @@ class KnitVersionedFile(VersionedFile):
         The basis knit will be used to the largest extent possible
         since it is assumed that accesses to it is faster.
         """
+        #profile notes:
+        # 4168 calls in 14912, 2289 internal
+        # 4168 in 9711 to read_records
+        # 52554 in 1250 to get_parents
+        # 170166 in 865 to list.append
+        
         # needed_revisions holds a list of (method, version_id) of
         # versions that is needed to be fetched to construct the final
         # version of the file.
@@ -749,13 +762,20 @@ class KnitVersionedFile(VersionedFile):
 
     def get_parents(self, version_id):
         """See VersionedFile.get_parents."""
-        self._check_versions_present([version_id])
-        return list(self._index.get_parents(version_id))
+        # perf notes:
+        # optimism counts!
+        # 52554 calls in 1264 872 internal down from 3674
+        try:
+            return self._index.get_parents(version_id)
+        except KeyError:
+            raise RevisionNotPresent(version_id, self.filename)
 
     def get_parents_with_ghosts(self, version_id):
         """See VersionedFile.get_parents."""
-        self._check_versions_present([version_id])
-        return list(self._index.get_parents_with_ghosts(version_id))
+        try:
+            return self._index.get_parents_with_ghosts(version_id)
+        except KeyError:
+            raise RevisionNotPresent(version_id, self.filename)
 
     def get_ancestry(self, versions):
         """See VersionedFile.get_ancestry."""
@@ -1126,7 +1146,7 @@ class _KnitData(_KnitComponentFile):
                                      len(lines),
                                      digest)],
             lines,
-            ["end %s\n\n" % version_id.encode('utf-8')]))
+            ["end %s\n" % version_id.encode('utf-8')]))
         data_file.close()
         length= sio.tell()
 
@@ -1166,22 +1186,19 @@ class _KnitData(_KnitComponentFile):
         return df, rec
 
     def _parse_record(self, version_id, data):
+        # profiling notes:
+        # 4168 calls in 2880 217 internal
+        # 4168 calls to _parse_record_header in 2121
+        # 4168 calls to readlines in 330
         df, rec = self._parse_record_header(version_id, data)
-        lines = int(rec[2])
-        record_contents = self._read_record_contents(df, lines)
-        l = df.readline()
+        record_contents = df.readlines()
+        l = record_contents.pop()
+        assert len(record_contents) == int(rec[2])
         if l.decode('utf-8') != 'end %s\n' % version_id:
             raise KnitCorrupt(self._filename, 'unexpected version end line %r, wanted %r' 
                         % (l, version_id))
         df.close()
         return record_contents, rec[3]
-
-    def _read_record_contents(self, df, record_lines):
-        """Read and return n lines from datafile."""
-        r = []
-        for i in range(record_lines):
-            r.append(df.readline())
-        return r
 
     def read_records_iter_raw(self, records):
         """Read text records from data file and yield raw data.
@@ -1226,6 +1243,10 @@ class _KnitData(_KnitComponentFile):
         will be read in the given order.  Yields (version_id,
         contents, digest).
         """
+        # profiling notes:
+        # 60890  calls for 4168 extractions in 5045, 683 internal.
+        # 4168   calls to readv              in 1411
+        # 4168   calls to parse_record       in 2880
 
         needed_records = []
         for version_id, pos, size in records:
@@ -1243,7 +1264,7 @@ class _KnitData(_KnitComponentFile):
                 self._records[record_id] = (digest, content)
     
         for version_id, pos, size in records:
-            yield version_id, copy(self._records[version_id][1]), copy(self._records[version_id][0])
+            yield version_id, list(self._records[version_id][1]), self._records[version_id][0]
 
     def read_records(self, records):
         """Read records into a dictionary."""
@@ -1378,7 +1399,9 @@ InterVersionedFile.register_optimiser(InterKnit)
 
 
 # make GzipFile faster:
+import struct
 import zlib
+from gzip import U32, LOWU32, FEXTRA, FCOMMENT, FNAME, FHCRC
 class GzipFile(gzip.GzipFile):
     """Knit tuned version of GzipFile.
 
@@ -1401,6 +1424,220 @@ class GzipFile(gzip.GzipFile):
 
     Yes, its only 1.6 seconds, but they add up.
     """
+
+    def _add_read_data(self, data):
+        # 4169 calls in 183
+        # temp var for len(data) and switch to +='s.
+        # 4169 in 139
+        len_data = len(data)
+        self.crc = zlib.crc32(data, self.crc)
+        self.extrabuf += data
+        self.extrasize += len_data
+        self.size += len_data
+
+    def _read(self, size=1024):
+        # various optimisations:
+        # reduces lsprof count from 2500 to 
+        # 8337 calls in 1272, 365 internal
+        """
+        if self.fileobj is None:
+            raise EOFError, "Reached EOF"
+
+        if self._new_member:
+            # If the _new_member flag is set, we have to
+            # jump to the next member, if there is one.
+            #
+            # First, check if we're at the end of the file;
+            # if so, it's time to stop; no more members to read.
+            next_header_bytes = self.fileobj.read(10)
+            if next_header_bytes == '':
+                raise EOFError, "Reached EOF"
+
+            self._init_read()
+            self._read_gzip_header(next_header_bytes)
+            self.decompress = zlib.decompressobj(-zlib.MAX_WBITS)
+            self._new_member = False
+
+        # Read a chunk of data from the file
+        buf = self.fileobj.read(size)
+
+        # If the EOF has been reached, flush the decompression object
+        # and mark this object as finished.
+
+        if buf == "":
+            self._add_read_data(self.decompress.flush())
+            assert len(self.decompress.unused_data) >= 8, "what does flush do?"
+            self._read_eof()
+            # tell the driving read() call we have stuffed all the data
+            # in self.extrabuf
+            raise EOFError, 'Reached EOF'
+
+        self._add_read_data(self.decompress.decompress(buf))
+
+        if self.decompress.unused_data != "":
+            # Ending case: we've come to the end of a member in the file,
+            # so seek back to the start of the data for the next member which
+            # is the length of the decompress objects unused data - the first
+            # 8 bytes for the end crc and size records.
+            #
+            # so seek back to the start of the unused data, finish up
+            # this member, and read a new gzip header.
+            # (The number of bytes to seek back is the length of the unused
+            # data, minus 8 because those 8 bytes are part of this member.
+            seek_length = len (self.decompress.unused_data) - 8
+            if seek_length:
+                assert seek_length > 0
+                self.fileobj.seek(-seek_length, 1)
+
+            # Check the CRC and file size, and set the flag so we read
+            # a new member on the next call
+            self._read_eof()
+            self._new_member = True
+
+    def _read_eof(self):
+        """tuned to reduce function calls and eliminate file seeking:
+        pass 1:
+        reduces lsprof count from 800 to 288
+        4168 in 296 
+        avoid U32 call by using struct format L
+        4168 in 200
+        """
+        # We've read to the end of the file, so we should have 8 bytes of 
+        # unused data in the decompressor. If we dont, there is a corrupt file.
+        # We use these 8 bytes to calculate the CRC and the recorded file size.
+        # We then check the that the computed CRC and size of the
+        # uncompressed data matches the stored values.  Note that the size
+        # stored is the true file size mod 2**32.
+        crc32, isize = struct.unpack("<LL", self.decompress.unused_data[0:8])
+        # note that isize is unsigned - it can exceed 2GB
+        if crc32 != U32(self.crc):
+            raise IOError, "CRC check failed"
+        elif isize != LOWU32(self.size):
+            raise IOError, "Incorrect length of data produced"
+
+    def _read_gzip_header(self, bytes=None):
+        """Supply bytes if the minimum header size is already read.
+        
+        :param bytes: 10 bytes of header data.
+        """
+        """starting cost: 300 in 3998
+        15998 reads from 3998 calls
+        final cost 168
+        """
+        if bytes is None:
+            bytes = self.fileobj.read(10)
+        magic = bytes[0:2]
+        if magic != '\037\213':
+            raise IOError, 'Not a gzipped file'
+        method = ord(bytes[2:3])
+        if method != 8:
+            raise IOError, 'Unknown compression method'
+        flag = ord(bytes[3:4])
+        # modtime = self.fileobj.read(4) (bytes [4:8])
+        # extraflag = self.fileobj.read(1) (bytes[8:9])
+        # os = self.fileobj.read(1) (bytes[9:10])
+        # self.fileobj.read(6)
+
+        if flag & FEXTRA:
+            # Read & discard the extra field, if present
+            xlen = ord(self.fileobj.read(1))
+            xlen = xlen + 256*ord(self.fileobj.read(1))
+            self.fileobj.read(xlen)
+        if flag & FNAME:
+            # Read and discard a null-terminated string containing the filename
+            while True:
+                s = self.fileobj.read(1)
+                if not s or s=='\000':
+                    break
+        if flag & FCOMMENT:
+            # Read and discard a null-terminated string containing a comment
+            while True:
+                s = self.fileobj.read(1)
+                if not s or s=='\000':
+                    break
+        if flag & FHCRC:
+            self.fileobj.read(2)     # Read & discard the 16-bit header CRC
+
+    def readline(self, size=-1):
+        """Tuned to remove buffer length calls in _unread and...
+        
+        also removes multiple len(c) calls, inlines _unread,
+        total savings - lsprof 5800 to 5300
+        phase 2:
+        4168 calls in 2233
+        8176 calls to read() in 1684
+        changing the min chunk size to 200 halved all the cache misses
+        leading to a drop to:
+        4168 calls in 1977
+        4168 call to read() in 1646
+        - i.e. just reduced the function call overhead. May be worth 
+          keeping.
+        """
+        if size < 0: size = sys.maxint
+        bufs = []
+        readsize = min(200, size)    # Read from the file in small chunks
+        while True:
+            if size == 0:
+                return "".join(bufs) # Return resulting line
+
+            # c is the chunk
+            c = self.read(readsize)
+            # number of bytes read
+            len_c = len(c)
+            i = c.find('\n')
+            if size is not None:
+                # We set i=size to break out of the loop under two
+                # conditions: 1) there's no newline, and the chunk is
+                # larger than size, or 2) there is a newline, but the
+                # resulting line would be longer than 'size'.
+                if i==-1 and len_c > size: i=size-1
+                elif size <= i: i = size -1
+
+            if i >= 0 or c == '':
+                # if i>= 0 we have a newline or have triggered the above
+                # if size is not None condition.
+                # if c == '' its EOF.
+                bufs.append(c[:i+1])    # Add portion of last chunk
+                # -- inlined self._unread --
+                ## self._unread(c[i+1:], len_c - i)   # Push back rest of chunk
+                self.extrabuf = c[i+1:] + self.extrabuf
+                self.extrasize = len_c - i + self.extrasize
+                self.offset -= len_c - i
+                # -- end inlined self._unread --
+                return ''.join(bufs)    # Return resulting line
+
+            # Append chunk to list, decrease 'size',
+            bufs.append(c)
+            size = size - len_c
+            readsize = min(size, readsize * 2)
+
+    def readlines(self, sizehint=0):
+        # optimise to avoid all the buffer manipulation
+        # lsprof changed from:
+        # 4168 calls in 5472 with 32000 calls to readline()
+        # to :
+        # 4168 calls in 417.
+        # Negative numbers result in reading all the lines
+        if sizehint <= 0:
+            sizehint = -1
+        content = self.read(sizehint)
+        return content.splitlines(True)
+
+    def _unread(self, buf, len_buf=None):
+        """tuned to remove unneeded len calls.
+        
+        because this is such an inner routine in readline, and readline is
+        in many inner loops, this has been inlined into readline().
+
+        The len_buf parameter combined with the reduction in len calls dropped
+        the lsprof ms count for this routine on my test data from 800 to 200 - 
+        a 75% saving.
+        """
+        if len_buf is None:
+            len_buf = len(buf)
+        self.extrabuf = buf + self.extrabuf
+        self.extrasize = len_buf + self.extrasize
+        self.offset -= len_buf
 
     def write(self, data):
         if self.mode != gzip.WRITE:
