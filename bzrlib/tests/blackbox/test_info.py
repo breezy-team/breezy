91d249b9d4dcce08b7b25bb1ba25de9ef192e41c
Olaf Conradi <olaf@conradi.org>
2006-04-30 22:42:40 +0200

    Fixed test case with bzr update inside test_info, it now uses the API.

--- bzrlib/tests/blackbox/test_info.py
+++ bzrlib/tests/blackbox/test_info.py
@@ -129,13 +129,8 @@ Revision store:
 
         # Branch and bind to standalone, needs upgrade to metadir
         # (creates backup as unknown)
-        #
-        # XXX: I can't get this to work through API (Olaf)
-        # https://launchpad.net/products/bzr/+bug/38089
-        self.runbzr('branch standalone bound')
-        #branch3 = branch1.bzrdir.sprout('bound').open_branch()
-        self.runbzr('upgrade --format=metadir bound')
-        #bzrlib.upgrade.upgrade('bound', 'metadir')
+        branch3 = branch1.bzrdir.sprout('bound').open_branch()
+        bzrlib.upgrade.upgrade('bound', bzrlib.bzrdir.BzrDirMetaFormat1())
         branch3 = bzrlib.bzrdir.BzrDir.open('bound').open_branch()
         branch3.bind(branch1)
         out, err = self.runbzr('info bound')
