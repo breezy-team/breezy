0d7fd814191afd00ebfd6c4c0224891758575964
Jelmer Vernooij <jelmer@samba.org>
2006-05-02 13:52:53 +0200

    Rename executable to simple 'svn2bzr', add setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+from distutils.command.install_scripts import install_scripts
+from distutils.command.build import build
+
+setup(name='svn2bzr',
+      version='0.6',
+      author='Gustavo Niemeyer',
+      author_email='gustavo@niemeyer.net',
+      url='http://www.bazaar-vcs.org/svn2bzr',
+      description='Conversion tool for Subversion branches',
+      license='GNU GPL v2',
+      scripts=['svn2bzr'],
+      data_files=[('man/man1', ['svn2bzr.1'])],
+     )
