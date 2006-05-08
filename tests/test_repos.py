1c744ea3dfc18175792f15350e8a117e61432deb
Jelmer Vernooij <jelmer@samba.org>
2006-05-08 17:59:48 +0200

    Add copyright headers, add test for branch format description

--- tests/test_repos.py
+++ tests/test_repos.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2006 Jelmer Vernooij <jelmer@samba.org>
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 import svn
 import format
 from tests import TestCaseWithSubversionRepository
@@ -7,6 +23,15 @@ from bzrlib.tests.repository_implementations.test_repository import TestCaseWith
 class TestRepositoryWorks(TestCaseWithSubversionRepository):
     def setUp(self):
         TestCaseWithSubversionRepository.setUp(self)
+
+    def test_format(self):
+        """ Test repository format is correct """
+        bzrdir = self.open_bzrdir()
+        self.assertEqual(bzrdir._format.get_format_string(), \
+                "Subversion Smart Server")
+        
+        self.assertEqual(bzrdir._format.get_format_description(), \
+                "Subversion Smart Server")
 
     def test_url(self):
         """ Test repository URL is kept """
