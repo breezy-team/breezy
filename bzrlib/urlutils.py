5477d5f7f8dbc038ce96a063e812edf70bc52f0b
John Arbash Meinel <john@arbash-meinel.com>
2006-05-11 18:45:01 -0700

    Fixing _find_scheme_and_separator

--- bzrlib/urlutils.py
+++ bzrlib/urlutils.py
@@ -95,8 +95,8 @@ def _find_scheme_and_separator(url):
     # (first slash after the ://)
     first_path_slash = path.find('/')
     if first_path_slash == -1:
-        return scheme_loc, None
-    return scheme_loc, first_path_slash+len(scheme)+3
+        return len(scheme), None
+    return len(scheme), first_path_slash+len(scheme)+3
 
 
 def join(base, *args):
