ad7c84469013718fb8de9027c61eb8c572fd0c83
Aaron Bentley <abentley@panoramicfeedback.com>
2006-05-15 09:52:00 -0400

    Get single-revision case working

--- bzrlib/changeset/apply_changeset.py
+++ bzrlib/changeset/apply_changeset.py
@@ -17,6 +17,7 @@ from bzrlib.merge import merge_inner
 from bzrlib.errors import BzrCommandError
 from bzrlib.diff import compare_trees
 from bzrlib.osutils import sha_string, split_lines
+from bzrlib.tree import EmptyTree
 
 
 def _install_info(repository, cset_info, cset_tree):
@@ -50,11 +51,13 @@ def install_revision(repository, rev, cset_tree):
     # install the inventory
     # TODO: Figure out how to handle ghosted revisions
     present_parents = []
-    parent_invs = []
+    parent_trees = {}
     for p_id in rev.parent_ids:
         if repository.has_revision(p_id):
             present_parents.append(p_id)
-            parent_invs.append(repository.get_inventory(p_id))
+            parent_trees[p_id] = repository.revision_tree(p_id)
+        else:
+            parent_trees[p_id] = EmptyTree()
 
     inv = cset_tree.inventory
     
@@ -63,11 +66,14 @@ def install_revision(repository, rev, cset_tree):
         w = repository.weave_store.get_weave_or_empty(ie.file_id,
                 repository.get_transaction())
         if ie.revision not in w:
+            text_parents = [p for (p,t) in parent_trees.iteritems() if 
+                            ie.file_id in t]
             repository.weave_store.add_text(ie.file_id, 
                                             rev.revision_id,
             cset_tree.get_file(ie.file_id).readlines(),
-            present_parents, repository.get_transaction())
-
+            text_parents, repository.get_transaction())
+    
+    repository.add_inventory(rev.revision_id, inv, present_parents)
     repository.add_revision(rev.revision_id, rev, inv)
 
 def apply_changeset(branch, from_file, reverse=False, auto_commit=False):
