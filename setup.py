ee4971cc71a249a5905094394c11e29577ea21f9
Jelmer Vernooij <jelmer@samba.org>
2006-05-16 20:50:49 +0200

    Add setup tool and license.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2.4
+
+from distutils.core import setup
+
+setup(name='bzr-svn',
+      description='Support for Subversion branches in Bazaar-NG',
+      keywords='plugin bzr svn',
+      version='0.1',
+      url='http://bazaar-vcs.org/BzrForeignBranches/Subversion',
+      download_url='http://samba.org/~jelmer/bzr/svn',
+      license='GPL',
+      author='Jelmer Vernooij',
+      author_email='jelmer@samba.org',
+      long_description="""
+      This plugin adds support for branching off Subversion 
+      repositories.
+      """,
+      package_dir={'bzrlib.plugins.svn':'.'},
+      packages=['bzrlib.plugins.svn'])
