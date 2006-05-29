83bdfd7a251cda4e45169c3fce763542de46d653
Aaron Bentley <aaron.bentley@utoronto.ca>
2006-05-29 19:01:33 -0400

    PEP8 updates

--- bzrlib/tests/test_patches.py
+++ bzrlib/tests/test_patches.py
@@ -14,6 +14,8 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
 import unittest
 import os.path
 
@@ -30,6 +32,7 @@ from bzrlib.patches import (MalformedLine,
                             iter_patched, 
                             parse_line,
                             parse_patch)
+
 
 class PatchesTester(unittest.TestCase):
     def datafile(self, filename):
