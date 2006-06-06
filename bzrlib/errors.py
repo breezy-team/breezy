b2dbf3988a6e43ab2492249391e076f9d101129d
Aaron Bentley <aaron.bentley@utoronto.ca>
2006-06-06 18:56:39 +0100

    Raise NotABundle when a non-bundle is supplied

--- bzrlib/errors.py
+++ bzrlib/errors.py
@@ -938,6 +938,12 @@ class TestamentMismatch(BzrNewError):
         self.measured = measured
 
 
+class NotABundle(BzrNewError):
+    """Not a bzr revision-bundle: %(text)r"""
+    def __init__(self, text):
+        self.text = text
+
+
 class BadBundle(Exception): pass
 
 
