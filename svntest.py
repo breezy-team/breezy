1516f54e5ac8e6834c0dd8adad31e0b60268f7c4
Jelmer Vernooij <jelmer@samba.org>
2006-06-21 19:03:00 +0200

    Implement some functionality.

--- svntest.py
+++ svntest.py
@@ -18,25 +18,44 @@ from bzrlib.tests import TestCaseInTempDir
 
 import svn.ra, svn.repos, svn.wc
 
-class TestCaseWithSvnRepository(TestCaseInTempDir):
+class TestCaseWithSubversionRepository(TestCaseInTempDir):
     """A test case that provides the ability to build Subversion 
     repositories."""
 
-    def make_repository(self):
+    def make_repository(self, relpath):
         """Create a repository.
 
         :return: Handle to the repository.
         """
-        pass
+        abspath = os.path.join(self.test_dir, relpath)
+        repos_url = "file://%s" % abspath
 
-    def make_fs(self, relpath):
-        """Create repository in relpath.
+        repos = svn.repos.create(abspath, '', '', None, None)
 
-        :param relpath: Path to create repository in.
-        :return: Subversion fs handle.
-        """
-        raise NotImplementedError(self.make_fs)
+        return repos_url
 
+    def make_remote_bzrdir(self, relpath):
+        """Create a repository."""
+
+        repos_url = self.make_repository(relpath)
+
+        return BzrDir.open(repos_url)
+
+    def make_local_bzrdir(self, relpath):
+        """Create a repository and checkout."""
+
+        repos_url = self.make_repository(relpath)
+
+        ctx = svn.client.create_context()
+        
+        rev = svn.core.svn_opt_revision_t()
+        rev.kind = svn.core.svn_opt_revision_head
+
+        svn.client.checkout2(repos_url, relpath, 
+                rev, rev, True, False, ctx)
+
+        return BzrDir.open(relpath)
+        
     def wc_commit(self, relpaths):
         """Commit current changes in specified working copy.
         
@@ -74,14 +93,15 @@ class TestCaseWithSvnRepository(TestCaseInTempDir):
         """
         raise NotImplementedError(self.build_tree)
 
-    def make_wc(self, relpath, reppath=""):
+    def make_wc(self, relpath, repos_url=None):
         """Create a repository and a checkout. Return the checkout.
 
         :param relpath: Optional relpath to check out if not the full 
             repository.
         :return: Subversion wc handle.
         """
-        raise NotImplementedError(self.make_fs_and_wc)
+        # FIXME
+        raise NotImplementedError(self.make_wc)
 
     def make_ra(self, relpath):
         """Create a repository and a ra connection to it. 
@@ -89,7 +109,10 @@ class TestCaseWithSvnRepository(TestCaseInTempDir):
         :param relpath: Path to create repository at.
         :return: The ra connection.
         """
-        raise NotImplementedError(self.make_ra)
+
+        repos_url = self.make_repository(relpath)
+
+        return svn.ra.open2(repos_url, svn.ra.callbacks2_t(), None)
 
     def dumpfile(self, repos):
         """Create a dumpfile for the specified repository.
@@ -97,12 +120,4 @@ class TestCaseWithSvnRepository(TestCaseInTempDir):
         :return: File name of the dumpfile.
         """
         raise NotImplementedError(self.dumpfile)
-
-    def make_wc(self, relpath=""):
-        """Create a repository and a checkout. Return the checkout.
-
-        :param relpath: Optional relpath to check out if not the full 
-            repository.
-        """
-        raise NotImplementedError(self.make_wc)
 
