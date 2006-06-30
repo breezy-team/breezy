faf8328b356e9d63e24440ebfcc89ec396f7d02d
John Arbash Meinel <john@arbash-meinel.com>
2006-06-30 08:37:54 -0500

    Clean up the documentation for run_bzr_error on Martin's suggestion.

--- bzrlib/tests/__init__.py
+++ bzrlib/tests/__init__.py
@@ -751,10 +751,25 @@ class TestCase(unittest.TestCase):
     def run_bzr_error(self, error_regexes, *args, **kwargs):
         """Run bzr, and check that stderr contains the supplied regexes
         
-        This defaults to retcode=3, so you must supply retcode=? if you expect
-        a different value.
+        :param error_regexes: Sequence of regular expressions which 
+            must each be found in the error output. The relative ordering
+            is not enforced.
+        :param args: command-line arguments for bzr
+        :param kwargs: Keyword arguments which are interpreted by run_bzr
+            This function changes the default value of retcode to be 3,
+            since in most cases this is run when you expect bzr to fail.
         :return: (out, err) The actual output of running the command (in case you
                  want to do more inspection)
+
+        Examples of use:
+            # Make sure that commit is failing because there is nothing to do
+            self.run_bzr_error(['no changes to commit'],
+                               'commit', '-m', 'my commit comment')
+            # Make sure --strict is handling an unknown file, rather than
+            # giving us the 'nothing to do' error
+            self.build_tree(['unknown'])
+            self.run_bzr_error(['Commit refused because there are unknown files'],
+                               'commit', '--strict', '-m', 'my commit comment')
         """
         kwargs.setdefault('retcode', 3)
         out, err = self.run_bzr(*args, **kwargs)
