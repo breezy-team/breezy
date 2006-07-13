a49020be4dc3de93a0e7490033ec34c4fe3ce6cb
John Arbash Meinel <john@arbash-meinel.com>
2006-07-12 19:07:38 -0500

    Workaround plugins modifying the DEFAULT_IGNORE list when testing exact results.

--- bzrlib/tests/test_workingtree.py
+++ bzrlib/tests/test_workingtree.py
@@ -318,8 +318,16 @@ class TestNonFormatSpecificCode(TestCaseWithTransport):
         # Setup the default ignore list to be empty
         ignores.set_user_ignores([])
 
-        self.build_tree_contents([('.bzrignore', 'CVS\n.hg\n')])
-        reference_output = tree._combine_ignore_rules(['CVS', '.hg'])[0]
-        regex_rules = tree._get_ignore_rules_as_regex()[0]
-        self.assertEqual(len(reference_output[1]), regex_rules[0].groups)
-        self.assertEqual(reference_output[1], regex_rules[1])
+        # some plugins (shelf) modifies the DEFAULT_IGNORE list in memory
+        # which causes this test to fail so force the DEFAULT_IGNORE
+        # list to be empty
+        orig_default = bzrlib.DEFAULT_IGNORE
+        try:
+            bzrlib.DEFAULT_IGNORE = []
+            self.build_tree_contents([('.bzrignore', 'CVS\n.hg\n')])
+            reference_output = tree._combine_ignore_rules(['CVS', '.hg'])[0]
+            regex_rules = tree._get_ignore_rules_as_regex()[0]
+            self.assertEqual(len(reference_output[1]), regex_rules[0].groups)
+            self.assertEqual(reference_output[1], regex_rules[1])
+        finally:
+            bzrlib.DEFAULT_IGNORE = orig_default
