80c1a64e8fe2a3dc22bfcacb11d0c218c8f429e2
Jan Balster <jan@merlinux.de>
2006-07-19 07:54:36 +0200

    benchmarks for "bzr bundle --revision ..-1"

--- bzrlib/benchmarks/__init__.py
+++ bzrlib/benchmarks/__init__.py
@@ -108,6 +108,7 @@ def test_suite():
     testmod_names = [ \
                    'bzrlib.benchmarks.bench_add',
                    'bzrlib.benchmarks.bench_bench',
+                   'bzrlib.benchmarks.bench_bundle',
                    'bzrlib.benchmarks.bench_checkout',
                    'bzrlib.benchmarks.bench_commit',
                    'bzrlib.benchmarks.bench_info',
--- bzrlib/benchmarks/bench_info.py
+++ bzrlib/benchmarks/bench_info.py
@@ -13,7 +13,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-"""Tests for bzr status performance."""
+"""Tests for bzr info performance."""
 
 
 from bzrlib.benchmarks import Benchmark
