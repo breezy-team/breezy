8ce8290888ada8551eb0d7d2987d7b54779c202b
James Westby <jw+debian@jameswestby.net>
2006-07-29 23:18:41 +0100

    Enable some

--- __init__.py
+++ __init__.py
@@ -20,9 +20,9 @@ class cmd_buildpackage(Command):
 
 	def run(self, package, version=None, verbose=False):
 		retcode = 0
-#   tree = WorkingTree.open_containing(package)[0]
-#   b = tree.branch
-#   rev_id = b.last_revision()
+    tree = WorkingTree.open_containing(package)[0]
+    b = tree.branch
+    rev_id = b.last_revision()
 #   t = b.repository.revision_tree(rev_id)
 #   export(t,"build-area/build",format,root)
 
