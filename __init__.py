5af1e758ce1612e33b1c288ae8958057148a0539
James Westby <jw+debian@jameswestby.net>
2006-07-31 19:23:24 +0100

    Can sort of build a package now

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import shutil
 
 from bzrlib.commands import Command, register_command
 from bzrlib.option import Option
@@ -11,44 +12,53 @@ from bzrlib.workingtree import WorkingTree
 from bzrlib.export import export
 
 class cmd_buildpackage(Command):
-	"""Build the package
-	"""
-	dry_run_opt = Option('dry-run', help="don't do anything")
-	Option.SHORT_OPTIONS['n'] = dry_run_opt
-	takes_args = ['package', 'version?']
-	takes_options = ['verbose',
-					 dry_run_opt]
+  """Build the package
+  """
+  dry_run_opt = Option('dry-run', help="don't do anything")
+  Option.SHORT_OPTIONS['n'] = dry_run_opt
+  takes_args = ['package', 'version?']
+  takes_options = ['verbose',
+           dry_run_opt]
 
-	def run(self, package, version=None, verbose=False):
-		retcode = 0
-		tree = WorkingTree.open_containing(package)[0]
-		b = tree.branch
-		rev_id = b.last_revision()
-		t = b.repository.revision_tree(rev_id)
-		export(t,"build-area/build",format,root)
+  def run(self, package, version=None, verbose=False):
+    retcode = 0
+    tree = WorkingTree.open_containing(package)[0]
+    b = tree.branch
+    rev_id = b.last_revision()
+    t = b.repository.revision_tree(rev_id)
+    if not os.path.exists('build-area'):
+      os.mkdir('build-area')
+    os.chdir('build-area')
+    if os.path.exists(package): 
+      shutil.rmtree(package)
+    export(t,package,None,None)
+    os.chdir(package)
+    os.system('dpkg-buildpackage -S -uc -us -rfakeroot')
+    os.chdir('..')
+    shutil.rmtree(package)
 
-		return retcode
+    return retcode
 
 class cmd_recordpackage(Command):
-	"""Record the package
-	"""
-	dry_run_opt = Option('dry-run', help="don't do anything")
-	Option.SHORT_OPTIONS['n'] = dry_run_opt
-	takes_args = ['package', 'version?']
-	takes_options = ['verbose',
-					 dry_run_opt]
+  """Record the package
+  """
+  dry_run_opt = Option('dry-run', help="don't do anything")
+  Option.SHORT_OPTIONS['n'] = dry_run_opt
+  takes_args = ['package', 'version?']
+  takes_options = ['verbose',
+           dry_run_opt]
 
-	def run(self, package, version=None, verbose=False):
-		retcode = 0
+  def run(self, package, version=None, verbose=False):
+    retcode = 0
 
-		return retcode
+    return retcode
 
 def test_suite():
-	from unittest import TestSuite, TestLoader
-	import test_buildpackage
-	suite = TestSuite()
-	suite.addTest(TestLoader().loadTestsFromModule(test_buildpackage))
-	return suite
+  from unittest import TestSuite, TestLoader
+  import test_buildpackage
+  suite = TestSuite()
+  suite.addTest(TestLoader().loadTestsFromModule(test_buildpackage))
+  return suite
 
 register_command(cmd_buildpackage)
 register_command(cmd_recordpackage)
