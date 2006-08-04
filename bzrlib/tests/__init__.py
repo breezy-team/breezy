77195dbdbbb3c8ca98ab3d2a9e8687cbba69386a
Aaron Bentley <abentley@panoramicfeedback.com>
2006-08-04 09:53:16 -0400

    Update test to use changes_from

--- bzrlib/tests/__init__.py
+++ bzrlib/tests/__init__.py
@@ -1263,7 +1263,6 @@ def test_suite():
                    'bzrlib.tests.test_config',
                    'bzrlib.tests.test_conflicts',
                    'bzrlib.tests.test_decorators',
-                   'bzrlib.tests.test_delta',
                    'bzrlib.tests.test_diff',
                    'bzrlib.tests.test_doc_generate',
                    'bzrlib.tests.test_errors',
