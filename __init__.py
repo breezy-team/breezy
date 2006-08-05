9d1e501993a8f56e774e56060698fe70c1d937c3
James Westby <jw+debian@jameswestby.net>
2006-08-05 17:56:04 +0100

    Add --merge for mergeWithUpstream equivalent. Make the design more OO.

--- __init__.py
+++ __init__.py
@@ -5,6 +5,8 @@ import sys
 import shutil
 import re
 import commands
+import tempfile
+import glob
 
 from bzrlib.commands import Command, register_command
 from bzrlib.option import Option
@@ -17,20 +19,25 @@ class DebianChanges(object):
   """Abstraction of the .changes file. Use it to find out what files were built
   """
 
-  def __init__(self, package, version):
+  def __init__(self, package, version, dir):
     status, arch = commands.getstatusoutput('dpkg-architecture -qDEB_BUILD_ARCH')
     if status > 0:
       raise DebianError("Could not find the build architecture")
     changes = str(package)+"_"+str(version)+"_"+str(arch)+".changes"
+    if dir is not None:
+      changes = dir+"/"+changes
     if not os.path.exists(changes):
-      raise DebianException("Could not find "+package)
+      raise DebianError("Could not find "+package)
     f = open(changes)
     p = re.compile('^ [a-z0-9]+ [0-9]+ [a-z]+ [a-z]+ (.*)$')
     files = []
     for line in f:
       m = p.match(line)
       if m is not None:
-        files.append(m.group(1))
+        file = m.group(1)
+        if dir is not None:
+          file = dir+"/"+file
+        files.append(file)
     self._files = files
     self._filename = changes
 
@@ -84,6 +91,98 @@ class DebianChangelog(object):
   def package(self):
     return self._package
 
+class BuildProperties(object):
+
+  def __init__(self, changelog, build_dir, tarball_dir):
+    self._changelog = changelog
+    self._build_dir = build_dir
+    self._tarball_dir = tarball_dir
+  
+  def package(self):
+    return self._changelog.package()
+
+  def upstream_version(self):
+    return self._changelog.upstream_version()
+
+  def debian_version(self):
+    return self._changelog.debian_version()
+
+  def full_version(self):
+    return self._changelog.full_version()
+
+  def build_dir(self):
+    return self._build_dir
+
+  def source_dir(self):
+    return self.build_dir()+"/"+self.package()+"-"+self.full_version()
+
+  def tarball_dir(self):
+    return self._tarball_dir
+
+class DebBuild(object):
+
+  def __init__(self, properties, tree):
+    self._properties = properties
+    self._tree = tree
+
+  def prepare(self):
+    build_dir = self._properties.build_dir()
+    if not os.path.exists(build_dir):
+      os.makedirs(build_dir)
+    source_dir = self._properties.source_dir()
+    if os.path.exists(source_dir):
+      shutil.rmtree(source_dir)
+
+  def export(self):
+    source_dir = self._properties.source_dir()
+    export(self._tree,source_dir,None,None)
+
+  def build(self, builder):
+    wd = os.getcwdu()
+    os.chdir(self._properties.source_dir())
+    os.system(builder)
+    os.chdir(wd)
+
+  def clean(self):
+    shutil.rmtree(self._properties.source_dir())
+
+  def move_result(self, result):
+    package = self._properties.package()
+    version = self._properties.full_version()
+    changes = DebianChanges(package, version, self._properties.build_dir())
+    files = changes.files()
+    if not os.path.exists(result):
+      os.makedirs(result)
+    shutil.move(changes.filename(), result)
+    for file in files:
+      shutil.move(file, result)
+
+class DebMergeBuild(DebBuild):
+
+  def export(self):
+    package = self._properties.package()
+    upstream = self._properties.upstream_version()
+    build_dir = self._properties.build_dir()
+    tarballdir = self._properties.tarball_dir()
+    tarball = tarballdir+"/"+package+"_"+upstream+".orig.tar.gz"
+    if not os.path.exists(tarballdir):
+      raise DebianError('Could not find dir with upstream tarballs: '+tarballdir)
+    if not os.path.exists(tarball):
+      raise DebianError('Could not find upstrean tarball at '+tarball)
+
+    tempdir = tempfile.mkdtemp(prefix='builddeb-', dir=build_dir)
+
+    os.system('tar xzf '+tarball+' -C '+tempdir)
+
+    source_dir = self._properties.source_dir()
+    export(self._tree,source_dir,None,None)
+
+    files = glob.glob(tempdir+'/*/*')
+
+    for file in files:
+      shutil.move(file, source_dir)
+
+    shutil.rmtree(tempdir)
 
 
 class DebianError(BzrNewError):
@@ -92,6 +191,17 @@ class DebianError(BzrNewError):
   def __init__(self, message):
     BzrNewError.__init__(self)
     self.message = message
+
+class BuildDebProperties(object):
+
+  def __init__(self):
+    self._merge = False
+    if os.path.exists('bzr-builddeb'):
+      if os.path.exists('bzr-builddeb/mergeWithUpstream'):
+        self._merge = True
+
+  def merge(self):
+    return self._merge
 
 class cmd_builddeb(Command):
   """Build the package
@@ -103,12 +213,14 @@ class cmd_builddeb(Command):
   dont_purge_opt = Option('dont-purge', help="Don't purge the build directory after building")
   result_opt = Option('result', help="Directory in which to place the resulting package files", type=str)
   builder_opt = Option('builder', help="Command to build the package", type=str)
+  merge_opt = Option('merge', help='Merge the debian part of the source in to the upstream tarball')
+#  Option.SHORT_OPTIONS['m'] = merge_opt
   takes_args = ['branch?', 'version?']
   aliases = ['bd']
   takes_options = ['verbose',
-           working_tree_opt, export_only_opt, dont_purge_opt, result_opt, builder_opt]
+           working_tree_opt, export_only_opt, dont_purge_opt, result_opt, builder_opt, merge_opt]
 
-  def run(self, branch=None, version=None, verbose=False, working_tree=False, export_only=False, dont_purge=False, result=None, builder=None):
+  def run(self, branch=None, version=None, verbose=False, working_tree=False, export_only=False, dont_purge=False, result=None, builder=None, merge=False):
     retcode = 0
 
     if branch is None:
@@ -130,36 +242,31 @@ class cmd_builddeb(Command):
       t = tree
     if not t.has_filename('debian/changelog'):
       raise DebianError("Could not open debian/changelog")
+
     changelog_id = t.inventory.path2id('debian/changelog')
     contents = t.get_file_text(changelog_id)
     changelog = DebianChangelog(contents)
-    version = changelog.full_version()
-    upstream = changelog.upstream_version()
-    package = changelog.package()
-
-    dir = package + "-" + upstream
-
     if branch is None:
-      os.chdir('..')
+      build_dir = '../build-area'
+      tarball_dir = '../tarballs'
+    else:
+      build_dir = 'build-area'
+      tarball_dir = 'tarballs'
+    properties = BuildProperties(changelog,build_dir,tarball_dir)
+    if merge:
+      build = DebMergeBuild(properties, t)
+    else:
+      build = DebBuild(properties, t)
 
-    if not os.path.exists('build-area'):
-      os.mkdir('build-area')
-    os.chdir('build-area')
-    if os.path.exists(dir): 
-      shutil.rmtree(dir)
-    export(t,dir,None,None)
+    build.prepare()
+    build.export()
+
     if not export_only:
-      os.chdir(dir)
-      os.system(builder)
-      os.chdir('..')
+      build.build(builder)
       if not dont_purge:
-        shutil.rmtree(dir)
+        build.clean()
       if result is not None:
-        changes = DebianChanges(package, version)
-        files = changes.files()
-        shutil.move(changes.filename(), result)
-        for file in files:
-          shutil.move(file, result)
+        build.move_result(result)
 
 
     return retcode
