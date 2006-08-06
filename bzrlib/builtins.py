80b96261260c6818a4f7cd438991f1e1c2138ffd
Matthieu Moy <Matthieu.Moy@imag.fr>
2006-08-06 23:43:16 +0530

    bzr cat now works nicely with revno:N:path too
    (even outside a branch or working tree).

--- bzrlib/builtins.py
+++ bzrlib/builtins.py
@@ -1639,13 +1639,18 @@ class cmd_cat(Command):
         if revision is not None and len(revision) != 1:
             raise BzrCommandError("bzr cat --revision takes exactly one number")
         tree = None
-        try:
-            tree, relpath = WorkingTree.open_containing(filename)
-            b = tree.branch
-        except NotBranchError:
-            pass
+        b = None
+        if revision is not None and revision[0].get_branch() is not None:
+            b = Branch.open(revision[0].get_branch())
+            relpath = filename
+        else:
+            try:
+                tree, relpath = WorkingTree.open_containing(filename)
+                b = tree.branch
+            except NotBranchError:
+                pass
 
-        if tree is None:
+        if b is None:
             b, relpath = Branch.open_containing(filename)
         if revision is None:
             revision_id = b.last_revision()
