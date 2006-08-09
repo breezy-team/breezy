e80c72a61e52d6b19564497fa95210e7c9243227
John Arbash Meinel <john@arbash-meinel.com>
2006-08-09 14:53:02 -0500

    Adding the AddFromBaseAction, which tries to reuse file ids from another tree

--- bzrlib/add.py
+++ bzrlib/add.py
@@ -79,6 +79,49 @@ class AddAction(object):
         return None
 
 
+class AddFromBaseAction(AddAction):
+    """This class will try to extract file ids from another tree."""
+
+    def __init__(self, base_tree, base_path, to_file=None, should_print=None):
+        super(AddFromBaseAction, self).__init__(to_file=to_file,
+                                                should_print=should_print)
+        self.base_tree = base_tree
+        self.base_path = base_path
+
+    def __call__(self, inv, parent_ie, path, kind):
+        # Place the parent call
+        # Now check to see if we can extract an id for this file
+        file_id, base_path = self._get_base_file_id(path, parent_ie)
+        if file_id is not None:
+            if self.should_print:
+                self._to_file.write('added %s w/ file id from %s\n'
+                                    % (path.raw_path, base_path))
+        else:
+            # we aren't doing anything special, so let the default
+            # reporter happen
+            file_id = super(AddFromBaseAction, self).__call__(
+                        inv, parent_ie, path, kind)
+        return file_id
+
+    def _get_base_file_id(self, path, parent_ie):
+        """Look for a file id in the base branch.
+
+        First, if the base tree has the parent directory,
+        we look for a file with the same name in that directory.
+        Else, we look for an entry in the base tree with the same path.
+        """
+
+        if (parent_ie.file_id in self.base_tree):
+            base_parent_ie = self.base_tree.inventory[parent_ie.file_id]
+            base_child_ie = base_parent_ie.children.get(path.base_path)
+            if base_child_ie is not None:
+                return (base_child_ie.file_id,
+                        self.base_tree.id2path(base_child_ie.file_id))
+        full_base_path = bzrlib.osutils.pathjoin(self.base_path, path.raw_path)
+        # This may return None, but it is our last attempt
+        return self.base_tree.path2id(full_base_path), full_base_path
+
+
 # TODO: jam 20050105 These could be used for compatibility
 #       however, they bind against the current stdout, not the
 #       one which exists at the time they are called, so they
--- bzrlib/tests/test_smart_add.py
+++ bzrlib/tests/test_smart_add.py
@@ -19,7 +19,12 @@ import os
 import unittest
 
 from bzrlib import errors, ignores, osutils
-from bzrlib.add import smart_add, smart_add_tree, AddAction
+from bzrlib.add import (
+    AddAction,
+    AddFromBaseAction,
+    smart_add,
+    smart_add_tree,
+    )
 from bzrlib.tests import TestCase, TestCaseWithTransport, TestSkipped
 from bzrlib.errors import NoSuchFile
 from bzrlib.inventory import InventoryFile, Inventory
@@ -145,7 +150,7 @@ class TestSmartAdd(TestCaseWithTransport):
         self.assertSubset(('inertiatic/foo.pyc',), ignored['*.py[co]'])
 
 
-class CustomIDAddAction(AddAction):
+class AddCustomIDAction(AddAction):
 
     def __call__(self, inv, parent_ie, path, kind):
         # The first part just logs if appropriate
@@ -242,7 +247,7 @@ class TestSmartAddTree(TestCaseWithTransport):
 
     def test_custom_ids(self):
         sio = StringIO()
-        action = CustomIDAddAction(to_file=sio, should_print=True)
+        action = AddCustomIDAction(to_file=sio, should_print=True)
         self.build_tree(['file1', 'dir1/', 'dir1/file2'])
 
         wt = self.make_branch_and_tree('.')
@@ -257,6 +262,109 @@ class TestSmartAddTree(TestCaseWithTransport):
                           ('file1', 'file-file1'),
                          ], [(path, ie.file_id) for path, ie
                                 in wt.inventory.iter_entries()])
+
+
+class TestAddFrom(TestCaseWithTransport):
+    """Tests for AddFromBaseAction"""
+
+    def make_base_tree(self):
+        self.base_tree = self.make_branch_and_tree('base')
+        self.build_tree(['base/a', 'base/b',
+                         'base/dir/', 'base/dir/a',
+                         'base/dir/subdir/',
+                         'base/dir/subdir/b',
+                        ])
+        self.base_tree.add(['a', 'b', 'dir', 'dir/a',
+                            'dir/subdir', 'dir/subdir/b'])
+        self.base_tree.commit('creating initial tree.')
+
+    def add_helper(self, base_tree, base_path, new_tree, file_list,
+                   should_print=False):
+        to_file = StringIO()
+        base_tree.lock_read()
+        try:
+            new_tree.lock_write()
+            try:
+                action = AddFromBaseAction(base_tree, base_path,
+                                           to_file=to_file,
+                                           should_print=should_print)
+                smart_add_tree(new_tree, file_list, action=action)
+            finally:
+                new_tree.unlock()
+        finally:
+            base_tree.unlock()
+        return to_file.getvalue()
+
+    def test_copy_all(self):
+        self.make_base_tree()
+        new_tree = self.make_branch_and_tree('new')
+        files = ['a', 'b',
+                 'dir/', 'dir/a',
+                 'dir/subdir/',
+                 'dir/subdir/b',
+                ]
+        self.build_tree(['new/' + fn for fn in files])
+        self.add_helper(self.base_tree, '', new_tree, ['new'])
+
+        for fn in files:
+            base_file_id = self.base_tree.path2id(fn)
+            new_file_id = new_tree.path2id(fn)
+            self.assertEqual(base_file_id, new_file_id)
+
+    def test_copy_from_dir(self):
+        self.make_base_tree()
+        new_tree = self.make_branch_and_tree('new')
+
+        self.build_tree(['new/a', 'new/b', 'new/c',
+                         'new/subdir/', 'new/subdir/b', 'new/subdir/d'])
+        self.add_helper(self.base_tree, 'dir', new_tree, ['new'])
+
+        # We 'a' and 'b' exist in the root, and they are being added
+        # in a new 'root'. Since ROOT ids are not unique, we will
+        # use those ids
+        # TODO: This will probably change once trees have a unique root id
+        # It is definitely arguable that 'a' should get the id of
+        # 'dir/a' not of 'a'.
+        self.assertEqual(self.base_tree.path2id('a'),
+                         new_tree.path2id('a'))
+        self.assertEqual(self.base_tree.path2id('b'),
+                         new_tree.path2id('b'))
+
+        # Because we specified 'dir/' as the base path, and we have
+        # nothing named 'subdir' in the base tree, we should grab the
+        # ids from there
+        self.assertEqual(self.base_tree.path2id('dir/subdir'),
+                         new_tree.path2id('subdir'))
+        self.assertEqual(self.base_tree.path2id('dir/subdir/b'),
+                         new_tree.path2id('subdir/b'))
+
+        # These should get newly generated ids
+        c_id = new_tree.path2id('c')
+        self.assertNotEqual(None, c_id)
+        self.failIf(c_id in self.base_tree)
+
+        d_id = new_tree.path2id('subdir/d')
+        self.assertNotEqual(None, d_id)
+        self.failIf(d_id in self.base_tree)
+
+    def test_copy_existing_dir(self):
+        self.make_base_tree()
+        new_tree = self.make_branch_and_tree('new')
+        self.build_tree(['new/subby/', 'new/subby/a', 'new/subby/b'])
+
+        subdir_file_id = self.base_tree.path2id('dir/subdir')
+        new_tree.add(['subby'], [subdir_file_id])
+        self.add_helper(self.base_tree, '', new_tree, ['new'])
+        # Because 'subby' already points to subdir, we should add
+        # 'b' with the same id
+        self.assertEqual(self.base_tree.path2id('dir/subdir/b'),
+                         new_tree.path2id('subby/b'))
+
+        # 'subby/a' should be added with a new id because there is no
+        # matching path or child of 'subby'.
+        a_id = new_tree.path2id('subby/a')
+        self.assertNotEqual(None, a_id)
+        self.failIf(a_id in self.base_tree)
 
 
 class TestAddNonNormalized(TestCaseWithTransport):
