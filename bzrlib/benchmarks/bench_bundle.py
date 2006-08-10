dc03b0bc1dbf93a689e3476b0d334f11ae27b7f5
Carl Friedrich Bolz <cfbolz@gmx.de>
2006-08-10 13:28:41 +0200

    low level bundle tests.

--- bzrlib/benchmarks/bench_bundle.py
+++ bzrlib/benchmarks/bench_bundle.py
@@ -15,7 +15,15 @@
 
 """Tests for bzr bundle performance."""
 
+
+import os
+from StringIO import StringIO
+
 from bzrlib.benchmarks import Benchmark
+from bzrlib.workingtree import WorkingTree
+from bzrlib.branch import Branch
+from bzrlib.bundle.serializer import write_bundle
+from bzrlib.revisionspec import RevisionSpec
 
 
 class BundleBenchmark(Benchmark):
@@ -23,6 +31,7 @@ class BundleBenchmark(Benchmark):
     The bundle tests should (also) be done at a lower level with
     direct call to the bzrlib."""
     
+
     def test_create_bundle_known_kernel_like_tree(self):
         """
         Create a bundle for a kernel sized tree with no ignored, unknowns,
@@ -43,7 +52,7 @@ class BundleBenchmark(Benchmark):
         Create a bundle for a heavily merged tree.""" 
         self.make_heavily_merged_tree()
         self.time(self.run_bzr, 'bundle', '--revision', '..-1')
-
+        
     def test_apply_bundle_known_kernel_like_tree(self):
         """
         Create a bundle for a kernel sized tree with no ignored, unknowns,
@@ -60,4 +69,68 @@ class BundleBenchmark(Benchmark):
             f.close()
         os.chdir('../branch_a')
         self.time(self.run_bzr, 'merge', '../bundle')
+
  
+class BundleLibraryLevelBenchmark(Benchmark):
+
+    def make_parametrized_tree(self, num_files, num_revisions,
+                               num_files_in_bundle):
+        """Create a tree with given parameters. Always creates 2 levels of
+        directories with the given number of files. Then the given number of
+        revisions are created, changing some lines in one files in each
+        revision. Only num_files_in_bundle files are changed in these
+        revisions.
+
+        :param num_files: number of files in tree
+        :param num_revisions: number of revisions
+        :param num_files_in_bundle: number of files changed in the revisions
+        """
+        # create files
+        directories = []
+        files = []
+        count = 0
+        for outer in range(num_files // 64 + 1):
+            directories.append("%s/" % outer)
+            for middle in range(8):
+                prefix = "%s/%s/" % (outer, middle)
+                directories.append(prefix)
+                for filename in range(min(8, num_files - count)):
+                    count += 1
+                    files.append(prefix + str(filename))
+        self.run_bzr('init')
+        self.build_tree(directories + files)
+        for d in directories:
+            self.run_bzr('add', d)
+        self.run_bzr('commit', '-m', 'initial repo layout')
+        # create revisions
+        affected_files = files[:num_files_in_bundle]
+        count = 0
+        for changes_file in range(num_revisions // num_files_in_bundle + 1):
+            for f in affected_files:
+                count += 1
+                if count >= num_revisions:
+                    break
+                content = "\n".join([str(i) for i in range(changes_file)] +
+                                    [str(changes_file)] * 5) + "\n"
+                self.build_tree_contents([(f, content)])
+                self.run_bzr("commit", '-m', 'some changes')
+        assert count >= num_revisions
+
+    for treesize, treesize_h in [(5, "small"), (100, "moderate"),
+                                 (1000, "big")]:
+        for bundlefiles, bundlefiles_h in [(5, "few"), (100, "some"),
+                                           (1000, "many")]:
+            if bundlefiles > treesize:
+                continue
+            for num_revisions in [1, 500, 1000]:
+                code = """
+def test_%s_files_%s_tree_%s_revision(self):
+    self.make_parametrized_tree(%s, %s, %s)
+    branch, _ = Branch.open_containing(".")
+    revision_history = branch.revision_history()
+    bundle_text = StringIO()
+    self.time(write_bundle, branch.repository, revision_history[-1],
+              None, bundle_text)""" % (
+                    bundlefiles_h, treesize_h, num_revisions,
+                    treesize, num_revisions, bundlefiles)
+              #exec code
