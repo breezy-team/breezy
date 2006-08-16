9d798c83d1737fe3acae43805bc91733d63eb7f9
Andrew Bennetts <andrew.bennetts@canonical.com>
2006-08-16 17:49:38 +1000

    Skip various test_sprout* tests when sprouting to non-local bzrdirs that can't have working trees; plus fix a test method naming clash and the bug it revealed in bzrdir.sprout.

--- bzrlib/bzrdir.py
+++ bzrlib/bzrdir.py
@@ -615,7 +615,8 @@ class BzrDir(object):
                 # XXX FIXME RBC 20060214 need tests for this when the basis
                 # is incomplete
                 result_repo.fetch(basis_repo, revision_id=revision_id)
-            result_repo.fetch(source_repository, revision_id=revision_id)
+            if source_repository is not None:
+                result_repo.fetch(source_repository, revision_id=revision_id)
         if source_branch is not None:
             source_branch.sprout(result, revision_id=revision_id)
         else:
