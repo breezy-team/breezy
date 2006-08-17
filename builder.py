ace4423c26d4c7d48856a50ad138ef3837d3e6c9
James Westby <jw+debian@jameswestby.net>
2006-08-17 18:48:43 +0100

    Place the package under the GPL

--- builder.py
+++ builder.py
@@ -1,3 +1,23 @@
+#    builder.py -- Classes for building packages
+#    Copyright (C) 2005 Jamie Wilkinson <jaq@debian.org> 
+#                  2006 James Westby <jw+debian@jameswestby.net>
+#    
+#    This file is part of bzr-builddeb.
+#
+#    bzr-builldeb is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    bzr-builddeb is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with bzr-builddeb; if not, write to the Free Software
+#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
 
 import glob
 import shutil
