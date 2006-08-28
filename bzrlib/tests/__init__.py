cc7086d96cc5df64231dd4293f8f464b8e89b015
Aaron Bentley <abentley@panoramicfeedback.com>
2006-08-28 15:55:25 -0400

    bug #54172: handle new directories properly in revert

--- bzrlib/tests/__init__.py
+++ bzrlib/tests/__init__.py
@@ -1361,6 +1361,7 @@ def test_suite():
                    'bzrlib.tests.test_progress',
                    'bzrlib.tests.test_reconcile',
                    'bzrlib.tests.test_repository',
+                   'bzrlib.tests.test_revert',
                    'bzrlib.tests.test_revision',
                    'bzrlib.tests.test_revisionnamespaces',
                    'bzrlib.tests.test_revisiontree',
