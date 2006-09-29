b06d106e6982a9752b22989b236d577a49ee96fa
Aaron Bentley <aaron.bentley@utoronto.ca>
2006-09-29 00:14:39 -0400

    Clean up subsume test

--- bzrlib/tests/test_subsume.py
+++ bzrlib/tests/test_subsume.py
@@ -15,7 +15,6 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 from bzrlib import bzrdir, errors, repository, workingtree, tests
-from bzrlib.tests.workingtree_implementations import TestCaseWithWorkingTree
 
 
 class TestWorkingTree(tests.TestCaseWithTransport):
