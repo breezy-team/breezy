a5ac42130c4cd87d1c97cb697f3cff04718dbbc7
John Arbash Meinel <john@arbash-meinel.com>
2006-09-29 12:51:01 -0500

    Refactor all 'raise ConnectionError' into a helper function

--- bzrlib/transport/ssh.py
+++ bzrlib/transport/ssh.py
@@ -106,17 +106,6 @@ def _get_ssh_vendor():
     return _ssh_vendor
 
 
-def _get_host_and_port(host, port):
-    """Get a user-visible host + port.
-
-    This handles the case when port is None, so we have nothing to display.
-    """
-    if port is None:
-        return host
-    return '%s:%s' % (host, port)
-
-
-
 def _ignore_sigint():
     # TODO: This should possibly ignore SIGHUP as well, but bzr currently
     # doesn't handle it itself.
@@ -171,6 +160,25 @@ class SSHVendor(object):
         """
         raise NotImplementedError(self.connect_ssh)
         
+    def _get_host_and_port(self, host, port):
+        """Get a user-visible host + port.
+
+        This handles the case when port is None, so we have nothing to display.
+        """
+        if port is None:
+            return host
+        return '%s:%s' % (host, port)
+
+    def _raise_connection_error(self, host, port=None, orig_error=None,
+                                msg='Unable to connect to SSH host %s'):
+        """Raise a ConnectionError with properly formatted host.
+
+        This just unifies all the locations that try to raise ConnectionError,
+        so that they format things properly.
+        """
+        raise ConnectionError(msg % self._get_host_and_port(host, port),
+                              orig_error=orig_error)
+
 
 class LoopbackVendor(SSHVendor):
     """SSH "vendor" that connects over a plain TCP socket, not SSH."""
@@ -180,9 +188,7 @@ class LoopbackVendor(SSHVendor):
         try:
             sock.connect((host, port))
         except socket.error, e:
-            raise ConnectionError('Unable to connect to SSH host %s'
-                                  % (_get_host_and_port(host, port),),
-                                  orig_error=e)
+            self._raise_connection_error(host, port=port, orig_error=e)
         return SFTPClient(LoopbackSFTP(sock))
 
 register_ssh_vendor('loopback', LoopbackVendor())
@@ -212,9 +218,7 @@ class ParamikoVendor(SSHVendor):
             t.set_log_channel('bzr.paramiko')
             t.start_client()
         except (paramiko.SSHException, socket.error), e:
-            raise ConnectionError('Unable to connect to SSH host %s'
-                                  % (_get_host_and_port(host, port),),
-                                  orig_error=e)
+            self._raise_connection_error(host, port=port, orig_error=e)
             
         server_key = t.get_remote_server_key()
         server_key_hex = paramiko.util.hexify(server_key.get_fingerprint())
@@ -248,9 +252,8 @@ class ParamikoVendor(SSHVendor):
         try:
             return t.open_sftp_client()
         except paramiko.SSHException, e:
-            raise ConnectionError('Unable to start sftp client %s'
-                                  % (_get_host_and_port(host, port),),
-                                  orig_error=e)
+            self._raise_connection_error(host, port=port, orig_error=e,
+                                         msg='Unable to start sftp client %s')
 
     def connect_ssh(self, username, password, host, port, command):
         t = self._connect(username, password, host, port)
@@ -260,9 +263,8 @@ class ParamikoVendor(SSHVendor):
             channel.exec_command(cmdline)
             return _ParamikoSSHConnection(channel)
         except paramiko.SSHException, e:
-            raise ConnectionError('Unable to invoke remote bzr %s'
-                                  % (_get_host_and_port(host, port),),
-                                  orig_error=e)
+            self._raise_connection_error(host, port=port, orig_error=e,
+                                         msg='Unable to invoke remote bzr %s')
 
 register_ssh_vendor('paramiko', ParamikoVendor())
 
@@ -284,18 +286,14 @@ class SubprocessVendor(SSHVendor):
             sock = self._connect(argv)
             return SFTPClient(sock)
         except (EOFError, paramiko.SSHException), e:
-            raise ConnectionError('Unable to connect to SSH host %s'
-                                  % (_get_host_and_port(host, port),),
-                                  orig_error=e)
+            self._raise_connection_error(host, port=port, orig_error=e)
         except (OSError, IOError), e:
             # If the machine is fast enough, ssh can actually exit
             # before we try and send it the sftp request, which
             # raises a Broken Pipe
             if e.errno not in (errno.EPIPE,):
                 raise
-            raise ConnectionError('Unable to connect to SSH host %s'
-                                  % (_get_host_and_port(host, port),),
-                                  orig_error=e)
+            self._raise_connection_error(host, port=port, orig_error=e)
 
     def connect_ssh(self, username, password, host, port, command):
         try:
@@ -303,18 +301,14 @@ class SubprocessVendor(SSHVendor):
                                                   command=command)
             return self._connect(argv)
         except (EOFError), e:
-            raise ConnectionError('Unable to connect to SSH host %s'
-                                  % (_get_host_and_port(host, port),),
-                                  orig_error=e)
+            self._raise_connection_error(host, port=port, orig_error=e)
         except (OSError, IOError), e:
             # If the machine is fast enough, ssh can actually exit
             # before we try and send it the sftp request, which
             # raises a Broken Pipe
             if e.errno not in (errno.EPIPE,):
                 raise
-            raise ConnectionError('Unable to connect to SSH host %s'
-                                  % (_get_host_and_port(host, port),),
-                                  orig_error=e)
+            self._raise_connection_error(host, port=port, orig_error=e)
 
     def _get_vendor_specific_argv(self, username, host, port, subsystem=None,
                                   command=None):
