f6edebca663b1a11ebb0839723a31bf91dc664df
John Arbash Meinel <john@arbash-meinel.com>
2006-10-05 15:37:25 +1000

    Add tests to cleanup the copyright of all source files

--- bzrlib/transport/http/response.py
+++ bzrlib/transport/http/response.py
@@ -1,5 +1,4 @@
-# Copyright (C) 2006 Michael Ellerman
-#           modified by John Arbash Meinel (Canonical Ltd)
+# Copyright (C) 2006 Canonical Ltd
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
