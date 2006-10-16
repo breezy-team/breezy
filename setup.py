b3d7a5ed7a3a78adc155e94a9be83ce9363ac4c6
Jelmer Vernooij <jelmer@samba.org>
2006-10-16 12:38:22 +0200

    Set up for 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='bzr-svn',
       description='Support for Subversion branches in Bazaar-NG',
       keywords='plugin bzr svn',
-      version='0.1',
+      version='0.2',
       url='http://bazaar-vcs.org/BzrForeignBranches/Subversion',
       download_url='http://bazaar-vcs.org/BzrSvn',
       license='GPL',
