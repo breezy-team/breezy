a0233b27129e2a52269b8ad16e9dc9b901b887e3
Martin Pool <mbp@sourcefrog.net>
2006-10-29 09:48:54 +0800

    Tests shouldn't assume os.listdir returns sorted results The manual specifically says it doesn't.

--- bzrlib/osutils.py
+++ bzrlib/osutils.py
@@ -1,6 +1,4 @@
-# Bazaar -- distributed version control
-#
-# Copyright (C) 2005 Canonical Ltd
+# Copyright (C) 2005, 2006 Canonical Ltd
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
