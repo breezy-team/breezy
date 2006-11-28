b8329a79652fb8b27b7482401327fc31cc50fef5
Kent Gibson <warthog618@gmail.com>
2006-11-29 01:11:21 +0800

    Revert unnecessary change to test_mixed_is_ignored.

--- bzrlib/tests/workingtree_implementations/test_is_ignored.py
+++ bzrlib/tests/workingtree_implementations/test_is_ignored.py
@@ -159,13 +159,13 @@ class TestIsIgnored(TestCaseWithWorkingTree):
     def test_mixed_is_ignored(self):
         tree = self.make_branch_and_tree('.')
         ignores._set_user_ignores(['*.py[co]', './.shelf'])
-        self.build_tree_contents([('.bzrignore', './rootdir\n.*.swp\n')])
+        self.build_tree_contents([('.bzrignore', './rootdir\n*.swp\n')])
 
         self.assertEqual('*.py[co]', tree.is_ignored('foo.pyc'))
         self.assertEqual('./.shelf', tree.is_ignored('.shelf'))
         self.assertEqual('./rootdir', tree.is_ignored('rootdir'))
-        self.assertEqual(None, tree.is_ignored('foo.py.swp'))
-        self.assertEqual('.*.swp', tree.is_ignored('.foo.py.swp'))
+        self.assertEqual('*.swp', tree.is_ignored('foo.py.swp'))
+        self.assertEqual('*.swp', tree.is_ignored('.foo.py.swp'))
         self.assertEqual(None, tree.is_ignored('.foo.py.swo'))
 
     def test_DEFAULT_IGNORE(self):
