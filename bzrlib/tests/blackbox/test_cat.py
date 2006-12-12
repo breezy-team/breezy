579e06eb9777c26fcb5d772f10f888e50f5f8567
Wouter van Heyst <larstiq@larstiq.dyndns.org>
2006-12-12 01:39:55 +0100

    Actually check if cat returns the right content.

--- bzrlib/tests/blackbox/test_cat.py
+++ bzrlib/tests/blackbox/test_cat.py
@@ -98,9 +98,10 @@ class TestCat(TestCaseWithTransport):
 
     def test_remote_cat(self):
         wt = self.make_branch_and_tree('.')
-        self.build_tree(['README', 'See INSTALL'])
+        self.build_tree(['README'])
         wt.add('README')
         wt.commit('Making sure there is a basis_tree available')
 
         url = self.get_readonly_url() + '/README'
-        self.run_bzr('cat', url)
+        out, err = self.run_bzr('cat', url)
+        self.assertEqual('contents of README\n', out)
