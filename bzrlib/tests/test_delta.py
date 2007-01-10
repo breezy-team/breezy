c964b1d4d6c96dd1e20b8b83aedabf6c2858df69
Aaron Bentley <abentley@panoramicfeedback.com>
2007-01-10 10:09:31 -0500

    change method names to assertFoo

--- bzrlib/tests/test_delta.py
+++ bzrlib/tests/test_delta.py
@@ -32,10 +32,10 @@ class InstrumentedReporter(object):
 
 class TestReportChanges(tests.TestCase):
 
-    def reportEqual(self, expected, file_id='fid', path='path',
-                    versioned_change='unchanged', renamed=False,
-                    modified='unchanged', exe_change=False,
-                    kind=('file', 'file'), old_path=None):
+    def assertReport(self, expected, file_id='fid', path='path',
+                     versioned_change='unchanged', renamed=False,
+                     modified='unchanged', exe_change=False,
+                     kind=('file', 'file'), old_path=None):
         result = []
         def result_line(format, *args):
             result.append(format % args)
@@ -49,54 +49,54 @@ class TestReportChanges(tests.TestCase):
         self.assertEqualDiff(expected, result[0])
 
     def test_rename(self):
-        self.reportEqual('R   old => path', renamed=True, old_path='old')
-        self.reportEqual('    path')
+        self.assertReport('R   old => path', renamed=True, old_path='old')
+        self.assertReport('    path')
 
     def test_kind(self):
-        self.reportEqual(' K  path => path/', modified='kind changed',
-                         kind=('file', 'directory'))
-        self.reportEqual(' K  path/ => path', modified='kind changed',
-                         kind=('directory', 'file'), old_path='old')
-        self.reportEqual('RK  old => path/', renamed=True,
-                         modified='kind changed', kind=('file', 'directory'),
-                         old_path='old')
+        self.assertReport(' K  path => path/', modified='kind changed',
+                          kind=('file', 'directory'))
+        self.assertReport(' K  path/ => path', modified='kind changed',
+                          kind=('directory', 'file'), old_path='old')
+        self.assertReport('RK  old => path/', renamed=True,
+                          modified='kind changed', 
+                          kind=('file', 'directory'), old_path='old')
     def test_new(self):
-        self.reportEqual(' N  path/', modified='created',
-                         kind=(None, 'directory'))
-        self.reportEqual('+   path/', versioned_change='added',
-                         modified='unchanged', kind=(None, 'directory'))
-        self.reportEqual('+N  path/', versioned_change='added',
-                         modified='created', kind=(None, 'directory'))
-        self.reportEqual('+M  path/', versioned_change='added',
-                         modified='modified', kind=(None, 'directory'))
-        self.reportEqual('+K  path => path/', versioned_change='added',
-                         modified='kind changed', kind=('file', 'directory'))
+        self.assertReport(' N  path/', modified='created',
+                          kind=(None, 'directory'))
+        self.assertReport('+   path/', versioned_change='added',
+                          modified='unchanged', kind=(None, 'directory'))
+        self.assertReport('+N  path/', versioned_change='added',
+                          modified='created', kind=(None, 'directory'))
+        self.assertReport('+M  path/', versioned_change='added',
+                          modified='modified', kind=(None, 'directory'))
+        self.assertReport('+K  path => path/', versioned_change='added',
+                          modified='kind changed', kind=('file', 'directory'))
 
     def test_removal(self):
-        self.reportEqual(' D  path/', modified='deleted',
-                         kind=('directory', None), old_path='old')
-        self.reportEqual('-   path/', versioned_change='removed',
-                         kind=(None, 'directory'))
-        self.reportEqual('-D  path', versioned_change='removed',
-                         modified='deleted', kind=('file', 'directory'))
+        self.assertReport(' D  path/', modified='deleted',
+                          kind=('directory', None), old_path='old')
+        self.assertReport('-   path/', versioned_change='removed',
+                          kind=(None, 'directory'))
+        self.assertReport('-D  path', versioned_change='removed',
+                          modified='deleted', kind=('file', 'directory'))
 
     def test_modification(self):
-        self.reportEqual(' M  path', modified='modified')
-        self.reportEqual(' M* path', modified='modified', exe_change=True)
+        self.assertReport(' M  path', modified='modified')
+        self.assertReport(' M* path', modified='modified', exe_change=True)
 
-    def report_changes_equal(self,
-                             file_id='fid',
-                             path='path',
-                             content_change=False,
-                             versioned=(True, True),
-                             parent_id=('pid', 'pid'),
-                             name=('name', 'name'),
-                             kind=('file', 'file'),
-                             executable=(False, False),
-                             versioned_change='unchanged',
-                             renamed=False,
-                             modified='unchanged',
-                             exe_change=False):
+    def assertChangesEqual(self,
+                           file_id='fid',
+                           path='path',
+                           content_change=False,
+                           versioned=(True, True),
+                           parent_id=('pid', 'pid'),
+                           name=('name', 'name'),
+                           kind=('file', 'file'),
+                           executable=(False, False),
+                           versioned_change='unchanged',
+                           renamed=False,
+                           modified='unchanged',
+                           exe_change=False):
         reporter = InstrumentedReporter()
         delta.report_changes([(file_id, path, content_change, versioned,
                                parent_id, name, kind, executable)], reporter)
@@ -112,49 +112,39 @@ class TestReportChanges(tests.TestCase):
     def test_report_changes(self):
         """Test change detection of report_changes"""
         #Ensure no changes are detected by default
-        self.report_changes_equal(modified='unchanged', renamed=False,
-                                  versioned_change='unchanged',
-                                  exe_change=False)
-        self.report_changes_equal(modified='kind changed',
-                                  kind=('file', 'directory'))
-        self.report_changes_equal(modified='created',
-                                  kind=(None, 'directory'))
-        self.report_changes_equal(modified='deleted',
-                                  kind=('directory', None))
-        self.report_changes_equal(content_change=True, modified='modified')
-        self.report_changes_equal(renamed=True, name=('old', 'new'))
-        self.report_changes_equal(renamed=True,
-                                  parent_id=('old-parent', 'new-parent'))
-        self.report_changes_equal(versioned_change='added',
-                                  versioned=(False, True))
-        self.report_changes_equal(versioned_change='removed',
-                                  versioned=(True, False))
+        self.assertChangesEqual(modified='unchanged', renamed=False,
+                                versioned_change='unchanged',
+                                exe_change=False)
+        self.assertChangesEqual(modified='kind changed',
+                                kind=('file', 'directory'))
+        self.assertChangesEqual(modified='created', kind=(None, 'directory'))
+        self.assertChangesEqual(modified='deleted', kind=('directory', None))
+        self.assertChangesEqual(content_change=True, modified='modified')
+        self.assertChangesEqual(renamed=True, name=('old', 'new'))
+        self.assertChangesEqual(renamed=True,
+                                parent_id=('old-parent', 'new-parent'))
+        self.assertChangesEqual(versioned_change='added',
+                                versioned=(False, True))
+        self.assertChangesEqual(versioned_change='removed',
+                                versioned=(True, False))
         # execute bit is only detected as "changed" if the file is and was
         # a regular file.
-        self.report_changes_equal(exe_change=True,
-                                  executable=(True, False))
-        self.report_changes_equal(exe_change=False,
-                                  executable=(True, False),
-                                  kind=('directory', 'directory'))
-        self.report_changes_equal(exe_change=False, modified='kind changed',
-                                  executable=(False, True),
-                                  kind=('directory', 'file'))
+        self.assertChangesEqual(exe_change=True, executable=(True, False))
+        self.assertChangesEqual(exe_change=False, executable=(True, False),
+                                kind=('directory', 'directory'))
+        self.assertChangesEqual(exe_change=False, modified='kind changed',
+                                executable=(False, True),
+                                kind=('directory', 'file'))
 
         # Now make sure they all work together
-        self.report_changes_equal(versioned_change='removed',
-                                  modified='deleted',
-                                  versioned=(True, False),
-                                  kind=('directory', None))
-        self.report_changes_equal(versioned_change='removed',
-                                  modified='created',
-                                  versioned=(True, False),
-                                  kind=(None, 'file'))
-        self.report_changes_equal(versioned_change='removed',
-                                  modified='modified',
-                                  renamed=True,
-                                  exe_change=True,
-                                  versioned=(True, False),
-                                  content_change=True,
-                                  name=('old', 'new'),
-                                  executable=(False, True)
-                                  )
+        self.assertChangesEqual(versioned_change='removed',
+                                modified='deleted', versioned=(True, False),
+                                kind=('directory', None))
+        self.assertChangesEqual(versioned_change='removed',
+                                modified='created', versioned=(True, False),
+                                kind=(None, 'file'))
+        self.assertChangesEqual(versioned_change='removed',
+                                modified='modified', renamed=True,
+                                exe_change=True, versioned=(True, False),
+                                content_change=True, name=('old', 'new'),
+                                executable=(False, True))
