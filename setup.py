f3d6858cbea2ad47a6d30a72d1eed552e71227b5
Wouter van Heyst <larstiq@larstiq.dyndns.org>
2007-01-18 19:37:21 +0100

    (Alexander Belchenko) add windows installer check for python2.5

--- setup.py
+++ setup.py
@@ -197,6 +197,12 @@ elif 'py2exe' in sys.argv:
     if sys.version.startswith('2.4'):
         # adding elementtree package
         additional_packages.append('elementtree')
+    elif sys.version.startswith('2.5'):
+        additional_packages.append('xml.etree')
+    else:
+        import warnings
+        warnings.warn('Unknown Python version.\n'
+                      'Please check setup.py script for compatibility.')
 
     options_list = {"py2exe": {"packages": BZRLIB['packages'] +
                                            additional_packages,
