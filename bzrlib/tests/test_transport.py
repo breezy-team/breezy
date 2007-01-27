9bc42e76e6c0cd0f4a5331239fc98ad3fdd872e7
ghigo <ghigo@venice>
2007-01-27 18:24:41 +0100

    uncomment test test__get_set_protocol_handlers

--- bzrlib/tests/test_transport.py
+++ bzrlib/tests/test_transport.py
@@ -50,14 +50,14 @@ from bzrlib.transport.local import LocalTransport
 class TestTransport(TestCase):
     """Test the non transport-concrete class functionality."""
 
-    #def test__get_set_protocol_handlers(self):
-        #handlers = _get_protocol_handlers()
-        #self.assertNotEqual({}, handlers)
-        #try:
-            #_set_protocol_handlers({})
-            #self.assertEqual({}, _get_protocol_handlers())
-        #finally:
-            #_set_protocol_handlers(handlers)
+    def test__get_set_protocol_handlers(self):
+        handlers = _get_protocol_handlers()
+        self.assertNotEqual([], handlers.keys( ))
+        try:
+            _clear_protocol_handlers()
+            self.assertEqual([], _get_protocol_handlers().keys())
+        finally:
+            _set_protocol_handlers(handlers)
 
     def test_get_transport_modules(self):
         handlers = _get_protocol_handlers()
