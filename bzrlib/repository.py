ef76efc8ad5bccfea9bd2c13766b5bb5113fd4ba
Martin Pool <mbp@sourcefrog.net>
2007-02-12 17:12:14 +1100

    Add deprecated forwarders for old formats from repository.py

--- bzrlib/repository.py
+++ bzrlib/repository.py
@@ -766,6 +766,42 @@ class Repository(object):
                     raise errors.NonAsciiRevisionId(method, self)
 
 
+
+# remove these delegates a while after bzr 0.15
+def __make_delegated(name, from_module):
+    def _deprecated_repository_forwarder():
+        symbol_versioning.warn('%s moved to %s in bzr 0.15'
+            % (name, from_module),
+            DeprecationWarning)
+        m = __import__(from_module, globals(), locals(), [name])
+        try:
+            return getattr(m, name)
+        except AttributeError:
+            raise AttributeError('module %s has no name %s'
+                    % (m, name))
+    globals()[name] = _deprecated_repository_forwarder
+
+for _name in [
+        'AllInOneRepository',
+        'WeaveMetaDirRepository',
+        'PreSplitOutRepositoryFormat',
+        'RepositoryFormat4',
+        'RepositoryFormat5',
+        'RepositoryFormat6',
+        'RepositoryFormat7',
+        ]:
+    __make_delegated(_name, 'bzrlib.repofmt.weaverepo')
+
+for _name in [
+        'KnitRepository',
+        'KnitRepository2',
+        'RepositoryFormatKnit',
+        'RepositoryFormatKnit1',
+        'RepositoryFormatKnit2',
+        ]:
+    __make_delegated(_name, 'bzrlib.repofmt.knitrepo')
+
+
 def install_revision(repository, rev, revision_tree):
     """Install all revision data into a repository."""
     present_parents = []
