fb9f30ba8726c3f724566c17f6d7a9c2bbbb7d4b
James Westby <jw+debian@jameswestby.net>
2007-03-02 21:34:40 +0000

    Also add the config tests to the testsuite.
      * Again use the doctest compatibility to add the tests to the suite.
      * And again there could be better tests.

--- tests/__init__.py
+++ tests/__init__.py
@@ -24,6 +24,7 @@ from unittest import TestSuite
 from bzrlib.tests import TestUtil
 
 import changes
+import config
 
 def test_suite():
     loader = TestUtil.TestLoader()
@@ -31,8 +32,15 @@ def test_suite():
     testmod_names = [
             'test_builder',
             ]
-    suite.addTest(loader.loadTestsFromModuleNames(["%s.%s" % (__name__, i) for i in testmod_names]))
-    suite.addTest(doctest.DocTestSuite(changes))
+    suite.addTest(loader.loadTestsFromModuleNames(["%s.%s" % (__name__, i)
+                                            for i in testmod_names]))
+
+    doctest_mod_names = [
+             'changes',
+             'config'
+             ]
+    for mod in doctest_mod_names:
+      suite.addTest(doctest.DocTestSuite(mod))
 
     return suite
 
