6041d42e5e2cf142c5ebaf07911ec2e0d7ba4dcc
jml@canonical.com <jml@canonical.com>
2007-04-11 10:27:03 +1000

    Basic docstrings for bugtracker.py

--- bzrlib/bugtracker.py
+++ bzrlib/bugtracker.py
@@ -14,7 +14,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+
+from bzrlib.lazy_import import lazy_import
+lazy_import(globals(), """
 from bzrlib import registry
+""")
 
 
 def get_url(tag, branch, bug_id):
@@ -31,24 +35,38 @@ class TrackerRegistry(registry.Registry):
                 return tracker
 
 tracker_registry = TrackerRegistry()
+"""Registry of bug trackers."""
 
 
 class LaunchpadTracker(object):
+    """The Launchpad bug tracker."""
 
     @classmethod
     def get(klass, tag, branch):
+        """Return a Launchpad tracker if tag is 'lp'. Return None otherwise."""
         if tag != 'lp':
             return None
         return klass()
 
     def get_bug_url(self, bug_id):
+        """Return a Launchpad URL for bug_id."""
         return 'https://launchpad.net/bugs/%s' % (bug_id,)
+
+tracker_registry.register('launchpad', LaunchpadTracker)
 
 
 class TracTracker(object):
+    """A Trac instance."""
 
     @classmethod
     def get(klass, tag, branch):
+        """Return a TracTracker for the given tag.
+
+        Looks in the configuration of 'branch' for a 'trac_<tag>_url' setting,
+        which should refer to the base URL of a project's Trac instance.
+        e.g.
+            trac_twisted_url = http://twistedmatrix.com
+        """
         url = branch.get_config().get_user_option('trac_%s_url' % (tag,))
         if url is None:
             return None
@@ -58,7 +76,7 @@ class TracTracker(object):
         self._url = url
 
     def get_bug_url(self, bug_id):
+        """Return a URL for a bug on this Trac instance."""
         return '%s/ticket/%s' % (self._url, bug_id)
 
-tracker_registry.register('launchpad', LaunchpadTracker)
 tracker_registry.register('trac', TracTracker)
