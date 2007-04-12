31ab7b9a107253833ae05472623a5e87e582c01d
John Arbash Meinel <john@arbash-meinel.com>
2007-04-12 15:56:34 -0500

    Implement TestCaseWithFTPServer using the new shiny Feature mechanism. Also update FtpServer so that it conforms to the expected setUp(vfs_server) functionality.

--- bzrlib/tests/test_ftp_transport.py
+++ bzrlib/tests/test_ftp_transport.py
@@ -14,11 +14,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-from bzrlib.tests import TestCaseWithTransport
+from bzrlib import tests
 import bzrlib.transport
 
 
-class TestCaseAFTP(TestCaseWithTransport):
+class TestCaseAFTP(tests.TestCaseWithTransport):
     """Test aftp transport."""
 
     def test_aftp_degrade(self):
@@ -28,3 +28,46 @@ class TestCaseAFTP(TestCaseWithTransport):
         self.failUnless(parent.is_active)
 
         self.assertEqual('aftp://host/path', t.abspath(''))
+
+
+class _MedusaFeature(tests.Feature):
+    """Some tests want an FTP Server, check if one is available.
+
+    Right now, the only way this is available is if 'medusa' is installed.
+    """
+
+    def _probe(self):
+        try:
+            import medusa
+            import medusa.filesys
+            import medusa.ftp_server
+            return True
+        except ImportError:
+            return False
+
+    def feature_name(self):
+        return 'medusa'
+
+MedusaFeature = _MedusaFeature()
+
+
+class TestCaseWithFTPServer(tests.TestCaseWithTransport):
+
+    _test_needs_features = [MedusaFeature]
+
+    def setUp(self):
+        from bzrlib.transport.ftp import FtpServer
+        self.transport_server = FtpServer
+        super(TestCaseWithFTPServer, self).setUp()
+
+
+
+class TestFTPServer(TestCaseWithFTPServer):
+
+    def test_basic_exists(self):
+        url = self.get_url()
+        self.assertStartsWith(url, 'ftp://')
+
+        t = self.get_transport()
+        t.put_bytes('foo', 'test bytes\n')
+        self.assertEqual('test bytes\n', t.get_bytes('foo'))
