a42958b44ed1e8f4aba9609c1f8fee1367b6ce6f
Alexander Belchenko <bialix@ukr.net>
2007-04-18 22:46:50 +0300

    shutil no more needed in clean_selftest_output()

--- bzrlib/tests/__init__.py
+++ bzrlib/tests/__init__.py
@@ -2306,8 +2306,6 @@ def clean_selftest_output(root=None, quiet=False):
     :param  quiet:  suppress report about deleting directories
     """
     import re
-    import shutil
-
     re_dir = re.compile(r'''test\d\d\d\d\.tmp''')
     if root is None:
         root = u'.'
