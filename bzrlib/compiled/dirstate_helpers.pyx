36dde604b5e3ee56820dab20fffd57e4d0cd3fc4
John Arbash Meinel <john@arbash-meinel.com>
2007-05-07 15:28:04 -0500

    Finish implementing _c_read_dirblocks for any number of parents. bench_dirstate.BenchmarkDirState.test__c_read_dirblocks_20k_tree_0_parents    OK      367ms/    4353ms bench_dirstate.BenchmarkDirState.test__c_read_dirblocks_20k_tree_1_parent     OK      594ms/    8958ms bench_dirstate.BenchmarkDirState.test__c_read_dirblocks_20k_tree_2_parents    OK      842ms/   10490ms bench_dirstate.BenchmarkDirState.test__py_read_dirblocks_20k_tree_0_parents   OK      560ms/    4298ms bench_dirstate.BenchmarkDirState.test__py_read_dirblocks_20k_tree_1_parent    OK      692ms/    8658ms bench_dirstate.BenchmarkDirState.test__py_read_dirblocks_20k_tree_2_parents   OK     1006ms/   10710ms
    So overall the performance benefit is about 15-30%

--- bzrlib/compiled/dirstate_helpers.pyx
+++ bzrlib/compiled/dirstate_helpers.pyx
@@ -270,8 +270,8 @@ cdef class Reader:
             PyList_Append(fields, self.get_next_str())
         return fields
 
-    cdef object _get_entry_0_parents(self, void **p_current_dirname,
-                                     int *new_block):
+    cdef object _get_entry(self, int num_trees, void **p_current_dirname,
+                           int *new_block):
         cdef object path_name_file_id_key
         cdef char *entry_size_str
         cdef unsigned long int entry_size
@@ -280,6 +280,7 @@ cdef class Reader:
         cdef char* dirname_str
         cdef char* trailing
         cdef int cur_size
+        cdef int i
         cdef object minikind
         cdef object fingerprint
         cdef object info
@@ -298,23 +299,24 @@ cdef class Reader:
                                  self.get_next_str(),
                                 )
 
-        minikind = self.get_next_str()
-        fingerprint = self.get_next_str()
-        entry_size_str = self.get_next(&cur_size)
-        entry_size = strtoul(entry_size_str, NULL, 10)
-        executable_str = self.get_next(&cur_size)
-        is_executable = (executable_str[0] == c'y')
-        info = self.get_next_str()
-
-        ret = (path_name_file_id_key, [
-            ( # Current tree
+        trees = []
+        for i from 0 <= i < num_trees:
+            minikind = self.get_next_str()
+            fingerprint = self.get_next_str()
+            entry_size_str = self.get_next(&cur_size)
+            entry_size = strtoul(entry_size_str, NULL, 10)
+            executable_str = self.get_next(&cur_size)
+            is_executable = (executable_str[0] == c'y')
+            info = self.get_next_str()
+            PyList_Append(trees, (
                 minikind,     # minikind
                 fingerprint,  # fingerprint
                 entry_size,   # size
                 is_executable,# executable
                 info,         # packed_stat or revision_id
-            )])
+            ))
 
+        ret = (path_name_file_id_key, trees)
         # Ignore the trailing newline
         trailing = self.get_next(&cur_size)
         if cur_size != 1 or trailing[0] != c'\n':
@@ -323,11 +325,18 @@ cdef class Reader:
                 % (cur_size, PyString_FromString(trailing), ret))
         return ret
 
-    def _parse_dirblocks_0_parents(self, state, entry_size):
+    def _parse_dirblocks(self, state):
+        """Parse all dirblocks in the state file."""
+        cdef int num_trees
         cdef object current_block
         cdef object entry
         cdef void * current_dirname
         cdef int new_block
+        cdef int expected_entry_count
+        cdef int entry_count
+
+        num_trees = state._num_present_parents() + 1
+        expected_entry_count = state._num_entries
 
         # Ignore the first record
         self.init()
@@ -337,15 +346,21 @@ cdef class Reader:
         obj = ''
         current_dirname = <void*>obj
         new_block = 0
+        entry_count = 0
 
         while self.cur < self.end_str:
-            entry = self._get_entry_0_parents(&current_dirname, &new_block)
+            entry = self._get_entry(num_trees, &current_dirname, &new_block)
             if new_block:
                 # new block - different dirname
                 current_block = []
                 PyList_Append(state._dirblocks,
                               (<object>current_dirname, current_block))
             PyList_Append(current_block, entry)
+            entry_count = entry_count + 1
+        if entry_count != expected_entry_count:
+            raise AssertionError('We read the wrong number of entries.'
+                    ' We expected to read %s, but read %s'
+                    % (expected_entry_count, entry_count))
         state._split_root_dirblock_into_contents()
 
 
@@ -359,113 +374,11 @@ def _c_read_dirblocks(state):
     :param state: A DirState object.
     :return: None
     """
-    cdef int cur
-    cdef int pos
-    cdef int entry_size
-    cdef int field_count
-    cdef int num_present_parents
-    cdef int expected_field_count
-    cdef int num_entries
-    cdef char *next_field
-
     state._state_file.seek(state._end_of_header)
     text = state._state_file.read()
     # TODO: check the crc checksums. crc_measured = zlib.crc32(text)
 
     reader = Reader(text)
 
-    num_present_parents = state._num_present_parents()
-    entry_size = state._fields_per_entry()
-
-    if num_present_parents == 0:
-        # Move the iterator to the current position
-        reader._parse_dirblocks_0_parents(state, entry_size)
-        state._dirblock_state = DirState.IN_MEMORY_UNMODIFIED
-        return
-    fields = reader.get_all_fields()
-
-    # skip the first field which is the trailing null from the header.
-    cur = 0
-
-    # Each line now has an extra '\n' field which is not used
-    # so we just skip over it
-    # entry size:
-    #  3 fields for the key
-    #  + number of fields per tree_data (5) * tree count
-    #  + newline
-    num_entries = state._num_entries
-    expected_field_count = entry_size * num_entries
-    field_count = len(fields)
-    if field_count - cur != expected_field_count:
-        # this checks our adjustment, and also catches file too short.
-        raise AssertionError(
-            'field count incorrect %s != %s, entry_size=%s, '
-            'num_entries=%s fields=%r' % (
-                field_count - cur, expected_field_count, entry_size,
-                state._num_entries, fields))
-
-    if num_present_parents == 1:
-        # Bind external functions to local names
-        _int = int
-        # We access all fields in order, so we can just iterate over
-        # them. Grab an straight iterator over the fields. (We use an
-        # iterator because we don't want to do a lot of additions, nor
-        # do we want to do a lot of slicing)
-        next = iter(fields).next
-        # Move the iterator to the current position
-        for x in xrange(cur):
-            next()
-        # The two blocks here are deliberate: the root block and the
-        # contents-of-root block.
-        state._dirblocks = [('', []), ('', [])]
-        current_block = state._dirblocks[0][1]
-        current_dirname = ''
-        append_entry = current_block.append
-        for count in xrange(state._num_entries):
-            dirname = next()
-            name = next()
-            file_id = next()
-            if dirname != current_dirname:
-                # new block - different dirname
-                current_block = []
-                current_dirname = dirname
-                state._dirblocks.append((current_dirname, current_block))
-                append_entry = current_block.append
-            # we know current_dirname == dirname, so re-use it to avoid
-            # creating new strings
-            entry = ((current_dirname, name, file_id),
-                     [(# Current Tree
-                         next(),                # minikind
-                         next(),                # fingerprint
-                         _int(next()),          # size
-                         next() == 'y',         # executable
-                         next(),                # packed_stat or revision_id
-                     ),
-                     ( # Parent 1
-                         next(),                # minikind
-                         next(),                # fingerprint
-                         _int(next()),          # size
-                         next() == 'y',         # executable
-                         next(),                # packed_stat or revision_id
-                     ),
-                     ])
-            trailing = next()
-            assert trailing == '\n'
-            # append the entry to the current block
-            append_entry(entry)
-        state._split_root_dirblock_into_contents()
-    else:
-        fields_to_entry = state._get_fields_to_entry()
-        entries = []
-        entries_append = entries.append
-        pos = cur
-        while pos < field_count:
-            entries_append(fields_to_entry(fields[pos:pos+entry_size]))
-            pos = pos + entry_size
-        state._entries_to_current_state(entries)
-    # To convert from format 2  => format 3
-    # state._dirblocks = sorted(state._dirblocks,
-    #                          key=lambda blk:blk[0].split('/'))
-    # To convert from format 3 => format 2
-    # state._dirblocks = sorted(state._dirblocks)
+    reader._parse_dirblocks(state)
     state._dirblock_state = DirState.IN_MEMORY_UNMODIFIED
