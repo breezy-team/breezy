ef980035e54740e517c5ee7bbeb52bf405d759a2
John Arbash Meinel <john@arbash-meinel.com>
2007-05-09 10:28:50 -0500

    Remove a lot of unused definitions.

--- bzrlib/compiled/dirstate_helpers.pyx
+++ bzrlib/compiled/dirstate_helpers.pyx
@@ -27,38 +27,15 @@ cdef extern from *:
 
 
 cdef extern from "stdlib.h":
-    struct _FILE:
-        pass
-    ctypedef _FILE FILE
-    size_t fread(void *ptr, size_t size, size_t count, FILE *stream)
     unsigned long int strtoul(char *nptr, char **endptr, int base)
 
 
 cdef extern from "Python.h":
-    # GetItem returns a borrowed reference
-    struct _PyObject:
-        pass
-    ctypedef _PyObject PyObject
-
-    struct _PyListObject:
-        int ob_size
-        PyObject **ob_item
-    ctypedef _PyListObject PyListObject
-
-    void *PyDict_GetItem(object p, object key)
-    int PyDict_SetItem(object p, object key, object val) except -1
-
     int PyList_Append(object lst, object item) except -1
     void *PyList_GetItem_object_void "PyList_GET_ITEM" (object lst, int index)
-    object PyList_GET_ITEM (object lst, int index)
     int PyList_CheckExact(object)
 
-    int PyTuple_CheckExact(object)
     void *PyTuple_GetItem_void_void "PyTuple_GET_ITEM" (void* tpl, int index)
-    object PyTuple_New(int)
-    int PyTuple_SetItem(object tpl, int offset, object val)
-    void PyTuple_SET_ITEM(object tpl, int offset, object val)
-    object PyTuple_Pack(int n, ...)
 
     char *PyString_AsString(object p)
     char *PyString_AS_STRING_void "PyString_AS_STRING" (void *p)
@@ -67,12 +44,6 @@ cdef extern from "Python.h":
     int PyString_Size(object p)
     int PyString_GET_SIZE_void "PyString_GET_SIZE" (void *p)
     int PyString_CheckExact(object p)
-
-    void Py_INCREF(object)
-    void Py_INCREF_PyObject "Py_INCREF" (PyObject *)
-    void Py_DECREF(object)
-
-    FILE *PyFile_AsFile(object p)
 
 
 cdef extern from "string.h":
