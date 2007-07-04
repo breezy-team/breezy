1a4cd099e21e87e396cea104e52041a7de6ade87
Martin Pool <mbp@sourcefrog.net>
2007-07-04 19:24:14 +1000

    hook docs

--- bzrlib/debug.py
+++ bzrlib/debug.py
@@ -16,6 +16,14 @@
 
 
 debug_flags = set()
-"""Set of flags that enable different debug behaviour"""
+"""Set of flags that enable different debug behaviour.
 
-# hooks - debug activity about hooks.
+These are set with eg ``-Dlock`` on the bzr command line.
+
+Options include:
+    
+ * hooks 
+ * lock - trace when lockdir locks are taken or released
+ * error - show stack traces for all top level exceptions
+
+"""
