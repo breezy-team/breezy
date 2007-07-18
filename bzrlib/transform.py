717cefd66369e35e6e5d7ec78f1ccdfd49b7b55d
Ian Clatworthy <ian.clatworthy@internode.on.net>
2007-07-18 17:10:53 +1000

    Deprecate old fns that use InventoryEntry methods that ought to go

--- bzrlib/transform.py
+++ bzrlib/transform.py
@@ -34,7 +34,8 @@ from bzrlib.inventory import InventoryEntry
 from bzrlib.osutils import (file_kind, supports_executable, pathjoin, lexists,
                             delete_any)
 from bzrlib.progress import DummyProgress, ProgressPhase
-from bzrlib.symbol_versioning import deprecated_function, zero_fifteen
+from bzrlib.symbol_versioning import deprecated_function, zero_fifteen, \
+        zero_nineteen
 from bzrlib.trace import mutter, warning
 from bzrlib import tree
 import bzrlib.ui
@@ -1424,6 +1425,7 @@ def find_interesting(working_tree, target_tree, filenames):
         working_tree.unlock()
 
 
+@deprecated_function(zero_nineteen)
 def change_entry(tt, file_id, working_tree, target_tree, 
                  trans_id_file_id, backups, trans_id, by_parent):
     """Replace a file_id's contents with those from a target tree."""
@@ -1480,6 +1482,7 @@ def _get_backup_name(name, by_parent, parent_trans_id, tt):
             return new_name
 
 
+@deprecated_function(zero_nineteen)
 def _entry_changes(file_id, entry, working_tree):
     """Determine in which ways the inventory entry has changed.
 
