6a4c008ba901a4aeb8ca53435e32b97d02fc7367
Daniel Watkins <D.M.Watkins@warwick.ac.uk>
2007-08-23 20:02:53 +0200

    Changed to remove need for escaping of quotes.

--- bzrlib/tests/test_errors.py
+++ bzrlib/tests/test_errors.py
@@ -119,14 +119,13 @@ class TestErrors(TestCaseWithTransport):
     def test_no_such_id(self):
         error = errors.NoSuchId("atree", "anid")
         self.assertEqualDiff("The file id \"anid\" is not present in the tree "
-            "\"atree\".",
+            "atree.",
             str(error))
 
     def test_no_such_revision_in_tree(self):
         error = errors.NoSuchRevisionInTree("atree", "anid")
-        self.assertEqualDiff("The revision id anid is not present in the tree "
-            "\"atree\".",
-            str(error))
+        self.assertEqualDiff("The revision id {anid} is not present in the"
+                             " tree atree.", str(error))
         self.assertIsInstance(error, errors.NoSuchRevision)
 
     def test_not_write_locked(self):
@@ -353,7 +352,7 @@ class TestErrors(TestCaseWithTransport):
         """Test the formatting of DuplicateRecordNameError."""
         e = errors.DuplicateRecordNameError(u"n\xe5me".encode('utf-8'))
         self.assertEqual(
-            "Container has multiple records with the same name: \"n\xc3\xa5me\"",
+            "Container has multiple records with the same name: n\xc3\xa5me",
             str(e))
 
 
