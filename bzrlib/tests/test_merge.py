1030395b04fa48ab8a6fabe06da09ef33206a19e
Aaron Bentley <aaron.bentley@utoronto.ca>
2007-09-04 23:51:23 -0400

    update new test case

--- bzrlib/tests/test_merge.py
+++ bzrlib/tests/test_merge.py
@@ -142,7 +142,7 @@ class TestMerge(TestCaseWithTransport):
                     this_tree=tree_b, ignore_zero=True)
         log = self._get_log(keep_log_file=True)
         self.failUnless('All changes applied successfully.\n' not in log)
-        tree_b.revert([])
+        tree_b.revert()
         merge_inner(tree_b.branch, tree_a, tree_b.basis_tree(), 
                     this_tree=tree_b, ignore_zero=False)
         log = self._get_log(keep_log_file=True)
