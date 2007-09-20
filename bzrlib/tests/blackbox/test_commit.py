555b576dd51cd5c75f444362ea058d0ffb146a37
Alexander Belchenko <bialix@ukr.net>
2007-09-20 12:30:25 +0300

    XFAIL test for bug #140419

--- bzrlib/tests/blackbox/test_commit.py
+++ bzrlib/tests/blackbox/test_commit.py
@@ -520,3 +520,21 @@ class TestCommit(ExternalBase):
         last_rev = tree.branch.repository.get_revision(tree.last_revision())
         properties = last_rev.properties
         self.assertEqual('John Doe', properties['author'])
+
+    def test_partial_commit_with_renames_in_tree(self):
+        # this test illustrate bug #140419
+        t = self.make_branch_and_tree('.')
+        self.build_tree(['dir/', 'dir/a', 'test'])
+        t.add(['dir/', 'dir/a', 'test'])
+        t.commit('initial commit')
+        # important part: file dir/a should change parent
+        # and should appear before old parent
+        # then during partial commit we have error
+        # parent_id {dir-XXX} not in inventory
+        t.rename_one('dir/a', 'a')
+        self.build_tree_contents([('test', 'changes in test')])
+        # check for error
+        self.run_bzr_error(['bzr: ERROR: parent_id .+ not in inventory'],
+                           'commit test -m "partial commit"')
+        self.expectFailure('Bug #140419',
+                           self.run_bzr, 'commit test -m "partial commit"')
