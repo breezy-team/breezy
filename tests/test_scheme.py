2bd327b97501e32d5f4d2363f9e38dfee4f44e6f
Jelmer Vernooij <jelmer@samba.org>
2007-10-17 12:47:26 +0200

    Add test for scheme initialization.

--- tests/test_scheme.py
+++ tests/test_scheme.py
@@ -138,6 +138,10 @@ class ListScheme(TestCase):
     def setUp(self):
         self.scheme = ListBranchingScheme(["foo", "bar/bloe"])
 
+    def test_create_from_string(self):
+        self.scheme = ListBranchingScheme('QlpoOTFBWSZTWXb2s-UAAADBAAAQAQCgACGYGYQYXckU4UJB29rPlA..')
+        self.assertEquals(["foo"], self.scheme.branch_list)
+
     def test_is_tag_empty(self):
         self.assertFalse(self.scheme.is_tag(""))
 
