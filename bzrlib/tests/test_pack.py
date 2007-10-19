4de3f5fdc15d7b203eab2b568af448ea4cf0dc77
Andrew Bennetts <andrew.bennetts@canonical.com>
2007-10-19 18:42:06 +1000

    Add a couple of docstrings to the tests.

--- bzrlib/tests/test_pack.py
+++ bzrlib/tests/test_pack.py
@@ -545,12 +545,18 @@ class PushParserTestCase(tests.TestCase):
 
         
 class TestContainerPushParser(PushParserTestCase):
+    """Tests for ContainerPushParser."""
 
     def test_construct(self):
         """ContainerPushParser can be constructed."""
         pack.ContainerPushParser()
 
     def test_multiple_records_at_once(self):
+        """If multiple records worth of data are fed to the parser in one
+        string, the parser will correctly parse all the records.
+
+        (A naive implementation might stop after parsing the first record.)
+        """
         parser = self.make_parser_expecting_record_type()
         parser.accept_bytes("B5\nname1\n\nbody1B5\nname2\n\nbody2")
         self.assertEqual(
