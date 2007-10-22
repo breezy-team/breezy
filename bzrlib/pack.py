cadb3b4335285fb0f4b5c6ffdf8618e5d6bade56
Andrew Bennetts <andrew.bennetts@canonical.com>
2007-10-22 14:50:17 +1000

    Extract a _serialise_byte_records function.

--- bzrlib/pack.py
+++ bzrlib/pack.py
@@ -103,6 +103,13 @@ class ContainerWriter(object):
             and thus are only suitable for use by a ContainerReader.
         """
         current_offset = self.current_offset
+        serialised_record = self._serialise_bytes_record(bytes, names)
+        self.write_func(serialised_record)
+        self.records_written += 1
+        # return a memo of where we wrote data to allow random access.
+        return current_offset, self.current_offset - current_offset
+
+    def _serialise_bytes_record(self, bytes, names):
         # Kind marker
         byte_sections = ["B"]
         # Length
@@ -126,10 +133,7 @@ class ContainerWriter(object):
         # memory pressure in that case. One possibly improvement here is to
         # check the size of the content before deciding to join here vs call
         # write twice.
-        self.write_func(''.join(byte_sections))
-        self.records_written += 1
-        # return a memo of where we wrote data to allow random access.
-        return current_offset, self.current_offset - current_offset
+        return ''.join(byte_sections)
 
 
 class ReadVFile(object):
