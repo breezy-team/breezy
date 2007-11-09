933758e8aecd0f040f85efe7aa0e6d5631f27bbe
Daniel Watkins <D.M.Watkins@warwick.ac.uk>
2007-11-09 14:10:57 +0000

    Modified tests to be more idiomatic.

--- bzrlib/tests/blackbox/test_remove.py
+++ bzrlib/tests/blackbox/test_remove.py
@@ -215,15 +215,14 @@ class TestRemove(ExternalBase):
         tree = self.make_branch_and_tree('.')
         self.build_tree(['a'])
         tree.add('a')
-        tree.commit(message="add a")
+        tree.commit("add a")
         tree.remove('a')
         self.run_bzr('remove a')
 
     def test_remove_file_and_containing_dir(self):
         tree = self.make_branch_and_tree('.')
-        self.build_tree(['config%s' % (os.sep,),
-                         osutils.joinpath(['config', 'file'])])
-        tree.add(['config', osutils.joinpath(['config', 'file'])])
-        tree.commit(message="add files")
-        self.run_bzr(['remove', osutils.joinpath(['config', 'file'])])
+        self.build_tree(['config/', 'config/file'])
+        tree.add(['config', 'config/file'])
+        tree.commit("add files")
+        self.run_bzr(['remove', 'config/file'])
         self.run_bzr('remove config')
