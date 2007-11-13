abe86de3415da208d6d2a9cb9acf052055cd8d86
Daniel Watkins <D.M.Watkins@warwick.ac.uk>
2007-11-13 01:23:44 +0000

    Cleaned up dodgy indentation.

--- bzrlib/plugin.py
+++ bzrlib/plugin.py
@@ -195,7 +195,7 @@ def load_from_dir(d):
                          name.replace('.','_').replace('-','_').replace(' ','_')
                 warning("Unable to load %r in %r as a plugin because this isn't"
                         " a valid module name; try renaming it to %r."
-                                                    % (name, d, sanitised_name))
+                        % (name, d, sanitised_name))
             else:
                 warning('Unable to load plugin %r from %r' % (name, d))
             log_exception_quietly()
