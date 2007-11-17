b5984d46f3118e6ac8c65744121b6298f610af96
Daniel Watkins <D.M.Watkins@warwick.ac.uk>
2007-11-17 17:57:43 +0000

    test__create_temp_file_with_commit_template_in_unicode_dir is now run with the various permutations of EncodingTestAdapter.

--- bzrlib/tests/__init__.py
+++ bzrlib/tests/__init__.py
@@ -94,6 +94,7 @@ from bzrlib.tests.TestUtil import (
                           TestSuite,
                           TestLoader,
                           )
+from bzrlib.tests.EncodingAdapter import EncodingTestAdapter
 from bzrlib.tests.treeshape import build_tree_contents
 from bzrlib.workingtree import WorkingTree, WorkingTreeFormat2
 
@@ -2511,6 +2512,10 @@ def test_suite():
     from bzrlib.tests.test_transport_implementations import TransportTestProviderAdapter
     adapter = TransportTestProviderAdapter()
     adapt_modules(test_transport_implementations, adapter, loader, suite)
+    adapt_tests(
+        ["bzrlib.tests.test_msgeditor.MsgEditorTest."
+         "test__create_temp_file_with_commit_template_in_unicode_dir"],
+        EncodingTestAdapter(), loader, suite)
     for package in packages_to_test():
         suite.addTest(package.test_suite())
     for m in MODULES_TO_TEST:
