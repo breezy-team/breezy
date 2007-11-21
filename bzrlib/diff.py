aa07bca0201ef833f35f1d3af05d77226e9ec17d
Aaron Bentley <abentley@panoramicfeedback.com>
2007-11-21 09:08:53 -0500

    Make old_tree/new_tree construction parameters of Differ

--- bzrlib/diff.py
+++ bzrlib/diff.py
@@ -409,7 +409,7 @@ def _show_diff_trees(old_tree, new_tree, to_file,
         extra_trees=extra_trees, require_versioned=True)
 
     has_changes = 0
-    differ = Differ(to_file, diff_file)
+    differ = Differ(old_tree, new_tree, to_file, diff_file)
     for path, file_id, kind in delta.removed:
         has_changes = 1
         path_encoded = path.encode(path_encoding, "replace")
@@ -417,7 +417,7 @@ def _show_diff_trees(old_tree, new_tree, to_file,
         old_name = '%s%s\t%s' % (old_label, path,
                                  _patch_header_date(old_tree, file_id, path))
         new_name = '%s%s\t%s' % (new_label, path, EPOCH_DATE)
-        differ.diff(file_id, old_tree, new_tree, old_name, new_name)
+        differ.diff(file_id, old_name, new_name)
     for path, file_id, kind in delta.added:
         has_changes = 1
         path_encoded = path.encode(path_encoding, "replace")
@@ -425,7 +425,7 @@ def _show_diff_trees(old_tree, new_tree, to_file,
         old_name = '%s%s\t%s' % (old_label, path, EPOCH_DATE)
         new_name = '%s%s\t%s' % (new_label, path,
                                  _patch_header_date(new_tree, file_id, path))
-        differ.diff(file_id, old_tree, new_tree, old_name, new_name)
+        differ.diff(file_id, old_name, new_name)
     for (old_path, new_path, file_id, kind,
          text_modified, meta_modified) in delta.renamed:
         has_changes = 1
@@ -501,27 +501,30 @@ def get_prop_change(meta_modified):
 def _maybe_diff_file_or_symlink(old_path, old_tree, file_id,
                                 new_path, new_tree, text_modified, differ):
     if text_modified:
-        differ.diff(file_id, old_tree, new_tree, old_path, new_path)
+        differ.diff(file_id, old_path, new_path)
 
 
 class Differ(object):
 
-    def __init__(self, to_file, text_diff):
+    def __init__(self, old_tree, new_tree, to_file, text_diff):
+        self.old_tree = old_tree
+        self.new_tree = new_tree
         self.to_file = to_file
         self.text_diff = text_diff
 
-    def diff(self, file_id, old_tree, new_tree, old_name, new_name):
+    def diff(self, file_id, old_name, new_name):
         try:
-            old_entry = old_tree.inventory[file_id]
+            old_entry = self.old_tree.inventory[file_id]
         except errors.NoSuchId:
             old_entry = None
         try:
-            new_entry = new_tree.inventory[file_id]
+            new_entry = self.new_tree.inventory[file_id]
         except errors.NoSuchId:
             new_entry = None
         if old_entry is None:
-            new_entry.diff(self.text_diff, new_name, new_tree, old_name,
-                           old_entry, old_tree, self.to_file, reverse=True)
+            new_entry.diff(self.text_diff, new_name, self.new_tree, old_name,
+                           old_entry, self.old_tree, self.to_file,
+                           reverse=True)
         else:
-            old_entry.diff(self.text_diff, old_name, old_tree, new_name,
-                           new_entry, new_tree, self.to_file)
+            old_entry.diff(self.text_diff, old_name, self.old_tree, new_name,
+                           new_entry, self.new_tree, self.to_file)
