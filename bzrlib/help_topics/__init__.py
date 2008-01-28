70a0448694759e0141c29ed2116e5e4aa7fdcfaf
Alexander Belchenko <bialix@ukr.net>
2008-01-28 10:59:58 +0200

    List possible values for BZR_SSH environment variable in env-variables help topic. (#181842)

--- bzrlib/help_topics/__init__.py
+++ bzrlib/help_topics/__init__.py
@@ -544,6 +544,7 @@ BZR_PLUGIN_PATH  Paths where bzr should look for plugins.
 BZR_HOME         Directory holding .bazaar config dir. Overrides HOME.
 BZR_HOME (Win32) Directory holding bazaar config dir. Overrides APPDATA and HOME.
 BZR_REMOTE_PATH  Full name of remote 'bzr' command (for bzr+ssh:// URLs).
+BZR_SSH          SSH client: paramiko (default), openssh, ssh, plink.
 ================ =================================================================
 """
 
