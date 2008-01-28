89aec8bb94d9f297fc52c1ae2dff60b506f3a33d
Jelmer Vernooij <jelmer@samba.org>
2008-01-28 21:44:49 +0100

    First step towards supporting multiple versions of the mappingsin the same version of bzr-svn.

---
+++ mapping.py
@@ -0,0 +1,87 @@
+# Copyright (C) 2005-2008 Jelmer Vernooij <jelmer@samba.org>
+ 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from bzrlib import registry
+
+from revids import parse_svn_revision_id, unescape_svn_path
+
+class BzrSvnMapping:
+    """Class that maps between Subversion and Bazaar semantics."""
+
+    @staticmethod
+    def parse_revision_id(revid):
+        raise NotImplementedError(self.parse_revision_id)
+
+
+class BzrSvnMappingv1(BzrSvnMapping):
+    @staticmethod
+    def parse_revision_id(revid):
+        revid = revid[len("svn-v1:"):]
+        at = revid.index("@")
+        fash = revid.rindex("-")
+        uuid = revid[at+1:fash]
+        branch_path = unescape_svn_path(revid[fash+1:])
+        revnum = int(revid[0:at])
+        assert revnum >= 0
+        return (uuid, branch_path, revnum, None, 1)
+
+
+class BzrSvnMappingv2(BzrSvnMapping):
+    @staticmethod
+    def parse_revision_id(revid):
+        revid = revid[len("svn-v2:"):]
+        at = revid.index("@")
+        fash = revid.rindex("-")
+        uuid = revid[at+1:fash]
+        branch_path = unescape_svn_path(revid[fash+1:])
+        revnum = int(revid[0:at])
+        assert revnum >= 0
+        return (uuid, branch_path, revnum, None, 2)
+
+
+class BzrSvnMappingv3(BzrSvnMapping):
+    @staticmethod
+    def parse_revision_id(revid):
+        (uuid, bp, rev, scheme) = parse_svn_revision_id(revid)
+        if scheme == "undefined":
+            scheme = None
+        return (uuid, bp, rev, scheme, 3)
+
+
+class BzrSvnMappingRegistry(registry.Registry):
+    def register(self, key, factory, help):
+        """Register a mapping between Bazaar and Subversion semantics.
+
+        The factory must be a callable that takes one parameter: the key.
+        It must produce an instance of BzrSvnMapping when called.
+        """
+        registry.Registry.register(self, key, factory, help)
+
+    def set_default(self, key):
+        """Set the 'default' key to be a clone of the supplied key.
+
+        This method must be called once and only once.
+        """
+        registry.Registry.register(self, 'default', self.get(key), 
+            self.get_help(key))
+
+mapping_registry = BzrSvnMappingRegistry()
+mapping_registry.register('v1', BzrSvnMappingv1,
+        'Original bzr-svn mapping format')
+mapping_registry.register('v2', BzrSvnMappingv2,
+        'Second format')
+mapping_registry.register('v3', BzrSvnMappingv3,
+        'Third format')
+mapping_registry.set_default('v3')
--- upgrade.py
+++ upgrade.py
@@ -20,6 +20,7 @@ from bzrlib.trace import info, mutter
 import bzrlib.ui as ui
 
 from errors import RebaseNotPresent
+from mapping import mapping_registry
 from revids import (generate_svn_revision_id, parse_svn_revision_id, 
                     MAPPING_VERSION,  unescape_svn_path)
 from scheme import BranchingScheme, guess_scheme_from_branch_path
@@ -38,29 +39,12 @@ def parse_legacy_revision_id(revid):
     :param revid: Revision id to parse
     :return: tuple with (uuid, branch_path, revision number, scheme, mapping version)
     """
-    if revid.startswith("svn-v1:"):
-        revid = revid[len("svn-v1:"):]
-        at = revid.index("@")
-        fash = revid.rindex("-")
-        uuid = revid[at+1:fash]
-        branch_path = unescape_svn_path(revid[fash+1:])
-        revnum = int(revid[0:at])
-        assert revnum >= 0
-        return (uuid, branch_path, revnum, None, 1)
-    elif revid.startswith("svn-v2:"):
-        revid = revid[len("svn-v2:"):]
-        at = revid.index("@")
-        fash = revid.rindex("-")
-        uuid = revid[at+1:fash]
-        branch_path = unescape_svn_path(revid[fash+1:])
-        revnum = int(revid[0:at])
-        assert revnum >= 0
-        return (uuid, branch_path, revnum, None, 2)
-    elif revid.startswith("svn-v3-"):
-        (uuid, bp, rev, scheme) = parse_svn_revision_id(revid)
-        if scheme == "undefined":
-            scheme = None
-        return (uuid, bp, rev, scheme, 3)
+    if revid.startswith("svn-"):
+        try:
+            mapping = mapping_registry.get(revid[len("svn-"):len("svn-vx")])
+            return mapping.parse_revision_id(revid)
+        except KeyError:
+            pass
 
     raise InvalidRevisionId(revid, None)
 
