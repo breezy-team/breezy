f9dc087d8661e1a6f96324b5626cb856a3b4dfe2
James Henstridge <james@jamesh.id.au>
2008-02-04 18:27:03 +0900

    * Remove relpath='' special case in MemoryTransport._abspath(), which   gave incorrect results for cloned transports. * Add a Transport.list_dir() test for a cloned transport.

--- bzrlib/tests/test_transport_implementations.py
+++ bzrlib/tests/test_transport_implementations.py
@@ -1066,12 +1066,12 @@ class TransportTests(TestTransportImplementation):
             self.assertRaises(TransportNotPossible, t.list_dir, '.')
             return
 
-        def sorted_list(d):
-            l = list(t.list_dir(d))
+        def sorted_list(d, transport):
+            l = list(transport.list_dir(d))
             l.sort()
             return l
 
-        self.assertEqual([], sorted_list('.'))
+        self.assertEqual([], sorted_list('.', t))
         # c2 is precisely one letter longer than c here to test that
         # suffixing is not confused.
         # a%25b checks that quoting is done consistently across transports
@@ -1083,8 +1083,13 @@ class TransportTests(TestTransportImplementation):
             self.build_tree(tree_names)
 
         self.assertEqual(
-            ['a', 'a%2525b', 'b', 'c', 'c2'], sorted_list('.'))
-        self.assertEqual(['d', 'e'], sorted_list('c'))
+            ['a', 'a%2525b', 'b', 'c', 'c2'], sorted_list('', t))
+        self.assertEqual(
+            ['a', 'a%2525b', 'b', 'c', 'c2'], sorted_list('.', t))
+        self.assertEqual(['d', 'e'], sorted_list('c', t))
+
+        # Cloning the transport produces an equivalent listing
+        self.assertEqual(['d', 'e'], sorted_list('', t.clone('c')))
 
         if not t.is_readonly():
             t.delete('c/d')
@@ -1093,12 +1098,13 @@ class TransportTests(TestTransportImplementation):
             os.unlink('c/d')
             os.unlink('b')
             
-        self.assertEqual(['a', 'a%2525b', 'c', 'c2'], sorted_list('.'))
-        self.assertEqual(['e'], sorted_list('c'))
+        self.assertEqual(['a', 'a%2525b', 'c', 'c2'], sorted_list('.', t))
+        self.assertEqual(['e'], sorted_list('c', t))
 
         self.assertListRaises(PathError, t.list_dir, 'q')
         self.assertListRaises(PathError, t.list_dir, 'c/f')
         self.assertListRaises(PathError, t.list_dir, 'a')
+
 
     def test_list_dir_result_is_url_escaped(self):
         t = self.get_transport()
