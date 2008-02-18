8fb94fae6ccfe641f1cdb2fdb82e6e270d20879c
Jeff Licquia <jeff@licquia.org>
2008-02-18 12:35:46 -0500

    Reformat setup according to proposal by Robert Collins.

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
+
 from distutils.core import setup
-setup(name="bisect",
-      version="1.0",
-      description="Git-style bisect plugin for bzr.",
-      author="Jeff Licquia",
-      author_email="jeff@licquia.org",
-      license="GNU GPL v2",
-      url="http://bzr.licquia.org/",
-      packages=["bzrlib.plugins.bisect"],
-      package_dir={"bzrlib.plugins.bisect": "."})
+
+# Queryable plugin variables, from a proposal by Robert Collins.
+
+bzr_plugin_name = 'bisect'
+bzr_plugin_version = '1.1pre'
+
+bzr_minimum_version = '0.18'
+bzr_maximum_version = None
+
+bzr_commands = [ 'bisect' ]
+
+if __name__ == "__main__":
+    setup(name=bzr_plugin_name,
+          version=bzr_plugin_version,
+          description="Git-style bisect plugin for bzr.",
+          author="Jeff Licquia",
+          author_email="jeff@licquia.org",
+          license="GNU GPL v2",
+          url="http://bzr.licquia.org/",
+          packages=["bzrlib.plugins.bisect"],
+          package_dir={"bzrlib.plugins.bisect": "."})
