fd0aa4bbd2b286a96da9716c1f056090303a627d
James Westby <jw+debian@jameswestby.net>
2008-03-13 18:37:47 +0000

    Make the inventory test match what the code produces.

--- tests/test_git_repository.py
+++ tests/test_git_repository.py
@@ -115,7 +115,8 @@ class TestGitRepositoryFeatures(tests.TestCaseInTempDir):
         builder.set_file('executable', 'content', True)
         builder.set_link('link', 'broken')
         builder.set_file('subdir/subfile', 'subdir text\n', False)
-        commit_handle = builder.commit('Joe Foo <joe@foo.com>', u'message')
+        commit_handle = builder.commit('Joe Foo <joe@foo.com>', u'message',
+            timestamp=1205433193)
         mapping = builder.finish()
         commit_id = mapping[commit_handle]
 
@@ -130,20 +131,20 @@ class TestGitRepositoryFeatures(tests.TestCaseInTempDir):
         self.assertEqualDiff(
             printed_inv,
             "('', False, InventoryDirectory('TREE_ROOT', u'', parent_id=None,"
-            " revision=None))\n"
+            " revision='git-experimental-r:69c39cfa65962f3cf16b9b3eb08a15954e9d8590'))\n"
             "(u'data', False, InventoryFile('data', u'data',"
             " parent_id='TREE_ROOT',"
-            " sha1='8e27be7d6154a1f68ea9160ef0e18691d20560dc', len=None))\n"
+            " sha1='aa785adca3fcdfe1884ae840e13c6d294a2414e8', len=5))\n"
             "(u'executable', True, InventoryFile('executable', u'executable',"
             " parent_id='TREE_ROOT',"
-            " sha1='6b584e8ece562ebffc15d38808cd6b98fc3d97ea', len=None))\n"
+            " sha1='040f06fd774092478d450774f5ba30c5da78acc8', len=7))\n"
             "(u'link', False, InventoryLink('link', u'link',"
-            " parent_id='TREE_ROOT', revision=None))\n"
+            " parent_id='TREE_ROOT', revision='git-experimental-r:69c39cfa65962f3cf16b9b3eb08a15954e9d8590'))\n"
             "(u'subdir', False, InventoryDirectory('subdir', u'subdir',"
-            " parent_id='TREE_ROOT', revision=None))\n"
+            " parent_id='TREE_ROOT', revision='git-experimental-r:69c39cfa65962f3cf16b9b3eb08a15954e9d8590'))\n"
             "(u'subdir/subfile', False, InventoryFile('subdir/subfile',"
             " u'subfile', parent_id='subdir',"
-            " sha1='0ddb53cbe2dd209f550dd8d7f1287a5ed9b1ee8b', len=None))")
+            " sha1='67b75c3e49f31fcadddbf9df6a1d8be8c3e44290', len=12))")
 
 
 class MemoryGitRepository(git_repository.GitRepository):
