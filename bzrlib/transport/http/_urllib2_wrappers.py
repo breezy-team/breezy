97f3fac1c40e008c761c6e5ed566fe82b9f07595
Andrew Bennetts <andrew.bennetts@canonical.com>
2008-03-16 14:53:09 -0500

    A hack to make urllib not call recv(1) lots and lots.

--- bzrlib/transport/http/_urllib2_wrappers.py
+++ bzrlib/transport/http/_urllib2_wrappers.py
@@ -81,6 +81,18 @@ class Response(httplib.HTTPResponse):
     # 8k chunks should be fine.
     _discarded_buf_size = 8192
 
+    def __init__(self, *args, **kwargs):
+        httplib.HTTPResponse.__init__(self, *args, **kwargs)
+        try:
+            # httplib creates a fileobject that doesn't do buffering, which
+            # makes fp.readline() very expensive because it only reads one byte
+            # at a time.  So we change the _rbufsize attribute here to fix
+            # this.
+            self.fp._rbufsize = 8192
+        except AttributeError:
+            # If self.fp is a cStringIO, it doesn't have a _rbufsize.
+            pass
+
     def begin(self):
         """Begin to read the response from the server.
 
@@ -534,7 +546,7 @@ class AbstractHTTPHandler(urllib2.AbstractHTTPHandler):
             req = request
             r = response
             r.recv = r.read
-            fp = socket._fileobject(r)
+            fp = socket._fileobject(r, bufsize=8192)
             resp = urllib2.addinfourl(fp, r.msg, req.get_full_url())
             resp.code = r.status
             resp.msg = r.reason
