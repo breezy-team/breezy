3fa334dd5c7c09b35dc69f85630e99717d053d5e
Jelmer Vernooij <jelmer@samba.org>
2008-03-24 02:12:19 +0100

    Allow exporting the commit message from a mapping.

--- commit.py
+++ commit.py
@@ -142,7 +142,7 @@ class SvnCommitBuilder(RootCommitBuilder):
             base_branch_props = {}
         else:
             base_branch_props = lazy_dict(lambda: self.repository.branchprop_list.get_properties(self.base_path, self.base_revnum))
-        (self._svn_revprops, self._svnprops) = self.base_mapping.export_revision(self.branch.get_branch_path(), timestamp, timezone, committer, revprops, revision_id, self.base_revno+1, merges, base_branch_props)
+        (self._svn_revprops, self._svnprops) = self.base_mapping.export_revision(self.branch.get_branch_path(), None, timestamp, timezone, committer, revprops, revision_id, self.base_revno+1, merges, base_branch_props)
 
         if len(merges) > 0:
             old_svk_features = parse_svk_features(base_branch_props.get(SVN_PROP_SVK_MERGE, ""))
