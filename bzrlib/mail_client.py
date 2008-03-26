de1aaa7bde60b623b9babe6cc845de8cd46cf054
Xavier Maillard <xma@gnu.org>
2008-03-26 07:21:48 +0100

    Add mail-mode GNU Emacs mail package as a mail_client option.
    Idea and original code by Bojan Nikolic modified in this way:
        
    1. define a different python class EmacsMailMode
    2. use mail-mode package instead of message-mode. mail-mode is the
       default mail mode in GNU Emacs.
    3. put the patch as a MIME attachment (see NOTE)
    4. add tests as required to fulfil merge strategy requirements
        
    To use this option, just put these lines into ~/.bazaar/bazaar.conf
        
    [DEFAULT]
    mail_client = emacs-mailmode
    
    NOTE: this will work only for GNU Emacs version from 22.1 and superior.
          The MIME attachment is handled by etach[1] package. Just put
          etach.el into your `load-path'. GNU Emacs is not shipped with a
          simple MIME package thus the need of a simple and reliable
          alternative (etach does both reading and writing of MIME objects).
    
    [1] http://rulnick.com/etach/download

--- bzrlib/mail_client.py
+++ bzrlib/mail_client.py
@@ -306,6 +306,37 @@ class XDGEmail(ExternalMailClient):
         return commandline
 
 
+class EmacsMailMode (ExternalMailClient):
+    """Call emacsclient in mail-mode. This only work for emacs >= 22.1"""
+    _client_commands = ['emacsclient']
+
+    def _get_compose_commandline (self, to, subject, attach_path):
+        commandline = ["--eval"]
+        #Ensure we can at least have an empty mail-mode buffer
+        _to = "nil"
+        _subject = "nil"
+
+        if to is not None:
+            _to = ("\"%s\"" % self._encode_safe(to))
+        if subject is not None:
+            _subject = ("\"%s\"" % self._encode_safe(subject))
+        mmform = "(mail nil %s %s)" %(_to ,_subject)
+
+        # call mail-mode, move the point to body and insert a new blank line
+        # we *must* force this point movement for the case when To is not passed
+        # with --mail-to. Without this, the patch could be inserted at the wrong place
+
+        commandline.append(mmform)
+        commandline.append("(mail-text)")
+        commandline.append("(newline)")
+
+        # ... and put a MIME attachment (if any)
+        if attach_path is not None:
+            ifform = "(attach \"%s\")" % self._encode_path(attach_path,'attachment')
+            commandline.append(ifform)
+        return commandline
+
+
 class MAPIClient(ExternalMailClient):
     """Default Windows mail client launched using MAPI."""
 
