b900d97648727eb8f6fa943679eed27b6ed2f94b
John Arbash Meinel <john@arbash-meinel.com>
2008-04-18 15:30:59 -0500

    bzr log bzr:// was failing at the first revision because it was trying to

--- bzrlib/remote.py
+++ bzrlib/remote.py
@@ -859,7 +859,8 @@ class RemoteRepository(object):
         body = self._serialise_search_recipe(recipe)
         path = self.bzrdir._path_for_remote_call(self._client)
         for key in keys:
-            assert type(key) is str
+            assert type(key) is str, \
+                "Key type is %s not a plain string" % (type(key),)
         verb = 'Repository.get_parent_map'
         args = (path,) + tuple(keys)
         try:
