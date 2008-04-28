b69a219a836e650f5efa4e48864f0aa262afc6cb
Aaron Bentley <aaron@aaronbentley.com>
2008-04-28 00:00:05 -0400

    Fix normal revision installation

--- bzrlib/bundle/serializer/v4.py
+++ bzrlib/bundle/serializer/v4.py
@@ -590,8 +590,9 @@ class RevisionInstaller(object):
             return
         if self._info['serializer'] == self._repository._serializer.format_num:
             self._repository._add_revision_text(revision_id, text)
-        revision = self._source_serializer.read_revision_from_string(text)
-        self._repository.add_revision(revision.revision_id, revision)
+        else:
+            revision = self._source_serializer.read_revision_from_string(text)
+            self._repository.add_revision(revision.revision_id, revision)
 
     def _install_signature(self, revision_id, metadata, text):
         transaction = self._repository.get_transaction()
