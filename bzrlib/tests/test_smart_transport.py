f9eef47a075226cd58d765dc82302c8b572aeeb4
Andrew Bennetts <andrew.bennetts@canonical.com>
2008-05-08 00:40:59 +1000

    Improve documentation of MockMedium.

--- bzrlib/tests/test_smart_transport.py
+++ bzrlib/tests/test_smart_transport.py
@@ -2517,14 +2517,53 @@ class TestSmartClientUnicode(tests.TestCase):
 
 
 class MockMedium(object):
+    """A mock medium that can be used to test _SmartClient.
+    
+    It can be given a series of requests to expect (and responses it should
+    return for them).  It can also be told when the client is expected to
+    disconnect a medium.  Expectations must be satisfied in the order they are
+    given, or else an AssertionError will be raised.
 
-    def __init__(self, *responses):
+    Typical use looks like::
+
+        medium = MockMedium()
+        medium.expect_request(...)
+        medium.expect_request(...)
+        medium.expect_request(...)
+    """
+
+    def __init__(self):
         self.base = 'dummy base'
-        self._mock_request = MockMediumRequest(self)
+        self._mock_request = _MockMediumRequest(self)
         self._expected_events = []
         
     def expect_request(self, request_bytes, response_bytes,
                        allow_partial_read=False):
+        """Expect 'request_bytes' to be sent, and reply with 'response_bytes'.
+
+        No assumption is made about how many times accept_bytes should be
+        called to send the request.  Similarly, no assumption is made about how
+        many times read_bytes/read_line are called by protocol code to read a
+        response.  e.g.::
+        
+            request.accept_bytes('ab')
+            request.accept_bytes('cd')
+            request.finished_writing()
+
+        and::
+        
+            request.accept_bytes('abcd')
+            request.finished_writing()
+
+        Will both satisfy ``medium.expect_request('abcd', ...)``.  Thus tests
+        using this should not break due to irrelevant changes in protocol
+        implementations.
+
+        :param allow_partial_read: if True, no assertion is raised if a
+            response is not fully read.  Setting this is useful when the client
+            is expected to disconnect without needing to read the complete
+            response.  Default is False.
+        """
         self._expected_events.append(('send request', request_bytes))
         if allow_partial_read:
             self._expected_events.append(
@@ -2533,9 +2572,16 @@ class MockMedium(object):
             self._expected_events.append(('read response', response_bytes))
 
     def expect_disconnect(self):
+        """Expect the client to call ``medium.disconnect()``."""
         self._expected_events.append('disconnect')
 
-    def assertEvent(self, observed_event):
+    def _assertEvent(self, observed_event):
+        """Raise AssertionError unless observed_event matches the next expected
+        event.
+
+        :seealso: expect_request
+        :seealso: expect_disconnect
+        """
         try:
             expected_event = self._expected_events.pop(0)
         except IndexError:
@@ -2561,12 +2607,13 @@ class MockMedium(object):
 
     def disconnect(self):
         if self._mock_request._read_bytes:
-            self.assertEvent(('read response', self._mock_request._read_bytes))
+            self._assertEvent(('read response', self._mock_request._read_bytes))
             self._mock_request._read_bytes = ''
-        self.assertEvent('disconnect')
+        self._assertEvent('disconnect')
 
 
-class MockMediumRequest(object):
+class _MockMediumRequest(object):
+    """A mock ClientMediumRequest used by MockMedium."""
 
     def __init__(self, mock_medium):
         self._medium = mock_medium
@@ -2578,11 +2625,11 @@ class MockMediumRequest(object):
         self._written_bytes += bytes
 
     def finished_writing(self):
-        self._medium.assertEvent(('send request', self._written_bytes))
+        self._medium._assertEvent(('send request', self._written_bytes))
         self._written_bytes = ''
 
     def finished_reading(self):
-        self._medium.assertEvent(('read response', self._read_bytes))
+        self._medium._assertEvent(('read response', self._read_bytes))
         self._read_bytes = ''
 
     def read_bytes(self, size):
