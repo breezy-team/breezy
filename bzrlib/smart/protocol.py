0184c0e3034c45eab9c659fdd14d9462bec6dd4c
Andrew Bennetts <andrew.bennetts@canonical.com>
2008-05-21 12:34:32 +1000

    Buffer encoding of v3 messages to minimise write/send calls.  Doubles the speed of pushing over TCP with 500ms latency loopback.

--- bzrlib/smart/protocol.py
+++ bzrlib/smart/protocol.py
@@ -1014,7 +1014,27 @@ class _ProtocolThreeEncoder(object):
     response_marker = request_marker = MESSAGE_VERSION_THREE
 
     def __init__(self, write_func):
-        self._write_func = write_func
+        self.start_buffering()
+        self._real_write_func = write_func
+
+    def _write_func(self, bytes):
+        if self.buffering:
+            self._buf += bytes
+        else:
+            self._real_write_func(bytes)
+
+    def flush(self):
+        if self._buf:
+            self._real_write_func(self._buf)
+            self._buf = ''
+
+    def start_buffering(self):
+        self.buffering = True
+        self._buf = ''
+
+    def stop_buffering(self):
+        self.flush()
+        self.buffering = False
 
     def _serialise_offsets(self, offsets):
         """Serialise a readv offset list."""
@@ -1046,6 +1066,7 @@ class _ProtocolThreeEncoder(object):
 
     def _write_end(self):
         self._write_func('e')
+        self.stop_buffering()
 
     def _write_prefixed_body(self, bytes):
         self._write_func('b')
@@ -1101,6 +1122,7 @@ class ProtocolThreeResponder(_ProtocolThreeEncoder):
         elif response.body_stream is not None:
             for chunk in response.body_stream:
                 self._write_prefixed_body(chunk)
+                self.flush()
         self._write_end()
         
 
