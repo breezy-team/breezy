c2a5bda09689988d6531011dea0b81bbf58f5a55
Andrew Bennetts <andrew.bennetts@canonical.com>
2008-05-22 17:03:44 +1000

    Docstring/NEWS tweaks requested by Ian's review.

--- bzrlib/transport/http/response.py
+++ bzrlib/transport/http/response.py
@@ -181,13 +181,15 @@ class RangeFile(object):
         self.read_range_definition()
 
     def read(self, size=-1):
-        """Read size bytes from the current position in the file.  Use size=-1
-        to read to EOF.
+        """Read size bytes from the current position in the file.
 
         Reading across ranges is not supported. We rely on the underlying http
         client to clean the socket if we leave bytes unread. This may occur for
         the final boundary line of a multipart response or for any range
         request not entirely consumed by the client (due to offset coalescing)
+
+        :param size:  The number of bytes to read.  Leave unspecified or pass
+            -1 to read to EOF.
         """
         if (self._size > 0
             and self._pos == self._start + self._size):
