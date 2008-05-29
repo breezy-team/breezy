7b7e2a74814dcb70b8f142d68f59767513efd608
Andrew Bennetts <andrew.bennetts@canonical.com>
2008-05-29 16:43:43 +1000

    Better infrastructure on SmartClientMedium for tracking the remote version.

--- bzrlib/tests/test_remote.py
+++ bzrlib/tests/test_remote.py
@@ -187,9 +187,8 @@ class FakeClient(_SmartClient):
 class FakeMedium(medium.SmartClientMedium):
 
     def __init__(self, client_calls, base):
-        self._remote_is_at_least_1_2 = True
+        medium.SmartClientMedium.__init__(self, base)
         self._client_calls = client_calls
-        self.base = base
 
     def disconnect(self):
         self._client_calls.append(('disconnect medium',))
@@ -252,6 +251,32 @@ class Test_ClientMedium_remote_path_from_transport(tests.TestCase):
                 '../xyz/', scheme + '//host/path', '../xyz/')
             self.assertRemotePathHTTP(
                 'xyz/', scheme + '//host/path', 'xyz/')
+
+
+class Test_ClientMedium_remote_is_at_least(tests.TestCase):
+    """Tests for the behaviour of client_medium.remote_is_at_least."""
+
+    def test_initially_unlimited(self):
+        """A fresh medium assumes that the remote side supports all
+        versions.
+        """
+        client_medium = medium.SmartClientMedium('dummy base')
+        self.assertTrue(client_medium._is_remote_at_least((99, 99)))
+    
+    def test__remote_is_not(self):
+        """Calling _remote_is_not ratchets down the known remote version."""
+        client_medium = medium.SmartClientMedium('dummy base')
+        # Mark the remote side as being less than 1.6.  The remote side may
+        # still be 1.5.
+        client_medium._remote_is_not((1, 6))
+        self.assertFalse(client_medium._is_remote_at_least((1, 6)))
+        self.assertTrue(client_medium._is_remote_at_least((1, 5)))
+        # Calling _remote_is_not again with a lower value works.
+        client_medium._remote_is_not((1, 5))
+        self.assertFalse(client_medium._is_remote_at_least((1, 5)))
+        # You cannot call _remote_is_not with a larger value.
+        self.assertRaises(
+            AssertionError, client_medium._remote_is_not, (1, 9))
 
 
 class TestBzrDirOpenBranch(tests.TestCase):
@@ -883,7 +908,7 @@ class TestRepositoryGetParentMap(TestRemoteRepository):
         repo, client = self.setup_fake_client_and_repository(transport_path)
         client.add_unknown_method_response('Repository,get_parent_map')
         client.add_success_response_with_body('', 'ok')
-        self.assertTrue(client._medium._remote_is_at_least_1_2)
+        self.assertTrue(client._medium._is_remote_at_least((1, 2)))
         rev_id = 'revision-id'
         expected_deprecations = [
             'bzrlib.remote.RemoteRepository.get_revision_graph was deprecated '
@@ -898,7 +923,7 @@ class TestRepositoryGetParentMap(TestRemoteRepository):
               ('quack/', ''))],
             client._calls)
         # The medium is now marked as being connected to an older server
-        self.assertFalse(client._medium._remote_is_at_least_1_2)
+        self.assertFalse(client._medium._is_remote_at_least((1, 2)))
 
     def test_get_parent_map_fallback_parentless_node(self):
         """get_parent_map falls back to get_revision_graph on old servers.  The
@@ -915,7 +940,7 @@ class TestRepositoryGetParentMap(TestRemoteRepository):
         transport_path = 'quack'
         repo, client = self.setup_fake_client_and_repository(transport_path)
         client.add_success_response_with_body(rev_id, 'ok')
-        client._medium._remote_is_at_least_1_2 = False
+        client._medium._remote_is_not((1, 2))
         expected_deprecations = [
             'bzrlib.remote.RemoteRepository.get_revision_graph was deprecated '
             'in version 1.4.']
