f16c8a494b57cfa09a3add6ca4a9ead0436d7d00
Pieter de Bie <pdebie@ai.rug.nl>
2008-05-30 14:55:15 +0200

    Classify bzr-fast-export
    This file was getting a bit messy with all the global variables and parameter
    passing.
    
    This commit changes bzr-fast-export to be a real class. This simplifies
    various methods and will allow easier hacking in the future.
    
    There is a single change, though: the next_available_branch_name has been
    simplified to just return a temporary branch name, as they aren't used for
    anything else.

--- exporters/bzr-fast-export.py
+++ exporters/bzr-fast-export.py
@@ -49,230 +49,219 @@ from email.Utils import quote, parseaddr
 import bzrlib.branch
 import bzrlib.revision
 
-REVID_TO_MARK = {}
+class BzrFastExporter:
 
-def main():
-    options, arguments = parse_options()
+    def __init__(self, options):
+        self.options = options
+        self.revid_to_mark = {}
+        self.branch_names = {}
+                
+        if options.import_marks:
+            self.import_marks()
+        
+        self.branch = bzrlib.branch.Branch.open_containing(options.repo)[0]
+        self.branch.repository.lock_read()
+        self.revmap = self.branch.get_revision_id_to_revno_map()
+            
+    def run(self):
+        try:
+            for revid in self.branch.revision_history():
+                if revid not in self.revid_to_mark:
+                    self.emit_commit(revid, options.git_branch)
+        finally:
+            self.branch.repository.unlock()
 
-    if options.import_marks:
-        branches, revid_to_mark = import_marks(options.import_marks)
-        _branch_names.update(branches)
-        REVID_TO_MARK.update(revid_to_mark)
+        if self.branch.supports_tags():
+            self.emit_tags()
 
-    branch = bzrlib.branch.Branch.open_containing(arguments[0])[0]
+        ##
+        self.export_marks()
 
-    branch.repository.lock_read()
-    try:
-        for revid in branch.revision_history():
-            if revid not in REVID_TO_MARK:
-                emit_commit(revid, branch, options.git_branch)
-    finally:
-        branch.repository.unlock()
+    def emit_commit(self, revid, git_branch):
+        revobj = self.branch.repository.get_revision(revid)
+        mark = self.revid_to_mark[revid] = len(self.revid_to_mark) + 1
+        stream = 'commit refs/heads/%s\nmark :%d\n' % (git_branch, mark)
 
-    if branch.supports_tags():
-        emit_tags(branch)
+        rawdate = '%d %s' % (int(revobj.timestamp), '%+03d%02d' % (
+                    revobj.timezone / 3600, (revobj.timezone / 60) % 60))
 
-    if options.export_marks:
-        export_marks(options.export_marks, _branch_names, REVID_TO_MARK)
+        author = revobj.get_apparent_author()
+        if author != revobj.committer:
+            stream += 'author %s %s\n' % (
+                self.name_with_angle_brackets(author), rawdate)
 
-##
+        stream += 'committer %s %s\n' % (
+                self.name_with_angle_brackets(revobj.committer), rawdate)
 
-def emit_commit(revid, branch, git_branch):
-    revobj = branch.repository.get_revision(revid)
-    mark = REVID_TO_MARK[revid] = len(REVID_TO_MARK) + 1
-    stream = 'commit refs/heads/%s\nmark :%d\n' % (git_branch, mark)
-
-    rawdate = '%d %s' % (int(revobj.timestamp), '%+03d%02d' % (
-                revobj.timezone / 3600, (revobj.timezone / 60) % 60))
-
-    author = revobj.get_apparent_author()
-    if author != revobj.committer:
-        stream += 'author %s %s\n' % (
-                name_with_angle_brackets(author), rawdate)
-
-    stream += 'committer %s %s\n' % (
-            name_with_angle_brackets(revobj.committer), rawdate)
-
-    stream += 'data %d\n%s\n' % (
+        stream += 'data %d\n%s\n' % (
             len(revobj.message.encode('utf-8')), revobj.message)
 
-    nparents = len(revobj.parent_ids)
+        nparents = len(revobj.parent_ids)
 
-    if nparents >= 1:
-        parent = revobj.parent_ids[0]
-        stream += 'from :%d\n' % (REVID_TO_MARK[parent],)
-        if nparents == 2:
-            pending = []
-            current = revobj.parent_ids[1]
-            new_git_branch = git_branch
-            while current not in REVID_TO_MARK:
-                pending.append(current)
-                r = branch.repository.get_revision(current)
-                if r.parent_ids:
-                    current = r.parent_ids[0]
-                else:
-                    # user did bzr merge -r 0..-1
-                    new_git_branch = next_available_branch_name(
-                            r.properties.get('branch-nick', None))
-                    break
-            for r in reversed(pending):
-                emit_commit(r, branch, git_branch=new_git_branch)
+        if nparents >= 1:
+            parent = revobj.parent_ids[0]
+            stream += 'from :%d\n' % (self.revid_to_mark[parent],)
+            if nparents == 2:
+                pending = []
+                current = revobj.parent_ids[1]
+                new_git_branch = git_branch
+                while current not in self.revid_to_mark:
+                    pending.append(current)
+                    r = self.branch.repository.get_revision(current)
+                    if r.parent_ids:
+                        current = r.parent_ids[0]
+                    else:
+                        # user did bzr merge -r 0..-1
+                        new_git_branch = self.next_available_branch_name()
+                        break
+                for r in reversed(pending):
+                    self.emit_commit(r, git_branch=new_git_branch)
+        
+                stream += 'merge :%d\n' % (self.revid_to_mark[revobj.parent_ids[1]],)
+        else:
+            parent = bzrlib.revision.NULL_REVISION
+          
+        sys.stdout.write(stream.encode('utf-8'))
 
-            stream += 'merge :%d\n' % (REVID_TO_MARK[revobj.parent_ids[1]],)
-    else:
-        parent = bzrlib.revision.NULL_REVISION
+        ##
 
-    sys.stdout.write(stream.encode('utf-8'))
+        tree_old = self.branch.repository.revision_tree(parent)
+        tree_new = self.branch.repository.revision_tree(revobj.revision_id)
+
+        changes = tree_new.changes_from(tree_old)
+
+        # make "modified" have 3-tuples, as added does
+        my_modified = [ x[0:3] for x in changes.modified ]
+
+        for (oldpath, newpath, id_, kind,
+                text_modified, meta_modified) in changes.renamed:
+            sys.stdout.write('R %s %s\n' % (self.my_quote(oldpath, True),
+                                                    self.my_quote(newpath)))
+            if text_modified or meta_modified:
+                my_modified.append((newpath, id_, kind))
+
+        for path, id_, kind in changes.removed:
+            sys.stdout.write('D %s\n' % (self.my_quote(path),))
+
+        for path, id_, kind1, kind2 in changes.kind_changed:
+            sys.stdout.write('D %s\n' % (self.my_quote(path),))
+            my_modified.append((path, id_, kind2))
+
+        for path, id_, kind in changes.added + my_modified:
+            if kind in ('file', 'symlink'):
+                entry = tree_new.inventory[id_]
+                if kind == 'file':
+                    mode = entry.executable and '755' or '644'
+                    text = tree_new.get_file_text(id_)
+                else: # symlink
+                    mode = '120000'
+                    text = entry.symlink_target
+            else:
+                continue
+
+            sys.stdout.write('M %s inline %s\n' % (mode, self.my_quote(path)))
+            sys.stdout.write('data %d\n%s\n' % (len(text), text))
+
+    def emit_tags(self):
+        for tag, revid in self.branch.tags.get_tag_dict().items():
+            try:
+                mark = self.revid_to_mark[revid]
+            except KeyError:
+                print >>sys.stderr, \
+                    'W: not creating tag %r pointing to non-existant revision %s' % (
+                            tag, revid)
+            else:
+                # According to git-fast-import(1), the extra LF is optional here;
+                # however, versions of git up to 1.5.4.3 had a bug by which the LF
+                # was needed. Always emit it, since it doesn't hurt and maintains
+                # compatibility with older versions.
+                # http://git.kernel.org/?p=git/git.git;a=commit;h=655e8515f279c01f525745d443f509f97cd805ab
+                sys.stdout.write('reset refs/tags/%s\nfrom :%d\n\n' % (
+                    tag, mark))
 
     ##
 
-    tree_old = branch.repository.revision_tree(parent)
-    tree_new = branch.repository.revision_tree(revobj.revision_id)
-    changes = tree_new.changes_from(tree_old)
+    def my_quote(self, string, quote_spaces=False):
+        """Encode path in UTF-8 and quote it if necessary.
 
-    # make "modified" have 3-tuples, as added does
-    my_modified = [ x[0:3] for x in changes.modified ]
-
-    for (oldpath, newpath, id_, kind,
-            text_modified, meta_modified) in changes.renamed:
-        sys.stdout.write('R %s %s\n' % (my_quote(oldpath, True),
-                                                my_quote(newpath)))
-        if text_modified or meta_modified:
-            my_modified.append((newpath, id_, kind))
-
-    for path, id_, kind in changes.removed:
-        sys.stdout.write('D %s\n' % (my_quote(path),))
-
-    for path, id_, kind1, kind2 in changes.kind_changed:
-        sys.stdout.write('D %s\n' % (my_quote(path),))
-        my_modified.append((path, id_, kind2))
-
-    for path, id_, kind in changes.added + my_modified:
-        if kind in ('file', 'symlink'):
-            entry = tree_new.inventory[id_]
-            if kind == 'file':
-                mode = entry.executable and '755' or '644'
-                text = tree_new.get_file_text(id_)
-            else: # symlink
-                mode = '120000'
-                text = entry.symlink_target
+        A quote is needed if path starts with a quote character ("). If
+        :param quote_spaces: is True, the path will be quoted if it contains any
+        space (' ') characters.
+        """
+        # TODO: escape LF
+        string = string.encode('utf-8')
+        if string.startswith('"') or quote_spaces and ' ' in string:
+            return '"%s"%s' % (quote(string),
+                    GIT_FAST_IMPORT_NEEDS_EXTRA_SPACE_AFTER_QUOTE and ' ' or '')
         else:
-            continue
+            return string
 
-        sys.stdout.write('M %s inline %s\n' % (mode, my_quote(path)))
-        sys.stdout.write('data %d\n%s\n' % (len(text), text))
-
-def emit_tags(branch):
-    for tag, revid in branch.tags.get_tag_dict().items():
-        try:
-            mark = REVID_TO_MARK[revid]
-        except KeyError:
-            print >>sys.stderr, \
-                'W: not creating tag %r pointing to non-existant revision %s' % (
-                        tag, revid)
+    def name_with_angle_brackets(self, string):
+        """Ensure there is a part with angle brackets in string."""
+        name, email = parseaddr(string)
+        if not name:
+            if '@' in email or '<' in string:
+                return '<%s>' % (email,)
+            else:
+                return '%s <>' % (string,)
         else:
-            # According to git-fast-import(1), the extra LF is optional here;
-            # however, versions of git up to 1.5.4.3 had a bug by which the LF
-            # was needed. Always emit it, since it doesn't hurt and maintains
-            # compatibility with older versions.
-            # http://git.kernel.org/?p=git/git.git;a=commit;h=655e8515f279c01f525745d443f509f97cd805ab
-            sys.stdout.write('reset refs/tags/%s\nfrom :%d\n\n' % (
-                tag, mark))
+            return '%s <%s>' % (name, email)
 
-##
-
-def my_quote(string, quote_spaces=False):
-    """Encode path in UTF-8 and quote it if necessary.
-
-    A quote is needed if path starts with a quote character ("). If
-    :param quote_spaces: is True, the path will be quoted if it contains any
-    space (' ') characters.
-    """
-    # TODO: escape LF
-    string = string.encode('utf-8')
-    if string.startswith('"') or quote_spaces and ' ' in string:
-        return '"%s"%s' % (quote(string),
-                GIT_FAST_IMPORT_NEEDS_EXTRA_SPACE_AFTER_QUOTE and ' ' or '')
-    else:
-        return string
-
-def name_with_angle_brackets(string):
-    """Ensure there is a part with angle brackets in string."""
-    name, email = parseaddr(string)
-    if not name:
-        if '@' in email or '<' in string:
-            return '<%s>' % (email,)
-        else:
-            return '%s <>' % (string,)
-    else:
-        return '%s <%s>' % (name, email)
-
-def next_available_branch_name(prefix=None):
-    """Return an unique branch name.
-
-    The returned name will start with :param prefix:, with an extra integer if
-    needed for uniqueness.
-
-    If prefix is None, the name will start with "tmp".
-    """
-    if prefix is None:
+    def next_available_branch_name(self):
+        """Return an unique branch name. The name will start with "tmp".
+        """
         prefix = 'tmp'
 
-    if prefix not in _branch_names:
-        _branch_names[prefix] = 0
-    else:
-        _branch_names[prefix] += 1
-        prefix = '%s.%d' % (prefix, _branch_names[prefix])
+        if prefix not in self.branch_names:
+            self.branch_names[prefix] = 0
+        else:
+            self.branch_names[prefix] += 1
+            prefix = '%s.%d' % (prefix, self.branch_names[prefix])
 
-    return prefix
+        return prefix
 
-_branch_names = {}
+    def export_marks(self):
+        if not self.options.export_marks:
+            return
+            
+        f = file(self.options.export_marks, 'w')
+        f.write('format=1\n')
 
-##
+        branch_names = [ '%s.%d' % x for x in self.branch_names.iteritems() ]
+        f.write('\0'.join(branch_names) + '\n')
 
-def export_marks(filename, branch_names_dict, revid_to_mark_dict):
-    f = file(filename, 'w')
-    f.write('format=1\n')
+        for mark, revid in sorted((y, x)
+                for x, y in self.revid_to_mark.iteritems()):
+            f.write(':%d %s\n' % (mark, revid))
 
-    branch_names = [ '%s.%d' % x for x in branch_names_dict.iteritems() ]
-    f.write('\0'.join(branch_names) + '\n')
+        f.close()
 
-    for mark, revid in sorted((y, x)
-            for x, y in revid_to_mark_dict.iteritems()):
-        f.write(':%d %s\n' % (mark, revid))
+    def import_marks(self):
+        f = file(options.import_marks)
 
-    f.close()
+        firstline = f.readline()
+        match = re.match(r'^format=(\d+)$', firstline)
 
-def import_marks(filename):
-    f = file(filename)
-    firstline = f.readline()
-    match = re.match(r'^format=(\d+)$', firstline)
+        if not match:
+            print >>sys.stderr, "%r doesn't look like a mark file" % (filename,)
+            sys.exit(1)
+        elif match.group(1) != '1':
+            print >>sys.stderr, 'format version in mark file not supported'
+            sys.exit(1)
 
-    if not match:
-        print >>sys.stderr, "%r doesn't look like a mark file" % (filename,)
-        sys.exit(1)
-    elif match.group(1) != '1':
-        print >>sys.stderr, 'format version in mark file not supported'
-        sys.exit(1)
 
-    branch_names = {}
 
-    for string in f.readline().rstrip('\n').split('\0'):
-        if not string:
-            continue
-        name, integer = string.rsplit('.', 1)
-        branch_names[name] = int(integer)
+        for string in f.readline().rstrip('\n').split('\0'):
+            if not string:
+                continue
+            name, integer = string.rsplit('.', 1)
+            self.branch_names[name] = int(integer)
 
-    revid_to_mark = {}
-
-    for line in f:
-        line = line.rstrip('\n')
-        mark, revid = line.split(' ', 1)
-        mark = mark[1:] # strip colon
-        revid_to_mark[revid] = int(mark)
-
-    return branch_names, revid_to_mark
-
+        for line in f:
+            line = line.rstrip('\n')
+            mark, revid = line.split(' ', 1)
+            mark = mark[1:] # strip colon
+            self.revid_to_mark[revid] = int(mark)
 ##
 
 def parse_options():
@@ -291,10 +280,13 @@ def parse_options():
 
     if len(args) != 1:
         p.error('need a branch to export')
+    options.repo = args
 
     return options, args
 
 ##
 
 if __name__ == '__main__':
-    sys.exit(main())
+    options, arguments = parse_options()
+    exporter = BzrFastExporter(options)
+    sys.exit(exporter.run())
