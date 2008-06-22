dd3567a73a50426f70f0e9aeeb472972dc490fd4
Jelmer Vernooij <jelmer@samba.org>
2008-06-22 21:15:37 +0200

    Move core.c functionality to python.

---
+++ core.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2006-2008 Jelmer Vernooij <jelmer@samba.org>
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+NODE_DIR = 2
+NODE_FILE = 1
+NODE_NONE = 0
+NODE_UNKNOWN = 3
+
+class SubversionException(Exception):
+    """A Subversion exception"""
