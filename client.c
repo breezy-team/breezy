2ad999719e697c76cf3e00082445b26b37a1a1f1
Jelmer Vernooij <jelmer@samba.org>
2008-06-24 16:24:09 +0200

    Fix another hardcoded pointer size.

--- client.c
+++ client.c
@@ -261,7 +261,7 @@ static int client_set_auth(PyObject *self, PyObject *auth, void *closure)
 	Py_XDECREF(client->py_auth);
 
 	if (auth == Py_None) {
-		auth_providers = apr_array_make(client->pool, 0, 4);
+		auth_providers = apr_array_make(client->pool, 0, sizeof(svn_auth_provider_object_t *));
 		if (auth_providers == NULL) {
 			PyErr_NoMemory();
 			return 1;
