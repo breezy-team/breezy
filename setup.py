dc6ae19e54b51e2962ef1e73a62a5a13cf21891b
Ian Clatworthy <ian.clatworthy@canonical.com>
2008-07-22 16:01:06 +1000

    added NEWS and setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2.4
+from distutils.core import setup
+
+bzr_plugin_name = 'fastimport'
+
+bzr_plugin_version = (0, 6, 0, 'dev', 0)
+bzr_minimum_version = (1, 0, 0)
+bzr_maximum_version = None
+
+if __name__ == 'main':
+    setup(name="fastimport",
+          version="0.6.0dev0",
+          description="fastimport format import into and export from Bazaar.",
+          author="Canonical Ltd",
+          author_email="bazaar@lists.canonical.com",
+          license = "GNU GPL v2",
+          url="https://launchpad.net/bzr-fastimport",
+          packages=['bzrlib.plugins.fastimport',
+                    'bzrlib.plugins.fastimport.processors',
+                    'bzrlib.plugins.fastimport.tests',
+                    ],
+          package_dir={'bzrlib.plugins.fastimport': '.'})
