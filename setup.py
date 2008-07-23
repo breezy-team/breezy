5eb816f16b7b9e06e616f4dcf20eacae1f195188
Ian Clatworthy <ian.clatworthy@canonical.com>
2008-07-23 10:58:41 +1000

    release 0.6

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 
 bzr_plugin_name = 'fastimport'
 
-bzr_plugin_version = (0, 6, 0, 'dev', 0)
+bzr_plugin_version = (0, 6, 0, 'final', 0)
 bzr_minimum_version = (1, 1, 0)
 bzr_maximum_version = None
 
 if __name__ == 'main':
     setup(name="fastimport",
-          version="0.6.0dev0",
-          description="fastimport format import into and export from Bazaar.",
+          version="0.6.0final0",
+          description="stream-based import into and export from Bazaar.",
           author="Canonical Ltd",
           author_email="bazaar@lists.canonical.com",
           license = "GNU GPL v2",
