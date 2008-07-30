488ece896994b932f82ded7cd97d51c31c5e81db
Mark Hammond <mhammond@skippinet.com.au>
2008-07-30 17:14:17 +1000

    remove unneeded file.

--- build.ps1
+++
@@ -1,9 +0,0 @@
-
-set-item env:LIB -value "C:\Program Files\Microsoft Visual C++ Toolkit 2003\lib"
-set-item env:INCLUDE -value "C:\Program Files\Microsoft Visual C++ Toolkit 2003\include"
-
-rm -r -for build
-
-python setup.py build_ext
-
-cp -for build\lib.win32-2.5\bzrlib\plugins\svn\*.pyd .\
