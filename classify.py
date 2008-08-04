c5fa11d704e2e5c93cb7dd55c0776cb86498a88f
Jelmer Vernooij <jelmer@samba.org>
2008-08-04 21:49:51 +0200

    Add copyright headers.

--- classify.py
+++ classify.py
@@ -1,6 +1,21 @@
+# Copyright (C) 2008 Jelmer Vernooij <jelmer@samba.org>
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """Classify a commit based on the types of files it changed."""
 
-from bzrlib import urlutils 
+from bzrlib import urlutils
 from bzrlib.trace import mutter
 
 
@@ -12,22 +27,21 @@ def classify_filename(name):
         None if determining the file type failed.
     """
     # FIXME: Use mime types? Ohcount? 
-    basename = urlutils.basename(name)
-    try:
-        extension = basename.split(".")[1]
-        if extension in ("c", "h", "py", "cpp", "rb", "ac"):
-            return "code"
-        if extension in ("html", "xml", "txt", "rst", "TODO"):
-            return "documentation"
-        if extension in ("po"):
-            return "translation"
-        if extension in ("svg", "png", "jpg"):
-            return "art"
-    except IndexError:
+    extension = os.path.splitext(name)[1]
+    if extension in (".c", ".h", ".py", ".cpp", ".rb", ".pm", ".pl", ".ac"):
+        return "code"
+    if extension in (".html", ".xml", ".txt", ".rst", ".TODO"):
+        return "documentation"
+    if extension in (".po",):
+        return "translation"
+    if extension in (".svg", ".png", ".jpg"):
+        return "art"
+    if not extension:
+        basename = urlutils.basename(name)
         if basename in ("README", "NEWS", "TODO", 
                         "AUTHORS", "COPYING"):
             return "documentation"
-        if basename in ("Makefile"):
+        if basename in ("Makefile",):
             return "code"
 
     mutter("don't know how to classify %s", name)
