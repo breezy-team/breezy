657fb058603906c348f0d917bb0c97199714ee3f
Jelmer Vernooij <jelmer@samba.org>
2008-08-09 04:31:11 +0200

    Upgrade fileid map version.

--- fileids.py
+++ fileids.py
@@ -68,7 +68,7 @@ def get_local_changes(paths, branch, mapping, layout, generate_revid,
     return new_paths
 
 
-FILEIDMAP_VERSION = 1
+FILEIDMAP_VERSION = 2
 
 def simple_apply_changes(new_file_id, changes, find_children=None):
     """Simple function that generates a dictionary with file id changes.
