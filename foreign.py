e58d48f1daa1668c52c66689df1cd1241579a452
Jelmer Vernooij <jelmer@samba.org>
2008-08-27 21:34:29 +0200

    Import utility functions for foreign branches.

---
+++ foreign.py
@@ -0,0 +1,52 @@
+# Copyright (C) 2008 Jelmer Vernooij <jelmer@samba.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+"""Foreign branch utilities."""
+
+class VcsMapping(object):
+    """Describes the mapping between the semantics of Bazaar and a foreign vcs.
+
+    """
+    experimental = False
+    roundtripping = False
+    revid_prefix = None
+
+
+class VcsMappingRegistry(registry.Registry):
+    """Registry for Bazaar<->foreign VCS mappings.
+    
+    There should be one instance of this registry for every foreign VCS.
+    """
+
+    def register(self, key, factory, help):
+        """Register a mapping between Bazaar and foreign VCS semantics.
+
+        The factory must be a callable that takes one parameter: the key.
+        It must produce an instance of VcsMapping when called.
+        """
+        registry.Registry.register(self, key, factory, help)
+
+    def set_default(self, key):
+        """Set the 'default' key to be a clone of the supplied key.
+
+        This method must be called once and only once.
+        """
+        self._set_default_key(key)
+
+    def get_default(self):
+        """Convenience function for obtaining the default mapping to use."""
+        return self.get(self._get_default_key())
+
