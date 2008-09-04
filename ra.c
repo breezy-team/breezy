8021a64a25f374274a41f01216b76c73a39982e5
Jelmer Vernooij <jelmer@samba.org>
2008-09-04 19:02:26 +0200

    Fix compatibility with svn 1.4 on Windows.

--- ra.c
+++ ra.c
@@ -2729,7 +2729,7 @@ static PyObject *get_ssl_client_cert_pw_file_provider(PyObject *self)
 	return (PyObject *)auth;
 }
 
-#if defined(WIN32)
+#if defined(WIN32) 
 static PyObject *get_windows_simple_provider(PyObject* self)
 {
 	AuthProviderObject *auth = PyObject_New(AuthProviderObject, &AuthProvider_Type);
@@ -2740,6 +2740,7 @@ static PyObject *get_windows_simple_provider(PyObject* self)
 	return (PyObject *)auth;
 }
 
+#if SVN_VER_MAJOR >= 1 && SVN_VER_MINOR >= 5
 static PyObject *get_windows_ssl_server_trust_provider(PyObject *self)
 {
 	AuthProviderObject *auth = PyObject_New(AuthProviderObject, &AuthProvider_Type);
@@ -2750,6 +2751,7 @@ static PyObject *get_windows_ssl_server_trust_provider(PyObject *self)
 	return (PyObject *)auth;
 }
 #endif
+#endif
 
 static PyMethodDef ra_module_methods[] = {
 	{ "version", (PyCFunction)version, METH_NOARGS, NULL },
@@ -2759,7 +2761,9 @@ static PyMethodDef ra_module_methods[] = {
 	{ "get_simple_provider", (PyCFunction)get_simple_provider, METH_NOARGS, NULL },
 #if defined(WIN32)
 	{ "get_windows_simple_provider", (PyCFunction)get_windows_simple_provider, METH_NOARGS, NULL },
+#if SVN_VER_MAJOR >= 1 && SVN_VER_MINOR >= 5
 	{ "get_windows_ssl_server_trust_provider", (PyCFunction)get_windows_ssl_server_trust_provider, METH_NOARGS, NULL },
+#endif
 #endif
 	{ "get_username_prompt_provider", (PyCFunction)get_username_prompt_provider, METH_VARARGS, NULL },
 	{ "get_simple_prompt_provider", (PyCFunction)get_simple_prompt_provider, METH_VARARGS, NULL },
