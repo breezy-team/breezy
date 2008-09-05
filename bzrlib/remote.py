8482a59e1d54d385ae8c9ef83c0880b144283d3e
Andrew Bennetts <andrew.bennetts@canonical.com>
2008-09-05 15:52:53 +1000

    Unexpected error responses from a smart server no longer cause the client to traceback.

--- bzrlib/remote.py
+++ bzrlib/remote.py
@@ -1705,6 +1705,9 @@ def _translate_error(err, **context):
       - bzrdir
       - token
       - other_branch
+
+    If the error from the server doesn't match a known pattern, then
+    UntranslateableErrorFromSmartServer is raised.
     """
     def find(name):
         try:
@@ -1732,5 +1735,5 @@ def _translate_error(err, **context):
         raise errors.DivergedBranches(find('branch'), find('other_branch'))
     elif err.error_verb == 'TipChangeRejected':
         raise errors.TipChangeRejected(err.error_args[0].decode('utf8'))
-    raise
+    raise errors.UntranslateableErrorFromSmartServer(err)
 
--- bzrlib/tests/test_remote.py
+++ bzrlib/tests/test_remote.py
@@ -718,7 +718,7 @@ class TestBranchSetLastRevisionInfo(tests.TestCase):
         client._calls = []
 
         err = self.assertRaises(
-            errors.ErrorFromSmartServer,
+            errors.UntranslateableErrorFromSmartServer,
             branch.set_last_revision_info, 123, 'revid')
         self.assertEqual(('UnexpectedError',), err.error_tuple)
         branch.unlock()
@@ -1102,7 +1102,7 @@ class TestRepositoryGetRevisionGraph(TestRemoteRepository):
         transport_path = 'sinhala'
         repo, client = self.setup_fake_client_and_repository(transport_path)
         client.add_error_response('AnUnexpectedError')
-        e = self.assertRaises(errors.ErrorFromSmartServer,
+        e = self.assertRaises(errors.UntranslateableErrorFromSmartServer,
             self.applyDeprecated, one_four, repo.get_revision_graph, revid)
         self.assertEqual(('AnUnexpectedError',), e.error_tuple)
 
@@ -1370,9 +1370,10 @@ class TestErrorTranslationRobustness(TestErrorTranslationBase):
         ErrorFromSmartServer is propagated unmodified.
         """
         error_tuple = ('An unknown error tuple',)
-        server_error = errors.ErrorFromSmartServer(error_tuple)
-        translated_error = self.translateErrorFromSmartServer(server_error)
-        self.assertEqual(server_error, translated_error)
+        server_err = errors.ErrorFromSmartServer(error_tuple)
+        translated_error = self.translateErrorFromSmartServer(server_err)
+        expected_error = errors.UntranslateableErrorFromSmartServer(server_err)
+        self.assertEqual(expected_error, translated_error)
 
     def test_context_missing_a_key(self):
         """In case of a bug in the client, or perhaps an unexpected response
