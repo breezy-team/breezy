8e5fee5f745586c68ba4ed63bbdcbbba35cb9d14
John Arbash Meinel <john@arbash-meinel.com>
2008-09-26 18:35:02 -0500

    Restore the 'if memory in debug.debug_flags' check for commands.py

--- bzrlib/commands.py
+++ bzrlib/commands.py
@@ -795,7 +795,8 @@ def run_bzr(argv):
             ret = apply_coveraged(opt_coverage_dir, run, *run_argv)
         else:
             ret = run(*run_argv)
-        trace.debug_memory('Process status after command:', short=False)
+        if 'memory' in debug.debug_flags:
+            trace.debug_memory('Process status after command:', short=False)
         return ret or 0
     finally:
         # reset, in case we may do other commands later within the same process
