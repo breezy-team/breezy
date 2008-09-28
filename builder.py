b2161cc900dbb5bba9300cf0f80eb0091b90f17d
James Westby <jw+debian@jameswestby.net>
2008-09-28 14:40:19 +0100

    Pass --only-source to "apt-get source" call
    This will prevent apt from assuming the package name we pass is a binary
    package name which it must look up the source for. This would cause problems
    when there are a source package and binary package that are named the same,
    but the binary package isn't built from the source package, e.g. firefox,
    linux. (bug #275446)

--- builder.py
+++ builder.py
@@ -221,7 +221,7 @@ class DebBuild(object):
         tarball_dir = self._properties.tarball_dir()
         if not os.path.exists(tarball_dir):
             os.makedirs(tarball_dir)
-        command = 'apt-get source -y --tar-only %s=%s' % \
+        command = 'apt-get source -y --only-source --tar-only %s=%s' % \
             (package, sources.Version)
         proc = subprocess.Popen(command, shell=True, cwd=tarball_dir)
         proc.wait()
