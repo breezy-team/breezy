d46e5a52803729e4c6561037c53b61793fedac1b
Aaron Bentley <aaron@aaronbentley.com>
2008-10-05 17:23:16 -0400

    Begin implementing UI

--- __init__.py
+++ __init__.py
@@ -14,6 +14,20 @@
 #    along with this program; if not, write to the Free Software
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+"""Reimplementation of shelf commands."""
+
+from bzrlib import commands
+
+
+class cmd_shelve2(commands.Command):
+
+    def run(self):
+        from bzrlib.plugins.shelf2.shelver import Shelver
+        Shelver.from_args().run()
+
+
+commands.register_command(cmd_shelve2)
+
 
 def test_suite():
     from bzrlib.plugins.shelf2 import tests
---
+++ shelver.py
@@ -0,0 +1,93 @@
+# Copyright (C) 2008 Aaron Bentley <aaron@aaronbentley.com>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+import copy
+from cStringIO import StringIO
+import sys
+
+from bzrlib import (diff, osutils, patches, workingtree)
+from bzrlib.plugins.bzrtools import hunk_selector
+from bzrlib.plugins.shelf2 import prepare_shelf
+
+
+class Shelver(object):
+
+    def __init__(self, work_tree, target_tree, path):
+        self.work_tree = work_tree
+        self.target_tree = target_tree
+        self.path = path
+        self.diff_file = StringIO()
+        self.text_differ = diff.DiffText(self.work_tree, self.target_tree,
+                                         self.diff_file)
+
+    @classmethod
+    def from_args(klass):
+        tree, path = workingtree.WorkingTree.open_containing('.')
+        return klass(tree, tree.basis_tree(), path)
+
+    def run(self):
+        creator = prepare_shelf.ShelfCreator(self.work_tree)
+        try:
+            self.target_tree.lock_read()
+            try:
+                for change in creator:
+                    if change[0] == 'modify text':
+                        self.handle_modify_text(change[1])
+            finally:
+                self.target_tree.unlock()
+        finally:
+            creator.finalize()
+
+    def get_parsed_patch(self, file_id):
+        old_path = self.work_tree.id2path(file_id)
+        new_path = self.target_tree.id2path(file_id)
+        try:
+            patch = self.text_differ.diff(file_id, old_path, new_path, 'file',
+                                          'file')
+            self.diff_file.seek(0)
+            return patches.parse_patch(self.diff_file)
+        finally:
+            self.diff_file.truncate(0)
+
+    def __getchar(self):
+        import tty
+        import termios
+        fd = sys.stdin.fileno()
+        settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, settings)
+        return ch
+
+    def prompt(self, question):
+        print question,
+        char = self.__getchar()
+        print ""
+        return char
+
+    def handle_modify_text(self, file_id):
+        parsed = self.get_parsed_patch(file_id)
+        selected_hunks = []
+        final_patch = copy.copy(parsed)
+        final_patch.hunks = []
+        for hunk in parsed.hunks:
+            print hunk
+            char = self.prompt('Shelve? [y/n]')
+            if char == 'y':
+                final_patch.hunks.append(hunk)
