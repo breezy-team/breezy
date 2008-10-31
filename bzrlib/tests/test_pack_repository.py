c29f835cd5a5f666359be94025640fda4eeebe53
Andrew Bennetts <andrew.bennetts@canonical.com>
2008-10-31 12:37:43 +1100

    Add a test that ensures that the autopack RPC is actually used for all pack formats.

--- bzrlib/tests/test_pack_repository.py
+++ bzrlib/tests/test_pack_repository.py
@@ -37,6 +37,10 @@ from bzrlib import (
     upgrade,
     workingtree,
     )
+from bzrlib.smart import (
+    client,
+    server,
+    )
 from bzrlib.tests import (
     TestCase,
     TestCaseWithTransport,
@@ -625,6 +629,44 @@ class TestPackRepositoryStacking(TestCaseWithTransport):
         self.assertEqual(2, len(list(index.iter_all_entries())))
         pack_names = [node[1][0] for node in index.iter_all_entries()]
         self.assertTrue(large_pack_name in pack_names)
+
+
+class TestSmartServerAutopack(TestCaseWithTransport):
+
+    def setUp(self):
+        super(TestSmartServerAutopack, self).setUp()
+        # Create a smart server that publishes whatever the backing VFS server
+        # does.
+        self.smart_server = server.SmartTCPServer_for_testing()
+        self.smart_server.setUp(self.get_server())
+        self.addCleanup(self.smart_server.tearDown)
+        # Log all HPSS calls into self.hpss_calls.
+        client._SmartClient.hooks.install_named_hook(
+            'call', self.capture_hpss_call, None)
+        self.hpss_calls = []
+
+    def capture_hpss_call(self, params):
+        self.hpss_calls.append(params.method)
+
+    def get_format(self):
+        return bzrdir.format_registry.make_bzrdir(self.format_name)
+
+    def test_autopack_rpc_is_used_when_using_hpss(self):
+        # Make local and remote repos
+        tree = self.make_branch_and_tree('local', format=self.get_format())
+        self.make_branch_and_tree('remote', format=self.get_format())
+        remote_branch_url = self.smart_server.get_url() + 'remote'
+        remote_branch = bzrdir.BzrDir.open(remote_branch_url).open_branch()
+        # Make 9 local revisions, and push them one at a time to the remote
+        # repo to produce 9 pack files.
+        for x in range(9):
+            tree.commit('commit %s' % x)
+            tree.branch.push(remote_branch)
+        # Make one more push to trigger an autopack
+        self.hpss_calls = []
+        tree.commit('commit triggering pack')
+        tree.branch.push(remote_branch)
+        self.assertTrue('PackRepository.autopack' in self.hpss_calls)
 
 
 def load_tests(basic_tests, module, test_loader):
