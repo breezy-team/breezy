6e2103282f37e1a58a64abc7b965ad8a2065560d
Miklos Vajna <vmiklos@frugalware.org>
2008-11-11 01:29:58 +0100

    port test-hg.sh

--- darcs/t/test-hg.sh
+++ darcs/t/test-hg.sh
@@ -1,3 +1,7 @@
+. lib.sh
+
+create_darcs test --old-fashioned-inventory
+
 rm -rf test.darcs test.hg
 mkdir test.hg
 cd test.hg
@@ -7,7 +11,7 @@ if [ "$1" != "--stdout" ]; then
 	../darcs-fast-export.py test |(cd test.hg; hg fastimport /dev/stdin)
 	if [ $? = 0 ]; then
 		echo "bugs:"
-		diff --exclude _darcs --exclude .hg -Naur test.hg test
+		diff --exclude _darcs --exclude .hg --exclude '*-darcs-backup*' -Naur test.hg test
 		exit $?
 	fi
 else
