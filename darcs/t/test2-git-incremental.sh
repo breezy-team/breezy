f59964a69dc75c2e5b29d2a2fd8239702110ce05
Miklos Vajna <vmiklos@frugalware.org>
2008-11-11 02:30:31 +0100

    add a new test to demonstrate incremental conversion

---
+++ darcs/t/test2-git-incremental.sh
@@ -0,0 +1,21 @@
+. lib.sh
+
+create_darcs test2 --darcs-2
+
+rm -rf test2.darcs test2.git
+mkdir test2.git
+cd test2.git
+git --bare init
+cd ..
+if [ "$1" != "--stdout" ]; then
+	dmark="$(pwd)/test2.dfe-marks"
+	gmark="$(pwd)/test2.gfi-marks"
+	rm -f $mark $gmark
+	../darcs-fast-export.py --export-marks=$dmark test2 |(cd test2.git; git fast-import --export-marks=$gmark)
+	diff_git test2 || die "initial conversion differs"
+	upd_file_darcs test2 file2 upd_contents
+	../darcs-fast-export.py --export-marks=$dmark --import-marks=$dmark test2 |(cd test2.git; git fast-import --export-marks=$gmark --import-marks=$gmark)
+	diff_git test2 || die "update differs"
+else
+	../darcs-fast-export.py test2
+fi
