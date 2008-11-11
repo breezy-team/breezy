aa99f1e968eddb68db9fd3c075eadfeae29dc6fa
Miklos Vajna <vmiklos@frugalware.org>
2008-11-11 01:31:22 +0100

    port test2-git

--- darcs/t/test2-git.sh
+++ darcs/t/test2-git.sh
@@ -1,3 +1,7 @@
+. lib.sh
+
+create_darcs test2 --darcs-2
+
 rm -rf test2.darcs test2.git
 mkdir test2.git
 cd test2.git
@@ -8,7 +12,7 @@ if [ "$1" != "--stdout" ]; then
 	if [ $? = 0 ]; then
 		git clone -q test2.git test2.git.nonbare
 		echo "bugs:"
-		diff --exclude _darcs --exclude .git -Naur test2.git.nonbare test2
+		diff --exclude _darcs --exclude .git --exclude '*-darcs-backup*' -Naur test2.git.nonbare test2
 		ret=$?
 		rm -rf test2.git.nonbare
 		exit $?
