12c92a0063c6109333510d9a636b771b5d0dd91b
Miklos Vajna <vmiklos@frugalware.org>
2008-11-15 23:43:29 +0100

    add d2x
    because i got tired typing those --import-marks/--export-marks manually
    all the time. :)

---
+++ darcs/t/test-git-d2x.sh
@@ -0,0 +1,17 @@
+. lib.sh
+
+create_darcs test --old-fashioned-inventory
+
+rm -rf test.git
+if [ "$1" != "--stdout" ]; then
+	d2x -f git test
+	diff_git test || die "initial conversion differs"
+	upd_file_darcs test file2 upd_contents
+	d2x -f git test
+	diff_git test || die "update differs"
+	upd_file_darcs test hungarian.gif "binary to text"
+	d2x -f git test
+	diff_git test || die "update2 differs"
+else
+	darcs-fast-export test
+fi
