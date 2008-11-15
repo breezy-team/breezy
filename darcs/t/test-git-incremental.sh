86d17ba7c8b36a2a04ffc9abbc3528b15a39c556
Miklos Vajna <vmiklos@frugalware.org>
2008-11-15 17:59:05 +0100

    add darcs1 -> git incremental testcase

---
+++ darcs/t/test-git-incremental.sh
@@ -0,0 +1,21 @@
+. lib.sh
+
+create_darcs test --old-fashioned-inventory
+
+rm -rf test.darcs test.git
+mkdir test.git
+cd test.git
+git --bare init
+cd ..
+if [ "$1" != "--stdout" ]; then
+	dmark="$(pwd)/test.dfe-marks"
+	gmark="$(pwd)/test.gfi-marks"
+	rm -f $mark $gmark
+	../darcs-fast-export.py --export-marks=$dmark test |(cd test.git; git fast-import --export-marks=$gmark)
+	diff_git test || die "initial conversion differs"
+	upd_file_darcs test file2 upd_contents
+	../darcs-fast-export.py --export-marks=$dmark --import-marks=$dmark test |(cd test.git; git fast-import --export-marks=$gmark --import-marks=$gmark)
+	diff_git test || die "update differs"
+else
+	../darcs-fast-export.py test
+fi
