e4ed786834d075131b52d66db792178351a1d4a4
Miklos Vajna <vmiklos@frugalware.org>
2008-11-22 02:28:25 +0100

    t: add create_git() to lib.sh

--- darcs/t/lib.sh
+++ darcs/t/lib.sh
@@ -59,6 +59,47 @@ third line"
 	cd ..
 }
 
+create_git()
+{
+	rm -rf $1
+	mkdir -p $1
+	cd $1
+	git init $2
+	echo A > file
+	git add file
+	git commit -a -m A
+	echo B > file
+	git commit -a -m B
+	git checkout -b tmp HEAD~1
+	echo C > file
+	git commit -a -m C
+	git checkout master
+	git merge tmp
+	echo D > file
+	echo "first line
+second line
+third line" | git commit -a -F -
+	git tag 1.0
+	echo e > file
+	git commit -a -m e
+	echo f > file
+	git config i18n.commitencoding ISO-8859-2
+	git commit --author="ιαυϋ <$DARCS_EMAIL>" -a -m f
+	cp ../data/hungarian.gif .
+	git add hungarian.gif
+	git commit -a -m "add a binary file"
+	rm file
+	echo test > file2
+	git add file2
+	git commit -a -m "replace file with file2"
+	touch file3
+	git add file3
+	git commit -a -m "add empty file"
+	rm file3
+	git commit -a -m "remove file"
+	cd ..
+}
+
 diff_git()
 {
 	rm -rf $1.git.nonbare
