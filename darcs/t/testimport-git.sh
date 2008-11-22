58a5947f6bf69ad862474ad6dd2d661542cbf18b
Miklos Vajna <vmiklos@frugalware.org>
2008-11-22 02:36:29 +0100

    handle progress command

--- darcs/t/testimport-git.sh
+++ darcs/t/testimport-git.sh
@@ -7,7 +7,7 @@ mkdir test.darcs
 cd test.darcs
 darcs init
 cd ..
-(cd test; git fast-export HEAD) | (cd test.darcs; darcs-fast-import)
+(cd test; git fast-export --progress=2 HEAD) | (cd test.darcs; darcs-fast-import)
 if [ $? != 0 ]; then
 	exit 1
 fi
