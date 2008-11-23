85453cea6a9e749bb27f640c768e502dca231249
Miklos Vajna <vmiklos@frugalware.org>
2008-11-23 22:00:12 +0100

    g-d: add pull subcommand

--- darcs/t/testimport-git-twoway-gd.sh
+++ darcs/t/testimport-git-twoway-gd.sh
@@ -7,14 +7,12 @@ mkdir test.git
 cd test.git
 git init
 git darcs add upstream ../test
-git darcs fetch upstream
-git merge darcs/upstream
+git darcs pull upstream
 cd ..
 diff_git test || die "initial fetch differs"
 upd_file_darcs test file2 upd_contents
 cd test.git
-git darcs fetch upstream
-git merge darcs/upstream
+git darcs pull upstream
 cd ..
 diff_git test || die "fetch #1 differs"
 upd_file_git test.git file2 upd_contents2
@@ -25,8 +23,7 @@ diff_git test || die "push #1 difers"
 upd_file_darcs test file2 upd_contents3
 upd_file_darcs test file2 upd_contents32
 cd test.git
-git darcs fetch upstream
-git merge darcs/upstream
+git darcs pull upstream
 cd ..
 diff_git test || die "fetch #2 (multiple commits) differs"
 upd_file_git test.git file2 upd_contents4
