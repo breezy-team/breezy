17965c2d1f878f510317a9cbd19e7faacc083863
Miklos Vajna <vmiklos@frugalware.org>
2008-11-23 02:35:44 +0100

    t: testimport-git-twoway.sh: do a bit more stress-test

--- darcs/t/testimport-git-twoway.sh
+++ darcs/t/testimport-git-twoway.sh
@@ -14,8 +14,17 @@ dmark="$(pwd)/test.dmarks"
 diff_importgit test || die "initial conversion differs"
 upd_file_git test file2 upd_contents
 (cd test; git fast-export --export-marks=$gmark --import-marks=$gmark HEAD) | (cd test.darcs; darcs-fast-import --export-marks=$dmark --import-marks=$dmark)
-diff_importgit test || die "git -> darcs update differs"
+diff_importgit test || die "git -> darcs update #1 differs"
 upd_file_darcs test.darcs file2 upd_contents2
 darcs-fast-export --export-marks=$dmark --import-marks=$dmark --working test/.git/darcs test.darcs | (cd test; git fast-import --export-marks=$gmark --import-marks=$gmark)
 (cd test; git checkout -f)
-diff_importgit test || die "git -> darcs update differs"
+diff_importgit test || die "git -> darcs update #2 differs"
+upd_file_git test file2 upd_contents3
+upd_file_git test file2 upd_contents32
+(cd test; git fast-export --export-marks=$gmark --import-marks=$gmark HEAD) | (cd test.darcs; darcs-fast-import --export-marks=$dmark --import-marks=$dmark)
+diff_importgit test || die "git -> darcs update #3 differs"
+upd_file_darcs test.darcs file2 upd_contents4
+upd_file_darcs test.darcs file2 upd_contents42
+darcs-fast-export --export-marks=$dmark --import-marks=$dmark --working test/.git/darcs test.darcs | (cd test; git fast-import --export-marks=$gmark --import-marks=$gmark)
+(cd test; git checkout -f)
+diff_importgit test || die "git -> darcs update #4 differs"
