9866da34583d9bccd6dffdecc885f00f6f78958f
James Westby <jw+debian@jameswestby.net>
2008-12-02 20:48:54 +0000

    Get the native export going.

--- source_distiller.py
+++ source_distiller.py
@@ -18,6 +18,12 @@
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 
+import os
+
+from bzrlib import errors as bzr_errors
+from bzrlib.export import export
+
+
 class SourceDistiller(object):
     """A source distiller extracts the source to build from a location.
 
@@ -25,18 +31,41 @@ class SourceDistiller(object):
     a location of your choice.
     """
 
-    def __init__(self, branch):
+    def __init__(self):
         """Create a SourceDistiller to distill from the specified branch.
 
-        :param location: a Branch to extract the source from.
+        See the specific subclass for required arguments.
         """
-        self.branch = branch
+        raise NotImplementedError(self.__init__)
 
     def distill(self, target):
         """Extract the source to a tree rooted at the given location.
 
+        The passed location cannot already exist. If it does then
+        FileExists will be raised.
+
         :param target: a string containing the location at which to 
             place the tree containing the buildable source.
         """
-        self.target = target
+        if os.path.exists(target):
+            raise bzr_errors.FileExists(target)
+        self._distill(target)
 
+    def _distill(self, target):
+        """Subclasses should override this to implement distill."""
+        raise NotImplementedError(self._distill)
+
+
+class NativeSourceDistiller(SourceDistiller):
+    """A SourceDistiller for unpacking a native package from a branch."""
+
+    def __init__(self, tree):
+        """Create a NativeSourceDistiller.
+
+        :param tree: the tree containing the native package source. Should
+            be locked at least for read.
+        """
+        self.tree = tree
+
+    def _distill(self, target):
+        export(self.tree, target, None, None)
--- tests/test_source_distiller.py
+++ tests/test_source_distiller.py
@@ -18,16 +18,36 @@
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 
+from bzrlib.errors import (
+        ObjectNotLocked,
+        FileExists,
+        )
 from bzrlib.tests import TestCaseWithTransport
 
 from bzrlib.plugins.builddeb.source_distiller import (
-        SourceDistiller,
+        NativeSourceDistiller,
         )
 
 
-class TestSourceDistiller(TestCaseWithTransport):
+class TestNativeSourceDistiller(TestCaseWithTransport):
 
-    def test_create(self):
+    def test_distill_target_exists(self):
         wt = self.make_branch_and_tree(".")
-        sd = SourceDistiller(wt.branch)
+        wt.lock_read()
+        self.addCleanup(wt.unlock)
+        sd = NativeSourceDistiller(wt)
+        self.build_tree(['target/'])
+        self.assertRaises(FileExists, sd.distill, 'target')
 
+    def test_distill_revision_tree(self):
+        wt = self.make_branch_and_tree(".")
+        self.build_tree(['a'])
+        wt.lock_write()
+        self.addCleanup(wt.unlock)
+        wt.add(['a'])
+        revid = wt.commit("one")
+        rev_tree = wt.basis_tree()
+        sd = NativeSourceDistiller(rev_tree)
+        sd.distill('target')
+        self.failUnlessExists('target')
+        self.failUnlessExists('target/a')
