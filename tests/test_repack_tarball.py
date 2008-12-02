726a1989d2dfd0ac48e57abcbab2a57d630fd82a
James Westby <jw+debian@jameswestby.net>
2008-12-02 16:25:25 +0000

    Reformat the repack_tarball tests.

--- tests/test_repack_tarball.py
+++ tests/test_repack_tarball.py
@@ -36,101 +36,99 @@ from bzrlib.plugins.builddeb.repack_tarball import (
 
 
 def touch(filename):
-  f = open(filename, 'w')
-  try:
-    f.write(' ')
-  finally:
-    f.close()
+    f = open(filename, 'w')
+    try:
+        f.write(' ')
+    finally:
+        f.close()
 
 
 class TestRepackTarball(TestCaseInTempDir):
 
-  basedir = 'package-0.2/'
-  files = ['README']
-  bare_tarball_name = 'package-0.2.tar'
-  tgz_tarball_name = 'package-0.2.tgz'
-  tar_gz_tarball_name = 'package-0.2.tar.gz'
-  tar_bz2_tarball_name = 'package-0.2.tar.bz2'
-  new_tarball = 'package_0.2.orig.tar.gz'
+    basedir = 'package-0.2/'
+    files = ['README']
+    bare_tarball_name = 'package-0.2.tar'
+    tgz_tarball_name = 'package-0.2.tgz'
+    tar_gz_tarball_name = 'package-0.2.tar.gz'
+    tar_bz2_tarball_name = 'package-0.2.tar.bz2'
+    new_tarball = 'package_0.2.orig.tar.gz'
 
-  def create_basedir(self):
-    """Create the basedir that the source can be built from"""
-    os.mkdir(self.basedir)
-    for filename in [os.path.join(self.basedir, file) for file in self.files]:
-      if filename.endswith('/'):
-        os.mkdir(filename)
-      else:
-        touch(filename)
+    def create_basedir(self):
+        """Create the basedir that the source can be built from"""
+        os.mkdir(self.basedir)
+        for filename in [os.path.join(self.basedir, file) for file in self.files]:
+            if filename.endswith('/'):
+                os.mkdir(filename)
+            else:
+                touch(filename)
 
-  def create_old_tarball(self):
-    self.create_basedir()
-    self.build_tarball()
+    def create_old_tarball(self):
+        self.create_basedir()
+        self.build_tarball()
 
-  def test_create_old_tarball(self):
-    self.create_old_tarball()
-    self.failUnlessExists(self.old_tarball)
+    def test_create_old_tarball(self):
+        self.create_old_tarball()
+        self.failUnlessExists(self.old_tarball)
 
-  def test_repack_tarball_non_extant(self):
-    error = NoSuchFile
-    if (get_repacker_class(self.old_tarball) is None):
-        # directory, can't really be detected remotely, so we have a
-        # error that could mean two things
-        error = UnsupportedRepackFormat
-    self.assertRaises(error, repack_tarball, self.old_tarball,
-            self.new_tarball)
+    def test_repack_tarball_non_extant(self):
+        error = NoSuchFile
+        if (get_repacker_class(self.old_tarball) is None):
+            # directory, can't really be detected remotely, so we have a
+            # error that could mean two things
+            error = UnsupportedRepackFormat
+        self.assertRaises(error, repack_tarball, self.old_tarball,
+              self.new_tarball)
 
-  def test_repack_tarball_result_extant(self):
-    self.create_old_tarball()
-    touch(self.new_tarball)
-    self.assertRaises(FileExists, repack_tarball, self.old_tarball,
-                      self.new_tarball)
+    def test_repack_tarball_result_extant(self):
+        self.create_old_tarball()
+        touch(self.new_tarball)
+        self.assertRaises(FileExists, repack_tarball, self.old_tarball,
+                          self.new_tarball)
 
-  def test_repack_tarball_creates_new(self):
-    self.create_old_tarball()
-    repack_tarball(self.old_tarball, self.new_tarball)
-    self.failUnlessExists(self.old_tarball)
-    self.failUnlessExists(self.new_tarball)
+    def test_repack_tarball_creates_new(self):
+        self.create_old_tarball()
+        repack_tarball(self.old_tarball, self.new_tarball)
+        self.failUnlessExists(self.old_tarball)
+        self.failUnlessExists(self.new_tarball)
 
-  def test_repack_tarball_contents(self):
-    self.create_old_tarball()
-    repack_tarball(self.old_tarball, self.new_tarball)
-    tar = tarfile.open(self.new_tarball, 'r:gz')
-    try:
-      members = tar.getnames()
-    finally:
-      tar.close()
-    self.assertEqual(members,
-                     [self.basedir] +
-                     [os.path.join(self.basedir, file) for file in self.files])
+    def test_repack_tarball_contents(self):
+        self.create_old_tarball()
+        repack_tarball(self.old_tarball, self.new_tarball)
+        tar = tarfile.open(self.new_tarball, 'r:gz')
+        try:
+            members = tar.getnames()
+        finally:
+            tar.close()
+        self.assertEqual(members,
+                         [self.basedir] +
+                         [os.path.join(self.basedir, file) for file in self.files])
 
-  def test_repack_tarball_with_target_dir(self):
-    self.create_old_tarball()
-    target_dir = 'tarballs'
-    repack_tarball(self.old_tarball, self.new_tarball, target_dir=target_dir)
-    self.failUnlessExists(target_dir)
-    self.failUnlessExists(os.path.join(target_dir, self.new_tarball))
-    self.failUnlessExists(self.old_tarball)
+    def test_repack_tarball_with_target_dir(self):
+        self.create_old_tarball()
+        target_dir = 'tarballs'
+        repack_tarball(self.old_tarball, self.new_tarball, target_dir=target_dir)
+        self.failUnlessExists(target_dir)
+        self.failUnlessExists(os.path.join(target_dir, self.new_tarball))
+        self.failUnlessExists(self.old_tarball)
 
-  def test_repack_tarball_with_target_dir_exists(self):
-    self.create_old_tarball()
-    target_dir = 'tarballs'
-    os.mkdir(target_dir)
-    repack_tarball(self.old_tarball, self.new_tarball, target_dir=target_dir)
-    self.failUnlessExists(target_dir)
-    self.failUnlessExists(os.path.join(target_dir, self.new_tarball))
-    self.failUnlessExists(self.old_tarball)
-    self.failIfExists(self.new_tarball)
+    def test_repack_tarball_with_target_dir_exists(self):
+        self.create_old_tarball()
+        target_dir = 'tarballs'
+        os.mkdir(target_dir)
+        repack_tarball(self.old_tarball, self.new_tarball, target_dir=target_dir)
+        self.failUnlessExists(target_dir)
+        self.failUnlessExists(os.path.join(target_dir, self.new_tarball))
+        self.failUnlessExists(self.old_tarball)
+        self.failIfExists(self.new_tarball)
 
-  def test_repack_tarball_with_target_dir_not_dir(self):
-    self.create_old_tarball()
-    target_dir = 'tarballs'
-    touch(target_dir)
-    # transport gives NoSuchFile rather than NotADirectory for this
-    self.assertRaises(NoSuchFile, repack_tarball, self.old_tarball,
-                      self.new_tarball, target_dir=target_dir)
-    self.failUnlessExists(self.old_tarball)
-    self.failIfExists(self.new_tarball)
-    self.failIfExists(os.path.join(target_dir, self.new_tarball))
-    self.failUnlessExists(target_dir)
-
-# vim: ts=2 sts=2 sw=2
+    def test_repack_tarball_with_target_dir_not_dir(self):
+        self.create_old_tarball()
+        target_dir = 'tarballs'
+        touch(target_dir)
+        # transport gives NoSuchFile rather than NotADirectory for this
+        self.assertRaises(NoSuchFile, repack_tarball, self.old_tarball,
+                          self.new_tarball, target_dir=target_dir)
+        self.failUnlessExists(self.old_tarball)
+        self.failIfExists(self.new_tarball)
+        self.failIfExists(os.path.join(target_dir, self.new_tarball))
+        self.failUnlessExists(target_dir)
--- tests/test_repack_tarball_extra.py
+++ tests/test_repack_tarball_extra.py
@@ -31,87 +31,85 @@ from bzrlib.plugins.builddeb.errors import UnsupportedRepackFormat
 
 
 def touch(filename):
-  f = open(filename, 'w')
-  try:
-    f.write(' ')
-  finally:
-    f.close()
+    f = open(filename, 'w')
+    try:
+        f.write(' ')
+    finally:
+        f.close()
 
 
 def create_basedir(basedir, files):
-  """Create the basedir that the source can be built from"""
-  os.mkdir(basedir)
-  for filename in [os.path.join(basedir, file) for file in files]:
-    if filename.endswith('/'):
-      os.mkdir(filename)
-    else:
-      touch(filename)
+    """Create the basedir that the source can be built from"""
+    os.mkdir(basedir)
+    for filename in [os.path.join(basedir, file) for file in files]:
+        if filename.endswith('/'):
+            os.mkdir(filename)
+        else:
+            touch(filename)
 
 
 def make_new_upstream_tarball(tarball):
-  tar = tarfile.open(tarball, 'w:gz')
-  try:
-    tar.add('package-0.2')
-  finally:
-    tar.close()
-  shutil.rmtree('package-0.2')
+    tar = tarfile.open(tarball, 'w:gz')
+    try:
+        tar.add('package-0.2')
+    finally:
+        tar.close()
+    shutil.rmtree('package-0.2')
 
 
 def make_new_upstream_tarball_bz2(tarball):
-  tar = tarfile.open(tarball, 'w:bz2')
-  try:
-    tar.add('package-0.2')
-  finally:
-    tar.close()
-  shutil.rmtree('package-0.2')
+    tar = tarfile.open(tarball, 'w:bz2')
+    try:
+        tar.add('package-0.2')
+    finally:
+        tar.close()
+    shutil.rmtree('package-0.2')
 
 
 class TestRepackTarballExtra(TestCaseInTempDir):
 
-  def test_repack_tarball_errors_unkown_format(self):
-    old_tarball = 'package-0.2.INVALID'
-    f = open(old_tarball, 'w')
-    f.close()
-    self.failUnlessExists(old_tarball)
-    self.assertRaises(UnsupportedRepackFormat, repack_tarball, old_tarball,
-                      'package_0.2.orig.tar.gz')
+    def test_repack_tarball_errors_unkown_format(self):
+        old_tarball = 'package-0.2.INVALID'
+        f = open(old_tarball, 'w')
+        f.close()
+        self.failUnlessExists(old_tarball)
+        self.assertRaises(UnsupportedRepackFormat, repack_tarball, old_tarball,
+                          'package_0.2.orig.tar.gz')
 
-  def test_conditional_repack_tarball_different(self):
-    tarball_name = 'package-0.2.tar.gz'
-    create_basedir('package-0.2/', files=['README'])
-    make_new_upstream_tarball(tarball_name)
-    target_dir = 'target'
-    os.mkdir(target_dir)
-    create_basedir('package-0.2/', files=['README', 'NEWS'])
-    make_new_upstream_tarball(os.path.join(target_dir, tarball_name))
-    self.assertRaises(FileExists, repack_tarball, tarball_name,
-        tarball_name, target_dir=target_dir)
-    self.failUnlessExists(tarball_name)
-    self.failUnlessExists(os.path.join(target_dir, tarball_name))
+    def test_conditional_repack_tarball_different(self):
+        tarball_name = 'package-0.2.tar.gz'
+        create_basedir('package-0.2/', files=['README'])
+        make_new_upstream_tarball(tarball_name)
+        target_dir = 'target'
+        os.mkdir(target_dir)
+        create_basedir('package-0.2/', files=['README', 'NEWS'])
+        make_new_upstream_tarball(os.path.join(target_dir, tarball_name))
+        self.assertRaises(FileExists, repack_tarball, tarball_name,
+            tarball_name, target_dir=target_dir)
+        self.failUnlessExists(tarball_name)
+        self.failUnlessExists(os.path.join(target_dir, tarball_name))
 
-  def test_conditional_repack_tarball_same(self):
-    tarball_name = 'package-0.2.tar.gz'
-    create_basedir('package-0.2/', files=['README'])
-    make_new_upstream_tarball(tarball_name)
-    target_dir = 'target'
-    os.mkdir(target_dir)
-    shutil.copy(tarball_name, target_dir)
-    repack_tarball(tarball_name, tarball_name, target_dir=target_dir)
-    self.failUnlessExists(tarball_name)
-    self.failUnlessExists(os.path.join(target_dir, tarball_name))
+    def test_conditional_repack_tarball_same(self):
+        tarball_name = 'package-0.2.tar.gz'
+        create_basedir('package-0.2/', files=['README'])
+        make_new_upstream_tarball(tarball_name)
+        target_dir = 'target'
+        os.mkdir(target_dir)
+        shutil.copy(tarball_name, target_dir)
+        repack_tarball(tarball_name, tarball_name, target_dir=target_dir)
+        self.failUnlessExists(tarball_name)
+        self.failUnlessExists(os.path.join(target_dir, tarball_name))
 
-  def test_conditional_repack_different_formats(self):
-    tarball_name = 'package-0.2.tar.gz'
-    bz2_tarball_name = 'package-0.2.tar.bz2'
-    create_basedir('package-0.2/', files=['README'])
-    make_new_upstream_tarball_bz2(bz2_tarball_name)
-    target_dir = 'target'
-    os.mkdir(target_dir)
-    create_basedir('package-0.2/', files=['README'])
-    make_new_upstream_tarball(os.path.join(target_dir, tarball_name))
-    self.assertRaises(FileExists, repack_tarball, bz2_tarball_name,
-        tarball_name, target_dir=target_dir)
-    self.failUnlessExists(bz2_tarball_name)
-    self.failUnlessExists(os.path.join(target_dir, tarball_name))
-
-# vim: ts=2 sts=2 sw=2
+    def test_conditional_repack_different_formats(self):
+        tarball_name = 'package-0.2.tar.gz'
+        bz2_tarball_name = 'package-0.2.tar.bz2'
+        create_basedir('package-0.2/', files=['README'])
+        make_new_upstream_tarball_bz2(bz2_tarball_name)
+        target_dir = 'target'
+        os.mkdir(target_dir)
+        create_basedir('package-0.2/', files=['README'])
+        make_new_upstream_tarball(os.path.join(target_dir, tarball_name))
+        self.assertRaises(FileExists, repack_tarball, bz2_tarball_name,
+            tarball_name, target_dir=target_dir)
+        self.failUnlessExists(bz2_tarball_name)
+        self.failUnlessExists(os.path.join(target_dir, tarball_name))
