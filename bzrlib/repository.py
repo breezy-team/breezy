8c430b38ebbf29b3c1859cf3737e29c00ab75798
John Arbash Meinel <john@arbash-meinel.com>
2008-12-09 22:34:21 -0600

    There was a test that asserted we called pb.update() with the last revision.

--- bzrlib/repository.py
+++ bzrlib/repository.py
@@ -3247,7 +3247,7 @@ class InterDifferingSerializer(InterKnitRepo):
             self.target.start_write_group()
             try:
                 pb.update('Transferring revisions', offset,
-                    len(revision_ids))
+                          len(revision_ids))
                 batch = revision_ids[offset:offset+batch_size]
                 basis_id, basis_tree = self._fetch_batch(batch,
                     basis_id, basis_tree)
@@ -3256,6 +3256,8 @@ class InterDifferingSerializer(InterKnitRepo):
                 raise
             else:
                 self.target.commit_write_group()
+        pb.update('Transferring revisions', len(revision_ids),
+                  len(revision_ids))
 
     @needs_write_lock
     def fetch(self, revision_id=None, pb=None, find_ghosts=False):
