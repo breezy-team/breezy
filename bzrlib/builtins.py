109d8b427c1634e02637a31c3770a27bc7e24e46
Aaron Bentley <aaron@aaronbentley.com>
2008-12-10 19:05:00 -0800

    Use open_containing when listing shelves

--- bzrlib/builtins.py
+++ bzrlib/builtins.py
@@ -4782,7 +4782,7 @@ class cmd_shelve(Command):
             return 0
 
     def run_for_list(self):
-        tree = WorkingTree.open('.')
+        tree = WorkingTree.open_containing('.')
         tree.lock_read()
         try:
             manager = tree.get_shelf_manager()
