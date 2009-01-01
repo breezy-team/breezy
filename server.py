92d54ebf9939b86ccb437a422c35f3984c612151
John Carr <john.carr@unrouted.co.uk>
2009-01-01 17:52:21 +0000

    Negotiate which revisions need to be sent to client, and iterate over them all

--- server.py
+++ server.py
@@ -92,3 +92,28 @@ class BzrBackend(Backend):
     def fetch_objects(self, determine_wants, graph_walker, progress):
         """ yield git objects to send to client """
         wants = determine_wants(self.get_refs())
+        commits_to_send = set([self.mapping.revision_id_foreign_to_bzr(w) for w in wants])
+        rev_done = set()
+
+        repo = Repository.open(self.directory)
+        
+        repo.lock_read()
+        try:
+            have = graph_walker.next()
+            while have:
+                rev_done.add(have)
+                if repo.has_revision(self.mapping.revision_id_foregin_to_bzr(sha)):
+                    graph_walker.ack(have)
+                have = graph_walker.next()
+
+            while commits_to_send:
+                commit = commits_to_send.pop(0)
+                if commit in rev_done:
+                    continue
+                rev_done.add(commit)
+
+                commits_to_send.update([rev for rev in commit.parent_ids if not rev in rev_done])
+
+                # generate and yield blob, tree and commit objects now.
+        finally:
+            repo.unlock()
