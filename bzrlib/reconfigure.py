f80a08c1b4074db6e26459697358151565e0ada0
Marius Kruger <amanic@gmail.com>
2009-02-06 03:47:40 +0200

    put bracket back where we found it

--- bzrlib/reconfigure.py
+++ bzrlib/reconfigure.py
@@ -210,8 +210,7 @@ class Reconfigure(object):
         return (self._unbind or self._bind or self._destroy_tree
                 or self._create_tree or self._destroy_reference
                 or self._create_branch or self._create_repository
-                or self._create_reference or self._destroy_repository
-                )
+                or self._create_reference or self._destroy_repository)
 
     def _check(self):
         """Raise if reconfiguration would destroy local changes"""
