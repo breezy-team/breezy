71ce0d2aa5a4630f4c98802ac32c8dd847e601dd
Ian Clatworthy <ian.clatworthy@canonical.com>
2009-02-21 14:20:23 +1000

    ensure version appears in bzr plugins output (Alexander Belchenko)

--- __init__.py
+++ __init__.py
@@ -45,6 +45,8 @@ To report bugs or publish enhancements, visit the bzr-fastimport project
 page on Launchpad, https://launchpad.net/bzr-fastimport.
 """
 
+version_info = (0, 8, 0, 'dev', 0)
+
 from bzrlib.commands import Command, register_command
 from bzrlib.option import Option, ListOption
 
