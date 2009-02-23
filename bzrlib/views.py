a9cd26d4ba5914566a07a6b4b73b6fadac3e9776
Eduardo Padoan <eduardo.padoan@gmail.com>
2009-02-23 02:57:15 -0300

    Moved diff._check_path_in_view() to views.check_path_in_view()

--- bzrlib/views.py
+++ bzrlib/views.py
@@ -27,6 +27,7 @@ import re
 
 from bzrlib import (
     errors,
+    osutils,
     )
 
 
@@ -273,3 +274,11 @@ def view_display_str(view_files, encoding=None):
         return ", ".join(view_files)
     else:
         return ", ".join([v.encode(encoding, 'replace') for v in view_files])
+
+
+def check_path_in_view(tree, relpath):
+    """If a working tree has a view enabled, check the path is within it."""
+    if tree.supports_views():
+        view_files = tree.views.lookup_view()
+        if  view_files and not osutils.is_inside_any(view_files, relpath):
+            raise errors.FileOutsideView(relpath, view_files)
