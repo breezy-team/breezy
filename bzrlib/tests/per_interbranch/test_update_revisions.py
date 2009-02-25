d895d8bfe8a8bf12fae2b73c6273caeefdd89126
Jelmer Vernooij <jelmer@samba.org>
2009-02-25 03:35:39 +0100

    Fix tests.

--- bzrlib/tests/per_interbranch/test_update_revisions.py
+++ bzrlib/tests/per_interbranch/test_update_revisions.py
@@ -29,8 +29,8 @@ class TestUpdateRevisions(TestCaseWithInterBranch):
         self.tree1 = self.make_branch_and_tree('tree1')
         rev1 = self.tree1.commit('one')
         dir2 = self.make_to_bzrdir('tree2')
-        dir2.create_repository()
-        self.tree1.branch.sprout(dir2)
+        dir2.create_repository().fetch(self.tree1.branch.repository)
+        branch2 = self.tree1.branch.sprout(dir2)
         self.tree2 = dir2.create_workingtree()
 
     def test_accepts_graph(self):
