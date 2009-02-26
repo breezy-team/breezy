c9938d0d464fd75153f47cd93f9a65c603bed9a7
Vincent Ladeuil <v.ladeuil+lp@free.fr>
2009-02-26 20:29:17 +0100

    Fix one more failing test, waiting for subtrees implementation. * bzrlib/tree.py: (InterTree.iter_changes): Restore the bogus indentation as it makes test_merge.TestMergerEntriesLCAOnDisk.test_nested_tree_subtree_renamed_and_modified fails

--- bzrlib/tree.py
+++ bzrlib/tree.py
@@ -1031,10 +1031,14 @@ class InterTree(InterObject):
                 if (self.source.get_symlink_target(file_id) !=
                     self.target.get_symlink_target(file_id)):
                     changed_content = True
-            elif from_kind == 'tree-reference':
-                if (self.source.get_reference_revision(file_id, from_path)
-                    != self.target.get_reference_revision(file_id, to_path)):
-                    changed_content = True
+                # XXX: Yes, the indentation below is wrong. But fixing it broke
+                # test_merge.TestMergerEntriesLCAOnDisk.
+                # test_nested_tree_subtree_renamed_and_modified. We'll wait for
+                # the fix from bzr.dev -- vila 2009026
+                elif from_kind == 'tree-reference':
+                    if (self.source.get_reference_revision(file_id, from_path)
+                        != self.target.get_reference_revision(file_id, to_path)):
+                        changed_content = True
             parent = (from_parent, to_entry.parent_id)
             name = (from_name, to_entry.name)
             executable = (from_executable, to_executable)
