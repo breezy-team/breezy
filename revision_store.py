25d7ee286d4a964076ef0a9b53642c4cb580b3af
Ian Clatworthy <ian.clatworthy@canonical.com>
2009-03-09 21:32:27 +1000

    parent_id_to_basename_index is no longer a serializer attribute - always required now

--- revision_store.py
+++ revision_store.py
@@ -58,7 +58,6 @@ class AbstractRevisionStore(object):
         serializer = self.repo._format._serializer
         search_key_name = serializer.search_key_name
         maximum_size = serializer.maximum_size
-        parent_id_basename_index = serializer.parent_id_basename_index
 
         # Maybe the rest of this ought to be part of the CHKInventory API?
         inv = inventory.CHKInventory(search_key_name)
@@ -67,12 +66,11 @@ class AbstractRevisionStore(object):
         search_key_func = chk_map.search_key_registry.get(search_key_name)
         inv.id_to_entry = chk_map.CHKMap(chk_store, None, search_key_func)
         inv.id_to_entry._root_node.set_maximum_size(maximum_size)
-        if parent_id_basename_index:
-            inv.parent_id_basename_to_file_id = chk_map.CHKMap(chk_store,
-                None, search_key_func)
-            inv.parent_id_basename_to_file_id._root_node.set_maximum_size(
-                maximum_size)
-            inv.parent_id_basename_to_file_id._root_node._key_width = 2
+        inv.parent_id_basename_to_file_id = chk_map.CHKMap(chk_store,
+            None, search_key_func)
+        inv.parent_id_basename_to_file_id._root_node.set_maximum_size(
+            maximum_size)
+        inv.parent_id_basename_to_file_id._root_node._key_width = 2
         return inv
 
     def get_inventory(self, revision_id):
