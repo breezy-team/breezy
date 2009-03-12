4cf3e9e468fd60bf7912ab3d85fb4643e1102080
Martin Pool <mbp@sourcefrog.net>
2009-03-12 17:26:03 +1000

    Transport activity now shows scheme and direction

--- bzrlib/ui/text.py
+++ bzrlib/ui/text.py
@@ -86,7 +86,8 @@ class TextUIFactory(CLIUIFactory):
         This may update a progress bar, spinner, or similar display.
         By default it does nothing.
         """
-        self._progress_view.show_transport_activity(byte_count)
+        self._progress_view._show_transport_activity(transport,
+            direction, byte_count)
 
     def _progress_updated(self, task):
         """A task has been updated and wants to be displayed.
@@ -154,8 +155,11 @@ class TextProgressView(object):
             spin_str =  r'/-\|'[self._spin_pos % 4]
             self._spin_pos += 1
             cols = 20
-            completion_fraction = \
-                self._last_task._overall_completion_fraction() or 0
+            if self._last_task is None:
+                completion_fraction = 0
+            else:
+                completion_fraction = \
+                    self._last_task._overall_completion_fraction() or 0
             markers = int(round(float(cols) * completion_fraction)) - 1
             bar_str = '[' + ('#' * markers + spin_str).ljust(cols) + '] '
             return bar_str
@@ -218,8 +222,8 @@ class TextProgressView(object):
         self._last_repaint = now
         self._repaint()
 
-    def show_transport_activity(self, byte_count):
-        """Called by transports as they do IO.
+    def _show_transport_activity(self, transport, direction, byte_count):
+        """Called by transports via the ui_factory, as they do IO.
 
         This may update a progress bar, spinner, or similar display.
         By default it does nothing.
@@ -236,8 +240,15 @@ class TextProgressView(object):
             # guard against clock stepping backwards, and don't update too
             # often
             rate = self._bytes_since_update / (now - self._transport_update_time)
-            msg = ("%6dkB @ %4dkB/s" %
-                (self._total_byte_count>>10, int(rate)>>10,))
+            scheme = getattr(transport, '_scheme', None) or repr(transport)
+            if direction == 'read':
+                dir_char = '>'
+            elif direction == 'write':
+                dir_char = '<'
+            else:
+                dir_char = '?'
+            msg = ("%.7s %s %6dkB %5dkB/s" %
+                    (scheme, dir_char, self._total_byte_count>>10, int(rate)>>10,))
             self._transport_update_time = now
             self._last_repaint = now
             self._bytes_since_update = 0
