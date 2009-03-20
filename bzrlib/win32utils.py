1f3b07f60e2df00ce6eea6d2101afc5921084250
John Arbash Meinel <john@arbash-meinel.com>
2009-03-20 10:06:20 -0500

    Some small fixes for the win32 'trace.debug_mem()' code.

--- bzrlib/win32utils.py
+++ bzrlib/win32utils.py
@@ -144,10 +144,22 @@ def debug_memory_win32api(message='', short=True):
         trace.note('Cannot debug memory on win32 without ctypes'
                    ' or win32process')
         return
+    if short:
+        trace.note('WorkingSize %7dK'
+                   '\tPeakWorking %7dK\t%s',
+                   info['WorkingSetSize'] / 1024,
+                   info['PeakWorkingSetSize'] / 1024,
+                   message)
+        return
+    if message:
+        trace.note('%s', message)
+    trace.note('WorkingSize %8dKiB'
+               '\tPeakWorking %8dKiB\t%s',
+               info['WorkingSetSize'] / 1024,
+               info['PeakWorkingSetSize'] / 1024,
+               message)
     trace.note('WorkingSize       %8d kB', info['WorkingSetSize'] / 1024)
     trace.note('PeakWorking       %8d kB', info['PeakWorkingSetSize'] / 1024)
-    if short:
-        return
     trace.note('PagefileUsage     %8d kB', info.get('PagefileUsage', 0) / 1024)
     trace.note('PeakPagefileUsage %8d kB', info.get('PeakPagefileUsage', 0) / 1024)
     trace.note('PrivateUsage      %8d kB', info.get('PrivateUsage', 0) / 1024)
