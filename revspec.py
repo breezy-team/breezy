dc4a21c175fa0c49f8e4510a3d97e76794ef0df4
Jelmer Vernooij <jelmer@samba.org>
2009-03-31 14:18:31 +0200

    Support short git revision specifiers.

--- revspec.py
+++ revspec.py
@@ -17,6 +17,7 @@
 
 from bzrlib.errors import (
     BzrError,
+    InvalidRevisionId,
     InvalidRevisionSpec,
     NoSuchRevision,
     )
@@ -28,6 +29,9 @@ from bzrlib.revisionspec import (
 from bzrlib.plugins.git import (
     lazy_check_versions,
     )
+from bzrlib.plugins.git.mapping import (
+    mapping_registry,
+    )
 
 class RevisionSpec_git(RevisionSpec):
     """Selects a revision using a Subversion revision number."""
@@ -37,16 +41,46 @@ class RevisionSpec_git(RevisionSpec):
     
     prefix = 'git:'
 
+    def _lookup_git_sha1(self, branch, sha1):
+        bzr_revid = branch.mapping.revision_id_foreign_to_bzr(git_sha1)
+        if branch.repository.has_revision(bzr_revid):
+            history = self._history(branch, bzr_revid)
+            return RevisionInfo.from_revision_id(branch, bzr_revid, history)
+        raise InvalidRevisionSpec(self.user_spec, branch)
+
+    def _history(self, branch, revid):
+        history = list(branch.repository.iter_reverse_revision_history(revid))
+        history.reverse()
+        return history
+
+    def _find_short_git_sha1(self, branch, sha1):
+        parse_revid = getattr(branch.repository, "lookup_git_revid",
+                              mapping_registry.parse_revision_id)
+        branch.repository.lock_read()
+        try:
+            graph = branch.repository.get_graph()
+            for revid, _ in graph.iter_ancestry([branch.last_revision()]):
+                try:
+                    foreign_revid, mapping = parse_revid(revid)
+                except InvalidRevisionId:
+                    continue
+                if foreign_revid.startswith(sha1):
+                    history = self._history(branch, revid)
+                    return RevisionInfo.from_revision_id(branch, revid, history)
+            raise InvalidRevisionSpec(self.user_spec, branch)
+        finally:
+            branch.repository.unlock()
+
     def _match_on(self, branch, revs):
         lazy_check_versions()
         loc = self.spec.find(':')
         git_sha1 = self.spec[loc+1:].encode("utf-8")
-        bzr_revid = branch.mapping.revision_id_foreign_to_bzr(git_sha1)
-        if branch.repository.has_revision(bzr_revid):
-            history = list(branch.repository.iter_reverse_revision_history(bzr_revid))
-            history.reverse()
-            return RevisionInfo.from_revision_id(branch, bzr_revid, history)
-        raise InvalidRevisionSpec(self.user_spec, branch)
+        if len(git_sha1) == 40:
+            return self._lookup_git_sha1(branch, git_sha1)
+        elif len(git_sha1) > 40:
+            raise InvalidRevisionSpec(self.user_spec, branch)
+        else:
+            return self._find_short_git_sha1(branch, git_sha1)
 
     def needs_branch(self):
         return True
