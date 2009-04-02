a94ca3161f43f18a179f17cfb81febe02ea2ed11
James Westby <james.westby@canonical.com>
2009-04-02 16:11:50 +0100

    Bump minimum bzrlib api version

--- meta.py
+++ meta.py
@@ -23,6 +23,6 @@ __version__ = '.'.join([str(x) for x in version_info[:3]])
 if version_info[3] != 'final':
     __version__ = "%s%s%d" % (__version__, version_info[3], version_info[4])
 
-bzr_minimum_api = (0, 18, 0)
+bzr_minimum_api = (1, 13, 0)
 
 bzr_commands = [ 'bisect' ]
