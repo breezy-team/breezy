0282fb6bd41a7aa40f210526891fea0a583a274d
John Arbash Meinel <john@arbash-meinel.com>
2009-04-08 15:23:07 -0500

    Add slots to _LeafNode and _InternalNode. We don't have many InternalNodes, but it is easy to make the change. We can have up to 1000 LeafNodes per btree index, and up to ~40 btree indices in practice (though you won't have 1k leaves per btree at that point.) Anyway, 100bytes * 40k leaf nodes is 4MB saved. At the point you have 40k leaf nodes, this is pretty small, but still worth doing.

--- bzrlib/btree_index.py
+++ bzrlib/btree_index.py
@@ -590,6 +590,8 @@ class BTreeBuilder(index.GraphIndexBuilder):
 class _LeafNode(object):
     """A leaf node for a serialised B+Tree index."""
 
+    __slots__ = ('keys')
+
     def __init__(self, bytes, key_length, ref_list_length):
         """Parse bytes to create a leaf node object."""
         # splitlines mangles the \r delimiters.. don't use it.
@@ -599,6 +601,8 @@ class _LeafNode(object):
 
 class _InternalNode(object):
     """An internal node for a serialised B+Tree index."""
+
+    __slots__ = ('keys', 'offset')
 
     def __init__(self, bytes):
         """Parse bytes to create an internal node object."""
