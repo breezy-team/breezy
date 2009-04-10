466c8a2333c0e0f262185b4b1f57fa5f8c7d17a6
Vincent Ladeuil <v.ladeuil+lp@free.fr>
2009-04-10 13:30:19 +0200

    Fix misleading signature and one typo.
    * bzrlib/repofmt/groupcompress_repo.py: 
    Fix typo.
    
    * bzrlib/inventory.py:
    (Inventory.has_filename): 'names' parameter renamed to
    'filename' (singular) to conform with all other implementations.

--- bzrlib/repofmt/groupcompress_repo.py
+++ bzrlib/repofmt/groupcompress_repo.py
@@ -14,7 +14,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-"""Repostory formats using CHK inventories and groupcompress compression."""
+"""Repository formats using CHK inventories and groupcompress compression."""
 
 import time
 
