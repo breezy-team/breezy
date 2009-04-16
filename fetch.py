099ddc1b4a850a308d36f5be0ae7414a16ddfe39
Jelmer Vernooij <jelmer@samba.org>
2009-04-16 23:05:30 +0200

    Store object hex sha's in InventoryEntry.text_id during fetch.

--- fetch.py
+++ fetch.py
@@ -135,6 +135,7 @@ def import_git_blob(texts, mapping, path, hexsha, base_inv, parent_id,
     # We just have to hope this is indeed utf-8:
     ie = cls(file_id, urlutils.basename(path).decode("utf-8"), parent_id)
     ie.executable = executable
+    ie.text_id = hexsha
     # See if this has changed at all
     try:
         base_ie = base_inv[file_id]
@@ -142,6 +143,8 @@ def import_git_blob(texts, mapping, path, hexsha, base_inv, parent_id,
         base_ie = None
         base_sha = None
     else:
+        if base_ie.text_id == hexsha:
+            return [], []
         try:
             base_sha = shagitmap.lookup_blob(file_id, base_ie.revision)
         except KeyError:
@@ -212,6 +215,7 @@ def import_git_tree(texts, mapping, path, hexsha, base_inv, parent_id,
     # We just have to hope this is indeed utf-8:
     ie = InventoryDirectory(file_id, urlutils.basename(path.decode("utf-8")), 
         parent_id)
+    ie.text_id = hexsha
     try:
         base_ie = base_inv[file_id]
     except NoSuchId:
@@ -221,6 +225,8 @@ def import_git_tree(texts, mapping, path, hexsha, base_inv, parent_id,
         texts.add_lines((file_id, ie.revision), (), [])
         invdelta.append((None, path, file_id, ie))
     else:
+        if base_ie.text_id == hexsha:
+            return [], {}, []
         # See if this has changed at all
         try:
             base_sha = shagitmap.lookup_tree(file_id, base_inv.revision_id)
