5ece978e1ce911abfa601dcee63873fd0e017c9b
Aaron Bentley <aaron@aaronbentley.com>
2009-04-30 10:04:38 -0400

    Update from review

--- bzrlib/tests/blackbox/test_reference.py
+++ bzrlib/tests/blackbox/test_reference.py
@@ -14,11 +14,13 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+
 from bzrlib import (
     branch as _mod_branch,
     bzrdir,
     )
 from bzrlib.tests import TestCaseWithTransport
+
 
 class TestReference(TestCaseWithTransport):
 
