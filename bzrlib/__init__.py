5433ec3e513f348689d38d95c5dd188677ee56f5
Andrew Bennetts <andrew.bennetts@canonical.com>
2009-05-04 10:09:26 +1000

    Bump api_minimum_version because of the removal of InstallFailed.

--- bzrlib/__init__.py
+++ bzrlib/__init__.py
@@ -53,7 +53,7 @@ __copyright__ = "Copyright 2005, 2006, 2007, 2008, 2009 Canonical Ltd."
 version_info = (1, 15, 0, 'dev', 0)
 
 # API compatibility version: bzrlib is currently API compatible with 1.13.
-api_minimum_version = (1, 13, 0)
+api_minimum_version = (1, 15, 0)
 
 def _format_version_tuple(version_info):
     """Turn a version number 2, 3 or 5-tuple into a short string.
