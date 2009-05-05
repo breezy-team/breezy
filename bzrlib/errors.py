b88b4456184a76ccbecdb3b31e256db7e7b6e8c5
Jelmer Vernooij <jelmer@samba.org>
2009-05-05 12:35:27 +0200

    Remove extra space.

--- bzrlib/errors.py
+++ bzrlib/errors.py
@@ -1174,7 +1174,7 @@ class DivergedBranches(BzrError):
 
     _fmt = ("These branches have diverged."
             " Use the missing command to see how.\n"
-            " Use the merge command to reconcile them.")
+            "Use the merge command to reconcile them.")
 
     def __init__(self, branch1, branch2):
         self.branch1 = branch1
