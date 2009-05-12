2e2aa1489477476cae51119d03b9ea6b714371c5
John Arbash Meinel <john@arbash-meinel.com>
2009-05-11 21:24:54 -0500

    Change PendingAncestryResult to strip ghosts from .get_keys()

--- bzrlib/graph.py
+++ bzrlib/graph.py
@@ -1556,7 +1556,7 @@ class PendingAncestryResult(object):
     def _get_keys(self, graph):
         NULL_REVISION = revision.NULL_REVISION
         keys = [key for (key, parents) in graph.iter_ancestry(self.heads)
-                if key != NULL_REVISION]
+                if key != NULL_REVISION and parents is not None]
         return keys
 
     def is_empty(self):
