52c51dae984d0f6f16b8f26ba76b742950c6a5dd
John Ferlito <johnf@inodes.org>
2009-05-25 20:59:42 +1000

    Update packaging scripts to make deployment a bit easier Update documentation for deploying to PPA

--- tools/packaging/update-control.sh
+++ tools/packaging/update-control.sh
@@ -7,27 +7,25 @@ if [ -z "$UBUNTU_RELEASES" ]; then
     exit 1
 fi
 
-if [ "x$VERSION" = "x" ]; then
+OLD_VERSION=$1
+NEW_VERSION=$2
+NEXT_VERSION=$3
+if [ -z "$OLD_VERSION" -o -z "$NEW_VERSION" -o -z "$NEXT_VERSION" ]; then
     echo "Missing version"
     echo "You want something like:"
-    echo "  VERSION=1.6~rc1-1~bazaar1 update-changelogs.sh"
-    echo "or"
-    echo "  VERSION=1.6-1~bazaar1 update-changelogs.sh"
+    echo "  update-control.sh 1.5 1.6 1.7"
     exit
 fi
 
-if [ -z "$1" ]; then
-    MSG="New upstream release"
-else
-    MSG=$1
+if [ "$PACKAGE" = "bzr" ]; then
+    continue
 fi
-
 for DISTRO in $UBUNTU_RELEASES; do
     PPAVERSION="$VERSION~${DISTRO}1"
     (
-        echo "Updating changelog for $DISTRO"
+        echo "Updating ccontrol for $DISTRO"
         cd "$PACKAGE-$DISTRO" &&
-            dch -v $PPAVERSION -D $DISTRO -c changelog "$MSG." &&
-            bzr commit -m "$MSG: $PPAVERSION"
+            sed -i -e "s/$NEW_VERSION~/$NEXT_VERSION~/;s/$OLD_VERSION~/$NEW_VERSION~/" control &&
+            bzr commit -m "New upstream release: $PPAVERSION, update control"
     )
 done
