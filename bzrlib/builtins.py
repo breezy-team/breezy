c5434d23b43de2f0da63ec74d62f66eed4d41035
Vincent Ladeuil <v.ladeuil+lp@free.fr>
2009-06-30 17:54:23 +0200

    Fixed as per jam's review.
    * tests/blackbox/test_push.py:
    (load_tests): One more changes type.
    (TestPushStrictMixin): Switch to using mixin, it's clearer.
    Define some default values as attributes so that they can be
    changed more easily.
    (TestPushStrictMixin.assertPushFails,
    TestPushStrictMixin.assertPushSucceeds): Use default values from
    attributes.
    (TestPushStrictWithChanges._out_of_sync_trees): Setup a new
    context and change the default values.
    
    * errors.py:
    (OutOfDateTree, UncommittedChanges): Add a more parameter so more
    details can be given.
    
    * builtins.py:
    (cmd_push.run): Add an additional check that the tree is still in
    sync with its branch before pushing.

--- bzrlib/builtins.py
+++ bzrlib/builtins.py
@@ -1067,7 +1067,14 @@ class cmd_push(Command):
             and (strict is None or strict)): # Default to True:
             changes = tree.changes_from(tree.basis_tree())
             if changes.has_changed() or len(tree.get_parent_ids()) > 1:
-                raise errors.UncommittedChanges(tree)
+                raise errors.UncommittedChanges(
+                    tree, more='Use --no-strict to force the push.')
+            if tree.last_revision() != tree.branch.last_revision():
+                # The tree has lost sync with its branch, there is little
+                # chance that the user is aware of it but he can still force
+                # the push with --no-strict
+                raise errors.OutOfDateTree(
+                    tree, more='Use --no-strict to force the push.')
 
         # Get the stacked_on branch, if any
         if stacked_on is not None:
