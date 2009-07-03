d5368bbeea571c0358f4234ea2f32d66c70619ae
Jonathan Lange <jml@canonical.com>
2009-07-03 15:09:09 +0100

    Remove unused import

--- bzrlib/plugins/launchpad/lp_registration.py
+++ bzrlib/plugins/launchpad/lp_registration.py
@@ -15,7 +15,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 
-from getpass import getpass
 import os
 from urlparse import urlsplit, urlunsplit
 import urllib
