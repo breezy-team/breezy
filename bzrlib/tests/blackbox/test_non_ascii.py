9ca669b95eb43d80359beddc5518c6b5066e27d0
Vincent Ladeuil <v.ladeuil+lp@free.fr>
2009-07-06 09:44:10 +0200

    Use KnownFailure so that XFAIL tests are displayed.
    * bzrlib/tests/blackbox/test_non_ascii.py:
    (TestNonAscii._check_OSX_can_roundtrip): Raise KnownFailure
    instead of TestSkipped to give better exposure.

--- bzrlib/tests/blackbox/test_non_ascii.py
+++ bzrlib/tests/blackbox/test_non_ascii.py
@@ -94,7 +94,7 @@ class TestNonAscii(tests.TestCaseWithTransport):
             normal_thing = unicodedata.normalize('NFD', path)
             mac_encoded = normal_thing.encode(fs_enc)
             if mac_encoded != encoded:
-                raise tests.TestSkipped(
+                raise tests.KnownFailure(
                     'Unable to roundtrip path %r on OSX filesystem'
                     ' using encoding "%s"'
                     % (path, fs_enc))
