71f4120d9e4a4d41142269356d3f19b07e421ad8
John Arbash Meinel <john@arbash-meinel.com>
2009-07-06 14:05:26 -0500

    Add a test that shows when parent texts, etc, are present, we don't request again.

--- bzrlib/tests/test__annotator.py
+++ bzrlib/tests/test__annotator.py
@@ -259,6 +259,35 @@ class TestAnnotator(tests.TestCaseWithMemoryTransport):
                          ], sorted(keys))
         self.assertEqual([spec_key], sorted(ann_keys))
 
+    def test_needed_keys_with_parent_texts(self):
+        self.make_many_way_common_merge_text()
+        # If 'D' and 'E' are already annotated, we don't need to extract all
+        # the texts
+        #  D   |  'simple|new content|'
+        #  |   |
+        #  |   E  'simple|new content|'
+        #  |  /
+        #  F-'    'simple|new content|'
+        self.ann._parent_map[self.fd_key] = (self.fb_key, self.fc_key)
+        self.ann._text_cache[self.fd_key] = ['simple\n', 'new content\n']
+        self.ann._annotations_cache[self.fd_key] = [
+            (self.fa_key,),
+            (self.fb_key, self.fc_key),
+            ]
+        self.ann._parent_map[self.fe_key] = (self.fa_key,)
+        self.ann._text_cache[self.fe_key] = ['simple\n', 'new content\n']
+        self.ann._annotations_cache[self.fe_key] = [
+            (self.fa_key,),
+            (self.fe_key,),
+            ]
+        keys, ann_keys = self.ann._get_needed_keys(self.ff_key)
+        self.assertEqual([self.ff_key], sorted(keys))
+        self.assertEqual({self.fd_key: 1,
+                          self.fe_key: 1,
+                          self.ff_key: 1,
+                         }, self.ann._num_needed_children)
+        self.assertEqual([], sorted(ann_keys))
+
     def test_record_annotation_removes_texts(self):
         self.make_many_way_common_merge_text()
         # Populate the caches
