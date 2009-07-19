bd0dae1c03ebfed4563ee85e1cd9c9af586e7a76
Aaron Bentley <aaron@aaronbentley.com>
2009-07-19 00:47:49 -0400

    Fix tabs.

--- bzrlib/tests/blackbox/test_merge.py
+++ bzrlib/tests/blackbox/test_merge.py
@@ -584,12 +584,12 @@ class TestMerge(ExternalBase):
                          list(this_tree.iter_changes(this_tree.basis_tree())))
 
     def test_merge_missing_second_revision_spec(self):
-	"""Merge uses branch basis when the second revision is unspecified."""
-	this = self.make_branch_and_tree('this')
-	this.commit('rev1')
-	other = self.make_branch_and_tree('other')
-	self.build_tree(['other/other_file'])
-	other.add('other_file')
-	other.commit('rev1b')
-	self.run_bzr('merge -d this other -r0..')
-	self.failUnlessExists('this/other_file')
+        """Merge uses branch basis when the second revision is unspecified."""
+        this = self.make_branch_and_tree('this')
+        this.commit('rev1')
+        other = self.make_branch_and_tree('other')
+        self.build_tree(['other/other_file'])
+        other.add('other_file')
+        other.commit('rev1b')
+        self.run_bzr('merge -d this other -r0..')
+        self.failUnlessExists('this/other_file')
