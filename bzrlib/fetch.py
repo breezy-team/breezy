8d778a1e952c5102ad108d2c228ffa33988622be
Andrew Bennetts <andrew.bennetts@canonical.com>
2009-08-07 14:29:36 +1000

    Elaborate some docstrings.

--- bzrlib/fetch.py
+++ bzrlib/fetch.py
@@ -260,6 +260,19 @@ class Inter1and2Helper(object):
 
 def _new_root_data_stream(
     root_keys_to_create, rev_id_to_root_id_map, parent_map, repo, graph=None):
+    """Generate a texts substream of synthesised root entries.
+
+    Used in fetches that do rich-root upgrades.
+    
+    :param root_keys_to_create: iterable of (root_id, rev_id) pairs describing
+        the root entries to create.
+    :param rev_id_to_root_id_map: dict of known rev_id -> root_id mappings for
+        calculating the parents.  If a parent rev_id is not found here then it
+        will be recalculated.
+    :param parent_map: a parent map for all the revisions in
+        root_keys_to_create.
+    :param graph: a graph to use instead of repo.get_graph().
+    """
     for root_key in root_keys_to_create:
         root_id, rev_id = root_key
         parent_keys = _parent_keys_for_root_version(
@@ -270,7 +283,10 @@ def _new_root_data_stream(
 
 def _parent_keys_for_root_version(
     root_id, rev_id, rev_id_to_root_id_map, parent_map, repo, graph=None):
-    """Get the parent keys for a given root id."""
+    """Get the parent keys for a given root id.
+    
+    A helper function for _new_root_data_stream.
+    """
     # Include direct parents of the revision, but only if they used the same
     # root_id and are heads.
     rev_parents = parent_map[rev_id]
