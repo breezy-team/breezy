b430fdf71981041d2d400bfacc259f9a33c79232
Ian Clatworthy <ian.clatworthy@canonical.com>
2009-09-03 12:29:16 +1000

    minor tweaks for better chm

--- doc/conf.py
+++ doc/conf.py
@@ -123,7 +123,7 @@ html_theme_options = {
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = release + " Table of Contents"
+html_short_title = "Table of Contents (%s)" % (release,)
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
