46df7d9cc3ec4484244352145aee2323e0065f89
Ian Clatworthy <ian.clatworthy@canonical.com>
2009-09-07 16:12:15 +1000

    cleanups for Spanish docs

--- doc/es/conf.py
+++ doc/es/conf.py
@@ -35,12 +35,12 @@ bzr_team = u'Bazaar Developers'
 
 # Translations
 bzr_titles = {
-        u'Table of Contents (%s)': None,
-        u'Bazaar User Guide': None,
+        u'Table of Contents (%s)': u'Contenidos (%s)',
+        u'Bazaar User Guide': u'Guia de Usuario',
         u'Bazaar User Reference': None,
         u'Bazaar Release Notes': None,
         u'Bazaar Upgrade Guide': None,
-        u'Bazaar in five minutes': None,
+        u'Bazaar in five minutes': u'Bazaar en cinco minutos',
         u'Bazaar Tutorial': None,
         u'Using Bazaar With Launchpad': None,
         u'Centralized Workflow Tutorial': None,
@@ -223,25 +223,25 @@ htmlhelp_basename = 'bzr-%s-user-docs' % (bzr_locale,)
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   # Manuals
-  ('user-guide/index', 'bzr-%s-user-guide.tex' % (bzr_locale,),
-    bzr_title(u'Bazaar User Guide'), bzr_team, 'manual'),
-  ('user-reference/bzr_man', 'bzr-%s-user-reference.tex' % (bzr_locale,),
-    bzr_title(u'Bazaar User Reference'), bzr_team, 'manual'),
-  ('release-notes/NEWS', 'bzr-%s-release-notes.tex' % (bzr_locale,),
-    bzr_title(u'Bazaar Release Notes'), bzr_team, 'manual'),
-  ('upgrade-guide/index', 'bzr-%s-upgrade-guide.tex' % (bzr_locale,),
-    bzr_title(u'Bazaar Upgrade Guide'), bzr_team, 'manual'),
+  #('user-guide/index', 'bzr-%s-user-guide.tex' % (bzr_locale,),
+  #  bzr_title(u'Bazaar User Guide'), bzr_team, 'manual'),
+  #('user-reference/bzr_man', 'bzr-%s-user-reference.tex' % (bzr_locale,),
+  #  bzr_title(u'Bazaar User Reference'), bzr_team, 'manual'),
+  #('release-notes/NEWS', 'bzr-%s-release-notes.tex' % (bzr_locale,),
+  #  bzr_title(u'Bazaar Release Notes'), bzr_team, 'manual'),
+  #('upgrade-guide/index', 'bzr-%s-upgrade-guide.tex' % (bzr_locale,),
+  #  bzr_title(u'Bazaar Upgrade Guide'), bzr_team, 'manual'),
   # Tutorials
   ('mini-tutorial/index', 'bzr-%s-tutorial-mini.tex' % (bzr_locale,),
     bzr_title(u'Bazaar in five minutes'), bzr_team, 'howto'),
-  ('tutorials/tutorial', 'bzr-%s-tutorial.tex' % (bzr_locale,),
-    bzr_title(u'Bazaar Tutorial'), bzr_team, 'howto'),
-  ('tutorials/using_bazaar_with_launchpad',
-    'bzr-%s-tutorial-with-launchpad.tex' % (bzr_locale,),
-    bzr_title(u'Using Bazaar With Launchpad'), bzr_team, 'howto'),
-  ('tutorials/centralized_workflow',
-    'bzr-%s-tutorial-centralized.tex' % (bzr_locale,),
-    bzr_title(u'Centralized Workflow Tutorial'), bzr_team, 'howto'),
+  #('tutorials/tutorial', 'bzr-%s-tutorial.tex' % (bzr_locale,),
+  #  bzr_title(u'Bazaar Tutorial'), bzr_team, 'howto'),
+  #('tutorials/using_bazaar_with_launchpad',
+  #  'bzr-%s-tutorial-with-launchpad.tex' % (bzr_locale,),
+  #  bzr_title(u'Using Bazaar With Launchpad'), bzr_team, 'howto'),
+  #('tutorials/centralized_workflow',
+  #  'bzr-%s-tutorial-centralized.tex' % (bzr_locale,),
+  #  bzr_title(u'Centralized Workflow Tutorial'), bzr_team, 'howto'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
