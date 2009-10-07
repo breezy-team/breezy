54a784a6a89446ab2c051968bc014af6effff9cd
Jelmer Vernooij <jelmer@samba.org>
2009-10-07 14:14:14 +0200

    Add docstring.

--- send.py
+++ send.py
@@ -18,6 +18,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+"""Support in "bzr send" for git-am style patches."""
+
 import time
 import bzrlib
 from bzrlib import (
