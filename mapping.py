397d9103a4b2a96ececf1a19ea30fb05f04218d0
Jelmer Vernooij <jelmer@samba.org>
2009-10-18 17:50:19 +0200

    Allow single place for configuration of default mapping.

--- mapping.py
+++ mapping.py
@@ -266,6 +266,7 @@ mapping_registry.register_lazy('git-v1', "bzrlib.plugins.git.mapping",
                                    "BzrGitMappingv1")
 mapping_registry.register_lazy('git-experimental', "bzrlib.plugins.git.mapping",
                                    "BzrGitMappingExperimental")
+mapping_registry.set_default('git-v1')
 
 
 class ForeignGit(ForeignVcs):
@@ -280,7 +281,7 @@ class ForeignGit(ForeignVcs):
 
 
 foreign_git = ForeignGit()
-default_mapping = BzrGitMappingv1()
+default_mapping = mapping_registry.get_default()()
 
 
 def text_to_blob(texts, entry):
