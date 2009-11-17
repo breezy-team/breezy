f8eb0a9c42cd9eaa7e0368141298a3b2899d584f
Gordon Tyler <gordon@doxxx.net>
2009-11-16 22:54:52 -0500

    Reverted cmd_mv back to using self.outf.write but checking for is_quiet.

--- bzrlib/builtins.py
+++ bzrlib/builtins.py
@@ -850,7 +850,8 @@ class cmd_mv(Command):
             # for cicp file-systems.
             rel_names = tree.get_canonical_inventory_paths(rel_names)
             for src, dest in tree.move(rel_names[:-1], rel_names[-1], after=after):
-                note("%s => %s", src, dest)
+                if not is_quiet():
+                    self.outf.write("%s => %s\n" % (src, dest))
         else:
             if len(names_list) != 2:
                 raise errors.BzrCommandError('to mv multiple files the'
@@ -900,7 +901,8 @@ class cmd_mv(Command):
             dest = osutils.pathjoin(dest_parent, dest_tail)
             mutter("attempting to move %s => %s", src, dest)
             tree.rename_one(src, dest, after=after)
-            note("%s => %s", src, dest)
+            if not is_quiet():
+                self.outf.write("%s => %s\n" % (src, dest))
 
 
 class cmd_pull(Command):
