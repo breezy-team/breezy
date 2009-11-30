5a8c3a9ed2ffce0eca7ea3742120423474156b7d
John Arbash Meinel <john@arbash-meinel.com>
2009-11-29 21:29:36 -0600

    Factor out the common code into a helper so that smart streaming also benefits.

--- bzrlib/repository.py
+++ bzrlib/repository.py
@@ -26,6 +26,7 @@ from bzrlib import (
     chk_map,
     debug,
     errors,
+    fetch as _mod_fetch,
     fifo_cache,
     generate_ids,
     gpg,
@@ -38,7 +39,6 @@ from bzrlib import (
     lru_cache,
     osutils,
     revision as _mod_revision,
-    static_tuple,
     symbol_versioning,
     tsort,
     ui,
@@ -3413,8 +3413,7 @@ class InterRepository(InterObject):
                    provided a default one will be created.
         :return: None.
         """
-        from bzrlib.fetch import RepoFetcher
-        f = RepoFetcher(to_repository=self.target,
+        f = _mod_fetch.RepoFetcher(to_repository=self.target,
                                from_repository=self.source,
                                last_revision=revision_id,
                                fetch_spec=fetch_spec,
@@ -3820,13 +3819,15 @@ class InterDifferingSerializer(InterRepository):
                 basis_id, delta, current_revision_id, parents_parents)
             cache[current_revision_id] = parent_tree
 
-    def _fetch_batch(self, revision_ids, basis_id, cache, graph=None):
+    def _fetch_batch(self, revision_ids, basis_id, cache, a_graph=None):
         """Fetch across a few revisions.
 
         :param revision_ids: The revisions to copy
         :param basis_id: The revision_id of a tree that must be in cache, used
             as a basis for delta when no other base is available
         :param cache: A cache of RevisionTrees that we can use.
+        :param a_graph: A Graph object to determine the heads() of the
+            rich-root data stream.
         :return: The revision_id of the last converted tree. The RevisionTree
             for it will be in cache
         """
@@ -3896,10 +3897,9 @@ class InterDifferingSerializer(InterRepository):
         from_texts = self.source.texts
         to_texts = self.target.texts
         if root_keys_to_create:
-            from bzrlib.fetch import _new_root_data_stream
-            root_stream = _new_root_data_stream(
+            root_stream = _mod_fetch._new_root_data_stream(
                 root_keys_to_create, self._revision_id_to_root_id, parent_map,
-                self.source, graph=graph)
+                self.source, graph=a_graph)
             to_texts.insert_record_stream(root_stream)
         to_texts.insert_record_stream(from_texts.get_record_stream(
             text_keys, self.target._format._fetch_order,
@@ -3962,21 +3962,11 @@ class InterDifferingSerializer(InterRepository):
         cache[basis_id] = basis_tree
         del basis_tree # We don't want to hang on to it here
         hints = []
-        if self._converting_to_rich_root:
-            st = static_tuple.StaticTuple
-            revision_keys = [st(r_id).intern() for r_id in revision_ids]
-            known_graph = self.source.revisions.get_known_graph_ancestry(
-                            revision_keys)
-            class ThunkIdsToKeysHeads(object):
-                def __init__(self, kg):
-                    self.kg = kg
-                def heads(self, revision_ids):
-                    revision_keys = [(r,) for r in revision_ids]
-                    heads = self.kg.heads(revision_keys)
-                    return set([h[0] for h in heads])
-            graph = ThunkIdsToKeysHeads(known_graph)
+        if self._converting_to_rich_root and len(revision_ids) > 100:
+            a_graph = _mod_fetch._get_rich_root_heads_graph(self.source,
+                                                            revision_ids)
         else:
-            graph = None
+            a_graph = None
 
         for offset in range(0, len(revision_ids), batch_size):
             self.target.start_write_group()
@@ -3985,7 +3975,7 @@ class InterDifferingSerializer(InterRepository):
                           len(revision_ids))
                 batch = revision_ids[offset:offset+batch_size]
                 basis_id = self._fetch_batch(batch, basis_id, cache,
-                                             graph=graph)
+                                             a_graph=the_graph)
             except:
                 self.target.abort_write_group()
                 raise
@@ -4464,8 +4454,7 @@ class StreamSource(object):
         fetching the inventory weave.
         """
         if self._rich_root_upgrade():
-            import bzrlib.fetch
-            return bzrlib.fetch.Inter1and2Helper(
+            return _mod_fetch.Inter1and2Helper(
                 self.from_repository).generate_root_texts(revs)
         else:
             return []
