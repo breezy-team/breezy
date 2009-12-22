14f9e91f35927eada88cdce80f0a94b1b674d828
John Arbash Meinel <john@arbash-meinel.com>
2009-12-22 09:39:20 -0600

    Compatibly rename ApportFeature to features.apport.

--- bzrlib/tests/features.py
+++ bzrlib/tests/features.py
@@ -16,9 +16,12 @@
 
 
 from bzrlib import tests
+from bzrlib.symbol_versioning import deprecated_in
 
 
-ApportFeature = tests.ModuleAvailableFeature('apport')
+apport = tests.ModuleAvailableFeature('apport')
+ApportFeature = tests._CompatabilityThunkFeature('bzrlib.tests.features',
+    'ApportFeature', 'bzrlib.tests.features.apport', deprecated_in((2,1,0)))
 paramiko = tests.ModuleAvailableFeature('paramiko')
 pycurl = tests.ModuleAvailableFeature('pycurl')
 subunit = tests.ModuleAvailableFeature('subunit')
