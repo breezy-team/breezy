a31c618513a2dc4c69ff6644fdafb804634eebe2
Gordon Tyler <gordon@doxxx.net>
2010-01-01 20:11:03 -0500

    Added test_from_string_path_with_backslashes, which tests the actual scenario in bug 392428.

--- bzrlib/tests/test_diff.py
+++ bzrlib/tests/test_diff.py
@@ -1303,6 +1303,17 @@ class TestDiffFromTool(TestCaseWithTransport):
                          diff_obj.command_template)
         self.assertEqual(['diff', '-u 5', 'old-path', 'new-path'],
                          diff_obj._get_command('old-path', 'new-path'))
+        
+    def test_from_string_path_with_backslashes(self):
+        if sys.platform != 'win32':
+            raise TestSkipped('only win32 has paths with backslashes')
+        tool = 'C:\\Tools\\Diff.exe'
+        diff_obj = DiffFromTool.from_string(tool, None, None, None)
+        self.addCleanup(diff_obj.finish)
+        self.assertEqual(['C:\\Tools\\Diff.exe', '@old_path', '@new_path'],
+                         diff_obj.command_template)
+        self.assertEqual(['C:\\Tools\\Diff.exe', 'old-path', 'new-path'],
+                         diff_obj._get_command('old-path', 'new-path'))
 
     def test_execute(self):
         output = StringIO()
