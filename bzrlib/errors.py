67ccecd9097f12206559d07d7a168e9650cc8660
Robert Collins <robertc@robertcollins.net>
2010-01-08 17:33:05 +1100

    Adjust errors.py to fix missing references to 'warn'.

--- bzrlib/errors.py
+++ bzrlib/errors.py
@@ -1247,8 +1247,8 @@ class NotAncestor(BzrError):
 class AmbiguousBase(BzrError):
 
     def __init__(self, bases):
-        warn("BzrError AmbiguousBase has been deprecated as of bzrlib 0.8.",
-                DeprecationWarning)
+        symbol_versioning.warn("BzrError AmbiguousBase has been deprecated "
+            "as of bzrlib 0.8.", DeprecationWarning, stacklevel=2)
         msg = ("The correct base is unclear, because %s are all equally close"
                 % ", ".join(bases))
         BzrError.__init__(self, msg)
@@ -2950,9 +2950,10 @@ class HookFailed(BzrError):
     _fmt = ("Hook '%(hook_name)s' during %(hook_stage)s failed:\n"
             "%(traceback_text)s%(exc_value)s")
 
-    def __init__(self, hook_stage, hook_name, exc_info):
-        warn("BzrError HookFailed has been deprecated as of bzrlib 2.1.",
-                DeprecationWarning)
+    def __init__(self, hook_stage, hook_name, exc_info, warn=True):
+        if warn:
+            symbol_versioning.warn("BzrError HookFailed has been deprecated "
+                "as of bzrlib 2.1.", DeprecationWarning, stacklevel=2)
         import traceback
         self.hook_stage = hook_stage
         self.hook_name = hook_name
