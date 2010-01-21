a7f2bd8a64c793b2b95e9f8586f959ee93fb30fe
Vincent Ladeuil <v.ladeuil+lp@free.fr>
2010-01-21 19:15:08 +0100

    Remove duplicated code.

--- bzrlib/tests/test_log.py
+++ bzrlib/tests/test_log.py
@@ -1347,41 +1347,42 @@ class TestShowChangedRevisions(tests.TestCaseWithTransport):
 
 class TestLogFormatter(tests.TestCase):
 
+    def setUp(self):
+        super(TestLogFormatter, self).setUp()
+        self.rev = revision.Revision('a-id')
+        self.lf = log.LogFormatter(None)
+
     def test_short_committer(self):
-        rev = revision.Revision('a-id')
-        rev.committer = 'John Doe <jdoe@example.com>'
-        lf = log.LogFormatter(None)
-        self.assertEqual('John Doe', lf.short_committer(rev))
-        rev.committer = 'John Smith <jsmith@example.com>'
-        self.assertEqual('John Smith', lf.short_committer(rev))
-        rev.committer = 'John Smith'
-        self.assertEqual('John Smith', lf.short_committer(rev))
-        rev.committer = 'jsmith@example.com'
-        self.assertEqual('jsmith@example.com', lf.short_committer(rev))
-        rev.committer = '<jsmith@example.com>'
-        self.assertEqual('jsmith@example.com', lf.short_committer(rev))
-        rev.committer = 'John Smith jsmith@example.com'
-        self.assertEqual('John Smith', lf.short_committer(rev))
+        def assertCommitter(expected, committer):
+            self.rev.committer = committer
+            self.assertEqual(expected, self.lf.short_committer(self.rev))
+
+        assertCommitter('John Doe', 'John Doe <jdoe@example.com>')
+        assertCommitter('John Smith', 'John Smith <jsmith@example.com>')
+        assertCommitter('John Smith', 'John Smith')
+        assertCommitter('jsmith@example.com', 'jsmith@example.com')
+        assertCommitter('jsmith@example.com', '<jsmith@example.com>')
+        assertCommitter('John Smith', 'John Smith jsmith@example.com')
 
     def test_short_author(self):
-        rev = revision.Revision('a-id')
-        rev.committer = 'John Doe <jdoe@example.com>'
-        lf = log.LogFormatter(None)
-        self.assertEqual('John Doe', lf.short_author(rev))
-        rev.properties['author'] = 'John Smith <jsmith@example.com>'
-        self.assertEqual('John Smith', lf.short_author(rev))
-        rev.properties['author'] = 'John Smith'
-        self.assertEqual('John Smith', lf.short_author(rev))
-        rev.properties['author'] = 'jsmith@example.com'
-        self.assertEqual('jsmith@example.com', lf.short_author(rev))
-        rev.properties['author'] = '<jsmith@example.com>'
-        self.assertEqual('jsmith@example.com', lf.short_author(rev))
-        rev.properties['author'] = 'John Smith jsmith@example.com'
-        self.assertEqual('John Smith', lf.short_author(rev))
-        del rev.properties['author']
-        rev.properties['authors'] = ('John Smith <jsmith@example.com>\n'
-                'Jane Rey <jrey@example.com>')
-        self.assertEqual('John Smith', lf.short_author(rev))
+        def assertAuthor(expected, author):
+            self.rev.properties['author'] = author
+            self.assertEqual(expected, self.lf.short_author(self.rev))
+
+        assertAuthor('John Smith', 'John Smith <jsmith@example.com>')
+        assertAuthor('John Smith', 'John Smith')
+        assertAuthor('jsmith@example.com', 'jsmith@example.com')
+        assertAuthor('jsmith@example.com', '<jsmith@example.com>')
+        assertAuthor('John Smith', 'John Smith jsmith@example.com')
+
+    def test_short_author_from_committer(self):
+        self.rev.committer = 'John Doe <jdoe@example.com>'
+        self.assertEqual('John Doe', self.lf.short_author(self.rev))
+
+    def test_short_author_from_authors(self):
+        self.rev.properties['authors'] = ('John Smith <jsmith@example.com>\n'
+                                          'Jane Rey <jrey@example.com>')
+        self.assertEqual('John Smith', self.lf.short_author(self.rev))
 
 
 class TestReverseByDepth(tests.TestCase):
