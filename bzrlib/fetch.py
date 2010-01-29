2425af6c4b6103440d1dd97916f1250c260134f1
Martin Pool <mbp@sourcefrog.net>
2010-01-29 20:01:58 +0100

    Even trivial fixes can have silly typos

--- bzrlib/fetch.py
+++ bzrlib/fetch.py
@@ -254,7 +254,9 @@ class Inter1and2Helper(object):
         root_id_order.sort(key=operator.itemgetter(0))
         # Create a record stream containing the roots to create.
         if len(revs) > 100:
-            raph = _get_rich_root_heads_graph(self.source, revs)
+            # XXX: not covered by tests, should have a flag to always run
+            # this. -- mbp 20100129
+            graph = _get_rich_root_heads_graph(self.source, revs)
         new_roots_stream = _new_root_data_stream(
             root_id_order, rev_id_to_root_id, parent_map, self.source, graph)
         return [('texts', new_roots_stream)]
