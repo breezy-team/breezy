b677de7bc6dc7efac93e8fb08c5b08e973d44c23
Jelmer Vernooij <jelmer@samba.org>
2010-01-29 11:06:34 +0100

    Support committing to a git branch from a bzr working tree.

--- commit.py
+++ commit.py
@@ -21,6 +21,7 @@
 from dulwich.index import (
     commit_tree,
     )
+import os
 import stat
 
 from bzrlib.repository import (
@@ -93,7 +94,8 @@ class GitCommitBuilder(CommitBuilder):
                 mode |= 0111
             self._any_changes = True
             self._blobs[path[1].encode("utf-8")] = (mode, sha)
-            yield file_id, path, (None, None)
+            file_sha1 = workingtree.get_file_sha1(file_id, path[1])
+            yield file_id, path[1], (file_sha1, os.lstat(workingtree.abspath(path[1])))
         # Fill in entries that were not changed
         basis_tree = workingtree.basis_tree()
         assert basis_tree.get_revision_id() == basis_revid
