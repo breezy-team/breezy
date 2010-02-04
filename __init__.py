6a4e5aba1ee2e9463e9acf0ec07a647d40695860
Martin von Gagern <Martin.vGagern@gmx.net>
2010-02-04 19:51:09 +0100

    Added GPL 2 license document and copyright notice.
    The wording of the copyright notice in the source files is actually based on
    the one suggested by GPL 3, but with the version number modified to 2.  This
    avoids reference to the postal address in favour of a web URL.  The license
    wasn't completely migrated to GPL 3 in order to allowing inclusion into a
    GPL2-only code tree like bzr itself.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2009, 2010  Martin von Gagern
+#
+# This file is part of bzr-bash-completion
+#
+# bzr-bash-completion free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 2 of the
+# License, or (at your option) any later version.
+#
+# bzr-bash-completion is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """Generate a shell function for bash command line completion.
 
 This plugin provides a command called bash-completion that generates a
