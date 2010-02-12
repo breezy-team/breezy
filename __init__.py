79b27e1dc48416b66d9de21ceb9609fd1971d9c7
Jelmer Vernooij <jelmer@samba.org>
2010-02-12 14:29:05 +0100

    Use local repo if possible.

--- __init__.py
+++ __init__.py
@@ -129,22 +129,19 @@ class LocalGitBzrDirFormat(GitBzrDirFormat):
         """
         lazy_check_versions()
         # we dont grok readonly - git isn't integrated with transport.
-        from bzrlib.plugins.git.transportgit import TransportRepo
-        try:
+        from bzrlib.transport.local import LocalTransport
+        if isinstance(transport, LocalTransport):
+            import dulwich
+            gitrepo = dulwich.repo.Repo(transport.local_abspath(".").encode(osutils._fs_enc))
+        else:
+            from bzrlib.plugins.git.transportgit import TransportRepo
             gitrepo = TransportRepo(transport)
-        except bzr_errors.NotLocalUrl:
-            raise bzr_errors.NotBranchError(path=transport.base)
         from bzrlib.plugins.git.dir import LocalGitDir, GitLockableFiles, GitLock
         lockfiles = GitLockableFiles(transport, GitLock())
         return LocalGitDir(transport, lockfiles, gitrepo, self)
 
     @classmethod
     def probe_transport(klass, transport):
-        # This should quickly filter out most things that are not
-        # git repositories, saving us the trouble from loading dulwich.
-        if not transport.has(".git") and not transport.has("objects"):
-            raise bzr_errors.NotBranchError(path=transport.base)
-
         lazy_check_versions()
         import dulwich
         format = klass()
