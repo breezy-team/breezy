879b84fa20f791bbde0aa35b905eea4f297aa37b
Parth Malwankar <parth.malwankar@gmail.com>
2010-02-23 19:44:02 +0530

    basic file grep is working

--- __init__.py
+++ __init__.py
@@ -16,7 +16,9 @@
 """bzr grep"""
 
 import os
+import sys
 
+from bzrlib import errors, lazy_regex
 from bzrlib.commands import Command, register_command, display_command
 from bzrlib.option import (
     Option,
@@ -24,9 +26,10 @@ from bzrlib.option import (
 
 from bzrlib.lazy_import import lazy_import
 lazy_import(globals(), """
+import re
+
 import bzrlib
 from bzrlib import (
-    errors,
     bzrdir,
     )
 """)
@@ -42,9 +45,6 @@ class cmd_grep(Command):
         'verbose',
         Option('line-number', short_name='n',
                help='prefix each line of output with 1-based line number.'),
-        Option('null', short_name='z',
-               help='Write an ascii NUL (\\0) separator '
-               'between files rather than a newline.'),
         Option('ignore-case', short_name='i',
                help='ignore case distinctions while matching.'),
         Option('recursive', short_name='R',
@@ -66,7 +66,6 @@ class cmd_grep(Command):
 
         print 'pattern:', pattern
         print 'path_list:', path_list
-        print 'ignore-case:', ignore_case
         print 'line-number:', line_number
         print 'null:', null
         print 'recursive:', recursive
@@ -74,6 +73,19 @@ class cmd_grep(Command):
         print '=' * 20
 
         tree, branch, relpath = bzrdir.BzrDir.open_containing_tree_or_branch('.')
+
+        re_flags = 0
+        if ignore_case:
+            re_flags = re.IGNORECASE
+
+        patternc = None
+
+        try:
+            # use python's re.compile as we need to catch re.error in case of bad pattern
+            lazy_regex.reset_compile()
+            patternc = re.compile(pattern, re_flags)
+        except re.error, e:
+            raise errors.BzrError("Invalid pattern: '%s'" % pattern)
 
         tree.lock_read()
         self.add_cleanup(tree.unlock)
@@ -86,11 +98,16 @@ class cmd_grep(Command):
                 print 'fid:', fid
                 print 'entry:', entry
                 if fc == 'V' and fkind == 'file':
-                    self.file_grep(fp)
+                    self.file_grep(fp, patternc)
                 print '~' * 30
 
-    def file_grep(self, path):
-        print path + ': grepping ...'
+    def file_grep(self, path, patternc):
+        index = 1
+        for line in open(path):
+            res = patternc.search(line)
+            if res:
+                self.outf.write("%s:%d:%s\n" % (path, index, line.strip()))
+            index += 1
 
 register_command(cmd_grep)
 
