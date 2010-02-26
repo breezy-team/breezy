a046dcf3d47af6e679f45c887eb713e1557f09c0
Robert Collins <robertc@robertcollins.net>
2010-02-26 16:57:19 +1100

    Add a setup.py for bzr-guess.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2.4
+from distutils.core import setup
+
+bzr_plugin_name = 'guess'
+
+bzr_plugin_version = (0, 0, 1, 'dev', 0)
+bzr_minimum_version = (0, 17, 0)
+
+if __name__ == 'main':
+    setup(name="bzr-guess plugin",
+          version="0.0.1dev0",
+          description="when a bzr command is mispelt, offer the closest match instead.",
+          author="Canonical Ltd",
+          author_email="bazaar@lists.canonical.com",
+          license = "GNU GPL v2",
+          url="https://launchpad.net/bzr-guess",
+          packages=['bzrlib.plugins.guess',
+                    ],
+          package_dir={'bzrlib.plugins.guess': '.'})
