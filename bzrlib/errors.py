6b8a25fc399b2bfc79363fc6926cf8e1a689bdd1
Gary van der Merwe <garyvdm@gmail.com>
2010-03-01 21:53:13 +0200

    Make it possible to detect a BoundBranchOutOfDate from commit.
    * ``bzrlib.builtins.Commit.run`` raises ``bzrlib.errors.BoundBranchOutOfDate``
      rather than ``bzrlib.errors.BzrCommandError`` when the bound branch is out
      of date. (Gary van der Merwe)
    * ``bzrlib.errors.BoundBranchOutOfDate`` has a new field ``extra_help``
      which can be set to add extra help to the error. (Gary van der Merwe)

--- bzrlib/errors.py
+++ bzrlib/errors.py
@@ -1297,12 +1297,13 @@ class UnlistableBranch(BzrError):
 class BoundBranchOutOfDate(BzrError):
 
     _fmt = ("Bound branch %(branch)s is out of date with master branch"
-            " %(master)s.")
+            " %(master)s.%(extra_help)s")
 
     def __init__(self, branch, master):
         BzrError.__init__(self)
         self.branch = branch
         self.master = master
+        self.extra_help = ''
 
 
 class CommitToDoubleBoundBranch(BzrError):
