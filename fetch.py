968bb607eceffc8f22bb520e44b500dd96203a24
Jelmer Vernooij <jelmer@samba.org>
2010-03-10 03:55:15 +0100

    Use convenience functions for start/stop write groups.

--- fetch.py
+++ fetch.py
@@ -353,7 +353,6 @@ def import_git_objects(repo, mapping, object_iter, target_git_object_retriever,
     :param mapping: Mapping to use
     :param object_iter: Iterator over Git objects.
     """
-    target_git_object_retriever._idmap.start_write_group() # FIXME: try/finally
     def lookup_object(sha):
         try:
             return object_iter[sha]
@@ -365,6 +364,7 @@ def import_git_objects(repo, mapping, object_iter, target_git_object_retriever,
     heads = list(set(heads))
     parent_invs_cache = lru_cache.LRUSizeCache(compute_size=approx_inv_size,
                                                max_size=MAX_INV_CACHE_SIZE)
+    target_git_object_retriever.start_write_group() # FIXME: try/finally
     # Find and convert commit objects
     while heads:
         if pb is not None:
@@ -417,7 +417,7 @@ def import_git_objects(repo, mapping, object_iter, target_git_object_retriever,
             hint = repo.commit_write_group()
             if hint is not None:
                 pack_hints.extend(hint)
-    target_git_object_retriever._idmap.commit_write_group()
+    target_git_object_retriever.commit_write_group()
     return pack_hints, last_imported
 
 
--- object_store.py
+++ object_store.py
@@ -63,6 +63,9 @@ class BazaarObjectStore(BaseObjectStore):
         else:
             self.mapping = mapping
         self._idmap = SqliteGitShaMap.from_repository(repository)
+        self.start_write_group = self._idmap.start_write_group
+        self.abort_write_group = self._idmap.abort_write_group
+        self.commit_write_group = self._idmap.commit_write_group
 
     def _update_sha_map(self, stop_revision=None):
         graph = self.repository.get_graph()
@@ -80,7 +83,7 @@ class BazaarObjectStore(BaseObjectStore):
             missing_revids.update(heads)
         if NULL_REVISION in missing_revids:
             missing_revids.remove(NULL_REVISION)
-        self._idmap.start_write_group()
+        self.start_write_group()
         try:
             pb = ui.ui_factory.nested_progress_bar()
             try:
@@ -89,8 +92,11 @@ class BazaarObjectStore(BaseObjectStore):
                     self._update_sha_map_revision(revid)
             finally:
                 pb.finished()
-        finally:
-            self._idmap.commit_write_group()
+        except:
+            self.abort_write_group()
+            raise
+        else:
+            self.commit_write_group()
 
     def __iter__(self):
         self._update_sha_map()
