e2cb5cd9b32975691320b901c6ce04e09f3fa170
Martin <gzlist@googlemail.com>
2010-04-16 15:40:45 +0100

    Move stub_ftp import to function scope so paramiko is not required for selftest tests

--- bzrlib/tests/blackbox/test_selftest.py
+++ bzrlib/tests/blackbox/test_selftest.py
@@ -22,7 +22,6 @@ from bzrlib import (
     )
 from bzrlib.tests import (
     features,
-    stub_sftp,
     )
 from bzrlib.transport import memory
 
@@ -75,6 +74,7 @@ class TestOptions(tests.TestCase, SelfTestPatch):
         # Test that we can pass a transport to the selftest core - sftp
         # version.
         self.requireFeature(features.paramiko)
+        import stub_sftp
         params = self.get_params_passed_to_core('selftest --transport=sftp')
         self.assertEqual(stub_sftp.SFTPAbsoluteServer,
             params[1]["transport"])
