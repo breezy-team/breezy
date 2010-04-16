350c1c8afdfe7c40295e5f10aca0b38864cf58cb
Martin <gzlist@googlemail.com>
2010-04-16 13:59:03 +0100

    Catch a couple of missed plugin module docstrings, note need for assignment to __doc__ in developer documentation and NEWS

--- bzrlib/plugins/netrc_credential_store/__init__.py
+++ bzrlib/plugins/netrc_credential_store/__init__.py
@@ -14,7 +14,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-"""Use ~/.netrc as a credential store for authentication.conf."""
+__doc__ = """Use ~/.netrc as a credential store for authentication.conf."""
 
 # Since we are a built-in plugin we share the bzrlib version
 from bzrlib import version_info
