34dfe9cd6a5004d51bcef4b7b30e225240424350
Martin von Gagern <Martin.vGagern@gmx.net>
2010-04-30 10:14:31 +0200

    Test bash completion for tags.
    This requires a working tree and a way for the completion script to call the
    appropriate bzr command.  TestBashCompletionInvocation deals with this.

--- tests/test_bashcomp.py
+++ tests/test_bashcomp.py
@@ -14,7 +14,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-from bzrlib.tests import TestCase, Feature
+from bzrlib.tests import TestCase, TestCaseWithTransport, Feature
 from bzrlib import commands
 from StringIO import StringIO
 from ..bashcomp import bash_completion_function
@@ -23,6 +23,7 @@ import subprocess
 
 
 class _BashFeature(Feature):
+    """Feature testing whether a bash executable is available."""
 
     bash_paths = ['/bin/bash', '/usr/bin/bash']
 
@@ -46,20 +47,21 @@ class _BashFeature(Feature):
 BashFeature = _BashFeature()
 
 
-class TestBashCompletion(TestCase):
+class BashCompletionMixin(object):
+    """Component for testing execution of a bash completion script."""
 
     _test_needs_features = [BashFeature]
 
-    def __init__(self, methodName='testMethod'):
-        super(TestBashCompletion, self).__init__(methodName)
-        self.script = None
-
-    def setUp(self):
-        super(TestBashCompletion, self).setUp()
-        commands.install_bzr_command_hooks()
-
     def complete(self, words, cword=-1, expect=None,
                  contains=[], omits=[]):
+        """Perform a bash completion.
+
+        :param words: a list of words representing the current command.
+        :param cword: the current word to complete, defaults to the last one.
+        :param expect: an exact iterable of expected completions.
+        :param contains: an iterable of words required in the completion result.
+        :param omits: an iterable of words forbidden in the completion result.
+        """
         if self.script is None:
             self.script = self.get_script()
         proc = subprocess.Popen([BashFeature.bash_path, '--noprofile'],
@@ -78,11 +80,13 @@ echo ${#COMPREPLY[*]}
 IFS=$'\\n'
 echo "${COMPREPLY[*]}"
 """ % { 'script': self.script,
-        'words': ' '.join(["'"+w.replace("'", "'\\''")+"'" for w in words]),
+        'words': 
         'cword': cword,
         'name': getattr(self, 'script_name', '_bzr'),
       }
         (out, err) = proc.communicate(input)
+        if '' != err:
+            raise AssertionError('Unexpected error message:\n%s' % err)
         self.assertEqual('', err, 'No messages to standard error')
         #import sys
         #print >>sys.stdout, '---\n%s\n---\n%s\n---\n' % (input, out)
@@ -94,18 +98,35 @@ echo "${COMPREPLY[*]}"
         if nlines == 0 and len(lines) == 1 and lines[0] == '':
             del lines[0]
         self.assertEqual(nlines, len(lines), 'No newlines in generated words')
+        res = set(lines)
         if expect is not None:
-            self.assertEqual(expect, lines)
-        for item in contains:
-            self.assertTrue(item in lines)
-        for item in omits:
-            self.assertFalse(item in lines)
-        return lines
+            self.assertEqual(set(expect), res)
+        missing = set(contains) - res
+        if missing:
+            raise AssertionError('Completion should contain %r but it has %r'
+                                 % (missing, res))
+        surplus = set(omits) & res
+        if surplus:
+            raise AssertionError('Completion should omit %r but it has %r'
+                                 % (surplus, res))
+        return res
 
     def get_script(self):
         out = StringIO()
         bash_completion_function(out, function_only=True)
         return out.getvalue()
+
+
+class TestBashCompletion(TestCase, BashCompletionMixin):
+    """Test bash completions that don't execute bzr."""
+
+    def __init__(self, methodName='testMethod'):
+        super(TestBashCompletion, self).__init__(methodName)
+        self.script = None
+
+    def setUp(self):
+        super(TestBashCompletion, self).setUp()
+        commands.install_bzr_command_hooks()
 
     def test_simple_scipt(self):
         """Ensure that the test harness works as expected"""
@@ -150,3 +171,39 @@ _bzr() {
     def test_init_format_2(self):
         c = self.complete(['bzr', 'init', '--format', '=', '2', 'directory'],
                           cword=4, contains=['2a'], omits=['1.9'])
+
+
+class TestBashCompletionInvoking(TestCaseWithTransport, BashCompletionMixin):
+    """Test bash completions that might execute bzr.
+
+    Only the syntax ``$(bzr ...`` is supported so far. The bzr command
+    will be replaced by the bzr instance running this selftest.
+    """
+
+    def __init__(self, methodName='testMethod'):
+        super(TestBashCompletionInvoking, self).__init__(methodName)
+        self.script = None
+
+    def setUp(self):
+        super(TestBashCompletionInvoking, self).setUp()
+        commands.install_bzr_command_hooks()
+
+    def get_script(self):
+        s = super(TestBashCompletionInvoking, self).get_script()
+        return s.replace("$(bzr ", "$('%s' " % self.get_bzr_path())
+
+    def test_revspec_tag_all(self):
+        wt = self.make_branch_and_tree('.', format='2a')
+        wt.branch.tags.set_tag('tag1', 'null:')
+        wt.branch.tags.set_tag('tag2', 'null:')
+        wt.branch.tags.set_tag('3tag', 'null:')
+        self.complete(['bzr', 'log', '-r', 'tag', ':'],
+                      expect=['tag1', 'tag2', '3tag'])
+
+    def test_revspec_tag_prefix(self):
+        wt = self.make_branch_and_tree('.', format='2a')
+        wt.branch.tags.set_tag('tag1', 'null:')
+        wt.branch.tags.set_tag('tag2', 'null:')
+        wt.branch.tags.set_tag('3tag', 'null:')
+        self.complete(['bzr', 'log', '-r', 'tag', ':', 't'],
+                      expect=['tag1', 'tag2'])
