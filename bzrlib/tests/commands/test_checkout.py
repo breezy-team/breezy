9da18d6263ebca144d0564cb9a418dff5b34f9e4
Parth Malwankar <parth.malwankar@gmail.com>
2010-05-03 14:03:32 +0530

    removed unused import.

--- bzrlib/tests/commands/test_checkout.py
+++ bzrlib/tests/commands/test_checkout.py
@@ -14,7 +14,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-from bzrlib import tests
 from bzrlib.builtins import cmd_checkout
 from bzrlib.tests.transport_util import TestCaseWithConnectionHookedTransport
 
