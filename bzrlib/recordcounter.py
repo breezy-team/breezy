aa7d4793b8eba11864f105ba5a13090ae7d2bd1b
Parth Malwankar <parth.malwankar@gmail.com>
2010-05-19 08:20:49 +0530

    close review comments from jam. News entry clarifies this change is 2a only. Fixed copyright and style in recordcounter.

--- bzrlib/recordcounter.py
+++ bzrlib/recordcounter.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2006-2010 Canonical Ltd
+# Copyright (C) 2010 Canonical Ltd
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,6 +14,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 """Record counting support for showing progress of revision fetch."""
+
 
 class RecordCounter(object):
     """Container for maintains estimates of work requires for fetch.
