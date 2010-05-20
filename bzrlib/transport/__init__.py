d03416e50e1aaf0cc5c25331091b1e549bfc2513
Mattias Eriksson <snaggen@acc.umu.se>
2010-05-20 07:41:22 +0200

    Moved the gio transport from bzrlib/transport/gio/__init_.py to bzrlib/transport/gio_transport.py

--- bzrlib/transport/__init__.py
+++ bzrlib/transport/__init__.py
@@ -1741,7 +1741,7 @@ register_transport_proto('aftp://', help="Access using active FTP.")
 register_lazy_transport('aftp://', 'bzrlib.transport.ftp', 'FtpTransport')
 
 register_transport_proto('gio+', help="Access using any GIO supported protocols.")
-register_lazy_transport('gio+', 'bzrlib.transport.gio', 'GioTransport')
+register_lazy_transport('gio+', 'bzrlib.transport.gio_transport', 'GioTransport')
 
 try:
     import kerberos
