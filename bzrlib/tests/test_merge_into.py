5cbfa60aedbaebc1f9a2c3ef2e0188da2d6783aa
Andrew Bennetts <andrew.bennetts@canonical.com>
2010-06-03 17:12:56 +1000

    Add assertTreeEntriesEqual to make tests more concise.

--- bzrlib/tests/test_merge_into.py
+++ bzrlib/tests/test_merge_into.py
@@ -65,34 +65,35 @@ class TestMergeIntoBase(tests.TestCaseWithTransport):
         operation = cleanup.OperationWithCleanups(merge.merge_into_helper)
         return operation.run_simple(location, merge_as, operation.add_cleanup)
 
+    def assertTreeEntriesEqual(self, expected_entries, tree):
+        """Assert that 'tree' contains the expected inventory entries.
+
+        :param expected_entries: sequence of (path, file-id) pairs.
+        """
+        files = [(path, ie.file_id) for path, ie in tree.iter_entries_by_dir()]
+        self.assertEqual(expected_entries, files)
+
 
 class TestMergeInto(TestMergeIntoBase):
 
     def test_newdir_with_unique_roots(self):
         """Merge a branch with a unique root into a new directory."""
         project_wt, lib_wt = self.setup_two_branches()
-
         self.do_merge_into('lib1', 'project/lib1')
-
         project_wt.lock_read()
         self.addCleanup(project_wt.unlock)
-        new_lib1_id = project_wt.path2id('lib1')
-        self.assertNotEqual(None, new_lib1_id)
         # The r1-lib1 revision should be merged into this one
-        self.assertEqual(['r1-project', 'r1-lib1'],
-                         project_wt.get_parent_ids())
-        files = [(path, ie.kind, ie.file_id)
-                 for path, ie in project_wt.iter_entries_by_dir()]
-        exp_files = [('', 'directory', 'project-root-id'),
-                     ('README', 'file', 'project-README-id'),
-                     ('dir', 'directory', 'project-dir-id'),
-                     ('lib1', 'directory', new_lib1_id),
-                     ('dir/file.c', 'file', 'project-file.c-id'),
-                     ('lib1/Makefile', 'file', 'lib1-Makefile-id'),
-                     ('lib1/README', 'file', 'lib1-README-id'),
-                     ('lib1/foo.c', 'file', 'lib1-foo.c-id'),
-                    ]
-        self.assertEqual(exp_files, files)
+        self.assertEqual(['r1-project', 'r1-lib1'], project_wt.get_parent_ids())
+        self.assertTreeEntriesEqual(
+            [('', 'project-root-id'),
+             ('README', 'project-README-id'),
+             ('dir', 'project-dir-id'),
+             ('lib1', 'lib1-root-id'),
+             ('dir/file.c', 'project-file.c-id'),
+             ('lib1/Makefile', 'lib1-Makefile-id'),
+             ('lib1/README', 'lib1-README-id'),
+             ('lib1/foo.c', 'lib1-foo.c-id'),
+            ], project_wt)
 
     def test_subdir(self):
         """Merge a branch into a subdirectory of an existing directory."""
@@ -102,23 +103,18 @@ class TestMergeInto(TestMergeIntoBase):
 
         project_wt.lock_read()
         self.addCleanup(project_wt.unlock)
-        new_lib1_id = project_wt.path2id('dir/lib1')
-        self.assertNotEqual(None, new_lib1_id)
         # The r1-lib1 revision should be merged into this one
-        self.assertEqual(['r1-project', 'r1-lib1'],
-                         project_wt.get_parent_ids())
-        files = [(path, ie.kind, ie.file_id)
-                 for path, ie in project_wt.iter_entries_by_dir()]
-        exp_files = [('', 'directory', 'project-root-id'),
-                     ('README', 'file', 'project-README-id'),
-                     ('dir', 'directory', 'project-dir-id'),
-                     ('dir/file.c', 'file', 'project-file.c-id'),
-                     ('dir/lib1', 'directory', new_lib1_id),
-                     ('dir/lib1/Makefile', 'file', 'lib1-Makefile-id'),
-                     ('dir/lib1/README', 'file', 'lib1-README-id'),
-                     ('dir/lib1/foo.c', 'file', 'lib1-foo.c-id'),
-                    ]
-        self.assertEqual(exp_files, files)
+        self.assertEqual(['r1-project', 'r1-lib1'], project_wt.get_parent_ids())
+        self.assertTreeEntriesEqual(
+            [('', 'project-root-id'),
+             ('README', 'project-README-id'),
+             ('dir', 'project-dir-id'),
+             ('dir/file.c', 'project-file.c-id'),
+             ('dir/lib1', 'lib1-root-id'),
+             ('dir/lib1/Makefile', 'lib1-Makefile-id'),
+             ('dir/lib1/README', 'lib1-README-id'),
+             ('dir/lib1/foo.c', 'lib1-foo.c-id'),
+            ], project_wt)
 
     def test_newdir_with_repeat_roots(self):
         """If the file-id of the dir to be merged already exists it a new ID
@@ -132,49 +128,39 @@ class TestMergeInto(TestMergeIntoBase):
         project_wt.lock_read()
         self.addCleanup(project_wt.unlock)
         # The r1-lib1 revision should be merged into this one
-        self.assertEqual(['r1-project', 'r1-lib1'],
-                         project_wt.get_parent_ids())
+        self.assertEqual(['r1-project', 'r1-lib1'], project_wt.get_parent_ids())
         new_lib1_id = project_wt.path2id('lib1')
         self.assertNotEqual(None, new_lib1_id)
-        files = [(path, ie.kind, ie.file_id)
-                 for path, ie in project_wt.iter_entries_by_dir()]
-        exp_files = [('', 'directory', root_id),
-                     ('README', 'file', 'project-README-id'),
-                     ('dir', 'directory', 'project-dir-id'),
-                     ('lib1', 'directory', new_lib1_id),
-                     ('dir/file.c', 'file', 'project-file.c-id'),
-                     ('lib1/Makefile', 'file', 'lib1-Makefile-id'),
-                     ('lib1/README', 'file', 'lib1-README-id'),
-                     ('lib1/foo.c', 'file', 'lib1-foo.c-id'),
-                    ]
-        self.assertEqual(exp_files, files)
+        self.assertTreeEntriesEqual(
+            [('', root_id),
+             ('README', 'project-README-id'),
+             ('dir', 'project-dir-id'),
+             ('lib1', new_lib1_id),
+             ('dir/file.c', 'project-file.c-id'),
+             ('lib1/Makefile', 'lib1-Makefile-id'),
+             ('lib1/README', 'lib1-README-id'),
+             ('lib1/foo.c', 'lib1-foo.c-id'),
+            ], project_wt)
 
     def test_name_conflict(self):
         """When the target directory name already exists a conflict is
         generated and the original directory is renamed to foo.moved.
         """
-        project_wt, lib_wt = self.setup_two_branches()
-        conflicts = self.do_merge_into('lib1', 'project/dir')
+        dest_wt = self.setup_simple_branch('dest', ['dir/', 'dir/file.txt'])
+        src_wt = self.setup_simple_branch('src', ['README'])
+        conflicts = self.do_merge_into('src', 'dest/dir')
         self.assertEqual(1, conflicts)
-        project_wt.lock_read()
-        self.addCleanup(project_wt.unlock)
-        new_lib1_id = project_wt.path2id('dir')
-        self.assertNotEqual(None, new_lib1_id)
+        dest_wt.lock_read()
+        self.addCleanup(dest_wt.unlock)
         # The r1-lib1 revision should be merged into this one
-        self.assertEqual(['r1-project', 'r1-lib1'],
-                         project_wt.get_parent_ids())
-        files = [(path, ie.kind, ie.file_id)
-                 for path, ie in project_wt.iter_entries_by_dir()]
-        exp_files = [('', 'directory', 'project-root-id'),
-                     ('README', 'file', 'project-README-id'),
-                     ('dir', 'directory', new_lib1_id),
-                     ('dir.moved', 'directory', 'project-dir-id'),
-                     ('dir/Makefile', 'file', 'lib1-Makefile-id'),
-                     ('dir/README', 'file', 'lib1-README-id'),
-                     ('dir/foo.c', 'file', 'lib1-foo.c-id'),
-                     ('dir.moved/file.c', 'file', 'project-file.c-id'),
-                    ]
-        self.assertEqual(exp_files, files)
+        self.assertEqual(['r1-dest', 'r1-src'], dest_wt.get_parent_ids())
+        self.assertTreeEntriesEqual(
+            [('', 'dest-root-id'),
+             ('dir', 'src-root-id'),
+             ('dir.moved', 'dest-dir-id'),
+             ('dir/README', 'src-README-id'),
+             ('dir.moved/file.txt', 'dest-file.txt-id'),
+            ], dest_wt)
 
     def test_only_subdir(self):
         """When the location points to just part of a tree, merge just that
@@ -189,27 +175,23 @@ class TestMergeInto(TestMergeIntoBase):
         # The r1-lib1 revision should NOT be merged into this one (this is a
         # partial merge).
         self.assertEqual(['r1-dest'], dest_wt.get_parent_ids())
-        files = [(path, ie.kind, ie.file_id)
-                 for path, ie in dest_wt.iter_entries_by_dir()]
-        exp_files = [('', 'directory', 'dest-root-id'),
-                     ('dir', 'directory', 'src-dir-id'),
-                     ('dir/foo.c', 'file', 'src-foo.c-id'),
-                    ]
-        self.assertEqual(exp_files, files)
+        self.assertTreeEntriesEqual(
+            [('', 'dest-root-id'),
+             ('dir', 'src-dir-id'),
+             ('dir/foo.c', 'src-foo.c-id'),
+            ], dest_wt)
 
     def test_only_file(self):
         """An edge case: merge just one file, not a whole dir."""
-        empty_wt = self.setup_simple_branch('empty')
+        dest_wt = self.setup_simple_branch('dest')
         two_file_wt = self.setup_simple_branch(
             'two-file', ['file1.txt', 'file2.txt'])
-        conflicts = self.do_merge_into('two-file/file1.txt', 'empty/file1.txt')
-        empty_wt.lock_read()
-        self.addCleanup(empty_wt.unlock)
+        conflicts = self.do_merge_into('two-file/file1.txt', 'dest/file1.txt')
+        dest_wt.lock_read()
+        self.addCleanup(dest_wt.unlock)
         # The r1-lib1 revision should NOT be merged into this one
-        self.assertEqual(['r1-empty'], empty_wt.get_parent_ids())
-        files = [(path, ie.kind, ie.file_id)
-                 for path, ie in empty_wt.iter_entries_by_dir()]
-        exp_files = [('', 'directory', 'empty-root-id'),
-                     ('file1.txt', 'file', 'two-file-file1.txt-id')]
-        self.assertEqual(exp_files, files)
+        self.assertEqual(['r1-dest'], dest_wt.get_parent_ids())
+        self.assertTreeEntriesEqual(
+            [('', 'dest-root-id'), ('file1.txt', 'two-file-file1.txt-id')],
+            dest_wt)
 
