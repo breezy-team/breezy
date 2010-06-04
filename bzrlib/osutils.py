5318c35d3c352073ee8e200f661af82cf9ab2a9d
Eric Moritz <eric@themoritzfamily.com>
2010-06-03 21:52:50 -0400

    Made it so that walkdirs throws a useful error when filenames aren't in the assumed utf8 encoding

--- bzrlib/osutils.py
+++ bzrlib/osutils.py
@@ -1644,10 +1644,12 @@ def walkdirs(top, prefix=""):
         dirblock = []
         append = dirblock.append
         try:
-            names = sorted(_listdir(top))
+            names = sorted(map(safe_unicode, _listdir(top)))
         except OSError, e:
             if not _is_error_enotdir(e):
                 raise
+        except errors.BzrBadParameterNotUnicode, e:
+            raise errors.BzrFilenameNotUTF8(e.param)
         else:
             for name in names:
                 abspath = top_slash + name
