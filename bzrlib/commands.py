978f3480e9e6270e492dbb5005da30655b3f3a58
Jason Spashett <jason@spashett.com>
2010-06-18 11:57:20 +0100

    Reverse merge -r 5280..5279

--- bzrlib/commands.py
+++ bzrlib/commands.py
@@ -1158,7 +1158,7 @@ def _specified_or_unicode_argv(argv):
     # For internal or testing use, argv can be passed.  Otherwise, get it from
     # the process arguments in a unicode-safe way.
     if argv is None:
-        return sys.argv[1:]
+        return osutils.get_unicode_argv()
     else:
         new_argv = []
         try:
