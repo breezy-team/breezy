104fc71d1c02af4e0212d30cababdcf483f7718a
Robert Collins <robertc@robertcollins.net>
2010-06-25 16:11:21 +1000

    The symbol_versioning module can now cleanup after itself - ``suppress_deprecation_warnings`` now returns a cleanup function. (Robert Collins)

--- bzrlib/symbol_versioning.py
+++ bzrlib/symbol_versioning.py
@@ -314,6 +314,7 @@ def suppress_deprecation_warnings(override=True):
 
     :param override: If True, always set the ignore, if False, only set the
         ignore if there isn't already a filter.
+    :return: A callable to remove the new warnings this added.
     """
     import warnings
     if not override and _check_for_filter(error_only=False):
@@ -321,6 +322,10 @@ def suppress_deprecation_warnings(override=True):
         # then skip it.
         return
     warnings.filterwarnings('ignore', category=DeprecationWarning)
+    filter = warnings.filters[0]
+    def cleanup():
+        warnings.filters.remove(filter)
+    return cleanup
 
 
 def activate_deprecation_warnings(override=True):
