0c6ea69db94eb1ebec2e190a4a74ab33ad071d51
Miklos Vajna <vmiklos@frugalware.org>
2010-06-27 19:37:50 +0200

    darcs-fast-import: add --symhack option

--- exporters/darcs/t/testimport-gitsymlink.sh
+++ exporters/darcs/t/testimport-gitsymlink.sh
@@ -27,5 +27,19 @@ cd ..
 # we *do* want this to fail, but with error code 2. that means that we
 # detected that symlinks are not supported and the user does not get a
 # meaningless exception
-[ $? = 2 ]
+if [ $? != 2 ]; then
+	exit 1
+fi
+
+# now try with the symhack option
+rm -rf test.darcs
+mkdir test.darcs
+cd test.darcs
+darcs init
+cd ..
+(cd test; git fast-export --progress=2 HEAD) | (cd test.darcs; darcs-fast-import --symhack)
+if [ $? != 0 ]; then
+	exit 1
+fi
+diff_importgit test
 exit $?
