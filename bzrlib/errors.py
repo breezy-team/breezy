023afb098a9763ae0c41cf64e9e9f72532db2141
Neil Martinsen-Burrell <nmb@wartburg.edu>
2010-07-17 22:48:41 -0500

    Dont print internal object names in invalid revision spec error

--- bzrlib/errors.py
+++ bzrlib/errors.py
@@ -1177,15 +1177,19 @@ class NoSuchRevisionInTree(NoSuchRevision):
 
 class InvalidRevisionSpec(BzrError):
 
-    _fmt = ("Requested revision: '%(spec)s' does not exist in branch:"
-            " %(branch)s%(extra)s")
-
     def __init__(self, spec, branch, extra=None):
         BzrError.__init__(self, branch=branch, spec=spec)
         if extra:
             self.extra = '\n' + str(extra)
         else:
             self.extra = ''
+
+    def __str__(self):
+        _fmt = ("Requested revision: '%s' does not exist in branch:"
+                " %s%s"
+                % (self.spec, self.branch.user_url, self.extra))
+
+        return str(_fmt)
 
 
 class HistoryMissing(BzrError):
