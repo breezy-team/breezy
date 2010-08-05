170992c483d6187c49847c60c6a4125ccd1a5016
Martin <gzlist@googlemail.com>
2010-08-05 19:13:23 +0100

    Less hacky spelling for avoiding unittest native load_tests, as per review by jam

--- bzrlib/tests/TestUtil.py
+++ bzrlib/tests/TestUtil.py
@@ -135,10 +135,13 @@ class TestLoader(unittest.TestLoader):
         >>>         result.addTests([test, test])
         >>>     return result
         """
-        basic_tests = super(TestLoader, self).loadTestsFromModule(module,
-        # GZ 2010-07-19: Python 2.7 unittest also uses load_tests but with a
-        #                different incompatible signature so have to avoid
-            **(sys.version_info > (2, 7) and {"use_load_tests": False} or {}))
+        if sys.version_info < (2, 7):
+            basic_tests = super(TestLoader, self).loadTestsFromModule(module)
+        else:
+            # GZ 2010-07-19: Python 2.7 unittest also uses load_tests but with
+            #                a different and incompatible signature
+            basic_tests = super(TestLoader, self).loadTestsFromModule(module,
+                use_load_tests=False)
         load_tests = getattr(module, "load_tests", None)
         if load_tests is not None:
             return load_tests(basic_tests, module, self)
