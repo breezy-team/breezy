7860ea77754dd2985b1244d03a0110c46b41a2c4
Martin Pool <mbp@sourcefrog.net>
2010-08-13 18:09:53 +1000

    Remove spuriously-resurrected test

--- bzrlib/tests/test_commands.py
+++ bzrlib/tests/test_commands.py
@@ -91,15 +91,6 @@ class TestCommands(tests.TestCase):
         c = self.get_command([option.Option('foo', hidden=False)])
         self.assertContainsRe(c.get_help_text(), '--foo')
 
-    def test_no_help_init_failure(self):
-        class cmd_foo(commands.Command):
-            pass
-        self.assertRaises(ValueError, cmd_foo)
-
-    def test_builtin_command_names(self):
-        cmds = commands.builtin_command_names()
-        self.assertTrue(len(cmds) > 0)
-        self.assertTrue('diff' in cmds)
 
 class TestGetAlias(tests.TestCase):
 
