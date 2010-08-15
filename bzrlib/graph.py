9a149a25e8a4c0b3bb65cafc40ddbb2b33e8c16f
Aaron Bentley <aaron@aaronbentley.com>
2010-08-15 10:52:10 -0400

    Extract iter_lefthand_ancestry from Repository.iter_ancestry.

--- bzrlib/graph.py
+++ bzrlib/graph.py
@@ -953,6 +953,25 @@ class Graph(object):
                 yield (ghost, None)
             pending = next_pending
 
+    def iter_lefthand_ancestry(self, start_key, stop_keys=None):
+        if stop_keys is None:
+            stop_keys = ()
+        next_key = start_key
+        def get_parents(key):
+            try:
+                return self._parents_provider.get_parent_map([key])[key]
+            except KeyError:
+                raise errors.RevisionNotPresent(next_key, self)
+        while True:
+            if next_key in stop_keys:
+                return
+            parents = get_parents(next_key)
+            yield next_key
+            if len(parents) == 0:
+                return
+            else:
+                next_key = parents[0]
+
     def iter_topo_order(self, revisions):
         """Iterate through the input revisions in topological order.
 
