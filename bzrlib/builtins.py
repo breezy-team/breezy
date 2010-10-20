a59aa50962394824725546d108ece50d87160b3e
Martin von Gagern <Martin.vGagern@gmx.net>
2010-10-20 14:06:13 +0200

    Avoid test-script command depending on testtools.
    In order to achieve this, we move the command to its own file which lazily
    loads all the internal workings upon execution.

--- bzrlib/builtins.py
+++ bzrlib/builtins.py
@@ -6109,6 +6109,6 @@ def _register_lazy_builtins():
         ('cmd_resolve', ['resolved'], 'bzrlib.conflicts'),
         ('cmd_conflicts', [], 'bzrlib.conflicts'),
         ('cmd_sign_my_commits', [], 'bzrlib.sign_my_commits'),
-        ('cmd_test_script', [], 'bzrlib.tests.script'),
+        ('cmd_test_script', [], 'bzrlib.cmd_test_script'),
         ]:
         builtin_command_registry.register_lazy(name, aliases, module_name)
