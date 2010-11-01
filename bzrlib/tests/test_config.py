f8fa4f7902d43ffbcfafde7c20a02709c604332b
Neil Martinsen-Burrell <nmb@wartburg.edu>
2010-11-01 14:08:33 -0500

    update tests for various platforms

--- bzrlib/tests/test_config.py
+++ bzrlib/tests/test_config.py
@@ -369,8 +369,10 @@ class TestConfigPath(tests.TestCase):
                 r'C:\Documents and Settings\bogus\Application Data'
             self.bzr_home = \
                 'C:/Documents and Settings/bogus/Application Data/bazaar/2.0'
-        else:
+        elif sys.platform == 'darwin':
             self.bzr_home = '/home/bogus/.bazaar'
+        else:
+            self.bzr_home = '/home/bogus/.config/bazaar'
 
     def test_config_dir(self):
         self.assertEqual(config.config_dir(), self.bzr_home)
