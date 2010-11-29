8205a2abe256a4d815488e486c11db86f9add13e
Andrew Bennetts <andrew.bennetts@canonical.com>
2010-11-29 11:34:03 +1100

    Cosmetic tweaks to TestXDGConfigDir.

--- bzrlib/tests/test_config.py
+++ bzrlib/tests/test_config.py
@@ -391,24 +391,22 @@ class TestXDGConfigDir(tests.TestCaseInTempDir):
     # subdirectory of $XDG_CONFIG_HOME
 
     def setUp(self):
+        if sys.platform in ('darwin', 'win32'):
+            raise tests.TestNotApplicable(
+                'XDG config dir not used on this platform')
         super(TestXDGConfigDir, self).setUp()
         os.environ['HOME'] = self.test_home_dir
+        # BZR_HOME overrides everything we want to test so unset it.
+        del os.environ['BZR_HOME']
 
     def test_xdg_config_dir_exists(self):
-        if sys.platform in ('darwin', 'win32'):
-            raise tests.TestNotApplicable
-        # this overrides everything we want to test so get rid of it
-        del os.environ['BZR_HOME'] 
+        """When ~/.config/bazaar exists, use it as the config dir."""
         newdir = osutils.pathjoin(self.test_home_dir, '.config', 'bazaar')
         os.makedirs(newdir)
         self.assertEqual(config.config_dir(), newdir)
 
     def test_xdg_config_home(self):
-        if sys.platform in ('darwin', 'win32'):
-            raise tests.TestNotApplicable
-        # this overrides everything we want to test so get rid of it
-        del os.environ['BZR_HOME']
-
+        """When XDG_CONFIG_HOME is set, use it."""
         xdgconfigdir = osutils.pathjoin(self.test_home_dir, 'xdgconfig')
         os.environ['XDG_CONFIG_HOME'] = xdgconfigdir
         newdir = osutils.pathjoin(xdgconfigdir, 'bazaar')
