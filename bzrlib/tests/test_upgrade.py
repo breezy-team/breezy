f58f13fcf6198badc2b6f4878321557b835055db
Matthew Fuller <fullermd@over-yonder.net>
2010-12-04 09:03:41 -0600

    Update new upgrade tests to match the new backup bzrdir names.

--- bzrlib/tests/test_upgrade.py
+++ bzrlib/tests/test_upgrade.py
@@ -442,7 +442,7 @@ class TestSmartUpgrade(TestCaseWithTransport):
         self.assertEqual(1, len(tried))
         self.assertEqual(1, len(worked))
         self.assertEqual(0, len(issues))
-        self.failUnlessExists('branch1/backup.bzr')
+        self.failUnlessExists('branch1/backup.bzr.~1~')
 
     def test_upgrade_standalone_branch_cleanup(self):
         control = self.make_standalone_branch()
@@ -453,7 +453,7 @@ class TestSmartUpgrade(TestCaseWithTransport):
         self.assertEqual(0, len(issues))
         self.failUnlessExists('branch1')
         self.failUnlessExists('branch1/.bzr')
-        self.failIfExists('branch1/backup.bzr')
+        self.failIfExists('branch1/backup.bzr.~1~')
 
     def make_repo_with_branches(self):
         repo = self.make_repository('repo', shared=True,
@@ -472,9 +472,9 @@ class TestSmartUpgrade(TestCaseWithTransport):
         self.assertEqual(3, len(tried))
         self.assertEqual(3, len(worked))
         self.assertEqual(0, len(issues))
-        self.failUnlessExists('repo/backup.bzr')
-        self.failUnlessExists('repo/branch1/backup.bzr')
-        self.failUnlessExists('repo/branch2/backup.bzr')
+        self.failUnlessExists('repo/backup.bzr.~1~')
+        self.failUnlessExists('repo/branch1/backup.bzr.~1~')
+        self.failUnlessExists('repo/branch2/backup.bzr.~1~')
 
     def test_upgrade_repo_with_branches_cleanup(self):
         control = self.make_repo_with_branches()
@@ -485,6 +485,6 @@ class TestSmartUpgrade(TestCaseWithTransport):
         self.assertEqual(0, len(issues))
         self.failUnlessExists('repo')
         self.failUnlessExists('repo/.bzr')
-        self.failIfExists('repo/backup.bzr')
-        self.failIfExists('repo/branch1/backup.bzr')
-        self.failIfExists('repo/branch2/backup.bzr')
+        self.failIfExists('repo/backup.bzr.~1~')
+        self.failIfExists('repo/branch1/backup.bzr.~1~')
+        self.failIfExists('repo/branch2/backup.bzr.~1~')
