466bdad5fedb348b0793d41fbce27f33400f7ed9
Max Bowsher <maxb@f2s.com>
2010-12-22 03:36:48 +0000

    Remove duplicate definition of cache_manager._Cleanup.

--- cache_manager.py
+++ cache_manager.py
@@ -61,33 +61,6 @@ class _Cleanup(object):
             shutil.rmtree(self.tempdir)
 
 
-class _Cleanup(object):
-    """This class makes sure we clean up when CacheManager goes away.
-
-    We use a helper class to ensure that we are never in a refcycle.
-    """
-
-    def __init__(self, disk_blobs):
-        self.disk_blobs = disk_blobs
-        self.tempdir = None
-        self.small_blobs = None
-
-    def __del__(self):
-        self.finalize()
-
-    def finalize(self):
-        if self.disk_blobs is not None:
-            for info in self.disk_blobs.itervalues():
-                if info[-1] is not None:
-                    os.unlink(info[-1])
-            self.disk_blobs = None
-        if self.small_blobs is not None:
-            self.small_blobs.close()
-            self.small_blobs = None
-        if self.tempdir is not None:
-            shutil.rmtree(self.tempdir)
-
-
 class CacheManager(object):
 
     _small_blob_threshold = 25*1024
