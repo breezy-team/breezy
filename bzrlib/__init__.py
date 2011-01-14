d30fd4e781f0acdb2d482656afc86f56e1d4159d
Vincent Ladeuil <v.ladeuil+lp@free.fr>
2011-01-13 23:34:20 -0600

    Open trunk as 2.4dev1, bumping api_minimum_version to 2.4.0.

--- bzrlib/__init__.py
+++ bzrlib/__init__.py
@@ -52,10 +52,10 @@ __copyright__ = "Copyright 2005-2010 Canonical Ltd."
 # Python version 2.0 is (2, 0, 0, 'final', 0)."  Additionally we use a
 # releaselevel of 'dev' for unreleased under-development code.
 
-version_info = (2, 3, 0, 'beta', 5)
+version_info = (2, 4, 0, 'dev', 1)
 
 # API compatibility version
-api_minimum_version = (2, 3, 0)
+api_minimum_version = (2, 4, 0)
 
 
 def _format_version_tuple(version_info):
