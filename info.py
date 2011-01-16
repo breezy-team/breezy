4221a3235a470b1e384e5c2d4c97703269d89111
Jelmer Vernooij <jelmer@samba.org>
2011-01-16 20:16:54 +0100

    Mark as compatible with bzr 2.4.

--- info.py
+++ info.py
@@ -9,7 +9,7 @@ bzr_plugin_version = (0, 5, 3, 'dev', 0)
 
 bzr_commands = ["git-import", "git-object", "git-refs", "git-apply"]
 
-bzr_compatible_versions = [(2, x, 0) for x in [0, 1, 2, 3]]
+bzr_compatible_versions = [(2, x, 0) for x in [0, 1, 2, 3, 4]]
 
 bzr_minimum_version = bzr_compatible_versions[0]
 
