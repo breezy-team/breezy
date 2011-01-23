051820e00eb7abdbf95c3347f155d5c42f2580b4
Jelmer Vernooij <jelmer@samba.org>
2011-01-23 01:18:12 +0100

    Use new hooks infrastructure for version info rio hooks.

--- bzrlib/version_info_formats/format_rio.py
+++ bzrlib/version_info_formats/format_rio.py
@@ -83,11 +83,13 @@ class RioVersionInfoBuilder(VersionInfoBuilder):
 class RioVersionInfoBuilderHooks(hooks.Hooks):
     """Hooks for rio-formatted version-info output."""
 
-    def __init__(self):
-        super(RioVersionInfoBuilderHooks, self).__init__()
-        self.create_hook(hooks.HookPoint('revision',
+    def __init__(self, module, member_name):
+        super(RioVersionInfoBuilderHooks, self).__init__(module, member_name)
+        self.add_hook('revision',
             "Invoked when adding information about a revision to the"
             " RIO stanza that is printed. revision is called with a"
-            " revision object and a RIO stanza.", (1, 15), None))
+            " revision object and a RIO stanza.", (1, 15), None)
 
-RioVersionInfoBuilder.hooks = RioVersionInfoBuilderHooks()
+
+RioVersionInfoBuilder.hooks = RioVersionInfoBuilderHooks(
+    "bzrlib.version_info_formats.format_rio", "RioVersionInfoBuilder.hooks")
