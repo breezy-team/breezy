3214162369931dfcdad57b2652067c21228193f8
Jelmer Vernooij <jelmer@samba.org>
2011-02-02 11:26:15 +0100

    Add download_url in setup.py for use on pypi.

--- setup.py
+++ setup.py
@@ -7,13 +7,16 @@ bzr_plugin_version = (0, 10, 0, 'dev', 0)
 bzr_minimum_version = (1, 1, 0)
 bzr_maximum_version = None
 
+version = "0.9.0dev0"
+
 if __name__ == '__main__':
     setup(name="bzr-fastimport",
-          version="0.9.0dev0",
+          version=version,
           description="stream-based import into and export from Bazaar.",
           author="Canonical Ltd",
           author_email="bazaar@lists.canonical.com",
           license = "GNU GPL v2",
+          download_url="http://launchpad.net/bzr-fastimport/trunk/%s/+download/bzr-fastimport-%s.tar.gz" % (version, version),
           url="https://launchpad.net/bzr-fastimport",
           scripts=[],
           packages=['bzrlib.plugins.fastimport',
