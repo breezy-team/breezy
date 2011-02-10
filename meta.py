5c7357a126b6ed3d4792ddb89fccf5f84ecf7f3d
Max Bowsher <maxb@f2s.com>
2011-02-10 08:42:19 +0000

    Fix invalid version info.

--- meta.py
+++ meta.py
@@ -18,7 +18,7 @@
 
 bzr_plugin_name = 'bisect'
 
-version_info = (1, 1, 0, 'pre', 0)
+version_info = (1, 1, 0, 'dev', 0)
 __version__ = '.'.join([str(x) for x in version_info[:3]])
 if version_info[3] != 'final':
     __version__ = "%s%s%d" % (__version__, version_info[3], version_info[4])
