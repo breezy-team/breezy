48dadd93e41279b0c61cd87c4211e4c610941a04
Jelmer Vernooij <jelmer@samba.org>
2011-02-18 20:01:43 +0100

    Fix hook resetting.

--- bzrlib/version_info_formats/format_rio.py
+++ bzrlib/version_info_formats/format_rio.py
@@ -83,13 +83,13 @@ class RioVersionInfoBuilder(VersionInfoBuilder):
 class RioVersionInfoBuilderHooks(hooks.Hooks):
     """Hooks for rio-formatted version-info output."""
 
-    def __init__(self, module, member_name):
-        super(RioVersionInfoBuilderHooks, self).__init__(module, member_name)
+    def __init__(self):
+        super(RioVersionInfoBuilderHooks, self).__init__(
+            "bzrlib.version_info_formats.format_rio", "RioVersionInfoBuilder.hooks")
         self.add_hook('revision',
             "Invoked when adding information about a revision to the"
             " RIO stanza that is printed. revision is called with a"
             " revision object and a RIO stanza.", (1, 15), None)
 
 
-RioVersionInfoBuilder.hooks = RioVersionInfoBuilderHooks(
-    "bzrlib.version_info_formats.format_rio", "RioVersionInfoBuilder.hooks")
+RioVersionInfoBuilder.hooks = RioVersionInfoBuilderHooks()
