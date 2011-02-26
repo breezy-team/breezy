a612d5f9d3500ef83dbd999714fd95eaa30fc9a1
Jelmer Vernooij <jelmer@samba.org>
2011-02-26 20:26:00 +0100

    Start on 0.2.0.

--- info.py
+++ info.py
@@ -1,5 +1,5 @@
 bzr_plugin_name = 'stats'
 
-bzr_plugin_version = (0, 1, 0, 'final', 0)
+bzr_plugin_version = (0, 2, 0, 'dev', 0)
 
 bzr_commands = ['credits', 'committer-statistics', 'ancestor-growth']
