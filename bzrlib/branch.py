b44659c81417b43df19fadaabd990ffb5e8ba80f
Jelmer Vernooij <jelmer@samba.org>
2011-03-07 17:28:25 +0100

    Fix format string for bzrbranch4.

--- bzrlib/branch.py
+++ bzrlib/branch.py
@@ -2351,7 +2351,7 @@ format_registry.register(__format8)
 format_registry.set_default(__format7)
 format_registry.register_extra_lazy("bzrlib.branch_weave",
     "BzrBranchFormat4")
-network_format_registry.register_lazy(BZR_BRANCH_FORMAT_4,
+network_format_registry.register_lazy("Bazaar-NG branch, format 6\n",
     "bzrlib.branch_weave", "BzrBranchFormat4")
 
 
