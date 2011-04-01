3e880d600e0b0e695a86ee52ac516dd298027688
mbp@sourcefrog.net <mbp@sourcefrog.net>
2011-04-01 17:55:40 +1100

    Tweak ReST syntax to avoid formatting error

--- bzrlib/builtins.py
+++ bzrlib/builtins.py
@@ -2788,7 +2788,7 @@ class cmd_ignore(Command):
             bzr ignore "RE:(?!debian/).*"
         
         Ignore everything except the "local" toplevel directory,
-        but always ignore "*~" autosave files, even under local/::
+        but always ignore autosave files ending in ~, even under local/::
         
             bzr ignore "*"
             bzr ignore "!./local"
