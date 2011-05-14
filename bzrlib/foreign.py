f59e1ad3982698b9fd8483ef895f914264cff4b8
Jelmer Vernooij <jelmer@samba.org>
2011-05-14 09:56:12 +0200

    Make lossy_push an argument to InterBranch.push.

--- bzrlib/foreign.py
+++ bzrlib/foreign.py
@@ -20,7 +20,6 @@
 
 from bzrlib.branch import (
     Branch,
-    InterBranch,
     )
 from bzrlib.commands import Command, Option
 from bzrlib.repository import Repository
@@ -315,7 +314,7 @@ class cmd_dpush(Command):
         target_branch.lock_write()
         try:
             try:
-                push_result = source_branch.lossy_push(target_branch)
+                push_result = source_branch.push(target_branch, lossy=True)
             except errors.LossyPushToSameVCS:
                 raise BzrCommandError("%r and %r are in the same VCS, lossy "
                     "push not necessary. Please use regular push." %
