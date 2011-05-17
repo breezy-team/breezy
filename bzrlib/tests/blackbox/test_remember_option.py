d5bd54fe8fda7ae4b7887682aeb93154bd7ebdf0
Vincent Ladeuil <v.ladeuil+lp@free.fr>
2011-05-17 14:53:54 +0100

    Bah, parametrization leads to a far too verbose parameter list, revert to mixin and inheritance.

--- bzrlib/tests/blackbox/test_remember_option.py
+++ bzrlib/tests/blackbox/test_remember_option.py
@@ -17,63 +17,38 @@
 
 from bzrlib import branch
 from bzrlib.tests import (
-    scenarios,
     script,
     )
 
 
-load_tests = scenarios.load_tests_apply_scenarios
-
-def send_first_use(test):
+def pull_first_use(test):
     test.run_script('''
-        $ bzr init grand_parent
-        $ cd grand_parent
-        $ echo grand_parent > file
-        $ bzr add
-        $ bzr commit -m 'initial commit'
-        $ cd ..
-        $ bzr branch grand_parent parent
+        $ bzr init parent
         $ cd parent
         $ echo parent > file
         $ bzr commit -m 'parent'
         $ cd ..
         $ bzr branch parent %(working_dir)s
-        $ cd %(working_dir)s
-        $ echo %(working_dir)s > file
-        $ bzr commit -m '%(working_dir)s'
+        $ cd parent
+        $ echo new content > file
+        $ bzr commit -m 'new content'
         $ cd ..
 ''' % {'working_dir': test.working_dir}, null_output_matches_anything=True)
 
 
-def send_next_uses(test):
-    test.setup_first_use()
-    # Do a first send that remembers the locations
-    test.do_command('../parent', '../grand_parent')
-    # Now create some new targets
-    test.run_script('''
-         $ bzr branch grand_parent new_grand_parent
-         $ bzr branch parent new_parent
-''', null_output_matches_anything=True)
 
-
-
-class TestRemember(script.TestCaseWithTransportAndScript):
+class TestRememberMixin(object):
     """--remember and --no-remember set locations or not."""
 
-    # scenarios arguments:
-    # - command: the command to run (expecting additional arguments from the
-    #   tests
-    # - working_dir: the dir where the command should be run (it should contain
-    #   a branch for which the tested locations are/will be set)
-    # - first_use: a callable setting the context where the command will run
-    #   for the first time
-    # - next_uses: a callable setting the context where the command will run
-    #   again (it generally will call first_use).
-
-    scenarios = [('send',
-                  {'command': ['send', '-o-',], 'working_dir': 'work',
-                   'first_use': send_first_use, 'next_uses': send_next_uses},)
-        ]
+    # the command to run (expecting additional arguments from the tests
+    command = []
+    # the dir where the command should be run (it should contain a branch for
+    # which the tested locations are/will be set)
+    working_dir = None
+    # argument list for the first command invocation
+    first_use_args = []
+    # argument list for the next command invocation
+    next_uses_args = []
 
     def do_command(self, *args):
         # We always expect the same result here and care only about the
@@ -81,43 +56,83 @@ class TestRemember(script.TestCaseWithTransportAndScript):
         out, err = self.run_bzr(self.command + list(args),
                                 working_dir=self.working_dir)
 
+    def test_first_use_no_option(self):
+        self.setup_first_use()
+        self.do_command(*self.first_use_args)
+        self.assertLocations(self.first_use_args)
+
+    def test_first_use_remember(self):
+        self.setup_first_use()
+        self.do_command('--remember', *self.first_use_args)
+        self.assertLocations(self.first_use_args)
+
+    def test_first_use_no_remember(self):
+        self.setup_first_use()
+        self.do_command('--no-remember', *self.first_use_args)
+        self.assertLocations([])
+
+    def test_next_uses_no_option(self):
+        self.setup_next_uses()
+        self.do_command(*self.next_uses_args)
+        self.assertLocations(self.first_use_args)
+
+    def test_next_uses_remember(self):
+        self.setup_next_uses()
+        self.do_command('--remember', *self.next_uses_args)
+        self.assertLocations(self.next_uses_args)
+
+    def test_next_uses_no_remember(self):
+        self.setup_next_uses()
+        self.do_command('--no-remember', *self.next_uses_args)
+        self.assertLocations(self.first_use_args)
+
+
+class TestSendRemember(script.TestCaseWithTransportAndScript,
+                       TestRememberMixin):
+
+    working_dir = 'work'
+    command = ['send', '-o-',]
+    first_use_args = ['../parent', '../grand_parent',]
+    next_uses_args = ['../new_parent', '../new_grand_parent']
+
     def setup_first_use(self):
-        self.first_use(self)
+        self.run_script('''
+            $ bzr init grand_parent
+            $ cd grand_parent
+            $ echo grand_parent > file
+            $ bzr add
+            $ bzr commit -m 'initial commit'
+            $ cd ..
+            $ bzr branch grand_parent parent
+            $ cd parent
+            $ echo parent > file
+            $ bzr commit -m 'parent'
+            $ cd ..
+            $ bzr branch parent %(working_dir)s
+            $ cd %(working_dir)s
+            $ echo %(working_dir)s > file
+            $ bzr commit -m '%(working_dir)s'
+            $ cd ..
+            ''' % {'working_dir': self.working_dir},
+                        null_output_matches_anything=True)
 
     def setup_next_uses(self):
-        self.next_uses(self)
+        self.setup_first_use()
+        # Do a first send that remembers the locations
+        self.do_command(*self.first_use_args)
+        # Now create some new targets
+        self.run_script('''
+            $ bzr branch grand_parent new_grand_parent
+            $ bzr branch parent new_parent
+            ''',
+                        null_output_matches_anything=True)
 
-    def assertLocations(self, expected_submit_branch, expected_public_branch):
+    def assertLocations(self, expected_locations):
+        if not expected_locations:
+            expected_submit_branch, expected_public_branch = None, None
+        else:
+            expected_submit_branch, expected_public_branch = expected_locations
         br, _ = branch.Branch.open_containing(self.working_dir)
         self.assertEquals(expected_submit_branch, br.get_submit_branch())
         self.assertEquals(expected_public_branch, br.get_public_branch())
 
-    def test_first_use_no_option(self):
-        self.setup_first_use()
-        self.do_command('../parent', '../grand_parent')
-        self.assertLocations('../parent', '../grand_parent')
-
-    def test_first_use_remember(self):
-        self.setup_first_use()
-        self.do_command('--remember', '../parent', '../grand_parent')
-        self.assertLocations('../parent', '../grand_parent')
-
-    def test_first_use_no_remember(self):
-        self.setup_first_use()
-        self.do_command('--no-remember', '../parent', '../grand_parent')
-        self.assertLocations(None, None)
-
-    def test_next_uses_no_option(self):
-        self.setup_next_uses()
-        self.do_command('../new_parent', '../new_grand_parent')
-        self.assertLocations('../parent', '../grand_parent')
-
-    def test_next_uses_remember(self):
-        self.setup_next_uses()
-        self.do_command('--remember', '../new_parent', '../new_grand_parent')
-        self.assertLocations('../new_parent', '../new_grand_parent')
-
-    def test_next_uses_no_remember(self):
-        self.setup_next_uses()
-        self.do_command('--no-remember', '../new_parent', '../new_grand_parent')
-        self.assertLocations('../parent', '../grand_parent')
