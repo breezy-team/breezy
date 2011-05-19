49346d12643d7e2896b945f06756a1b8a0fc49b7
Neil Martinsen-Burrell <nmb@wartburg.edu>
2011-05-18 20:37:00 -0500

    fix off by one knock-on error in blackbox test

--- bzrlib/tests/blackbox/test_logformats.py
+++ bzrlib/tests/blackbox/test_logformats.py
@@ -112,7 +112,7 @@ log_format=line
         """Author field should get larger for column widths over 80"""
         wt = self.make_branch_and_tree('.')
         wt.commit('revision with a long author', committer='Person with' 
-                  'long name SENTINEL')
+                  ' long name SENTINEL')
         log, err = self.run_bzr('log --line')
         self.assertNotContainsString(log, 'SENTINEL')
         self.overrideEnv('BZR_COLUMNS', '116')
