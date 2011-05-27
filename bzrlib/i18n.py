d1a5285d2e784d733c42f53672c58cc0279694b7
Vincent Ladeuil <v.ladeuil+lp@free.fr>
2011-05-27 23:38:33 +0200

    Slightly different wrapping of the GPL header broke test_source :-/

--- bzrlib/i18n.py
+++ bzrlib/i18n.py
@@ -4,10 +4,10 @@
 # Copyright (C) 2007,2009 Alexander Belchenko <bialix@ukr.net>
 # Copyright (C) 2011 Canonical Ltd
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,7 +16,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 # This module is copied from Bazaar Explorer and modified for bzr.
 
