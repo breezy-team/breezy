4d8cc8decfd63422b0fcc41530ae44e366cabbed
Jonathan Riddell <jriddell@canonical.com>
2011-07-01 11:42:26 +0100

    doc string formatting

--- bzrlib/gpg.py
+++ bzrlib/gpg.py
@@ -77,7 +77,8 @@ disabled.')
 
 class LoopbackGPGStrategy(object):
     """A GPG Strategy that acts like 'cat' - data is just passed through.
-    Used in tests."""
+    Used in tests.
+    """
 
     @staticmethod
     def verify_signatures_available():
@@ -163,6 +164,8 @@ class GPGStrategy(object):
     @staticmethod
     def verify_signatures_available():
         """
+        check if this strategy can verify signatures
+
         :return: boolean if this strategy can verify signatures
         """
         try:
