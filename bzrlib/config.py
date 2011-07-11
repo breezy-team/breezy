b10a48fc57571d82b7d1df5db37ca5889409d692
Jonathan Riddell <jriddell@canonical.com>
2011-07-11 13:03:39 +0100

    use get_user_option so you don't have to define special methods

--- bzrlib/config.py
+++ bzrlib/config.py
@@ -231,9 +231,6 @@ class Config(object):
     def _get_signing_policy(self):
         """Template method to override signature creation policy."""
 
-    def _get_gpg_signing_key(self):
-        """Template method to override default gpg key."""
-
     option_ref_re = None
 
     def expand_options(self, string, env=None):
@@ -542,7 +539,7 @@ class Config(object):
 
     def gpg_signing_key(self):
         """GPG user-id to sign commits"""
-        key = self._get_gpg_signing_key()
+        key = self.get_user_option('gpg_signing_key')
         if key == "default" or key == None:
             return self.user_email()
         else:
@@ -837,10 +834,6 @@ class IniBasedConfig(Config):
         policy = self._get_user_option('create_signatures')
         if policy:
             return self._string_to_signing_policy(policy)
-
-    def _get_gpg_signing_key(self):
-        """See Config._get_gpg_signing_key"""
-        return self._get_user_option('gpg_signing_key')
 
     def _get_user_id(self):
         """Get the user id from the 'email' key in the current section."""
@@ -1381,10 +1374,6 @@ class BranchConfig(Config):
     def _get_signing_policy(self):
         """See Config._get_signing_policy."""
         return self._get_best_value('_get_signing_policy')
-
-    def _get_gpg_signing_key(self):
-        """See Config._get_gpg_signing_key."""
-        return self._get_best_value('_get_gpg_signing_key')
 
     def _get_user_option(self, option_name):
         """See Config._get_user_option."""
