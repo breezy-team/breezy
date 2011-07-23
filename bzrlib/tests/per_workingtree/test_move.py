f3593937c678fa4ac152ea3c268fca65d3e8e01b
Jelmer Vernooij <jelmer@samba.org>
2011-07-23 14:45:33 +0200

    Cope with versioned directories in test_move_directory_into_parent.

--- bzrlib/tests/per_workingtree/test_move.py
+++ bzrlib/tests/per_workingtree/test_move.py
@@ -348,6 +348,9 @@ class TestMove(TestCaseWithWorkingTree):
         tree._validate()
 
     def test_move_directory_into_parent(self):
+        if not self.workingtree_format.supports_versioned_directories:
+            raise tests.TestNotApplicable(
+                "test requires versioned directories")
         tree = self.make_branch_and_tree('.')
         self.build_tree(['c/', 'c/b/', 'c/b/d/'])
         tree.add(['c', 'c/b', 'c/b/d'],
