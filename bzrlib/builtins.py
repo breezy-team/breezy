56708036321ff51ee6268acdaa197ae4327dc90e
Paul Stewart <paulbrianstewart@gmail.com>
2011-07-26 13:34:00 -0700

    Changed the empty commit message to ask the user either enter a commit message or use --message ' ' to leave a blank commit message

--- bzrlib/builtins.py
+++ bzrlib/builtins.py
@@ -3328,7 +3328,10 @@ class cmd_commit(Command):
                     raise errors.BzrCommandError("please specify a commit"
                         " message with either --message or --file")
             if my_message == "":
-                raise errors.BzrCommandError("empty commit message specified")
+                raise errors.BzrCommandError("Empty commit message specified"
+                        " Please specify a commit message with either"
+                        " --message or --file or leave a blank message"
+                        " with --message ' ' ")
             return my_message
 
         # The API permits a commit with a filter of [] to mean 'select nothing'
