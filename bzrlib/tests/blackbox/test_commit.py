aebc2c0f2325fbc4804bcb3307c29ba96943f622
paulbrianstewart@gmail.com <paulbrianstewart@gmail.com>
2011-08-13 16:07:36 -0700

    Added punctuation to the commit message

--- bzrlib/tests/blackbox/test_commit.py
+++ bzrlib/tests/blackbox/test_commit.py
@@ -750,10 +750,10 @@ altered in u2
     def test_commit_hook_template_rejected(self):
         tree = self.setup_commit_with_template()
         expected = tree.last_revision()
-        out, err = self.run_bzr_error(["Empty commit message specified"
+        out, err = self.run_bzr_error(["Empty commit message specified."
                   " Please specify a commit message with either"
                   " --message or --file or leave a blank message"
-                  " with --message \"\" "],
+                  " with --message \"\"."],
             "commit tree/hello.txt", stdin="n\n")
         self.assertEqual(expected, tree.last_revision())
 
