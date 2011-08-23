d63390b4ff3805c0d1b57df7e567b9759a5eb082
Jelmer Vernooij <jelmer@samba.org>
2011-08-23 02:05:08 +0200

    Mark as compatible with the bzr 2.5 API.

--- info.py
+++ info.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 bzr_plugin_name = 'rewrite'
 
-bzr_plugin_version = (0, 7, 0, 'dev', 0)
+bzr_plugin_version = (0, 6, 3, 'dev', 0)
 
 bzr_compatible_versions = [
     (1, 14, 0), (1, 15, 0), (1, 16, 0), (1, 17, 0), (1, 18, 0),
-    (2, 0, 0), (2, 1, 0), (2, 2, 0), (2, 3, 0), (2, 4, 0)]
+    (2, 0, 0), (2, 1, 0), (2, 2, 0), (2, 3, 0), (2, 4, 0), (2, 5, 0)]
 
 bzr_minimum_version = bzr_compatible_versions[0]
 
