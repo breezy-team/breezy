0a266df9928ce5432d06390ab44d5f221de0fe83
Martin von Gagern <Martin.vGagern@gmx.net>
2011-09-09 15:31:01 +0200

    Add blackbox test for --include-sidelines option.

--- bzrlib/tests/blackbox/test_log.py
+++ bzrlib/tests/blackbox/test_log.py
@@ -549,6 +549,21 @@ class TestLogMerges(TestLogWithLogCatcher):
         self.assertEqual('', err_n0)
         self.assertEqual(out_im, out_n0)
 
+    def test_include_sidelines(self):
+        # Confirm --include-sidelines gives the same output as -n0
+        self.assertLogRevnos(['--include-sidelines'],
+                             ['2', '1.1.2', '1.2.1', '1.1.1', '1'],
+                             working_dir='level0')
+        self.assertLogRevnos(['--include-sidelines'],
+                             ['2', '1.1.2', '1.2.1', '1.1.1', '1'],
+                             working_dir='level0')
+        out_im, err_im = self.run_bzr('log --include-sidelines',
+                                      working_dir='level0')
+        out_n0, err_n0 = self.run_bzr('log -n0', working_dir='level0')
+        self.assertEqual('', err_im)
+        self.assertEqual('', err_n0)
+        self.assertEqual(out_im, out_n0)
+
     def test_force_merge_revisions_N(self):
         self.assertLogRevnos(['-n2'],
                              ['2', '1.1.2', '1.1.1', '1'],
