8549dcd845b73beb8694b1fa3dc46e861e1ff157
Benoît Pierre <benoit.pierre@gmail.com>
2011-10-02 14:32:32 +0200

    Add ui.confirm.
    Work similarly to VIM confirm function: ask the user for confirmation,
    providing multiple choices.
    
    - provide stub for base UIFactory class
    - change UIFactory.get_boolean implementation to use it

--- bzrlib/ui/__init__.py
+++ bzrlib/ui/__init__.py
@@ -324,6 +324,26 @@ class UIFactory(object):
             warnings.warn(fail)   # so tests will fail etc
             return fail
 
+    def confirm(self, msg, choices, default=None):
+        """Ask the user for confirmation, providing multiple choices.
+
+        :param msg: message to be shown as part of the prompt.
+
+        :param choices: list of choices, with the individual choices separated
+        by '\n', e.g.: confirm("Save changes?", "&Yes\n&No\n&Cancel").
+        The letter after the '&' is the shortcut key for that choice. Thus you
+        can type 'c' to select "Cancel". Shorcuts are case insensitive. The
+        shortcut does not need to be the first letter. If a shorcut key is not
+        provided, the first letter for the choice will be used.
+
+        :param default: default choice (index), returned for example when
+        enter is pressed for the console version.
+
+        :return: the index fo the user choice (so '0', '1' or '2' for
+        respectively yes/no/cancel in the previous example).
+        """
+        raise NotImplementedError(self.confirm)
+
     def get_boolean(self, prompt):
         """Get a boolean question answered from the user.
 
@@ -331,7 +351,8 @@ class UIFactory(object):
             line without terminating \\n.
         :return: True or False for y/yes or n/no.
         """
-        raise NotImplementedError(self.get_boolean)
+        choice = self.confirm(prompt + '?', '&yes\n&no', default=None)
+        return 0 == choice
 
     def get_integer(self, prompt):
         """Get an integer from the user.
