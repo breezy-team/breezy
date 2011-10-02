df7da313668be6aec722d0e3b7716324acbafe7e
Benoît Pierre <benoit.pierre@gmail.com>
2011-10-02 14:35:52 +0200

    Fix script tests.

--- bzrlib/tests/test_script.py
+++ bzrlib/tests/test_script.py
@@ -610,16 +610,14 @@ class TestShelve(script.TestCaseWithTransportAndScript):
     def test_shelve(self):
         self.run_script("""
             $ bzr shelve -m 'shelve bar'
-            # Shelve? [yNfq?]
-            <y
-            # Shelve 1 change(s)? [yNfq?]
+            2>Shelve? ([y]es, [N]o, [f]inish, [q]uit): y
             <y
             2>Selected changes:
             2> M  file
+            2>Shelve 1 change(s)? ([y]es, [N]o, [f]inish, [q]uit): y
+            <y
             2>Changes shelved with id "1".
             """,
-                        # shelve uses \r that can't be represented in the
-                        # script ?
                         null_output_matches_anything=True)
         self.run_script("""
             $ bzr shelve --list
@@ -630,12 +628,9 @@ class TestShelve(script.TestCaseWithTransportAndScript):
         # We intentionally provide no input here to test EOF
         self.run_script("""
             $ bzr shelve -m 'shelve bar'
-            # Shelve? [yNfq?]
-            # Shelve 1 change(s)? [yNfq?]
+            2>Shelve? ([y]es, [N]o, [f]inish, [q]uit): 
             2>No changes to shelve.
             """,
-                        # shelve uses \r that can't be represented in the
-                        # script ?
                         null_output_matches_anything=True)
         self.run_script("""
             $ bzr st
