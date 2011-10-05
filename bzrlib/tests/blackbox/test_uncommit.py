52970acde8a4396e7100709971517f8bcbbb5c0d
Benoît Pierre <benoit.pierre@gmail.com>
2011-10-06 00:14:33 +0200

    Fix uncommit tests.

--- bzrlib/tests/blackbox/test_uncommit.py
+++ bzrlib/tests/blackbox/test_uncommit.py
@@ -72,7 +72,7 @@ class TestUncommit(TestCaseWithTransport):
         $ bzr uncommit
         ...
         The above revision(s) will be removed.
-        2>Uncommit these revisions? ([y]es, [n]o): n
+        2>Uncommit these revisions? ([y]es, [n]o): no
         <n
         Canceled
         """)
