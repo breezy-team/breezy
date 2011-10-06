d52b46933780d17141bcc572b7c031506f127b4e
Jelmer Vernooij <jelmer@samba.org>
2011-10-06 02:11:52 +0200

    Remove old FSF address. Thanks Dan Callaghan.

--- branch_mapper.py
+++ branch_mapper.py
@@ -11,8 +11,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """An object that maps git ref names to bzr branch names.  Note that it is not
 used to map git ref names to bzr tag names."""
