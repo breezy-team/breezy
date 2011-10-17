5a0a976257544a0e920d670f95b9c698f474ae44
Jelmer Vernooij <jelmer@samba.org>
2011-10-17 10:38:54 +0200

    Tweak error message.

--- bzrlib/errors.py
+++ bzrlib/errors.py
@@ -3359,7 +3359,11 @@ class HpssVfsRequestNotAllowed(BzrError):
 
 class UnsupportedKindChange(BzrError):
 
-    _fmt = ("Unsupported file kind change for %(path)s")
+    _fmt = ("Kind change from %(from_kind)s to %(to_kind)s for "
+            "%(path)s not supported by format %(format)r")
 
-    def __init__(self, path):
+    def __init__(self, path, from_kind, to_kind, format):
         self.path = path
+        self.from_kind = from_kind
+        self.to_kind = to_kind
+        self.format = format
