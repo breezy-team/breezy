fd686890de3f03d35f9cd29fb9d1f4d04e683323
Jelmer Vernooij <jelmer@samba.org>
2011-11-08 18:51:27 +0100

    release 0.6.5.

--- info.py
+++ info.py
@@ -5,11 +5,11 @@ dulwich_minimum_version = (0, 8, 1)
 # versions ending in 'exp' mean experimental mappings
 # versions ending in 'dev' mean development version
 # versions ending in 'final' mean release (well tested, etc)
-bzr_plugin_version = (0, 6, 5, 'dev', 0)
+bzr_plugin_version = (0, 6, 5, 'final', 0)
 
 bzr_commands = ["git-import", "git-object", "git-refs", "git-apply"]
 
-bzr_compatible_versions = [(2, x, 0) for x in [3, 4]]
+bzr_compatible_versions = [(2, x, 0) for x in [4]]
 
 bzr_minimum_version = bzr_compatible_versions[0]
 
