50cf0e98f982370e3ef0f63748b6a2e46033ecc8
Jelmer Vernooij <jelmer@samba.org>
2011-11-10 16:40:44 +0100

    Fix use of -r.

--- cmds.py
+++ cmds.py
@@ -747,7 +747,7 @@ class cmd_merge_upstream(Command):
             if version is None and upstream_revisions is not None:
                 # Look up the version from the upstream revision
                 version = upstream_branch_source.get_version(package,
-                    current_version, upstream_revisions)
+                    current_version, upstream_revisions[None])
             elif version is None and primary_upstream_source is not None:
                 version = primary_upstream_source.get_latest_version(
                     package, current_version)
