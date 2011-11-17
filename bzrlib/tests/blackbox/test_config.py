1afe3ac33f2744de2ff3ea34356c268048ae8226
Jelmer Vernooij <jelmer@samba.org>
2011-11-17 14:37:29 +0100

    Add test for number of roundtrips of 'bzr config -d'.

--- bzrlib/tests/blackbox/test_config.py
+++ bzrlib/tests/blackbox/test_config.py
@@ -318,3 +318,18 @@ class TestConfigRemoveOption(tests.TestCaseWithTransport):
             bazaar:
               file = bazaar
             ''')
+
+
+class TestSmartServerConfig(tests.TestCaseWithTransport):
+
+    def test_simple_branch_config(self):
+        self.setup_smart_server_with_call_log()
+        t = self.make_branch_and_tree('branch')
+        self.reset_smart_call_log()
+        out, err = self.run_bzr(['config', '-d', self.get_url('branch')])
+        # This figure represent the amount of work to perform this use case. It
+        # is entirely ok to reduce this number if a test fails due to rpc_count
+        # being too low. If rpc_count increases, more network roundtrips have
+        # become necessary for this use case. Please do not adjust this number
+        # upwards without agreement from bzr's network support maintainers.
+        self.assertLength(5, self.hpss_calls)
