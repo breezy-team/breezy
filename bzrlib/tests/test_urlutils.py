b1d228bb9899708d4603e49c8ad787db6a63d351
Martin Packman <martin.packman@canonical.com>
2011-11-18 17:00:13 +0000

    Test urlutils.file_relpath against both posix and win32 implementations

--- bzrlib/tests/test_urlutils.py
+++ bzrlib/tests/test_urlutils.py
@@ -920,7 +920,33 @@ class TestURL(TestCase):
 
 class TestFileRelpath(TestCase):
 
-    def test_same_url(self):
+    # GZ 2011-11-18: A way to override all path handling functions to one
+    #                platform or another for testing would be nice.
+
+    def _with_posix_paths(self):
+        self.overrideAttr(urlutils, "local_path_from_url",
+            urlutils._posix_local_path_from_url)
+        self.overrideAttr(urlutils, "MIN_ABS_FILEURL_LENGTH", len("file:///"))
+        self.overrideAttr(osutils, "normpath", osutils._posix_normpath)
+        self.overrideAttr(osutils, "abspath", osutils._posix_abspath)
+        self.overrideAttr(osutils, "normpath", osutils._posix_normpath)
+        self.overrideAttr(osutils, "pathjoin", osutils.posixpath.join)
+        self.overrideAttr(osutils, "split", osutils.posixpath.split)
+        self.overrideAttr(osutils, "MIN_ABS_PATHLENGTH", 1)
+
+    def _with_win32_paths(self):
+        self.overrideAttr(urlutils, "local_path_from_url",
+            urlutils._win32_local_path_from_url)
+        self.overrideAttr(urlutils, "MIN_ABS_FILEURL_LENGTH",
+            urlutils.WIN32_MIN_ABS_FILEURL_LENGTH)
+        self.overrideAttr(osutils, "abspath", osutils._win32_abspath)
+        self.overrideAttr(osutils, "normpath", osutils._win32_normpath)
+        self.overrideAttr(osutils, "pathjoin", osutils._win32_pathjoin)
+        self.overrideAttr(osutils, "split", osutils.ntpath.split)
+        self.overrideAttr(osutils, "MIN_ABS_PATHLENGTH", 3)
+
+    def test_same_url_posix(self):
+        self._with_posix_paths()
         self.assertEquals("",
             urlutils.file_relpath("file:///a", "file:///a"))
         self.assertEquals("",
@@ -928,7 +954,21 @@ class TestFileRelpath(TestCase):
         self.assertEquals("",
             urlutils.file_relpath("file:///a/", "file:///a"))
 
-    def test_child(self):
+    def test_same_url_win32(self):
+        self._with_win32_paths()
+        self.assertEquals("",
+            urlutils.file_relpath("file:///A:/", "file:///A:/"))
+        self.assertEquals("",
+            urlutils.file_relpath("file:///A|/", "file:///A:/"))
+        self.assertEquals("",
+            urlutils.file_relpath("file:///A:/b/", "file:///A:/b/"))
+        self.assertEquals("",
+            urlutils.file_relpath("file:///A:/b", "file:///A:/b/"))
+        self.assertEquals("",
+            urlutils.file_relpath("file:///A:/b/", "file:///A:/b"))
+
+    def test_child_posix(self):
+        self._with_posix_paths()
         self.assertEquals("b",
             urlutils.file_relpath("file:///a", "file:///a/b"))
         self.assertEquals("b",
@@ -936,7 +976,21 @@ class TestFileRelpath(TestCase):
         self.assertEquals("b/c",
             urlutils.file_relpath("file:///a", "file:///a/b/c"))
 
-    def test_sibling(self):
+    def test_child_win32(self):
+        self._with_win32_paths()
+        self.assertEquals("b",
+            urlutils.file_relpath("file:///A:/", "file:///A:/b"))
+        self.assertEquals("b",
+            urlutils.file_relpath("file:///A|/", "file:///A:/b"))
+        self.assertEquals("c",
+            urlutils.file_relpath("file:///A:/b", "file:///A:/b/c"))
+        self.assertEquals("c",
+            urlutils.file_relpath("file:///A:/b/", "file:///A:/b/c"))
+        self.assertEquals("c/d",
+            urlutils.file_relpath("file:///A:/b", "file:///A:/b/c/d"))
+
+    def test_sibling_posix(self):
+        self._with_posix_paths()
         self.assertRaises(PathNotChild,
             urlutils.file_relpath, "file:///a/b", "file:///a/c")
         self.assertRaises(PathNotChild,
@@ -944,8 +998,25 @@ class TestFileRelpath(TestCase):
         self.assertRaises(PathNotChild,
             urlutils.file_relpath, "file:///a/b/", "file:///a/c/")
 
-    def test_parent(self):
+    def test_sibling_win32(self):
+        self._with_win32_paths()
+        self.assertRaises(PathNotChild,
+            urlutils.file_relpath, "file:///A:/b", "file:///A:/c")
+        self.assertRaises(PathNotChild,
+            urlutils.file_relpath, "file:///A:/b/", "file:///A:/c")
+        self.assertRaises(PathNotChild,
+            urlutils.file_relpath, "file:///A:/b/", "file:///A:/c/")
+
+    def test_parent_posix(self):
+        self._with_posix_paths()
         self.assertRaises(PathNotChild,
             urlutils.file_relpath, "file:///a/b", "file:///a")
         self.assertRaises(PathNotChild,
             urlutils.file_relpath, "file:///a/b", "file:///a/")
+
+    def test_parent_win32(self):
+        self._with_win32_paths()
+        self.assertRaises(PathNotChild,
+            urlutils.file_relpath, "file:///A:/b", "file:///A:/")
+        self.assertRaises(PathNotChild,
+            urlutils.file_relpath, "file:///A:/b/c", "file:///A:/b")
