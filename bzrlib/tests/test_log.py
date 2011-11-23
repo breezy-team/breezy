27e1bcf11ccd74548361a4481bd52094db00b161
Jelmer Vernooij <jelmer@samba.org>
2011-11-23 12:22:56 +0100

    Move log tests to bzrlib.tests.blackbox.test_log.

--- bzrlib/tests/test_log.py
+++ bzrlib/tests/test_log.py
@@ -1636,35 +1636,3 @@ class TestLogDefaults(TestCaseForLogFormatter):
         # should now only have 2 revisions:
         self.assertEquals(len(log_formatter.revisions), 2)
 
-
-class TestSmartServerLog(tests.TestCaseWithTransport):
-
-    def test_standard_log(self):
-        self.setup_smart_server_with_call_log()
-        t = self.make_branch_and_tree('branch')
-        self.build_tree_contents([('branch/foo', 'thecontents')])
-        t.add("foo")
-        t.commit("message")
-        self.reset_smart_call_log()
-        out, err = self.run_bzr(['log', self.get_url('branch')])
-        # This figure represent the amount of work to perform this use case. It
-        # is entirely ok to reduce this number if a test fails due to rpc_count
-        # being too low. If rpc_count increases, more network roundtrips have
-        # become necessary for this use case. Please do not adjust this number
-        # upwards without agreement from bzr's network support maintainers.
-        self.assertLength(17, self.hpss_calls)
-
-    def test_verbose_log(self):
-        self.setup_smart_server_with_call_log()
-        t = self.make_branch_and_tree('branch')
-        self.build_tree_contents([('branch/foo', 'thecontents')])
-        t.add("foo")
-        t.commit("message")
-        self.reset_smart_call_log()
-        out, err = self.run_bzr(['log', '-v', self.get_url('branch')])
-        # This figure represent the amount of work to perform this use case. It
-        # is entirely ok to reduce this number if a test fails due to rpc_count
-        # being too low. If rpc_count increases, more network roundtrips have
-        # become necessary for this use case. Please do not adjust this number
-        # upwards without agreement from bzr's network support maintainers.
-        self.assertLength(17, self.hpss_calls)
