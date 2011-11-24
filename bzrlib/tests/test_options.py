c320e3e35190f42bd3d4779096bf4d155900896a
Martin Packman <martin.packman@canonical.com>
2011-11-24 17:01:07 +0000

    Allow a bracketed suffix in option help test

--- bzrlib/tests/test_options.py
+++ bzrlib/tests/test_options.py
@@ -352,9 +352,9 @@ class TestOptionDefinitions(TestCase):
     def test_option_grammar(self):
         msgs = []
         # Option help should be written in sentence form, and have a final
-        # period and be all on a single line, because the display code will
-        # wrap it.
-        option_re = re.compile(r'^[A-Z][^\n]+\.$')
+        # period with an optional bracketed suffix. All the text should be on
+        # one line, because the display code will wrap it.
+        option_re = re.compile(r'^[A-Z][^\n]+\.(?: \([^\n]+\))?$')
         for scope, opt in self.get_builtin_command_options():
             for name, _, _, helptxt in opt.iter_switches():
                 if name != opt.name:
