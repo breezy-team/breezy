ed6a55329a2863edb52247505945384808a0909e
Martin Pool <mbp@canonical.com>
2011-11-30 08:37:27 +1100

    Get the test names right

--- bzrlib/tests/per_merger.py
+++ bzrlib/tests/per_merger.py
@@ -191,18 +191,18 @@ class TestMergeImplementation(TestCaseWithTransport):
         self.assertRaises(errors.LockError, wt.unlock)
 
     def test_merge_with_pending_deletion_empty(self):
+        wt = self.make_branch_and_tree('this')
+        (limbodir, deletiondir) =  self.get_limbodir_deletiondir(wt)
+        os.mkdir(deletiondir)
+        self.assertRaises(errors.ExistingPendingDeletion, self.do_merge, wt, wt)
+        self.assertRaises(errors.LockError, wt.unlock)
+
+    def test_merge_with_pending_deletion_non_empty(self):
         """Also see bug 427773"""
         wt = self.make_branch_and_tree('this')
         (limbodir, deletiondir) =  self.get_limbodir_deletiondir(wt)
         os.mkdir(deletiondir)
         os.mkdir(os.path.join(deletiondir, 'something'))
-        self.assertRaises(errors.ExistingPendingDeletion, self.do_merge, wt, wt)
-        self.assertRaises(errors.LockError, wt.unlock)
-
-    def test_merge_with_pending_deletion_non_empty(self):
-        wt = self.make_branch_and_tree('this')
-        (limbodir, deletiondir) =  self.get_limbodir_deletiondir(wt)
-        os.mkdir(deletiondir)
         self.assertRaises(errors.ExistingPendingDeletion, self.do_merge, wt, wt)
         self.assertRaises(errors.LockError, wt.unlock)
 
