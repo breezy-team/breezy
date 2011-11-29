15d7be84dd67c322bf56cef1e65ab274a193ccee
Jelmer Vernooij <jelmer@samba.org>
2011-11-29 19:50:53 +0100

    Fix call counts.

--- bzrlib/tests/blackbox/test_checkout.py
+++ bzrlib/tests/blackbox/test_checkout.py
@@ -185,7 +185,7 @@ class TestSmartServerCheckout(TestCaseWithTransport):
         # being too low. If rpc_count increases, more network roundtrips have
         # become necessary for this use case. Please do not adjust this number
         # upwards without agreement from bzr's network support maintainers.
-        self.assertLength(17, self.hpss_calls)
+        self.assertLength(10, self.hpss_calls)
 
     def test_lightweight_checkout(self):
         self.setup_smart_server_with_call_log()
@@ -200,4 +200,4 @@ class TestSmartServerCheckout(TestCaseWithTransport):
         # being too low. If rpc_count increases, more network roundtrips have
         # become necessary for this use case. Please do not adjust this number
         # upwards without agreement from bzr's network support maintainers.
-        self.assertLength(41, self.hpss_calls)
+        self.assertLength(30, self.hpss_calls)
