84d38b7d1f845d4b0e502a90393f896da6db4b72
Jelmer Vernooij <jelmer@samba.org>
2011-12-11 17:03:42 +0100

    Cope with lock_write call.

--- bzrlib/tests/blackbox/test_reconcile.py
+++ bzrlib/tests/blackbox/test_reconcile.py
@@ -85,4 +85,4 @@ class TestSmartServerReconcile(tests.TestCaseWithTransport):
         # being too low. If rpc_count increases, more network roundtrips have
         # become necessary for this use case. Please do not adjust this number
         # upwards without agreement from bzr's network support maintainers.
-        self.assertLength(9, self.hpss_calls)
+        self.assertLength(10, self.hpss_calls)
