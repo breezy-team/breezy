0697eb712eae01f29347c0b3797235f4eda2bb56
Vincent Ladeuil <v.ladeuil+lp@free.fr>
2012-01-05 16:57:44 +0100

    Oops, remove debug setting.

--- bzrlib/tests/test_server.py
+++ bzrlib/tests/test_server.py
@@ -621,7 +621,7 @@ class TestingSmartConnectionHandler(SocketServer.BaseRequestHandler,
             return
 
 
-_DEFAULT_TESTING_CLIENT_TIMEOUT = 600.0
+_DEFAULT_TESTING_CLIENT_TIMEOUT = 60.0
 
 class TestingSmartServer(TestingThreadingTCPServer, server.SmartTCPServer):
 
