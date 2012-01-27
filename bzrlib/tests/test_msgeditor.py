63c79bc419e35fc49fe4523ac0f144ae04489d9d
Jelmer Vernooij <jelmer@samba.org>
2012-01-27 23:09:19 +0100

    Use config stacks in a few more places in the test suite.

--- bzrlib/tests/test_msgeditor.py
+++ bzrlib/tests/test_msgeditor.py
@@ -81,8 +81,7 @@ added:
 """)
 
     def make_multiple_pending_tree(self):
-        from bzrlib import config
-        config.GlobalConfig().set_user_option('email',
+        config.GlobalStack().set_user_option('email',
                                               'Bilbo Baggins <bb@hobbit.net>')
         tree = self.make_branch_and_tree('a')
         tree.commit('Initial checkin.', timestamp=1230912900, timezone=0)
