62699ecf35ebfe6ceea826a1afe1b8c7d0d5be57
Jelmer Vernooij <jelmer@samba.org>
2012-07-02 22:18:21 +0200

    Add comment.

--- bzrlib/branchfmt/__init__.py
+++ bzrlib/branchfmt/__init__.py
@@ -14,7 +14,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-"""Branch formats"""
+"""Branch formats.
+
+This package contains various branch format implementations. Ideally
+all specific format implementations will be moved out of bzrlib.branch
+into this package.
+"""
 
 from __future__ import absolute_import
 
