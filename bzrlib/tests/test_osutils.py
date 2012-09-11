9738452c4ed61461f699f761dc8ca62fb7a3b858
John Arbash Meinel <john@arbash-meinel.com>
2012-09-11 16:26:46 +0400

    Finish backporting the osutils.send_all changes.

--- bzrlib/tests/test_osutils.py
+++ bzrlib/tests/test_osutils.py
@@ -821,6 +821,24 @@ class TestSendAll(tests.TestCase):
             self.assertRaises(errors.ConnectionReset,
                 osutils.send_all, sock, 'some more content')
 
+    def test_send_with_no_progress(self):
+        # See https://bugs.launchpad.net/bzr/+bug/1047309
+        # It seems that paramiko can get into a state where it doesn't error,
+        # but it returns 0 bytes sent for requests over and over again.
+        class NoSendingSocket(object):
+            def __init__(self):
+                self.call_count = 0
+            def send(self, bytes):
+                self.call_count += 1
+                if self.call_count > 100:
+                    # Prevent the test suite from hanging
+                    raise RuntimeError('too many calls')
+                return 0
+        sock = NoSendingSocket()
+        self.assertRaises(errors.ConnectionReset,
+                          osutils.send_all, sock, 'content')
+        self.assertEqual(1, sock.call_count)
+
 
 class TestWin32Funcs(tests.TestCase):
     """Test that _win32 versions of os utilities return appropriate paths."""
