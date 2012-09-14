ec48ca165520f04692aad018013b1bb731eb380d
Jelmer Vernooij <jelmer@samba.org>
2012-09-14 18:42:42 +0200

    Fix GPL.

--- bzrlib/smart/ping.py
+++ bzrlib/smart/ping.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2008 Canonical Ltd
+# Copyright (C) 2008-2012 Canonical Ltd
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -12,11 +12,11 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 """Ping plugin for bzr."""
 
-from bzrlib.commands import Command, register_command
+from bzrlib.commands import Command
 from bzrlib.lazy_import import lazy_import
 
 lazy_import(globals(), """
