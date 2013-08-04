90dea67dff589c5961da33abcfaeee39e1ccb3bb
Jelmer Vernooij <jelmer@samba.org>
2013-08-04 14:38:04 +0000

    Add support for bzr 2.6.0.

--- info.py
+++ info.py
@@ -7,11 +7,11 @@ dulwich_minimum_version = (0, 8, 3)
 # versions ending in 'exp' mean experimental mappings
 # versions ending in 'dev' mean development version
 # versions ending in 'final' mean release (well tested, etc)
-bzr_plugin_version = (0, 6, 10, 'final', 0)
+bzr_plugin_version = (0, 6, 11, 'dev', 0)
 
 bzr_commands = ["git-import", "git-object", "git-refs", "git-apply"]
 
-bzr_compatible_versions = [(2, x, 0) for x in [5]]
+bzr_compatible_versions = [(2, x, 0) for x in [5, 6]]
 
 bzr_minimum_version = bzr_compatible_versions[0]
 
