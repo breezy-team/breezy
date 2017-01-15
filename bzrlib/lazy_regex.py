57fba7306696351378fb815e228a3529ffb443ca
Vincent Ladeuil <v.ladeuil+lp@free.fr>
2017-01-15 20:36:48 +0000

    Fix python re monkey-patching leak

--- bzrlib/lazy_regex.py
+++ bzrlib/lazy_regex.py
@@ -131,3 +131,13 @@ if _real_re_compile is lazy_compile:
     raise AssertionError(
         "re.compile has already been overridden as lazy_compile, but this would" \
         " cause infinite recursion")
+
+
+# re.finditer get confused if it receives a LazyRegex
+if getattr(re, 'finditer', None is not None):
+    def finditer_public(pattern, string, flags=0):
+        if isinstance(pattern, LazyRegex):
+            return pattern.finditer(string)
+        else:
+            return _real_re_compile(pattern, flags).finditer(string)
+re.finditer = finditer_public
