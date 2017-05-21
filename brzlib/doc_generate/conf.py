acb2114ecfa0e1f155c2303c0c94bcd161a59dc2
Jelmer Vernooĳ <jelmer@jelmer.uk>
2017-05-21 12:51:58 +0000

    Comment out icon.

--- brzlib/doc_generate/conf.py
+++ brzlib/doc_generate/conf.py
@@ -135,7 +135,8 @@ html_theme_options = {
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "bzr.ico"
+# TODO(jelmer): add a brz.ico
+# html_favicon = "brz.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
