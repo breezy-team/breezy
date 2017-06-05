9c1c8f0c05b9e137b7618b6c6b4cbee8ebc5efca
Martin <gzlist@googlemail.com>
2017-06-05 22:41:18 +0100

    Now this really is the biggest of the big

--- breezy/plugins/bisect/tests.py
+++ breezy/plugins/bisect/tests.py
@@ -47,7 +47,7 @@ class BisectTestCase(TestCaseWithTransport):
         with open("test_file") as f:
             content = f.read().strip()
         if content != rev_contents[rev]:
-            rev_ids = dict((rev_contents[k], k) for k in rev_contents))
+            rev_ids = dict((rev_contents[k], k) for k in rev_contents)
             found_rev = rev_ids[content]
             raise AssertionError("expected rev %0.1f, found rev %0.1f"
                                  % (rev, found_rev))
