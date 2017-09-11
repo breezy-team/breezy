d6d0df37185f90ae789fd5625405cac784cd67e1
Jelmer Vernooĳ <jelmer@jelmer.uk>
2017-09-11 03:52:57 +0100

    Fix copyright header.

--- breezy/tests/per_workingtree/test_shelf_manager.py
+++ breezy/tests/per_workingtree/test_shelf_manager.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017 Jelmer Vernooij
+# Copyright (C) 2017 Breezy Developers
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
