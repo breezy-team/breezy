1b1cd7cf5b0bdf252352d7686f3fe84b406e5093
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-02-27 14:50:13 +0000

    Raise FetchLimitUnsupported.

--- fetch.py
+++ fetch.py
@@ -554,6 +554,8 @@ class InterFromGitRepository(InterRepository):
     def search_missing_revision_ids(self,
             find_ghosts=True, revision_ids=None, if_present_ids=None,
             limit=None):
+        if limit is not None:
+            raise errors.FetchLimitUnsupported(self)
         git_shas = []
         todo = []
         if revision_ids:
@@ -795,6 +797,8 @@ class InterGitGitRepository(InterFromGitRepository):
         return None, old_refs, new_refs
 
     def fetch_objects(self, determine_wants, mapping=None, limit=None):
+        if limit is not None:
+            raise errors.FetchLimitUnsupported(self)
         graphwalker = self.target._git.get_graph_walker()
         if (isinstance(self.source, LocalGitRepository) and
             isinstance(self.target, LocalGitRepository)):
@@ -855,7 +859,7 @@ class InterGitGitRepository(InterFromGitRepository):
         else:
             determine_wants = lambda x: [y for y in args if not y in r.object_store and y != ZERO_SHA]
         wants_recorder = DetermineWantsRecorder(determine_wants)
-        self.fetch_objects(wants_recorder, mapping)
+        self.fetch_objects(wants_recorder, mapping, limit=limit)
         return wants_recorder.remote_refs
 
     @staticmethod
