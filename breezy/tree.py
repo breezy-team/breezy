5f8cc6e512955ad1375b2b1e740ae3fc4141efae
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-03-09 18:27:46 +0000

    Swap order of arguments to Tree.file_content_matches.

--- breezy/tree.py
+++ breezy/tree.py
@@ -836,8 +836,9 @@ class InterTree(InterObject):
         if source_kind != target_kind:
             changed_content = True
         elif source_kind == 'file':
-            if not self.file_content_matches(file_id, file_id, source_path,
-                    target_path, source_stat, target_stat):
+            if not self.file_content_matches(
+                    source_path, target_path, file_id, file_id, source_stat,
+                    target_stat):
                 changed_content = True
         elif source_kind == 'symlink':
             if (self.source.get_symlink_target(source_path, file_id) !=
@@ -1153,26 +1154,22 @@ class InterTree(InterObject):
                     yield result
 
     def file_content_matches(
-            self, source_file_id, target_file_id, source_path=None,
-            target_path=None, source_stat=None, target_stat=None):
+            self, source_path, target_path=None, source_file_id=None,
+            target_file_id=None, source_stat=None, target_stat=None):
         """Check if two files are the same in the source and target trees.
 
         This only checks that the contents of the files are the same,
         it does not touch anything else.
 
-        :param source_file_id: File id of the file in the source tree
-        :param target_file_id: File id of the file in the target tree
         :param source_path: Path of the file in the source tree
         :param target_path: Path of the file in the target tree
+        :param source_file_id: File id of the file in the source tree
+        :param target_file_id: File id of the file in the target tree
         :param source_stat: Optional stat value of the file in the source tree
         :param target_stat: Optional stat value of the file in the target tree
         :return: Boolean indicating whether the files have the same contents
         """
         with self.lock_read():
-            if source_path is None:
-                source_path = self.source.id2path(source_file_id)
-            if target_path is None:
-                target_path = self.target.id2path(target_file_id)
             source_verifier_kind, source_verifier_data = (
                     self.source.get_file_verifier(
                         source_path, source_file_id, source_stat))
