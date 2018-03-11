e5a7eeab3e9644c60398d2f91c849db34bc2d24a
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-03-11 23:21:40 +0000

    Support verifying signatures.

--- repository.py
+++ repository.py
@@ -452,7 +452,36 @@ class LocalGitRepository(GitRepository):
 
         This is never the case for Git repositories.
         """
-        return False
+        try:
+            self.get_signature_text(revision_id)
+        except errors.NoSuchRevision:
+            return False
+        else:
+            return True
+
+    def verify_revision_signature(self, revision_id, gpg_strategy):
+        """Verify the signature on a revision.
+
+        :param revision_id: the revision to verify
+        :gpg_strategy: the GPGStrategy object to used
+
+        :return: gpg.SIGNATURE_VALID or a failed SIGNATURE_ value
+        """
+        from breezy import gpg
+        with self.lock_read():
+            git_commit_id, mapping = self.lookup_bzr_revision_id(revision_id)
+            try:
+                commit = self._git.object_store[git_commit_id]
+            except KeyError:
+                raise errors.NoSuchRevision(self, revision_id)
+
+            if commit.gpgsig is None:
+                return gpg.SIGNATURE_NOT_SIGNED, None
+
+            without_sig = Commit.from_string(commit.as_raw_string())
+            without_sig.gpgsig = None
+
+            return gpg_strategy.verify(without_sig.as_raw_string(), commit.gpgsig)
 
     def lookup_bzr_revision_id(self, bzr_revid, mapping=None):
         """Lookup a bzr revision id in a Git repository.
