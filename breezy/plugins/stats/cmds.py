840548f7f8fa8fcdae5a2203f024883df92da32b
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-03-17 15:56:32 +0000

    Use itertools magic.

--- breezy/plugins/stats/cmds.py
+++ breezy/plugins/stats/cmds.py
@@ -17,6 +17,8 @@
 
 from __future__ import absolute_import
 
+import itertools
+
 from ... import (
     branch,
     commands,
@@ -145,8 +147,8 @@ def get_revisions_and_committers(a_repo, revids):
     combo_count = {}
     with ui.ui_factory.nested_progress_bar() as pb:
         trace.note('getting revisions')
-        revisions = []
-        for count, (revid, rev) in enumerate(a_repo.iter_revisions(revids)):
+        revision_iters = itertools.tee(a_repo.iter_revisions(revids))
+        for count, (revid, rev) in enumerate(revision_iters[0]):
             pb.update('checking', count, len(revids))
             for author in rev.get_apparent_authors():
                 # XXX: There is a chance sometimes with svn imports that the
@@ -155,8 +157,8 @@ def get_revisions_and_committers(a_repo, revids):
                 email_users.setdefault(email, set()).add(username)
                 combo = (username, email)
                 combo_count[combo] = combo_count.setdefault(combo, 0) + 1
-            revisions.append(rev)
-    return revisions, collapse_email_and_users(email_users, combo_count)
+    return ((rev for (revid, rev) in revision_iters[1]),
+            collapse_email_and_users(email_users, combo_count))
 
 
 def get_info(a_repo, revision):
