f8e08feea84135e669f1c78fd5a80e5133af23a3
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-03-30 17:18:43 +0100

    Remove generic (non-bzr-specific) fastimport commands.
    I'm adding these as indepdendent binaries in python-fastimport.

--- breezy/plugins/fastimport/__init__.py
+++ breezy/plugins/fastimport/__init__.py
@@ -46,9 +46,6 @@ valuable to retain. For further details on importing, manipulating and
 reporting on fast-import streams, see the online help for the commands::
 
   bzr help fast-import
-  bzr help fast-import-filter
-  bzr help fast-import-info
-  bzr help fast-import-query
 
 Finally, you may wish to generate a fast-import dump file from a Bazaar
 repository. The fast-export command is provided for that purpose.
@@ -79,9 +76,6 @@ def test_suite():
 
 for name in [
         "fast_import",
-        "fast_import_filter",
-        "fast_import_info",
-        "fast_import_query",
         "fast_export",
         ]:
     plugin_cmds.register_lazy("cmd_%s" % name, [], "breezy.plugins.fastimport.cmds")
