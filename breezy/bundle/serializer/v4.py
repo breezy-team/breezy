c42227e37b3da6a0b9a6bdd6e6bc316b6e9379a4
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-06-17 16:27:34 +0100

    Use format registry for bundles.

--- breezy/bundle/serializer/v4.py
+++ breezy/bundle/serializer/v4.py
@@ -101,8 +101,7 @@ class BundleWriter(object):
 
     def begin(self):
         """Start writing the bundle"""
-        self._fileobj.write(bundle_serializer._get_bundle_header(
-            bundle_serializer.v4_string))
+        self._fileobj.write(bundle_serializer._get_bundle_header('4'))
         self._fileobj.write('#\n')
         self._container.begin()
 
