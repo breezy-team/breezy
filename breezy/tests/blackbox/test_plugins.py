1342f464507a96cb7ef187edc7c4c3938ab57765
Martin <gzlist@googlemail.com>
2018-07-01 12:53:23 +0200

    Tweak copyright headers

--- breezy/tests/blackbox/test_plugins.py
+++ breezy/tests/blackbox/test_plugins.py
@@ -1,4 +1,5 @@
-# Copyright (C) 2005-2012, 2016 Canonical Ltd, 2017 Breezy developers
+# Copyright (C) 2005-2012, 2016 Canonical Ltd
+# Copyright (C) 2017-2018 Breezy developers
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
