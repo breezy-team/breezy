23764bc0ffdb0c4cfa0998d2ceb890f970b22f0a
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-07-06 20:12:34 +0100

    don't make assumptions about the order in which revision ids are returned by write_bundle.

--- breezy/bundle/serializer/v08.py
+++ breezy/bundle/serializer/v08.py
@@ -121,8 +121,19 @@ class BundleSerializerV08(BundleSerializer):
             with ui.ui_factory.nested_progress_bar() as pb:
                 self._write_revisions(pb)
 
-    def write_bundle(self, repository, target, base, fileobj):
-        return self._write_bundle(repository, target, base, fileobj)
+    def write_bundle(self, repository, revision_id, base_revision_id, out):
+        """Helper function for translating write_bundle to write"""
+        forced_bases = {revision_id:base_revision_id}
+        if base_revision_id is NULL_REVISION:
+            base_revision_id = None
+        graph = repository.get_graph()
+        revision_ids = graph.find_unique_ancestors(revision_id,
+            [base_revision_id])
+        revision_ids = list(repository.get_graph().iter_topo_order(
+            revision_ids))
+        revision_ids.reverse()
+        self.write(repository, revision_ids, forced_bases, out)
+        return revision_ids
 
     def _write_main_header(self):
         """Write the header for the changes"""
--- breezy/tests/test_bundle.py
+++ breezy/tests/test_bundle.py
@@ -469,8 +469,7 @@ class BundleTester(object):
                              len(bundle_rev.parent_ids))
         self.assertEqual(rev_ids,
                          [r.revision_id for r in bundle.real_revisions])
-        self.valid_apply_bundle(base_rev_id, bundle,
-                                   checkout_dir=checkout_dir)
+        self.valid_apply_bundle(base_rev_id, bundle, checkout_dir=checkout_dir)
 
         return bundle
 
@@ -1662,7 +1661,7 @@ class V4_2aBundleTester(V4BundleTester):
                                        revision_id=b'a@cset-0-2a').open_branch()
         bundle_txt, rev_ids = self.create_bundle_text(b'a@cset-0-2a',
                                                       b'a@cset-0-3')
-        self.assertEqual([b'a@cset-0-2b', b'a@cset-0-3'], rev_ids)
+        self.assertEqual(set([b'a@cset-0-2b', b'a@cset-0-3']), set(rev_ids))
         bundle = read_bundle(bundle_txt)
         target.lock_write()
         self.addCleanup(target.unlock)
