33a3a83c8d8722e6f06bccbacb4e74fa758978c1
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-08-17 21:57:24 +0100

    Start work on propose command.

---
+++ cmds.py
@@ -0,0 +1,54 @@
+# Copyright (C) 2018 Jelmer Vernooij <jelmer@jelmer.uk>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+"""Propose command implementations."""
+
+from ... import (
+    branch as _mod_branch,
+    controldir,
+    errors,
+    )
+from ...i18n import gettext
+from ...commands import Command
+from . import (
+    propose as _mod_propose,
+    )
+
+
+class cmd_propose_merge(Command):
+    __doc__ = """Propose a branch for merging.
+
+    This command creates a merge proposal for the local
+    branch to the target branch. The format of the merge
+    proposal depends on the submit branch.
+    """
+
+    takes_options = ['directory']
+    takes_args = ['submit_branch?']
+
+    aliases = ['propose']
+
+    def run(self, submit_branch=None, directory='.'):
+        tree, branch, relpath = controldir.ControlDir.open_containing_tree_or_branch(
+            directory)
+        if submit_branch is None:
+            submit_branch = branch.get_submit_branch()
+        if submit_branch is None:
+            raise errors.BzrCommandError(gettext("No location specified or remembered"))
+        else:
+            target = _mod_branch.Branch.open(submit_branch)
+        proposer = _mod_propose.get_proposer(branch, target)
+        proposer.create_proposal()
---
+++ propose.py
@@ -0,0 +1,86 @@
+# Copyright (C) 2018 Breezy Developers
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+"""Helper functions for proposing merges."""
+
+from __future__ import absolute_import
+
+from ... import (
+    errors,
+    hooks,
+    registry,
+    )
+
+
+class ProposerUnavailable(errors.BzrError):
+
+    _fmt = "Unable to determine how to propose a merge to %(branch)s."
+
+    def __init__(self, branch):
+        errors.BzrError.__init__(self)
+        self.branch = branch
+
+
+class ProposeMergeHooks(hooks.Hooks):
+    """Hooks for proposing a merge on Launchpad."""
+
+    def __init__(self):
+        hooks.Hooks.__init__(self, __name__, "Proposer.hooks")
+        self.add_hook('get_prerequisite',
+            "Return the prerequisite branch for proposing as merge.", (3, 0))
+        self.add_hook('merge_proposal_body',
+            "Return an initial body for the merge proposal message.", (3, 0))
+
+
+class MergeProposer(object):
+    """Merge proposal creator.
+
+    :param source_branch: Branch to propose for merging
+    :param target_branch: Target branch
+    """
+
+    hooks = ProposeMergeHooks()
+
+    def __init__(self, source_branch, target_branch):
+        self.source_branch = source_branch
+        self.target_branch = target_branch
+
+    @classmethod
+    def is_compatible(cls, target_branch, source_branch):
+        raise NotImplementedError(self.is_compatible)
+
+    def create_proposal(self):
+        """Create a proposal to merge a branch for merging.
+        """
+        raise NotImplementedError(self.create_proposal)
+
+
+def get_proposer(branch, target_branch):
+    """Create a merge proposal for branch to target_branch.
+
+    :param branch: A branch object
+    :param target_branch: Target branch object
+    """
+    for name, proposer_cls in proposers.items():
+        if proposer_cls.is_compatible(target_branch, branch):
+            break
+    else:
+        raise ProposerUnavailable(target_branch.user_url)
+
+    return proposer_cls(branch, target_branch)
+
+
+proposers = registry.Registry()
