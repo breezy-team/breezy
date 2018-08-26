d0a35b28dc5173cab6eb17d014b47c9b61d56510
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-08-26 19:36:59 +0100

    Cope with git support now being built-in.

--- directory.py
+++ directory.py
@@ -89,19 +89,12 @@ class VcsDirectory(object):
                 return urls[version]["Svn"]
 
         if "Git" in urls[version]:
-            try:
-                from .. import git
-            except ImportError:
-                note("This package uses git. If you would like to "
-                        "access it with bzr then please install brz-git "
-                        "and re-run the command.")
-            else:
-                url = urls[version]["Git"]
-                if ' -b ' in url:
-                    (url, branch) = url.split(' -b ', 1)
-                    url = urlutils.join_segment_parameters(
-                        url, {'branch': branch})
-                return url
+            url = urls[version]["Git"]
+            if ' -b ' in url:
+                (url, branch) = url.split(' -b ', 1)
+                url = urlutils.join_segment_parameters(
+                    url, {'branch': branch})
+            return url
 
         if "Hg" in urls[version]:
             try:
