ddd5da10d80cd20723785ffc42eb05b234d3a6f0
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-09-10 21:02:20 +0100

    Allow port to be empty when parsing URL.

--- breezy/tests/test_urlutils.py
+++ breezy/tests/test_urlutils.py
@@ -932,6 +932,15 @@ class TestURL(TestCase):
         self.assertIsNot(url, url3)
         self.assertEqual(url, url3)
 
+    def test_parse_empty_port(self):
+        parsed = urlutils.URL.from_string('http://example.com:/one')
+        self.assertEqual('http', parsed.scheme)
+        self.assertIs(None, parsed.user)
+        self.assertIs(None, parsed.password)
+        self.assertEqual('example.com', parsed.host)
+        self.assertIs(None, parsed.port)
+        self.assertEqual('/one', parsed.path)
+
 
 class TestFileRelpath(TestCase):
 
