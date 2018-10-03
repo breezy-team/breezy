fbb4f0da9b459591832bcf9031ae2d8c2f36fb9b
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-10-03 09:37:30 +0100

    Add basic tests.

--- breezy/git/tests/test_transportgit.py
+++ breezy/git/tests/test_transportgit.py
@@ -20,11 +20,15 @@ from __future__ import absolute_import
 
 from dulwich.objects import Blob
 from dulwich.tests.test_object_store import PackBasedObjectStoreTests
+from dulwich.tests.test_refs import RefsContainerTests
 from dulwich.tests.utils import make_object
 
 from ...tests import TestCaseWithTransport
 
-from ..transportgit import TransportObjectStore
+from ..transportgit import (
+    TransportObjectStore,
+    TransportRefsContainer,
+    )
 
 
 class TransportObjectStoreTests(PackBasedObjectStoreTests, TestCaseWithTransport):
@@ -55,9 +59,22 @@ class TransportObjectStoreTests(PackBasedObjectStoreTests, TestCaseWithTransport
 
 # FIXME: Unfortunately RefsContainerTests requires on a specific set of refs existing.
 
-# class TransportRefContainerTests(RefsContainerTests, TestCaseWithTransport):
-#
-#    def setUp(self):
-#        TestCaseWithTransport.setUp(self)
-#        self._refs = TransportRefsContainer(self.get_transport())
+class TransportRefContainerTests(TestCaseWithTransport):
+
+   def setUp(self):
+       TestCaseWithTransport.setUp(self)
+       self._refs = TransportRefsContainer(self.get_transport())
+
+   def test_packed_refs_missing(self):
+        self.assertEqual({}, self._refs.get_packed_refs())
+
+   def test_packed_refs(self):
+        self.get_transport().put_bytes_non_atomic('packed-refs',
+                b'# pack-refs with: peeled fully-peeled sorted \n'
+                b'2001b954f1ec392f84f7cec2f2f96a76ed6ba4ee refs/heads/master')
+        self.assertEqual(
+                {b'refs/heads/master': b'2001b954f1ec392f84f7cec2f2f96a76ed6ba4ee'},
+                self._refs.get_packed_refs())
+
+
 
