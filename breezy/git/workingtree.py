e9eca7e56f49eb62aa9926a1509a1555959d0537
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-11-05 23:01:03 +0000

    fix git working tree tests.

--- breezy/git/workingtree.py
+++ breezy/git/workingtree.py
@@ -756,7 +756,7 @@ class GitWorkingTree(MutableGitIndexTree,workingtree.WorkingTree):
         with self.lock_read():
             root_ie = self._get_dir_ie(u"", None)
             if include_root and not from_dir:
-                yield "", "V", root_ie.kind, root_ie.file_id, root_ie
+                yield "", "V", root_ie
             dir_ids[u""] = root_ie.file_id
             if recursive:
                 path_iterator = sorted(self._iter_files_recursive(from_dir, include_dirs=True))
@@ -785,16 +785,13 @@ class GitWorkingTree(MutableGitIndexTree,workingtree.WorkingTree):
                         if self._has_dir(encoded_path):
                             ie = self._get_dir_ie(path, self.path2id(path))
                             status = "V"
-                            file_id = ie.file_id
                         elif self.is_ignored(path):
                             status = "I"
                             ie = fk_entries[kind]()
-                            file_id = None
                         else:
                             status = "?"
                             ie = fk_entries[kind]()
-                            file_id = None
-                        yield posixpath.relpath(path, from_dir), status, kind, file_id, ie
+                        yield posixpath.relpath(path, from_dir), status, ie
                     continue
                 if value is not None:
                     ie = self._get_file_ie(name, path, value, dir_ids[parent])
