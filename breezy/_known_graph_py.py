8dbc5af938713cc1ab11f05d18b6ffd05f7a5029
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-11-23 02:42:39 +0000

    Fix two more tests.

--- breezy/_known_graph_py.py
+++ breezy/_known_graph_py.py
@@ -196,7 +196,7 @@ class KnownGraph(object):
         # We use a deque rather than a simple list stack, to go for BFD rather
         # than DFD. So that if a longer path is possible, we walk it before we
         # get to the final child
-        pending = collections.deque([node])
+        pending = deque([node])
         while pending:
             node = pending.popleft()
             next_gdfo = node.gdfo + 1
