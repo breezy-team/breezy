1ece42d3f24329306a9f366d97d1ad53b9854099
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-03-05 08:23:01 +0000

    Avoid c99 style initialization.

--- breezy/_patiencediff_c.c
+++ breezy/_patiencediff_c.c
@@ -1163,14 +1163,47 @@ static char PatienceSequenceMatcher_doc[] =
 
 
 static PyTypeObject PatienceSequenceMatcherType = {
-    PyObject_HEAD_INIT(NULL)
-    .tp_name = "PatienceSequenceMatcher",
-    .tp_basicsize = sizeof(PatienceSequenceMatcher),
-    .tp_dealloc = (destructor)PatienceSequenceMatcher_dealloc,
-    .tp_flags = Py_TPFLAGS_DEFAULT,
-    .tp_doc = PatienceSequenceMatcher_doc,
-    .tp_methods = PatienceSequenceMatcher_methods,
-    .tp_new = PatienceSequenceMatcher_new,
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "PatienceSequenceMatcher", /* tp_name */
+    sizeof(PatienceSequenceMatcher), /* tp_basicsize */
+    sizeof(PyObject *), /* tp_itemsize */
+    (destructor)PatienceSequenceMatcher_dealloc, /* tp_dealloc */
+    0, /* tp_print */
+    0, /* tp_getattr */
+    0, /* tp_setattr */
+    0, /* tp_compare */
+    0, /* tp_repr */
+    0, /* tp_as_number */
+    0, /* tp_as_sequence */
+    0, /* tp_as_mapping */
+    0, /* tp_hash */
+    0, /* tp_call */
+    0,                                           /* tp_str */
+    0,                                           /* tp_getattro */
+    0,                                           /* tp_setattro */
+    0,                                           /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT, /* tp_flags */
+    PatienceSequenceMatcher_doc, /* tp_doc */
+    0,          /* tp_traverse */
+    0,                                           /* tp_clear */
+    0,                     /* tp_richcompare */
+    0,                                           /* tp_weaklistoffset */
+    // without implementing tp_iter, Python will fall back to PySequence*
+    // which seems to work ok, we may need something faster/lighter in the
+    // future.
+    0,                                           /* tp_iter */
+    0,                                           /* tp_iternext */
+    PatienceSequenceMatcher_methods, /* tp_methods */
+    0,                                           /* tp_members */
+    0,                                           /* tp_getset */
+    0,                                           /* tp_base */
+    0,                                           /* tp_dict */
+    0,                                           /* tp_descr_get */
+    0,                                           /* tp_descr_set */
+    0,                                           /* tp_dictoffset */
+    0,                                           /* tp_init */
+    0,                                           /* tp_alloc */
+    PatienceSequenceMatcher_new, /* tp_new */
 };
 
 
