0718e9261228d6f19ad5f0e3f4d201577db381e9
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-04-30 23:25:20 +0000

    Pass along committer information.

--- upstream/pristinetar.py
+++ upstream/pristinetar.py
@@ -273,7 +273,8 @@ class PristineTarSource(UpstreamSource):
 
     def import_component_tarball(self, package, version, tree, parent_ids,
             component=None, md5=None, tarball=None, author=None, timestamp=None,
-            subdir=None, exclude=None, force_pristine_tar=False):
+            subdir=None, exclude=None, force_pristine_tar=False,
+            committer=None):
         """Import a tarball.
 
         :param package: Package name
@@ -283,6 +284,7 @@ class PristineTarSource(UpstreamSource):
         :param exclude: Exclude directories
         :param force_pristine_tar: Whether to force creating a pristine-tar
             branch if one does not exist.
+        :param committer: Committer identity to use
         """
         if exclude is None:
             exclude = []
@@ -338,7 +340,7 @@ class PristineTarSource(UpstreamSource):
         with tree.lock_write():
             builder = tree.branch.get_commit_builder(
                     parents=parent_ids, revprops=revprops, timestamp=timestamp,
-                    timezone=timezone)
+                    timezone=timezone, committer=committer)
             try:
                 changes = [c for c in tree.iter_changes(basis_tree) if
                            include_change(c)]
