665f376c051de7f2f91708e6008d550535571bf9
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-05-11 14:35:48 +0000

    Split out extract functionality into a separate file.

--- tests/__init__.py
+++ tests/__init__.py
@@ -123,6 +123,7 @@ def load_tests(loader, basic_tests, pattern):
             'test_config',
             'test_dep3',
             'test_dh_make',
+            'test_extract',
             'test_hooks',
             'test_import_dsc',
             'test_merge_changelog',
