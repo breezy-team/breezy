8885f27f657b63697572959d08ca7789ce9538be
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-06-03 21:45:21 +0000

    Fix test; .git is now properly recognized as a control dir name.

--- breezy/tests/test_controldir.py
+++ breezy/tests/test_controldir.py
@@ -241,7 +241,7 @@ class IsControlFilenameTest(tests.TestCase):
 
     def test_is_bzrdir(self):
         self.assertTrue(controldir.is_control_filename('.bzr'))
+        self.assertTrue(controldir.is_control_filename('.git'))
 
     def test_is_not_bzrdir(self):
-        self.assertFalse(controldir.is_control_filename('.git'))
         self.assertFalse(controldir.is_control_filename('bla'))
