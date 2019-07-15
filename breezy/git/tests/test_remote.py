f3dcbb8bcd7686c23bc1512c7f23e5404bb55a99
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-07-15 23:40:33 +0000

    Fix URL parsing for Git.

--- breezy/git/tests/test_remote.py
+++ breezy/git/tests/test_remote.py
@@ -44,6 +44,7 @@ from ..remote import (
     HeadUpdateFailed,
     RemoteGitError,
     RemoteGitBranchFormat,
+    _git_url_and_path_from_transport,
     )
 
 from dulwich import porcelain
@@ -601,3 +602,34 @@ class RemoteControlDirTests(TestCaseWithTransport):
             author=b'author <author@example.com>')
         remote = ControlDir.open(self.remote_url)
         self.assertEqual('master', remote.open_branch().nick)
+
+
+class GitUrlAndPathFromTransportTests(TestCase):
+
+    def test_file(self):
+        split_url = _git_url_and_path_from_transport('file:///home/blah')
+        self.assertEqual(split_url.scheme, 'file')
+        self.assertEqual(split_url.path, '/home/blah')
+
+    def test_file_segment_params(self):
+        split_url = _git_url_and_path_from_transport('file:///home/blah,branch=master')
+        self.assertEqual(split_url.scheme, 'file')
+        self.assertEqual(split_url.path, '/home/blah')
+
+    def test_git_smart(self):
+        split_url = _git_url_and_path_from_transport(
+            'git://github.com/dulwich/dulwich,branch=master')
+        self.assertEqual(split_url.scheme, 'git')
+        self.assertEqual(split_url.path, '/dulwich/dulwich')
+
+    def test_https(self):
+        split_url = _git_url_and_path_from_transport(
+            'https://github.com/dulwich/dulwich')
+        self.assertEqual(split_url.scheme, 'https')
+        self.assertEqual(split_url.path, '/dulwich/dulwich')
+
+    def test_https_segment_params(self):
+        split_url = _git_url_and_path_from_transport(
+            'https://github.com/dulwich/dulwich,branch=master')
+        self.assertEqual(split_url.scheme, 'https')
+        self.assertEqual(split_url.path, '/dulwich/dulwich')
