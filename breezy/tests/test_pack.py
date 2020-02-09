01e6492396fbba94125e3088cc9a66c82a5bf853
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-02-09 13:40:47 +0000

    Fix tests.

--- breezy/tests/test_pack.py
+++ breezy/tests/test_pack.py
@@ -202,7 +202,7 @@ class TestContainerWriter(tests.TestCase):
     def test_add_second_bytes_record_gets_higher_offset(self):
         self.writer.begin()
         self.writer.add_bytes_record([b'a', b'bc'], len(b'abc'), names=[])
-        offset, length = self.writer.add_bytes_record(b'abc', names=[])
+        offset, length = self.writer.add_bytes_record([b'abc'], len(b'abc'), names=[])
         self.assertEqual((49, 7), (offset, length))
         self.assertOutput(
             b'Bazaar pack format 1 (introduced in 0.18)\n'
