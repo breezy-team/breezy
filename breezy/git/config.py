b9421eaf0bc2a16c87da1aba55b1dad8cafed6b5
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-03-21 22:11:40 +0000

    handle remote repos.

--- breezy/git/config.py
+++ breezy/git/config.py
@@ -82,17 +82,20 @@ class GitBranchStack(config._CompatibleStack):
     """GitBranch stack."""
 
     def __init__(self, branch):
+        section_getters = [self._get_overrides]
         lstore = config.LocationStore()
         loc_matcher = config.LocationMatcher(lstore, branch.base)
+        section_getters.append(loc_matcher.get_sections)
         # FIXME: This should also be looking in .git/config for
         # local git branches.
-        cstore = GitConfigStore(branch.repository._git.get_config())
+        git = getattr(branch.repository, '_git', None)
+        if git:
+            cstore = GitConfigStore(git.get_config())
+            section_getters.append(cstore.get_sections)
         gstore = config.GlobalStore()
+        section_getters.append(gstore.get_sections)
         super(GitBranchStack, self).__init__(
-            [self._get_overrides,
-             loc_matcher.get_sections,
-             cstore.get_sections,
-             gstore.get_sections],
+            section_getters,
             # All modifications go to the corresponding section in
             # locations.conf
             lstore, branch.base)
