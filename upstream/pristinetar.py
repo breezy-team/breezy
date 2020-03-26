b8ec2ca98f7a89c9cd4fd9e809cd910437b5b843
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-03-26 23:16:12 +0000

    Split tag logic out of upstream.pristinetar.

--- upstream/pristinetar.py
+++ upstream/pristinetar.py
@@ -27,7 +27,6 @@ import configparser
 from debian.copyright import globs_to_re
 import errno
 import os
-import re
 import shutil
 import subprocess
 import tempfile
@@ -60,6 +59,15 @@ from ....trace import (
     warning,
     )
 
+from .tags import (
+    GbpTagFormatError,
+    gbp_expand_tag_name,
+    upstream_tag_name,
+    is_upstream_tag,
+    possible_upstream_tag_names,
+    upstream_tag_version,
+    )
+
 
 class PristineTarError(BzrError):
     _fmt = 'There was an error using pristine-tar: %(error)s.'
@@ -78,14 +86,6 @@ class PristineTarDeltaAbsent(PristineTarError):
 
 class PristineTarDeltaExists(PristineTarError):
     _fmt = 'An existing pristine tar entry exists for %(filename)s'
-
-
-class GbpTagFormatError(BzrError):
-    _fmt = 'Unknown variable %(variable)s in tag %(tag_name)s.'
-
-    def __init__(self, tag_name, variable):
-        self.variable = variable
-        self.tag_name = tag_name
 
 
 def git_store_pristine_tar(branch, filename, tree_id, delta, force=False):
@@ -172,8 +172,7 @@ def make_pristine_tar_delta(dest, tarball_path):
 
 def make_pristine_tar_delta_from_tree(
         tree, tarball_path, subdir=None, exclude=None):
-    tmpdir = tempfile.mkdtemp(prefix="builddeb-pristine-")
-    try:
+    with tempfile.TemporaryDirectory(prefix="builddeb-pristine-") as tmpdir:
         dest = os.path.join(tmpdir, "orig")
         with tree.lock_read():
             export(tree, dest, format='dir', subdir=subdir)
@@ -196,8 +195,6 @@ def make_pristine_tar_delta_from_tree(
                        '  pristine-tar -vdk gendelta %s -'
                        % (preserved, tarball_path))
             raise
-    finally:
-        shutil.rmtree(tmpdir)
 
 
 def revision_has_pristine_tar_delta(rev):
@@ -230,31 +227,6 @@ def revision_pristine_tar_format(rev):
     assert revision_has_pristine_tar_delta(rev)
     raise AssertionError(
         "Not handled new delta type in pristine_tar_format")
-
-
-def gbp_expand_tag_name(tag_format, version):
-    # See gbp/pkg/pkgpolicy.py in gbp-buildpackage
-    version_mangle_re = (
-        r'%\(version'
-        r'%(?P<M>[^%])'
-        r'%(?P<R>([^%]|\\%))+'
-        r'\)s')
-
-    ret = tag_format
-    m = re.search(version_mangle_re, tag_format)
-    if m:
-        ret = re.sub(version_mangle_re, "%(version)s", tag_format)
-        version = version.replace(
-            m.group('M'), m.group('R').replace(r'\%', '%'))
-
-    vars = {
-        'version': version,
-        'hversion': version.replace('.', '-'),
-        }
-    try:
-        return ret % vars
-    except KeyError as e:
-        raise GbpTagFormatError(tag_format, e.args[0])
 
 
 class PristineTarSource(UpstreamSource):
@@ -313,20 +285,8 @@ class PristineTarSource(UpstreamSource):
         """
         if self.gbp_tag_format is not None:
             return gbp_expand_tag_name(self.gbp_tag_format, version)
-        if getattr(self.branch.repository, '_git', None):
-            # In git, the convention is to use a slash
-            if distro is None:
-                name = "upstream/" + version.replace('~', '_')
-            else:
-                name = "upstream-%s/%s" % (distro, version.replace('~', '_'))
-        else:
-            if distro is None:
-                name = "upstream-" + version
-            else:
-                name = "upstream-%s-%s" % (distro, version)
-        if component is not None:
-            name += "/%s" % component
-        return name
+        git_style = bool(getattr(self.branch.repository, '_git', None))
+        return upstream_tag_name(version, component, distro, git_style)
 
     def tag_version(self, version, revid, component=None):
         """Tags the upstream branch's last revision with an upstream version.
@@ -574,18 +534,12 @@ class PristineTarSource(UpstreamSource):
         return False
 
     def possible_tag_names(self, version, component):
-        tags = [self.tag_name(version, component=component),
-                self.tag_name(version, component=component, distro="debian"),
-                self.tag_name(version, component=component, distro="ubuntu"),
-                ]
-        if component is None:
-            # compatibility with git-buildpackage
-            tags.append("upstream/%s" % version)
-            tags.append("upstream-%s" % version)
-            if "~" in version:
-                tags.append("upstream/%s" % version.replace("~", "_"))
-            # compatibility with svn-buildpackage
-            tags.append("upstream_%s" % version)
+        tags = []
+        if self.gbp_tag_format:
+            tags.append(gbp_expand_tag_name(self.gbp_tag_format, version))
+
+        tags.extend(possible_upstream_tag_names(version, component))
+
         return tags
 
     def get_pristine_tar_delta(self, package, version, dest_filename,
@@ -611,8 +565,7 @@ class PristineTarSource(UpstreamSource):
     def reconstruct_pristine_tar(self, revid, package, version, dest_filename):
         """Reconstruct a pristine-tar tarball from a bzr revision."""
         tree = self.branch.repository.revision_tree(revid)
-        tmpdir = tempfile.mkdtemp(prefix="builddeb-pristine-")
-        try:
+        with tempfile.TemporaryDirectory(prefix="bd-pristine-") as tmpdir:
             dest = os.path.join(tmpdir, "orig")
             try:
                 delta = self.get_pristine_tar_delta(
@@ -621,8 +574,6 @@ class PristineTarSource(UpstreamSource):
                 reconstruct_pristine_tar(dest, delta, dest_filename)
             except PristineTarDeltaAbsent:
                 export(tree, dest_filename, per_file_timestamps=True)
-        finally:
-            shutil.rmtree(tmpdir)
 
     def _components_by_version(self):
         ret = {}
@@ -642,37 +593,4 @@ class PristineTarSource(UpstreamSource):
         return ret.items()
 
 
-def is_upstream_tag(tag):
-    """Return true if tag is an upstream tag.
 
-    :param tag: The string name of the tag.
-    :return: True if the tag name is one generated by upstream tag operations.
-    """
-    return (tag.startswith('upstream-') or tag.startswith('upstream/') or
-            tag.startswith('upstream_'))
-
-
-def upstream_tag_version(tag):
-    """Return the upstream version portion of an upstream tag name.
-
-    :param tag: The string name of the tag.
-    :return: tuple with version portion of the tag and component name
-    """
-    assert is_upstream_tag(tag), "Not an upstream tag: %s" % tag
-    if tag.startswith('upstream/'):
-        tag = tag[len('upstream/'):]
-    elif tag.startswith('upstream_'):
-        tag = tag[len('upstream_'):]
-    elif tag.startswith('upstream-'):
-        tag = tag[len('upstream-'):]
-        if tag.startswith('debian-'):
-            tag = tag[len('debian-'):]
-        elif tag.startswith('ubuntu-'):
-            tag = tag[len('ubuntu-'):]
-    tag = tag.replace('_', '~')
-    if '/' not in tag:
-        return (None, tag)
-    (version, component) = tag.rsplit('/', 1)
-    if component == "":
-        component = None
-    return (component, version)
