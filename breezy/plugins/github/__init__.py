f1ade5c87d103ba74fb157ace8c18fa4fb87204e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-06-01 14:06:02 +0000

    Split github support out into a separate plugin.

--- breezy/plugins/github/__init__.py
+++ breezy/plugins/github/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 Jelmer Vernooij <jelmer@jelmer.uk>
+# Copyright (C) 2020 Jelmer Vernooij <jelmer@jelmer.uk>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,30 +14,18 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-"""Management of hosted branches."""
+"""Support for GitHub."""
 
 from __future__ import absolute_import
 
 from ... import version_info  # noqa: F401
 from ...commands import plugin_cmds
 
-plugin_cmds.register_lazy("cmd_propose_merge", ["propose"], __name__ + ".cmds")
-plugin_cmds.register_lazy("cmd_land_merge_proposal", ["land"], __name__ + ".cmds")
-plugin_cmds.register_lazy("cmd_publish_derived", ['publish'], __name__ + ".cmds")
-plugin_cmds.register_lazy("cmd_find_merge_proposal", ['find-proposal'], __name__ + ".cmds")
-plugin_cmds.register_lazy("cmd_gitlab_login", ["gl-login"], __name__ + ".cmds")
-plugin_cmds.register_lazy(
-    "cmd_my_merge_proposals", ["my-proposals"],
-    __name__ + ".cmds")
+plugin_cmds.register_lazy("cmd_github_login", ["gh-login"], __name__ + ".cmds")
 
 
 from ...propose import hosters
-hosters.register_lazy(
-    "launchpad", "breezy.plugins.propose.launchpad",
-    "Launchpad")
-hosters.register_lazy(
-    "gitlab", "breezy.plugins.propose.gitlabs",
-    "GitLab")
+hosters.register_lazy("github", __name__ + '.hoster', "GitHub")
 
 
 def test_suite():
--- breezy/plugins/propose/cmds.py
+++ breezy/plugins/propose/cmds.py
@@ -238,43 +238,6 @@ class cmd_find_merge_proposal(Command):
             self.outf.write(gettext('Merge proposal: %s\n') % mp.url)
 
 
-class cmd_github_login(Command):
-    __doc__ = """Log into GitHub.
-
-    When communicating with GitHub, some commands need to authenticate to
-    GitHub.
-    """
-
-    takes_args = ['username?']
-
-    def run(self, username=None):
-        from github import Github, GithubException
-        from breezy.config import AuthenticationConfig
-        authconfig = AuthenticationConfig()
-        if username is None:
-            username = authconfig.get_user(
-                'https', 'github.com', prompt=u'GitHub username', ask=True)
-        password = authconfig.get_password('https', 'github.com', username)
-        client = Github(username, password)
-        user = client.get_user()
-        try:
-            authorization = user.create_authorization(
-                scopes=['user', 'repo', 'delete_repo'], note='Breezy',
-                note_url='https://github.com/breezy-team/breezy')
-        except GithubException as e:
-            errs = e.data.get('errors', [])
-            if errs:
-                err_code = errs[0].get('code')
-                if err_code == u'already_exists':
-                    raise errors.BzrCommandError('token already exists')
-            raise errors.BzrCommandError(e.data['message'])
-        # TODO(jelmer): This should really use something in
-        # AuthenticationConfig
-        from .github import store_github_token
-        store_github_token(scheme='https', host='github.com',
-                           token=authorization.token)
-
-
 class cmd_gitlab_login(Command):
     __doc__ = """Log into a GitLab instance.
 
