d280ca6276e398f8292e9fce1e5f9be03a3ebf28
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-08-22 16:34:02 +0000

    Avoid use of sixish.

--- breezy/bzr/tests/test_conflicts.py
+++ breezy/bzr/tests/test_conflicts.py
@@ -26,7 +26,6 @@ from ...tests import (
     scenarios,
     )
 from ...tests.test_conflicts import vary_by_conflicts
-from ...sixish import text_type
 
 
 from .. import conflicts as bzr_conflicts
@@ -40,7 +39,7 @@ class TestPerConflict(tests.TestCase):
     scenarios = scenarios.multiply_scenarios(vary_by_conflicts())
 
     def test_stringification(self):
-        text = text_type(self.conflict)
+        text = str(self.conflict)
         self.assertContainsString(text, self.conflict.path)
         self.assertContainsString(text.lower(), "conflict")
         self.assertContainsString(repr(self.conflict),
@@ -51,14 +50,14 @@ class TestPerConflict(tests.TestCase):
         o = bzr_conflicts.Conflict.factory(**p.as_stanza().as_dict())
         self.assertEqual(o, p)
 
-        self.assertIsInstance(o.path, text_type)
+        self.assertIsInstance(o.path, str)
 
         if o.file_id is not None:
             self.assertIsInstance(o.file_id, bytes)
 
         conflict_path = getattr(o, 'conflict_path', None)
         if conflict_path is not None:
-            self.assertIsInstance(conflict_path, text_type)
+            self.assertIsInstance(conflict_path, str)
 
         conflict_file_id = getattr(o, 'conflict_file_id', None)
         if conflict_file_id is not None:
