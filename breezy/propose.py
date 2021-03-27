c99b3bfd0c426fee6a2887373fbaa2a376e69c82
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-03-27 10:37:44 +0000

    Cope with empty lines.

--- breezy/propose.py
+++ breezy/propose.py
@@ -390,7 +390,11 @@ class Hoster(object):
 
 def determine_title(description):
     """Determine the title for a merge proposal based on full description."""
-    firstline = description.splitlines()[0]
+    for firstline in description.splitlines():
+        if firstline.strip():
+            break
+    else:
+        raise ValueError
     try:
         i = firstline.index('. ')
     except ValueError:
