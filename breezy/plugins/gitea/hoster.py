eed76c8663f3b2f87336705c3132e29674afa435
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-03-29 01:00:43 +0100

    initial work on gitea support

---
+++ breezy/plugins/gitea/hoster.py
@@ -0,0 +1,134 @@
+# Copyright (C) 2021 Breezy Developers
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+"""Support for GitLab."""
+
+from __future__ import absolute_import
+
+from ...propose import (
+    determine_title,
+    Hoster,
+    MergeProposal,
+    MergeProposalBuilder,
+    MergeProposalExists,
+    NoSuchProject,
+    PrerequisiteBranchUnsupported,
+    SourceNotDerivedFromTarget,
+    UnsupportedHoster,
+    )
+
+
+def default_config_path():
+    return os.path.join(bedding.config_dir(), 'gitea.conf')
+
+
+def store_gitea_token(name, url, private_token):
+    """Store a gitea token in a configuration file."""
+    import configparser
+    config = configparser.ConfigParser()
+    path = default_config_path()
+    config.read([path])
+    config.add_section(name)
+    config[name]['url'] = url
+    config[name]['private_token'] = private_token
+    with open(path, 'w') as f:
+        config.write(f)
+
+
+def iter_tokens():
+    import configparser
+    config = configparser.ConfigParser()
+    config.read([default_config_path()])
+    for name, section in config.items():
+        yield name, section
+
+
+def get_credentials_by_url(url):
+    for name, credentials in iter_tokens():
+        if 'url' not in credentials:
+            continue
+        if credentials['url'].rstrip('/') == url.rstrip('/'):
+            return credentials
+    else:
+        return None
+
+
+def parse_gitea_url(url):
+    (scheme, user, password, host, port, path) = urlutils.parse_url(
+        url)
+    if scheme not in ('git+ssh', 'https', 'http'):
+        raise NotGitLabUrl(url)
+    if not host:
+        raise NotGitLabUrl(url)
+    path = path.strip('/')
+    if path.endswith('.git'):
+        path = path[:-4]
+    return host, path
+
+
+def parse_gitea_branch_url(branch):
+    url = urlutils.strip_segment_parameters(branch.user_url)
+    host, path = parse_gitea_url(url)
+    return host, path, branch.name
+
+
+class Gitea(Hoster):
+    """Gitea hoster implementation."""
+
+    supports_merge_proposal_labels = False
+    supports_merge_proposal_commit_message = False
+    supports_allow_collaboration = False
+    merge_proposal_description_format = 'markdown'
+
+    def __repr__(self):
+        return "<Gitea(%r)>" % self.base_url
+
+    @property
+    def base_url(self):
+        return self.transport.base
+
+    @property
+    def base_hostname(self):
+        return urlutils.parse_url(self.base_url)[3]
+
+    def hosts(self, branch):
+        try:
+            (host, project, branch_name) = parse_gitea_branch_url(branch)
+        except NotGitLabUrl:
+            return False
+        return self.base_hostname == host
+
+   @classmethod
+    def probe_from_url(cls, url, possible_transports=None):
+        try:
+            (host, project) = parse_gitea_url(url)
+        except NotGitLabUrl:
+            raise UnsupportedHoster(url)
+        transport = get_transport(
+            'https://%s' % host, possible_transports=possible_transports)
+        credentials = get_credentials_by_url(transport.base)
+        if credentials is not None:
+            return cls(transport, credentials.get('private_token'))
+        raise UnsupportedHoster(url)
+
+    @classmethod
+    def iter_instances(cls):
+        for name, credentials in iter_tokens():
+            if 'url' not in credentials:
+                continue
+            yield cls(
+                get_transport(credentials['url']),
+                private_token=credentials.get('private_token'))
