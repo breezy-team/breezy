a067becb4599f4f9e5a5d6dd904394915e3fe6f3
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-08-16 23:18:26 +0100

    Really basic implementation of brz.rs.

--- setup.py
+++ setup.py
@@ -521,7 +521,7 @@ if 'bdist_wininst' in sys.argv:
         return docs
 
     # python's distutils-based win32 installer
-    ARGS = {'scripts': ['brz', 'tools/win32/brz-win32-bdist-postinstall.py'],
+    ARGS = {'scripts': ['tools/win32/brz-win32-bdist-postinstall.py'],
             'ext_modules': ext_modules,
             # help pages
             'data_files': find_docs(),
@@ -766,8 +766,7 @@ else:
 
     DATA_FILES = DATA_FILES + I18N_FILES
     # std setup
-    ARGS = {'scripts': ['brz',
-                        # TODO(jelmer): Only install the git scripts if
+    ARGS = {'scripts': [# TODO(jelmer): Only install the git scripts if
                         # Dulwich was found.
                         'breezy/git/git-remote-bzr',
                         'breezy/git/bzr-receive-pack',
