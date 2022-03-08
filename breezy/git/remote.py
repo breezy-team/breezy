6f8e4703a2f66cbc46ec87738084975f3f70f8d8
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-03-08 00:12:21 +0000

    Raise LockContention error from remote Git.

--- breezy/git/remote.py
+++ breezy/git/remote.py
@@ -39,6 +39,7 @@ from ..errors import (
     DivergedBranches,
     InProcessTransport,
     InvalidRevisionId,
+    LockContention,
     NoSuchFile,
     NoSuchRevision,
     NoSuchTag,
@@ -389,12 +390,15 @@ class DefaultProgressReporter(object):
 
     def __init__(self, pb):
         self.pb = pb
+        self.errors = []
 
     def progress(self, text):
         text = text.rstrip(b"\r\n")
         text = text.decode('utf-8', 'surrogateescape')
         if text.lower().startswith('error: '):
-            trace.show_error('git: %s', text[len(b'error: '):])
+            error = text[len(b'error: '):]
+            self.errors.append(error)
+            trace.show_error('git: %s', error)
         else:
             trace.mutter("git: %s", text)
             g = self._GIT_PROGRESS_PARTIAL_RE.match(text)
@@ -408,6 +412,9 @@ class DefaultProgressReporter(object):
                     self.pb.update(text, None, int(total))
                 else:
                     trace.note("%s", text)
+
+
+_LOCK_REF_ERROR_MATCHER = re.compile(b'cannot lock ref \'(.*)\': (.*)')
 
 
 class RemoteGitDir(GitDir):
@@ -480,8 +487,10 @@ class RemoteGitDir(GitDir):
     def send_pack(self, get_changed_refs, generate_pack_data, progress=None):
         if progress is None:
             pb = ui.ui_factory.nested_progress_bar()
-            progress = DefaultProgressReporter(pb).progress
+            progress_reporter = DefaultProgressReporter(pb)
+            progress = progress_reporter.progress
         else:
+            progress_reporter = None
             pb = None
 
         def get_changed_refs_wrapper(remote_refs):
@@ -489,9 +498,17 @@ class RemoteGitDir(GitDir):
                 update_refs_container(self._refs, remote_refs)
             return get_changed_refs(remote_refs)
         try:
-            return self._client.send_pack(
+            result = self._client.send_pack(
                 self._client_path, get_changed_refs_wrapper,
                 generate_pack_data, progress)
+            for ref, error in result.ref_status.items():
+                result.ref_status[ref] = RemoteGitError(error)
+            if progress_reporter:
+                for error in progress_reporter.errors:
+                    m = _LOCK_REF_ERROR_MATCHER.match(error)
+                    if m:
+                        result.ref_status[m.group(1)] = LockContention(m.group(1), m.group(2))
+            return result
         except HangupException as e:
             raise parse_git_hangup(self.transport.external_url(), e)
         except GitProtocolError as e:
@@ -527,7 +544,7 @@ class RemoteGitDir(GitDir):
         result = self.send_pack(get_changed_refs, generate_pack_data)
         error = result.ref_status.get(refname)
         if error:
-            raise RemoteGitError(error)
+            raise error
 
     @property
     def user_url(self):
@@ -686,7 +703,7 @@ class RemoteGitDir(GitDir):
             new_refs = dw_result.refs
             error = dw_result.ref_status.get(actual_refname)
             if error:
-                raise RemoteGitError(error)
+                raise error
             for ref, error in dw_result.ref_status.items():
                 if error:
                     trace.warning('unable to open ref %s: %s',
@@ -1015,7 +1032,7 @@ class RemoteGitTagDict(GitTags):
             get_changed_refs, generate_pack_data)
         error = result.ref_status.get(ref)
         if error:
-            raise RemoteGitError(error)
+            raise error
 
 
 class RemoteGitBranch(GitBranch):
@@ -1103,7 +1120,7 @@ class RemoteGitBranch(GitBranch):
             get_changed_refs, generate_pack_data)
         error = result.ref_status.get(self.ref)
         if error:
-            raise RemoteGitError(error)
+            raise error
         self._sha = sha
 
 
