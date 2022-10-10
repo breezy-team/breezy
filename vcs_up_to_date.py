3062219c0698b3bbb16987fc061edefea9dcb18d
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-10-10 07:37:42 +0100

    Make deb-vcs-up-to-date a module.

---
+++ vcs_up_to_date.py
@@ -0,0 +1,151 @@
+#!/usr/bin/python3
+# Copyright (C) 2022 Jelmer Vernooij <jelmer@jelmer.uk>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import asyncio
+import logging
+import os
+
+from debian.changelog import Changelog
+from debmutate.changelog import distribution_is_unreleased
+
+from breezy.workingtree import WorkingTree
+from breezy.plugins.debian.util import MissingChangelogError
+from breezy.plugins.debian.apt_repo import (
+    LocalApt,
+    RemoteApt,
+    NoAptSources,
+    AptSourceError,
+)
+
+
+class PackageMissingInArchive(Exception):
+    def __init__(self, package):
+        self.package = package
+        super(PackageMissingInArchive, self).__init__(
+            "package %s is missing in archive" % package)
+
+
+class NewArchiveVersion(Exception):
+    def __init__(self, archive_version, tree_version):
+        self.archive_version = archive_version
+        self.tree_version = tree_version
+        super(NewArchiveVersion, self).__init__(
+            "archive version %s is newer than version %s in tree" % (
+                archive_version, tree_version))
+
+
+class TreeVersionNotInArchive(Exception):
+    def __init__(self, tree_version, archive_versions):
+        self.tree_version = tree_version
+        self.archive_versions = archive_versions
+        super(TreeVersionNotInArchive, self).__init__(
+            "tree version %s does not appear in archive" %
+            tree_version
+        )
+
+
+def check_up_to_date(tree, subpath, apt):
+    cl_path = os.path.join(subpath, "debian/changelog")
+    try:
+        with tree.get_file(cl_path) as f:
+            tree_cl = Changelog(f)
+            if distribution_is_unreleased(tree_cl[0].distributions):
+                try:
+                    last_released_tree_version = tree_cl[1].version
+                except IndexError:
+                    last_released_tree_version = None
+            else:
+                last_released_tree_version = tree_cl[0].version
+            package = tree_cl.package
+    except FileNotFoundError:
+        raise MissingChangelogError([cl_path])
+
+    archive_versions = []
+    with apt:
+        for entry in apt.iter_source_by_name(package):
+            archive_versions.append(entry['Version'])
+
+    archive_versions.sort()
+
+    if not archive_versions:
+        raise PackageMissingInArchive(package)
+
+    if last_released_tree_version not in archive_versions:
+        raise TreeVersionNotInArchive(
+            last_released_tree_version, archive_versions)
+
+    if archive_versions[-1] > last_released_tree_version:
+        raise NewArchiveVersion(
+            archive_versions[-1], last_released_tree_version)
+
+
+async def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--apt-repository', type=str,
+        help='APT repository to use. Defaults to locally configured.',
+        default=(
+            os.environ.get('APT_REPOSITORY')
+            or os.environ.get('REPOSITORIES')))
+    parser.add_argument(
+        '--apt-repository-key', type=str,
+        help=('APT repository key to use for validation, '
+              'if --apt-repository is set.'),
+        default=os.environ.get('APT_REPOSITORY_KEY'))
+    parser.add_argument(
+        'directory', default='.', nargs='?',
+        type=str, help='Path to working tree')
+
+    import breezy.bzr  # noqa: F401
+    import breezy.git  # noqa: F401
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+    if args.apt_repository:
+        apt = RemoteApt.from_string(
+            args.apt_repository, args.apt_repository_key)
+    else:
+        apt = LocalApt()
+    tree, subpath = WorkingTree.open_containing(args.directory)
+
+    try:
+        check_up_to_date(tree, subpath, apt)
+    except TreeVersionNotInArchive as exc:
+        logging.fatal(
+            'Last released tree version %s not in archive',
+            exc.tree_version)
+        return 1
+    except NewArchiveVersion as exc:
+        logging.fatal(
+            'New archive version %s is missing in tree',
+            exc.archive_version)
+        return 1
+    except PackageMissingInArchive as exc:
+        logging.fatal(
+            '%s not found in the specified archive', exc.package)
+        return 1
+
+    return 0
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(asyncio.run(main()))
