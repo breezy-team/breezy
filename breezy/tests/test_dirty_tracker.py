c9eab1b53431430b9de1af95cee96ed1a92b34a4
Jelmer Vernooij <jelmer@jelmer.uk>
2022-11-02 15:48:58 +0000

    Make dirty_worker a context manager.
    Actually test the breezy dirty manager, not the lintian-brush ones.

--- breezy/tests/test_dirty_tracker.py
+++ breezy/tests/test_dirty_tracker.py
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-"""Tests for lintian_brush.dirty_tracker."""
+"""Tests for breezy.dirty_tracker."""
 
 import os
 
@@ -30,69 +30,89 @@ class DirtyTrackerTests(TestCaseWithTransport):
         super(DirtyTrackerTests, self).setUp()
         self.tree = self.make_branch_and_tree('tree')
         try:
-            from lintian_brush.dirty_tracker import DirtyTracker
-        except ModuleNotFoundError:
-            self.skipTest('pyinotify not available')
+            from breezy.dirty_tracker import DirtyTracker
+        except ModuleNotFoundError as e:
+            if e.name == 'inotify':
+                self.skipTest('inotify not available')
+            raise
         self.tracker = DirtyTracker(self.tree)
 
     def test_nothing_changes(self):
-        self.assertFalse(self.tracker.is_dirty())
+        with self.tracker:
+            self.assertFalse(self.tracker.is_dirty())
 
     def test_regular_file_added(self):
-        self.build_tree_contents([('tree/foo', 'bar')])
-        self.assertTrue(self.tracker.is_dirty())
-        self.assertEqual(self.tracker.relpaths(), set(['foo']))
+        with self.tracker:
+            self.build_tree_contents([('tree/foo', 'bar')])
+            self.assertTrue(self.tracker.is_dirty())
+            self.assertEqual(self.tracker.relpaths(), set(['foo']))
 
     def test_many_added(self):
-        self.build_tree_contents(
-            [('tree/f%d' % d, 'content') for d in range(100)])
-        self.assertTrue(self.tracker.is_dirty())
-        self.assertEqual(
-            self.tracker.relpaths(), set(['f%d' % d for d in range(100)]))
+        with self.tracker:
+            self.build_tree_contents(
+                [('tree/f%d' % d, 'content') for d in range(100)])
+            self.assertTrue(self.tracker.is_dirty())
+            self.assertEqual(
+                self.tracker.relpaths(), set(['f%d' % d for d in range(100)]))
 
     def test_regular_file_in_subdir_added(self):
-        self.build_tree_contents([('tree/foo/', ), ('tree/foo/blah', 'bar')])
-        self.assertTrue(self.tracker.is_dirty())
-        self.assertEqual(self.tracker.relpaths(), set(['foo', 'foo/blah']))
+        with self.tracker:
+            self.build_tree_contents([('tree/foo/', ), ('tree/foo/blah', 'bar')])
+            self.assertTrue(self.tracker.is_dirty())
+            self.assertEqual(self.tracker.relpaths(), set(['foo', 'foo/blah']))
 
     def test_directory_added(self):
-        self.build_tree_contents([('tree/foo/', )])
-        self.assertTrue(self.tracker.is_dirty())
-        self.assertEqual(self.tracker.relpaths(), set(['foo']))
+        with self.tracker:
+            self.build_tree_contents([('tree/foo/', )])
+            self.assertTrue(self.tracker.is_dirty())
+            self.assertEqual(self.tracker.relpaths(), set(['foo']))
 
     def test_file_removed(self):
-        self.build_tree_contents([('tree/foo', 'foo')])
-        self.assertTrue(self.tracker.is_dirty())
-        self.tracker.mark_clean()
-        self.build_tree_contents([('tree/foo', 'bar')])
-        self.assertTrue(self.tracker.is_dirty())
-        self.assertEqual(self.tracker.relpaths(), set(['foo']))
+        with self.tracker:
+            self.build_tree_contents([('tree/foo', 'foo')])
+            self.assertTrue(self.tracker.is_dirty())
+            self.tracker.mark_clean()
+            self.build_tree_contents([('tree/foo', 'bar')])
+            self.assertTrue(self.tracker.is_dirty())
+            self.assertEqual(self.tracker.relpaths(), set(['foo']))
 
     def test_control_file(self):
-        self.tree.commit('Some change')
-        self.assertFalse(self.tracker.is_dirty())
-        self.assertEqual(self.tracker.relpaths(), set([]))
+        with self.tracker:
+            self.tree.commit('Some change')
+            self.assertFalse(self.tracker.is_dirty())
+            self.assertEqual(self.tracker.relpaths(), set([]))
 
     def test_renamed(self):
-        self.build_tree_contents([('tree/foo', 'bar')])
-        self.tracker.mark_clean()
-        self.assertFalse(self.tracker.is_dirty())
-        os.rename('tree/foo', 'tree/bar')
-        self.assertTrue(self.tracker.is_dirty())
-        self.assertEqual(self.tracker.relpaths(), set(['foo', 'bar']))
+        with self.tracker:
+            self.build_tree_contents([('tree/foo', 'bar')])
+            self.tracker.mark_clean()
+            self.assertFalse(self.tracker.is_dirty())
+            os.rename('tree/foo', 'tree/bar')
+            self.assertTrue(self.tracker.is_dirty())
+            self.assertEqual(self.tracker.relpaths(), set(['foo', 'bar']))
 
     def test_deleted(self):
-        self.build_tree_contents([('tree/foo', 'bar')])
-        self.tracker.mark_clean()
-        self.assertFalse(self.tracker.is_dirty())
-        os.unlink('tree/foo')
-        self.assertTrue(self.tracker.is_dirty(), self.tracker._process.paths)
-        self.assertEqual(self.tracker.relpaths(), set(['foo']))
+        with self.tracker:
+            self.build_tree_contents([('tree/foo', 'bar')])
+            self.tracker.mark_clean()
+            self.assertFalse(self.tracker.is_dirty())
+            os.unlink('tree/foo')
+            self.assertTrue(self.tracker.is_dirty(), self.tracker._paths)
+            self.assertEqual(self.tracker.relpaths(), set(['foo']))
 
     def test_added_then_deleted(self):
-        self.tracker.mark_clean()
-        self.assertFalse(self.tracker.is_dirty())
-        self.build_tree_contents([('tree/foo', 'bar')])
-        os.unlink('tree/foo')
-        self.assertFalse(self.tracker.is_dirty())
-        self.assertEqual(self.tracker.relpaths(), set([]))
+        with self.tracker:
+            self.tracker.mark_clean()
+            self.assertFalse(self.tracker.is_dirty())
+            self.build_tree_contents([('tree/foo', 'bar')])
+            os.unlink('tree/foo')
+            self.assertFalse(self.tracker.is_dirty())
+            self.assertEqual(self.tracker.relpaths(), set([]))
+
+    def test_file_modified(self):
+        self.build_tree_contents([('tree/foo', 'bla')])
+        with self.tracker:
+            self.assertFalse(self.tracker.is_dirty())
+            self.build_tree_contents([('tree/foo', 'bar')])
+            self.assertTrue(self.tracker.is_dirty())
+            self.assertEqual(self.tracker.relpaths(), set(['foo']))
