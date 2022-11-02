2b0a7aa3bba2fc98bc720584458f6d8f7b978cef
Jelmer Vernooij <jelmer@jelmer.uk>
2022-11-02 16:04:54 +0000

    Migrate from pyinotify to inotify.

--- breezy/dirty_tracker.py
+++ breezy/dirty_tracker.py
@@ -18,70 +18,76 @@
 """Track whether a particular directory structure is dirty."""
 
 import os
-from pyinotify import (
-    WatchManager,
-    IN_CREATE,
-    IN_CLOSE_WRITE,
-    IN_Q_OVERFLOW,
-    IN_DELETE,
-    IN_MOVED_TO,
-    IN_MOVED_FROM,
-    IN_ATTRIB,
-    ProcessEvent,
-    Notifier,
-    Event,
-)
+import inotify.adapters
+import inotify.constants
 from typing import Set
 from .workingtree import WorkingTree
 
 
 MASK = (
-    IN_CLOSE_WRITE | IN_DELETE | IN_Q_OVERFLOW | IN_MOVED_TO | IN_MOVED_FROM | IN_ATTRIB
+    inotify.constants.IN_CLOSE_WRITE | inotify.constants.IN_DELETE
+    | inotify.constants.IN_CREATE | inotify.constants.IN_ISDIR
+    | inotify.constants.IN_Q_OVERFLOW | inotify.constants.IN_MOVED_TO
+    | inotify.constants.IN_MOVED_FROM | inotify.constants.IN_ATTRIB
 )
 
 
-class _Process(ProcessEvent):  # type: ignore
-
-    paths: Set[str]
-    created: Set[str]
-
-    def my_init(self) -> None:
-        self.paths = set()
-        self.created = set()
-
-    def process_default(self, event: Event) -> None:
-        path = os.path.join(event.path, event.name)
-        if event.mask & IN_CREATE:
-            self.created.add(path)
-        self.paths.add(path)
-        if event.mask & IN_DELETE and path in self.created:
-            self.paths.remove(path)
-            self.created.remove(path)
+class DirtyTrackerInvalid(Exception):
+    """Dirty tracker invalid."""
 
 
 class DirtyTracker(object):
     """Track the changes to (part of) a working tree."""
 
+    _paths: Set[str]
+    _created: Set[str]
+
     def __init__(self, tree: WorkingTree, subpath: str = ".") -> None:
         self._tree = tree
         self._subpath = subpath
+        self._inotify = None
+        self._paths = set()
+        self._created = set()
+        self._invalid = False
 
     def __enter__(self):
-        self._wm = WatchManager()
-        self._process = _Process()
-        self._notifier = Notifier(self._wm, self._process)
-        self._notifier.coalesce_events(True)
+        self._start_watching()
 
-        def check_excluded(p: str) -> bool:
-            return self._tree.is_control_filename(self._tree.relpath(p))  # type: ignore
+    def _start_watching(self):
+        self._inotify = inotify.adapters.Inotify(block_duration_s=0)
+        start_path = self._tree.abspath(self._subpath)
+        self._inotify.add_watch(start_path, MASK)
+        for (dirpath, dirnames, filenames) in os.walk(start_path):
+            for dirname in list(dirnames):
+                if self._tree.is_control_filename(dirname):
+                    dirnames.remove(dirname)
+                else:
+                    self._inotify.add_watch(
+                        os.path.join(dirpath, dirname), MASK)
+        self._invalid = False
 
-        self._wdd = self._wm.add_watch(
-            self._tree.abspath(self._subpath),
-            MASK,
-            rec=True,
-            auto_add=True,
-            exclude_filter=check_excluded,
-        )
+    def __exit__(self, exc_val, exc_type, exc_tb):
+        del self._inotify
+        self._inotify = None
+        return False
+
+    def _track_new_directory(self, start_path):
+        self._inotify.add_watch(start_path, MASK)
+        for (dirpath, dirnames, filenames) in os.walk(start_path):
+            for dirname in list(dirnames):
+                path = os.path.join(dirpath, filename)
+                self._created.add(path)
+                self._paths.add(path)
+                if self._tree.is_control_filename(dirname):
+                    dirnames.remove(dirname)
+                else:
+                    self._inotify.add_watch(path, MASK)
+            for filename in filenames:
+                if self._tree.is_control_filename(filename):
+                    continue
+                path = os.path.join(dirpath, filename)
+                self._created.add(path)
+                self._paths.add(path)
 
         return self
 
@@ -91,25 +97,46 @@ class DirtyTracker(object):
         return False
 
     def _process_pending(self) -> None:
-        if self._notifier.check_events(timeout=0):
-            self._notifier.read_events()
-        self._notifier.process_events()
+        if self._inotify is None:
+            raise RuntimeError("context manager not active")
+        for (header, type_names, dirpath, name) in (
+                self._inotify.event_gen(yield_nones=False, timeout_s=0.0001)):
+            path = os.path.join(dirpath, name)
+            if header.mask & inotify.constants.IN_Q_OVERFLOW:
+                self._invalid = True
+            if header.mask & inotify.constants.IN_ISDIR:
+                # Need to update tracking
+                if (header.mask & inotify.constants.IN_MOVED_TO
+                        or header.mask & inotify.constants.IN_CREATE):
+                    self._track_new_directory(path)
+            if header.mask & inotify.constants.IN_CREATE:
+                self._created.add(path)
+            self._paths.add(path)
+            if header.mask & inotify.constants.IN_DELETE and path in self._created:
+                self._paths.remove(path)
+                self._created.remove(path)
 
     def mark_clean(self) -> None:
         """Mark the subtree as not having any changes."""
         self._process_pending()
-        self._process.paths.clear()
-        self._process.created.clear()
+        self._paths.clear()
+        self._created.clear()
+        if self._invalid:
+            self._start_watching()
 
     def is_dirty(self) -> bool:
         """Check whether there are any changes."""
         self._process_pending()
-        return bool(self._process.paths)
+        if self._invalid:
+            raise DirtyTrackerInvalid()
+        return bool(self._paths)
 
     def paths(self) -> Set[str]:
         """Return the paths that have changed."""
         self._process_pending()
-        return self._process.paths
+        if self._invalid:
+            raise DirtyTrackerInvalid()
+        return self._paths
 
     @property
     def _paths(self):
