f882d9828a54d00c9dcbd655f74b619d726e6069
Jelmer Vernooij <jelmer@jelmer.uk>
2022-11-17 17:56:23 +0000

    Drop reference to Bazaar in 'brz plugins'.

--- breezy/builtins.py
+++ breezy/builtins.py
@@ -5207,7 +5207,7 @@ class cmd_plugins(Command):
     adding new commands, providing additional network transports and
     customizing log output.
 
-    See the Bazaar Plugin Guide <https://www.breezy-vcs.org/doc/plugins/en/>
+    See the Breezy Plugin Guide <https://www.breezy-vcs.org/doc/plugins/en/>
     for further information on plugins including where to find them and how to
     install them. Instructions are also provided there on how to write new
     plugins using the Python programming language.
