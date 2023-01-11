6442a50fabe8e51fbfbd8bd7e8b43a136919a592
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-01-11 18:26:59 +0000

    Fix anonymous access of lp: URLs.

--- breezy/plugins/launchpad/lp_api.py
+++ breezy/plugins/launchpad/lp_api.py
@@ -154,11 +154,25 @@ def connect_launchpad(base_url, timeout=None, proxy_info=None,
         cache_directory = None
     credential_store = get_credential_store()
     authorization_engine = get_auth_engine(base_url)
-    return Launchpad.login_with(
-        'breezy', base_url, cache_directory, timeout=timeout,
-        credential_store=credential_store,
-        authorization_engine=authorization_engine,
-        proxy_info=proxy_info, version=version)
+    from .account import get_lp_login
+    lp_user = get_lp_login()
+    if lp_user is None:
+        trace.mutter(
+            'Accessing launchpad API anonymously, since no username is set.')
+        return Launchpad.login_anonymously(
+            consumer_name='breezy',
+            service_root=base_url,
+            launchpadlib_dir=cache_directory,
+            timeout=timeout,
+            proxy_info=proxy_info,
+            version=version)
+    else:
+        return Launchpad.login_with(
+            application_name='breezy', service_root=base_url,
+            launchpadlib_dir=cache_directory, timeout=timeout,
+            credential_store=credential_store,
+            authorization_engine=authorization_engine,
+            proxy_info=proxy_info, version=version)
 
 
 
