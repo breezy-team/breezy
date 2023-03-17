695044ea3d1424ec2418c124d229036c508a7a17
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-03-17 17:01:19 +0000

    Split rio crate

--- crates/rio/src/rio.rs
+++ crates/rio/src/rio.rs
@@ -1,5 +1,6 @@
 use regex::Regex;
 
+/// Verify whether a tag is validly formatted
 pub fn valid_tag(tag: &str) -> bool {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"^[-a-zA-Z0-9_]+$").unwrap();
