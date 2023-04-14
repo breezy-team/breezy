0781be268086d15157018062f544730bc7dc3d08
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-04-14 11:44:25 +0000

    Add FileId type.

--- crates/bazaar/src/lib.rs
+++ crates/bazaar/src/lib.rs
@@ -1,1 +1,23 @@
 pub mod filters;
+use std::fmt::{Debug, Error, Formatter};
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+struct FileId(Vec<u8>);
+
+impl Debug for FileId {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        write!(f, "{}", String::from_utf8(self.0.clone()).unwrap())
+    }
+}
+
+impl From<Vec<u8>> for FileId {
+    fn from(v: Vec<u8>) -> Self {
+        FileId(v)
+    }
+}
+
+impl From<FileId> for Vec<u8> {
+    fn from(v: FileId) -> Self {
+        v.0
+    }
+}
