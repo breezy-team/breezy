52d2c0128ba80eadae1b72ff12a64c7dac07031e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-04-14 23:03:08 +0000

    Add RevisionId type.

--- crates/bazaar/src/lib.rs
+++ crates/bazaar/src/lib.rs
@@ -2,7 +2,7 @@ pub mod filters;
 use std::fmt::{Debug, Error, Formatter};
 
 #[derive(Clone, PartialEq, Eq, Hash)]
-struct FileId(Vec<u8>);
+pub struct FileId(Vec<u8>);
 
 impl Debug for FileId {
     fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
@@ -19,5 +19,32 @@ impl From<Vec<u8>> for FileId {
 impl From<FileId> for Vec<u8> {
     fn from(v: FileId) -> Self {
         v.0
+    }
+}
+
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub struct RevisionId(Vec<u8>);
+
+impl Debug for RevisionId {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        write!(f, "{}", String::from_utf8(self.0.clone()).unwrap())
+    }
+}
+
+impl From<Vec<u8>> for RevisionId {
+    fn from(v: Vec<u8>) -> Self {
+        RevisionId(v)
+    }
+}
+
+impl From<RevisionId> for Vec<u8> {
+    fn from(v: RevisionId) -> Self {
+        v.0
+    }
+}
+
+impl RevisionId {
+    pub fn is_null(&self) -> bool {
+        self.0 == b"null:"
     }
 }
