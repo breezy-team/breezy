841d9967a9f997c3d341eb47dc8ec9cdd24d707d
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-04-17 19:53:13 +0000

    Move bazaar.bzr.generate_ids to rust

--- crates/bazaar/src/lib.rs
+++ crates/bazaar/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod filters;
 use std::fmt::{Debug, Error, Formatter};
 
+pub mod gen_ids;
+
 #[derive(Clone, PartialEq, Eq, Hash)]
 pub struct FileId(Vec<u8>);
 
@@ -19,6 +21,12 @@ impl From<Vec<u8>> for FileId {
 impl From<FileId> for Vec<u8> {
     fn from(v: FileId) -> Self {
         v.0
+    }
+}
+
+impl FileId {
+    pub fn generate(name: &str) -> Self {
+        Self::from(gen_ids::gen_file_id(name))
     }
 }
 
@@ -43,8 +51,14 @@ impl From<RevisionId> for Vec<u8> {
     }
 }
 
+const NULL_REVISION: &[u8] = b"null:";
+
 impl RevisionId {
     pub fn is_null(&self) -> bool {
-        self.0 == b"null:"
+        self.0 == NULL_REVISION
+    }
+
+    pub fn generate(username: &str, timestamp: Option<u64>) -> Self {
+        Self::from(gen_ids::gen_revision_id(username, timestamp))
     }
 }
