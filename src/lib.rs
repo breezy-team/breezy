f93f667d9ac8a849c46989ecd19fc78385cd7074
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-04-22 11:07:11 +0000

    Make i18n a feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,17 @@
+#[cfg(feature = "i18n")]
 pub mod i18n;
+
+#[cfg(not(feature = "i18n"))]
+pub mod i18n {
+    pub fn gettext(msgid: &str) -> String {
+        msgid.to_string()
+    }
+
+    pub fn nggettext(msgid: &str, msgid_plural: &str, n: usize) -> String {
+        if n == 1 {
+            msgid.to_string()
+        } else {
+            msgid_plural.to_string()
+        }
+    }
+}
