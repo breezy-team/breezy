bbc2a4bd8ad8a5498d81ffe409927a142331a1e0
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-04-22 03:27:46 +0000

    Reformat

--- crates/cmd-py/src/lib.rs
+++ crates/cmd-py/src/lib.rs
@@ -1,36 +1,36 @@
 use pyo3::prelude::*;
 use std::path::PathBuf;
 
-#[pyfunction(name="disable_i18n")]
+#[pyfunction(name = "disable_i18n")]
 fn i18n_disable_i18n() {
     breezy::i18n::disable();
 }
 
-#[pyfunction(name="dgettext")]
+#[pyfunction(name = "dgettext")]
 fn i18n_dgettext(domain: &str, msgid: &str) -> PyResult<String> {
     Ok(breezy::i18n::dgettext(domain, msgid))
 }
 
-#[pyfunction(name="install")]
+#[pyfunction(name = "install")]
 fn i18n_install(lang: Option<&str>, locale_base: Option<PathBuf>) -> PyResult<()> {
     let locale_base = locale_base.as_ref().map(|p| p.as_path());
     breezy::i18n::install(lang, locale_base)?;
     Ok(())
 }
 
-#[pyfunction(name="install_plugin")]
+#[pyfunction(name = "install_plugin")]
 fn i18n_install_plugin(name: &str, locale_base: Option<PathBuf>) -> PyResult<()> {
     let locale_base = locale_base.as_ref().map(|p| p.as_path());
     breezy::i18n::install_plugin(name, locale_base)?;
     Ok(())
 }
 
-#[pyfunction(name="gettext")]
+#[pyfunction(name = "gettext")]
 fn i18n_gettext(msgid: &str) -> PyResult<String> {
     Ok(breezy::i18n::gettext(msgid))
 }
 
-#[pyfunction(name="ngettext")]
+#[pyfunction(name = "ngettext")]
 fn i18n_ngettext(msgid: &str, msgid_plural: &str, n: u32) -> PyResult<String> {
     Ok(breezy::i18n::ngettext(msgid, msgid_plural, n))
 }
