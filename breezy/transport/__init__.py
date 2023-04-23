ec2d63b5daf05751d6a374d54bd836f1a45dea2f
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-04-23 16:00:36 +0000

    Fix formatting

--- breezy/transport/__init__.py
+++ breezy/transport/__init__.py
@@ -32,8 +32,8 @@ from io import BytesIO
 from stat import S_ISDIR
 from typing import Any, Callable, Dict, TypeVar
 
-from .. import errors, hooks, osutils, registry, ui, urlutils
 from .. import _transport_rs  # type: ignore
+from .. import errors, hooks, osutils, registry, ui, urlutils
 from ..trace import mutter
 
 # a dictionary of open file streams. Keys are absolute paths, values are
