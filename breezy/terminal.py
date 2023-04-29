9f7f903216103307525175f1bcac82ca1197065b
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-04-29 16:54:43 +0000

    Integrate breezy._termcolor

--- breezy/terminal.py
+++ breezy/terminal.py
@@ -73,3 +73,47 @@ def colorstring(text, fgcolor=None, bgcolor=None):
         code.append(b'4' + colors[bgcolor])
 
     return b"".join((b"\033[", b';'.join(code), b"m", text, b"\033[0m"))
+
+
+class FG:
+    """Unix terminal foreground color codes (16-color)."""
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+
+    # Bold Foreground
+    BOLD_RED = '\033[1;31m'
+    BOLD_GREEN = '\033[1;32m'
+    BOLD_YELLOW = '\033[1;33m'
+    BOLD_BLUE = '\033[1;34m'
+    BOLD_MAGENTA = '\033[1;35m'
+    BOLD_CYAN = '\033[1;36m'
+    BOLD_WHITE = '\033[1;37m'
+
+    NONE = '\033[0m'
+
+
+class BG:
+    """Unix terminal background color codes (16-color)."""
+    BLACK = '\033[40m'
+    RED = '\033[41m'
+    GREEN = '\033[42m'
+    YELLOW = '\033[43m'
+    BLUE = '\033[44m'
+    MAGENTA = '\033[45m'
+    CYAN = '\033[46m'
+    WHITE = '\033[47m'
+
+    NONE = '\033[0m'
+
+
+def color_string(s, fg, bg=''):
+    return fg + bg + s + FG.NONE
+
+
+def re_color_string(compiled_pattern, s, fg):
+    return compiled_pattern.sub(fg + r'\1' + FG.NONE, s)
