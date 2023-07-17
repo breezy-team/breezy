57b2e98bd159935bf28acaf1462f2d2403f80ba1
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-07-17 16:00:49 +0000

    Support resolving git repositories through lp:
    Simplify lp: URL resolution, just relying on API rather than local guessing

--- breezy/plugins/launchpad/lp_directory.py
+++ breezy/plugins/launchpad/lp_directory.py
@@ -38,7 +38,7 @@ def _requires_launchpad_login(scheme, netloc, path, query,
     The URL is specified by its parsed components, as returned from
     urlsplit.
     """
-    return (scheme in ('bzr+ssh', 'sftp')
+    return (scheme in ('bzr+ssh', 'sftp', 'git+ssh')
             and (netloc.endswith('launchpad.net') or
                  netloc.endswith('launchpad.test')))
 
@@ -64,61 +64,46 @@ def _resolve_via_api(path, url, api_base_url=LPNET_SERVICE_ROOT):
     from .lp_api import connect_launchpad
     lp = connect_launchpad(api_base_url, version='devel')
     subpaths = []
+    lp_branch = None
+    git_repo = None
     while path:
         lp_branch = lp.branches.getByPath(path=path)
-        if lp_branch:
+        git_repo = lp.git_repositories.getByPath(path=path)
+        if lp_branch and git_repo:
+            target = git_repo.target
+            vcs = target.vcs
+            trace.warning(
+                "Found both a Bazaar branch and a git repository at lp:%s. Using "
+                "%s, since that is the projects' default vcs", path, vcs)
+            if vcs == "Git":
+                lp_branch = None
+            elif vcs == "Bazaar":
+                git_repo = None
+            else:
+                raise errors.BzrError(
+                    "Unknown default vcs %s for %s" % (vcs, target))
+        if lp_branch or git_repo:
             break
         path, subpath = split(path)
         subpaths.insert(0, subpath)
-    if not lp_branch:
-        raise InvalidURL(f"Unknown Launchpad path: {url}")
-    return {'urls': [join(lp_branch.composePublicURL(scheme='bzr+ssh'), *subpaths),
-                     join(lp_branch.composePublicURL(scheme='http'), *subpaths)]}
-
-
-def _resolve_locally(path, url, _api_resolver):
-    # This is the best I could work out. If an lp: url
-    # includes ~user, then it is specially validated. Otherwise, it is just
-    # sent to +branch/$path.
-    _, netloc, _, _, _ = urlsplit(url)
-    if netloc == '':
-        netloc = DEFAULT_INSTANCE
-    base_url = LAUNCHPAD_DOMAINS[netloc]
-    base = 'bzr+ssh://bazaar.{}/'.format(base_url)
-    maybe_invalid = False
-    if path.startswith('~'):
-        # A ~user style path, validate it a bit.
-        # If a path looks fishy, fall back to asking the API to
-        # resolve it for us.
-        parts = path.split('/')
-        if (len(parts) < 3
-                or (parts[1] in ('ubuntu', 'debian') and len(parts) < 5)):
-            # This special case requires 5-parts to be valid.
-            maybe_invalid = True
+    if lp_branch:
+        return {'urls': [join(lp_branch.composePublicURL(scheme='bzr+ssh'), *subpaths),
+                         join(lp_branch.composePublicURL(scheme='http'), *subpaths)]}
+    elif git_repo:
+        return {'urls': [join(git_repo.git_ssh_url, *subpaths),
+                         join(git_repo.git_https_url, *subpaths)]}
     else:
-        base += '+branch/'
-    if maybe_invalid:
-        return _api_resolver(path, url)
-    return {'urls': [base + path]}
+        raise InvalidURL(f"Unknown Launchpad path: {url}")
 
 
-def _resolve(url, _api_resolver=_resolve_via_api, _lp_login=None):
+def _resolve(url, _lp_login=None):
     """Resolve the base URL for this transport."""
     url, path = _update_url_scheme(url)
     if _lp_login is None:
         _lp_login = get_lp_login()
     path = path.strip('/')
     path = _expand_user(path, url, _lp_login)
-    if _lp_login is not None:
-        result = _resolve_locally(path, url, _api_resolver)
-        if 'launchpad' in debug.debug_flags:
-            local_res = result
-            result = _api_resolver(path, url)
-            trace.note(gettext(
-                'resolution for {0}\n  local: {1}\n remote: {2}').format(
-                url, local_res['urls'], result['urls']))
-    else:
-        result = _api_resolver(path, url)
+    result = _resolve_via_api(path, url)
 
     if 'launchpad' in debug.debug_flags:
         trace.mutter("resolve_lp_path(%r) == %r", url, result)
@@ -139,13 +124,7 @@ def _resolve(url, _api_resolver=_resolve_via_api, _lp_login=None):
                         'write to Launchpad or access private data.  See "bzr help launchpad-login".')
                     _warned_login = True
         else:
-            # Use the URL if we can create a transport for it.
-            try:
-                transport.get_transport(url)
-            except (errors.PathError, errors.TransportError):
-                pass
-            else:
-                break
+            break
     else:
         raise InvalidURL(path=url, extra='no supported schemes')
     return url
