a2a963b6adeb34fd0065c81dd829ec08f8d52fda
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-09-24 10:48:46 +0000

    Add old_label/new_label arguments to show_diff_trees

--- src/diff.rs
+++ src/diff.rs
@@ -1,14 +1,17 @@
 use crate::tree::Tree;
 use pyo3::prelude::*;
+use pyo3::types::PyDict;
 use std::io::Write;
 
 #[pyclass]
 struct PyOut(Box<dyn Write + Send>);
 
-pub fn show_diff_trees(
+pub fn show_diff_trees<W: Write>(
     tree1: &dyn Tree,
     tree2: &dyn Tree,
     o: Box<dyn Write + Send>,
+    old_label: Option<&str>,
+    new_label: Option<&str>,
 ) -> PyResult<()> {
     Python::with_gil(|py| -> PyResult<()> {
         let m = py.import("breezy.diff")?;
@@ -16,7 +19,16 @@ pub fn show_diff_trees(
 
         let o = PyOut(o);
 
-        f.call1((tree1.to_object(py), tree2.to_object(py), o))?;
+        let kwargs = PyDict::new(py);
+        if let Some(old_label) = old_label {
+            kwargs.set_item("old_label", old_label)?;
+        }
+
+        if let Some(new_label) = new_label {
+            kwargs.set_item("new_label", new_label)?;
+        }
+
+        f.call((tree1.to_object(py), tree2.to_object(py), o), Some(kwargs))?;
 
         Ok(())
     })?;
