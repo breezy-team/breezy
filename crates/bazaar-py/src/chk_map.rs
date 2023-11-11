a84bb513ab61b12ef2aa7bb3a22e51cf45f7022a
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-11-11 15:04:09 +0000

    Move common prefix finding to rust

--- crates/bazaar-py/src/chk_map.rs
+++ crates/bazaar-py/src/chk_map.rs
@@ -28,10 +28,24 @@ fn _bytes_to_text_key(py: Python, key: Vec<u8>) -> PyResult<(&PyBytes, &PyBytes)
     Ok((PyBytes::new(py, ret.0), PyBytes::new(py, ret.1)))
 }
 
+#[pyfunction]
+fn common_prefix_pair(py: Python, key: &[u8], key2: &[u8]) -> Py<PyBytes> {
+    PyBytes::new(py, bazaar::chk_map::common_prefix_pair(key, key2)).into_py(py)
+}
+
+#[pyfunction]
+fn common_prefix_many(py: Python, keys: Vec<&[u8]>) -> Option<Py<PyBytes>> {
+    bazaar::chk_map::common_prefix_many(keys.into_iter())
+        .as_ref()
+        .map(|v| PyBytes::new(py, v).into_py(py))
+}
+
 pub(crate) fn _chk_map_rs(py: Python) -> PyResult<&PyModule> {
     let m = PyModule::new(py, "chk_map")?;
     m.add_wrapped(wrap_pyfunction!(_search_key_16))?;
     m.add_wrapped(wrap_pyfunction!(_search_key_255))?;
     m.add_wrapped(wrap_pyfunction!(_bytes_to_text_key))?;
+    m.add_wrapped(wrap_pyfunction!(common_prefix_pair))?;
+    m.add_wrapped(wrap_pyfunction!(common_prefix_many))?;
     Ok(m)
 }
