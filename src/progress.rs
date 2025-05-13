200081ed6213a55df2184221e82dc02f6c9be33d
Jelmer Vernooij <jelmer@jelmer.uk>
2025-05-13 12:19:04 +0000

    Add basic ProgressTask

--- src/progress.rs
+++ src/progress.rs
@@ -49,3 +49,50 @@ pub fn supports_progress<F: Write + AsRawFd>(f: &F) -> bool {
         Err(_) => false,
     }
 }
+
+/// Model component of a progress indicator.
+///
+/// Most code that needs to indicate progress should update one of these,
+/// and it will in turn update the display, if one is present.
+///
+/// Code updating the task may also set fields as hints about how to display
+/// it: show_pct, show_spinner, show_eta, show_count, show_bar.  UIs
+/// will not necessarily respect all these fields.
+pub trait ProgressTask {
+    /// Report updated task message and if relevent progress counters.
+    fn update(&self, msg: &str, current_cnt: Option<u64>, total_cnt: Option<u64>);
+
+    /// Report that the task has made progress
+    fn tick(&self);
+
+    /// Report that the task is finished.
+    fn finished(&self);
+
+    /// Create a sub-task of this task
+    fn make_sub_task(&self) -> Box<dyn ProgressTask>;
+
+    /// Clear the progress display.
+    fn clear(&self);
+}
+
+/// Progress-bar standin that does nothing.
+///
+/// This was previously often constructed by application code if no progress
+/// bar was explicitly passed in.  That's no longer recommended: instead, just
+/// create a progress task from the ui_factory.  This class can be used in
+/// test code that needs to fake a progress task for some reason.
+pub struct DummyProgress;
+
+impl ProgressTask for DummyProgress {
+    fn update(&self, _msg: &str, _current_cnt: Option<u64>, _total_cnt: Option<u64>) {}
+
+    fn tick(&self) {}
+
+    fn finished(&self) {}
+
+    fn make_sub_task(&self) -> Box<dyn ProgressTask> {
+        Box::new(DummyProgress)
+    }
+
+    fn clear(&self) {}
+}
