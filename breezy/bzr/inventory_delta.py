5ad65478f161553dcc515e9e7ace16ce073b5a9a
Jelmer Vernooĳ <jelmer@jelmer.uk>
2026-04-12 00:31:04 +0100

    Use constructor kwargs in inventory_delta entry construction

--- breezy/bzr/inventory_delta.py
+++ breezy/bzr/inventory_delta.py
@@ -27,11 +27,123 @@ __all__ = ["InventoryDeltaSerializer"]
 from .._bzr_rs import inventory as _inventory_delta_rs
 from ..revision import RevisionID
 
-InventoryDeltaError = _inventory_delta_rs.InventoryDeltaError
-IncompatibleInventoryDelta = _inventory_delta_rs.IncompatibleInventoryDelta
-parse_inventory_entry = _inventory_delta_rs.parse_inventory_entry
-serialize_inventory_entry = _inventory_delta_rs.serialize_inventory_entry
-InventoryDelta = _inventory_delta_rs.InventoryDelta
+FORMAT_1 = b"bzr inventory delta v1 (bzr 1.14)"
+
+
+class InventoryDeltaError(errors.BzrError):
+    """An error when serializing or deserializing an inventory delta."""
+
+    # Most errors when serializing and deserializing are due to bugs, although
+    # damaged input (i.e. a bug in a different process) could cause
+    # deserialization errors too.
+    internal_error = True
+
+    def __init__(self, format_string, **kwargs):
+        # Let each error supply a custom format string and arguments.
+        self._fmt = format_string
+        super().__init__(**kwargs)
+
+
+class IncompatibleInventoryDelta(errors.BzrError):
+    """The delta could not be deserialised because its contents conflict with
+    the allow_versioned_root or allow_tree_references flags of the
+    deserializer.
+    """
+
+    internal_error = False
+
+
+def _directory_content(entry):
+    """Serialize the content component of entry which is a directory.
+
+    :param entry: An InventoryDirectory.
+    """
+    return b"dir"
+
+
+def _file_content(entry):
+    """Serialize the content component of entry which is a file.
+
+    :param entry: An InventoryFile.
+    """
+    if entry.executable:
+        exec_bytes = b"Y"
+    else:
+        exec_bytes = b""
+    size_exec_sha = entry.text_size, exec_bytes, entry.text_sha1
+    if None in size_exec_sha:
+        raise InventoryDeltaError(
+            "Missing size or sha for %(fileid)r", fileid=entry.file_id
+        )
+    return b"file\x00%d\x00%s\x00%s" % size_exec_sha
+
+
+def _link_content(entry):
+    """Serialize the content component of entry which is a symlink.
+
+    :param entry: An InventoryLink.
+    """
+    target = entry.symlink_target
+    if target is None:
+        raise InventoryDeltaError("Missing target for %(fileid)r", fileid=entry.file_id)
+    return b"link\x00%s" % target.encode("utf8")
+
+
+def _reference_content(entry):
+    """Serialize the content component of entry which is a tree-reference.
+
+    :param entry: A TreeReference.
+    """
+    tree_revision = entry.reference_revision
+    if tree_revision is None:
+        raise InventoryDeltaError(
+            "Missing reference revision for %(fileid)r", fileid=entry.file_id
+        )
+    return b"tree\x00%s" % tree_revision
+
+
+def _dir_to_entry(
+    content, name, parent_id, file_id, last_modified, _type=inventory.InventoryDirectory
+):
+    """Convert a dir content record to an InventoryDirectory."""
+    return _type(file_id, name, parent_id, revision=last_modified)
+
+
+def _file_to_entry(
+    content, name, parent_id, file_id, last_modified, _type=inventory.InventoryFile
+):
+    """Convert a dir content record to an InventoryFile."""
+    return _type(
+        file_id,
+        name,
+        parent_id,
+        revision=last_modified,
+        text_size=int(content[1]),
+        text_sha1=content[3],
+        executable=bool(content[2]),
+    )
+
+
+def _link_to_entry(
+    content, name, parent_id, file_id, last_modified, _type=inventory.InventoryLink
+):
+    """Convert a link content record to an InventoryLink."""
+    return _type(
+        file_id,
+        name,
+        parent_id,
+        revision=last_modified,
+        symlink_target=content[1].decode("utf8"),
+    )
+
+
+def _tree_to_entry(
+    content, name, parent_id, file_id, last_modified, _type=inventory.TreeReference
+):
+    """Convert a tree content record to a TreeReference."""
+    return _type(
+        file_id, name, parent_id, revision=last_modified, reference_revision=content[1]
+    )
 
 
 class InventoryDeltaSerializer:
